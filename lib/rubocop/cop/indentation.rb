eeb8115a29a54f67ff0fa82cea65a8eec64296f9
Jonas Arvidsson <jonas054@gmail.com>
2012-12-02 23:08:33 +0100

    New case/when algorithm. Much more complicated, but seems to work.

--- lib/rubocop/cop/indentation.rb
+++ lib/rubocop/cop/indentation.rb
@@ -17,12 +17,26 @@ module Rubocop
 
       # Does a depth first search for :when, yielding the index of the
       # corresponding :case for each one.
-      def each_when(sexp, case_ix = -1)
-        case_ix += 1 if sexp[0] == :case
-        yield case_ix if sexp[0] == :when
-        sexp.grep(Array).each do |s|
-          each_when(s, case_ix) { |cix| yield cix }
+      def each_when(sexp, case_ix = -1, &block)
+        case sexp[0]
+        when :case
+          case_ix += 1
+          case_ix = next_when(sexp, case_ix, &block)
+        when :when
+          yield case_ix
+          all_except_when = sexp.grep(Array).find_all { |s| s[0] != :when }
+          case_ix_deep = each_when(all_except_when, case_ix, &block)
+          case_ix_next = next_when(sexp, case_ix, &block)
+          case_ix = (case_ix_next == case_ix) ? case_ix_deep : case_ix_next
+        else
+          sexp.grep(Array).each { |s| case_ix = each_when(s, case_ix, &block) }
         end
+        case_ix
+      end
+
+      def next_when(sexp, case_ix, &block)
+        nxt = sexp.find { |s| Array === s && s[0] == :when } or return case_ix
+        each_when(nxt, case_ix, &block)
       end
     end
   end
