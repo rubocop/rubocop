f68568538e7faece256c8e91037520f50f133cb2
Jonas Arvidsson <jonas054@gmail.com>
2012-12-19 19:43:19 +0100

    Using single quotes when possible.

--- spec/rubocop/cops/space_after_comma_etc_spec.rb
+++ spec/rubocop/cops/space_after_comma_etc_spec.rb
@@ -5,32 +5,31 @@ module Rubocop
     describe SpaceAfterCommaEtc do
       let (:space) { SpaceAfterCommaEtc.new }
 
-      it "registers an offence for block argument commas" do
-        space.inspect_source "file.rb", ["each { |s,t| }"]
+      it 'registers an offence for block argument commas' do
+        space.inspect_source('file.rb', ['each { |s,t| }'])
         space.offences.map(&:message).should ==
-          ["Space missing after comma."]
+          ['Space missing after comma.']
       end
 
-      it "registers an offence for colon without space after it" do
-        space.inspect_source "file.rb", ["x = w ? {a:3}:4"]
+      it 'registers an offence for colon without space after it' do
+        space.inspect_source('file.rb', ['x = w ? {a:3}:4'])
         space.offences.map(&:message).should ==
-          ["Space missing after colon."] * 2
+          ['Space missing after colon.'] * 2
       end
 
-      it "registers an offence for semicolon without space after it" do
-        space.inspect_source "file.rb", ["x = 1;y = 2"]
+      it 'registers an offence for semicolon without space after it' do
+        space.inspect_source('file.rb', ['x = 1;y = 2'])
         space.offences.map(&:message).should ==
-          ["Space missing after semicolon."]
+          ['Space missing after semicolon.']
       end
 
-      it "allows the colons in symbols" do
-        space.inspect_source "file.rb", ["x = :a"]
+      it 'allows the colons in symbols' do
+        space.inspect_source('file.rb', ['x = :a'])
         space.offences.map(&:message).should == []
       end
 
-      it "allows colons in strings" do
-        space.inspect_source("file.rb",
-                             ['str << ":"'])
+      it 'allows colons in strings' do
+        space.inspect_source('file.rb', ["str << ':'"])
         space.offences.map(&:message).should == []
       end
     end
--- spec/rubocop/cops/surrounding_space_spec.rb
+++ spec/rubocop/cops/surrounding_space_spec.rb
@@ -6,14 +6,14 @@ module Rubocop
       let (:space) { SurroundingSpace.new }
 
       it 'registers an offence for assignment without space on both sides' do
-        space.inspect_source "file.rb", ["x=0", "y= 0", "z =0"]
+        space.inspect_source('file.rb', ['x=0', 'y= 0', 'z =0'])
         space.offences.size.should == 3
         space.offences.first.message.should ==
           "Surrounding space missing for operator '='."
       end
 
       it 'registers an offence for binary operators that could be unary' do
-        space.inspect_source "file.rb", ["a-3", "x&0xff", "z+0"]
+        space.inspect_source('file.rb', ['a-3', 'x&0xff', 'z+0'])
         space.offences.map(&:message).should ==
           ["Surrounding space missing for operator '-'.",
            "Surrounding space missing for operator '&'.",
@@ -21,31 +21,31 @@ module Rubocop
       end
 
       it 'registers an offence for arguments to a method' do
-        space.inspect_source("file.rb", ["puts 1+2"])
+        space.inspect_source('file.rb', ['puts 1+2'])
         space.offences.map(&:message).should ==
           ["Surrounding space missing for operator '+'."]
       end
 
-      it "accepts parentheses in block parameter list" do
-        space.inspect_source("file.rb",
-                             ["list.inject(Tms.new) { |sum, (label, item)|",
-                              "}"])
+      it 'accepts parentheses in block parameter list' do
+        space.inspect_source('file.rb',
+                             ['list.inject(Tms.new) { |sum, (label, item)|',
+                              '}'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts operator symbols" do
-        space.inspect_source("file.rb", ['func(:-)'])
+      it 'accepts operator symbols' do
+        space.inspect_source('file.rb', ['func(:-)'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts ranges" do
-        space.inspect_source("file.rb", ['a, b = (1..2), (1...3)'])
+      it 'accepts ranges' do
+        space.inspect_source('file.rb', ['a, b = (1..2), (1...3)'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts scope operator" do
+      it 'accepts scope operator' do
         source = ['@io.class == Zlib::GzipWriter']
-        space.inspect_source("file.rb", source)
+        space.inspect_source('file.rb', source)
         space.offences.map(&:message).should == []
       end
 
@@ -55,72 +55,72 @@ module Rubocop
         space.offences.map(&:message).should == []
       end
 
-      it "accepts exclamation point negation" do
-        space.inspect_source("file.rb", ['x = !a&&!b'])
+      it 'accepts exclamation point negation' do
+        space.inspect_source('file.rb', ['x = !a&&!b'])
         space.offences.map(&:message).should ==
           ["Surrounding space missing for operator '&&'."]
       end
 
-      it "accepts exclamation point definition" do
-        space.inspect_source("file.rb", ['  def !',
+      it 'accepts exclamation point definition' do
+        space.inspect_source('file.rb', ['  def !',
                                          '    !__getobj__',
                                          '  end'])
         space.offences.should == []
         space.offences.map(&:message).should == []
       end
 
-      it "accepts a unary" do
-        space.inspect_source("file.rb",
-                             ["  def bm(label_width = 0, *labels, &blk)",
-                              "    benchmark(CAPTION, label_width, FORMAT,",
-                              "              *labels, &blk)",
-                              "  end",
-                              ""])
+      it 'accepts a unary' do
+        space.inspect_source('file.rb',
+                             ['  def bm(label_width = 0, *labels, &blk)',
+                              '    benchmark(CAPTION, label_width, FORMAT,',
+                              '              *labels, &blk)',
+                              '  end',
+                              ''])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts splat operator" do
-        space.inspect_source("file.rb", ['return *list if options'])
+      it 'accepts splat operator' do
+        space.inspect_source('file.rb', ['return *list if options'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts square brackets as method name" do
-        space.inspect_source("file.rb", ['def Vector.[](*array)',
+      it 'accepts square brackets as method name' do
+        space.inspect_source('file.rb', ['def Vector.[](*array)',
                                          'end'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts def of operator" do
-        space.inspect_source("file.rb", ['def +(other); end'])
+      it 'accepts def of operator' do
+        space.inspect_source('file.rb', ['def +(other); end'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts an assignment with spaces" do
-        space.inspect_source "file.rb", ["x = 0"]
+      it 'accepts an assignment with spaces' do
+        space.inspect_source('file.rb', ['x = 0'])
         space.offences.size.should == 0
       end
 
       it "accepts some operators that are exceptions and don't need spaces" do
-        space.inspect_source "file.rb", ["(1..3)",
-                                         "ActionController::Base",
-                                         "each { |s, t| }"]
+        space.inspect_source('file.rb', ['(1..3)',
+                                         'ActionController::Base',
+                                         'each { |s, t| }'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts an assignment followed by newline" do
-        space.inspect_source "file.rb", ["x =\n  0"]
+      it 'accepts an assignment followed by newline' do
+        space.inspect_source('file.rb', ['x =\n  0'])
         space.offences.size.should == 0
       end
 
-      it "accepts unary operators without space" do
-        space.inspect_source "file.rb", ["[].map(&:size)",
-                                         "-3",
-                                         "x = +2"]
+      it 'accepts unary operators without space' do
+        space.inspect_source('file.rb', ['[].map(&:size)',
+                                         '-3',
+                                         'x = +2'])
         space.offences.map(&:message).should == []
       end
 
-      it "accepts square brackets called with method call syntax" do
-        space.inspect_source("file.rb", ['subject.[](0)'])
+      it 'accepts square brackets called with method call syntax' do
+        space.inspect_source('file.rb', ['subject.[](0)'])
         space.offences.map(&:message).should == []
       end
     end
