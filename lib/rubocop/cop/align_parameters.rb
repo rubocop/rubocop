38849aa9cab9aad1e03cc02553fd02844a988a69
Jonas Arvidsson <jonas054@gmail.com>
2012-12-27 22:45:26 +0100

    Refactored align_parameters.rb.

--- lib/rubocop/cop/align_parameters.rb
+++ lib/rubocop/cop/align_parameters.rb
@@ -10,25 +10,33 @@ module Rubocop
 
       def inspect(file, source, tokens, sexp)
         @tokens = tokens
-        each(:method_add_arg, sexp) do |s|
-          next if s[1][0] != :fcall
-          next if s[1][1][0..1] == [:@ident, "lambda"]
-          next if s[2..-1][0][0] != :arg_paren
-          fail unless s[2..-1][0][1][0] == :args_add_block
-          args = s[2..-1][0][1][1]
-          method_name_ix = @tokens.map { |t| t[0] }.index(s[1][1][-1])
+        each(:method_add_arg, sexp) do |method_add_arg|
+          args = get_args(method_add_arg) or next
+          first_arg, rest_of_args = divide_args(args)
+          method_name_pos = method_add_arg[1][1][-1]
+          method_name_ix = @tokens.index { |t| t[0] == method_name_pos }
           @first_lparen_ix = method_name_ix +
             @tokens[method_name_ix..-1].index { |t| t[1] == :on_lparen }
-          first_arg, rest_of_args = divide_args(args)
-          pos_of_1st_arg = position_of(first_arg) or next # give up
+          pos_of_1st_arg = position_of(first_arg) or next # Give up.
           rest_of_args.each do |arg|
-            pos = position_of(arg) or next # give up
+            pos = position_of(arg) or next # Give up if no position found.
             if pos[1] != pos_of_1st_arg[1]
               index = pos[0] - 1
               add_offence(:convention, index, source[index], ERROR_MESSAGE)
             end
           end
         end
+      end
+
+      def get_args(method_add_arg)
+        fcall = method_add_arg[1]
+        return nil if fcall[0] != :fcall
+        return nil if fcall[1][0..1] == [:@ident, "lambda"]
+        arg_paren = method_add_arg[2..-1][0]
+        return nil if arg_paren[0] != :arg_paren
+        args_add_block = arg_paren[1]
+        fail unless args_add_block[0] == :args_add_block
+        args_add_block[1].empty? ? [args_add_block[2]] : args_add_block[1]
       end
 
       def divide_args(args)
@@ -43,7 +51,7 @@ module Rubocop
       end
 
       def position_of(sexp)
-        pos = find_pos_in_sexp(sexp) or return nil # not found
+        pos = find_pos_in_sexp(sexp) or return nil # Nil means not found.
         ix = @tokens.index { |t| t[0] == pos }
         start_ix = ix.downto(0) do |i|
           break i + 1 if @tokens[i][2] == "\n" || i == @first_lparen_ix
