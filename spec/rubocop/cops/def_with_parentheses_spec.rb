8e8b3de273ab67c5ae61192ccebce11e38a1c065
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-07 23:46:09 +0300

    Migrate spec from should to expect

--- spec/rubocop/cops/def_with_parentheses_spec.rb
+++ spec/rubocop/cops/def_with_parentheses_spec.rb
@@ -11,22 +11,22 @@ module Rubocop
         src = ['def func()',
                'end']
         inspect_source(def_par, '', src)
-        def_par.offences.map(&:message).should ==
+        expect(def_par.offences.map(&:message)).to eq(
           ["Omit the parentheses in defs when the method doesn't accept any " +
-           'arguments.']
+           'arguments.'])
       end
 
       it 'accepts def with arg and parens' do
         src = ['def func(a)',
                'end']
         inspect_source(def_par, '', src)
-        def_par.offences.map(&:message).should == []
+        expect(def_par.offences.map(&:message)).to be_empty
       end
 
       it 'accepts empty parentheses in one liners' do
         src = ["def to_s() join '/' end"]
         inspect_source(def_par, '', src)
-        def_par.offences.map(&:message).should == []
+        expect(def_par.offences.map(&:message)).to be_empty
       end
     end
   end
