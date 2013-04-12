af15dafffa2223f2df3555281a2d167a5d1dca1b
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-12 18:25:34 +0300

    Remove some debug code

--- lib/rubocop/cop/avoid_for.rb
+++ lib/rubocop/cop/avoid_for.rb
@@ -3,12 +3,10 @@
 module Rubocop
   module Cop
     class AvoidFor < Cop
-      ERROR_MESSAGE = "Prefer each over for."
+      ERROR_MESSAGE = 'Prefer *each* over *for*.'
 
       def inspect(file, source, tokens, sexp)
         each(:for, sexp) do |s|
-          puts s.inspect
-
           add_offence(:convention,
                       s[1][1][2].lineno,
                       ERROR_MESSAGE)
