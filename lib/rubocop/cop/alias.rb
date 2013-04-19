cf02abf447540259d7abb5029bb3e8ad92718a28
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-19 16:05:09 +0300

    Use the lexer instead of parser for alias

--- lib/rubocop/cop/alias.rb
+++ lib/rubocop/cop/alias.rb
@@ -6,20 +6,11 @@ module Rubocop
       ERROR_MESSAGE = 'Use alias_method instead of alias.'
 
       def inspect(file, source, tokens, sexp)
-        each(:alias, sexp) do |s|
-          if s[1][1][0] == :symbol
-            # alias :full_name :name
-            lineno = s[1][1][1][2].lineno
-          else
-            # alias full_name name
-            lineno = s[1][1][2].lineno
+        tokens.each_index do |ix|
+          t = tokens[ix]
+          if t.type == :on_kw && t.text == 'alias'
+            add_offence(:convention, t.pos.lineno, ERROR_MESSAGE)
           end
-
-          add_offence(
-            :convention,
-            lineno,
-            ERROR_MESSAGE
-          )
         end
       end
     end
