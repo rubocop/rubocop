d9c74ef7ea46ea60efe56b944e24e8d444825dce
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-19 12:24:22 +0300

    Don't run example in the presence of bundler

--- spec/rubocop/cops/syntax_spec.rb
+++ spec/rubocop/cops/syntax_spec.rb
@@ -7,11 +7,13 @@ module Rubocop
     describe Syntax do
       let(:sc) { Syntax.new }
 
-      it 'registers an offence for unused variables', ruby: 2.0 do
-        sc.inspect('file.rb', ['x = 5', 'puts 10'], nil, nil)
-        expect(sc.offences.size).to eq(1)
-        expect(sc.offences.first.message)
-          .to eq('Assigned but unused variable - x')
+      unless defined? Bundler
+        it 'registers an offence for unused variables', ruby: 2.0 do
+          sc.inspect('file.rb', ['x = 5', 'puts 10'], nil, nil)
+          expect(sc.offences.size).to eq(1)
+          expect(sc.offences.first.message)
+            .to eq('Assigned but unused variable - x')
+        end
       end
 
       describe '#process_line' do
