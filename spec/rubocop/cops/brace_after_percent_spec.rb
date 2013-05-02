aac7b7f477f6584278967f44cd8128b0907f29e2
Jurriaan Pruis <email@jurriaanpruis.nl>
2013-05-02 13:58:41 +0200

    fixed BraceAfterPercent spec for Ruby < 2.0

--- spec/rubocop/cops/brace_after_percent_spec.rb
+++ spec/rubocop/cops/brace_after_percent_spec.rb
@@ -9,7 +9,10 @@ module Rubocop
       literals = %w(q Q r i I w W x s)
 
       literals.each do |literal|
-        it "registers an offence for %#{literal}[" do
+        # %i and %I are new in ruby 2.0
+        tag = literal.downcase == 'i' ? {ruby: 2.0} : {}
+
+        it "registers an offence for %#{literal}[", tag  do
           inspect_source(bap,
                          'file.rb',
                          ["puts %#{literal}[test top]"])
@@ -18,7 +21,7 @@ module Rubocop
             .to eq([BraceAfterPercent::ERROR_MESSAGE])
         end
 
-        it "does not registers an offence for %#{literal}(" do
+        it "does not registers an offence for %#{literal}(", tag do
           inspect_source(bap,
                          'file.rb',
                          ["puts %#{literal}(test top)"])
