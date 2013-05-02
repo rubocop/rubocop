d2b621c0ebea5da17749277a080da17b5ec841c7
Jurriaan Pruis <email@jurriaanpruis.nl>
2013-05-02 13:42:11 +0200

    Fixed BraceAfterPercent

--- lib/rubocop/cop/brace_after_percent.rb
+++ lib/rubocop/cop/brace_after_percent.rb
@@ -5,9 +5,11 @@ module Rubocop
     class BraceAfterPercent < Cop
       ERROR_MESSAGE = 'Prefer () as delimiters for all % literals.'
       LITERALS = {
-        on_tstring_beg: '%q',
+        on_tstring_beg: ['%q', '%Q'],
         on_words_beg: '%W',
         on_qwords_beg: '%w',
+        on_qsymbols_beg: '%i',
+        on_symbols_beg: '%I',
         on_regexp_beg: '%r',
         on_symbeg: '%s',
         on_backtick: '%x'
@@ -16,10 +18,12 @@ module Rubocop
       def inspect(file, source, tokens, sexp)
         tokens.each_index do |ix|
           t = tokens[ix]
-          token = LITERALS[t.type]
-          if token && t.text.downcase.start_with?(token) && t.text[2] != '('
-            add_offence(:convention, t.pos.lineno,
-                        ERROR_MESSAGE)
+          literals = Array(LITERALS[t.type])
+          literals.each do |literal|
+            if literal && t.text.start_with?(literal) && t.text[2] != '('
+              add_offence(:convention, t.pos.lineno,
+                          ERROR_MESSAGE)
+            end
           end
         end
       end
--- spec/rubocop/cops/brace_after_percent_spec.rb
+++ spec/rubocop/cops/brace_after_percent_spec.rb
@@ -6,21 +6,24 @@ module Rubocop
   module Cop
     describe BraceAfterPercent do
       let(:bap) { BraceAfterPercent.new }
+      literals = %w(q Q r i I w W x s)
 
-      it 'registers an offence for %w[' do
-        inspect_source(bap,
-                       'file.rb',
-                       ['puts %w[test top]'])
-        expect(bap.offences.size).to eq(1)
-        expect(bap.offences.map(&:message))
-          .to eq([BraceAfterPercent::ERROR_MESSAGE])
-      end
+      literals.each do |literal|
+        it "registers an offence for %#{literal}[" do
+          inspect_source(bap,
+                         'file.rb',
+                         ["puts %#{literal}[test top]"])
+          expect(bap.offences.size).to eq(1)
+          expect(bap.offences.map(&:message))
+            .to eq([BraceAfterPercent::ERROR_MESSAGE])
+        end
 
-      it 'registers an offence for %w(' do
-        inspect_source(bap,
-                       'file.rb',
-                       ['puts %w(test top)'])
-        expect(bap.offences).to be_empty
+        it "does not registers an offence for %#{literal}(" do
+          inspect_source(bap,
+                         'file.rb',
+                         ["puts %#{literal}(test top)"])
+          expect(bap.offences).to be_empty
+        end
       end
     end
   end
