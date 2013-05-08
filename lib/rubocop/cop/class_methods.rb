191566814dc01fa408841a78ac9e337b9d5d0854
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-08 17:15:51 +0300

    Simplify a bit the ClassMethods cop

--- lib/rubocop/cop/class_methods.rb
+++ lib/rubocop/cop/class_methods.rb
@@ -6,13 +6,7 @@ module Rubocop
       ERROR_MESSAGE = 'Prefer self over class/module for class/module methods.'
 
       def inspect(file, source, tokens, sexp)
-        each(:class, sexp) { |d| process_defs(d) }
-        each(:module, sexp) { |d| process_defs(d) }
-      end
-
-      private
-
-      def process_defs(sexp)
+        # defs nodes correspond to class & module methods
         each(:defs, sexp) do |s|
           if s[1][0] == :var_ref && s[1][1][0] == :@const
             add_offence(:convention,
