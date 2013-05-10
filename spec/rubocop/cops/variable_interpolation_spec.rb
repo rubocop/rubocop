c64e97bfa10a6e8a3f309f754aebb3ff0928eb20
Jonas Arvidsson <jonas054@gmail.com>
2013-05-10 15:04:21 +0200

    Handle regexp back references in VariableInterpolation.
    This is a bug fix. Strings like "#$1" caused a crash earlier.

--- spec/rubocop/cops/variable_interpolation_spec.rb
+++ spec/rubocop/cops/variable_interpolation_spec.rb
@@ -16,6 +16,15 @@ module Rubocop
           .to eq(['Replace interpolated var $test with expression #{$test}.'])
       end
 
+      it 'registers an offence for interpolated regexp back references' do
+        inspect_source(vi,
+                       'file.rb',
+                       ['puts "this is a #$1"'])
+        expect(vi.offences.size).to eq(1)
+        expect(vi.offences.map(&:message))
+          .to eq(['Replace interpolated var $1 with expression #{$1}.'])
+      end
+
       it 'registers an offence for interpolated instance variables' do
         inspect_source(vi,
                        'file.rb',
@@ -37,7 +46,7 @@ module Rubocop
       it 'does not register an offence for variables in expressions' do
         inspect_source(vi,
                        'file.rb',
-                       ['puts "this is a #{@test}"'])
+                       ['puts "this is a #{@test} #{@@t} #{$t} #{$1}"'])
         expect(vi.offences).to be_empty
       end
     end
