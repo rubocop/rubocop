4b33e3b1dd2088c39a988e456ab57fa0e60a3c72
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-11 08:31:40 +0300

    Simplify spec a bit

--- spec/rubocop/cops/new_lambda_literal_spec.rb
+++ spec/rubocop/cops/new_lambda_literal_spec.rb
@@ -9,14 +9,13 @@ module Rubocop
 
       it 'registers an offence for an old lambda call' do
         inspect_source(lambda_literal, 'file.rb', ['f = lambda { |x| x }'])
-        expect(lambda_literal.offences.map(&:message)).to eq(
-          ['The new lambda literal syntax is preferred in Ruby 1.9.'])
+        expect(lambda_literal.offences.size).to eq(1)
       end
 
       it 'accepts the new lambda literal' do
         inspect_source(lambda_literal, 'file.rb', ['lambda = ->(x) { x }',
                                               'lambda.(1)'])
-        expect(lambda_literal.offences.map(&:message)).to be_empty
+        expect(lambda_literal.offences).to be_empty
       end
     end
   end
