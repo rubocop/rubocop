c4918e624822b7d44ac66c091e739c7cf637be90
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-13 18:28:54 +0300

    Port SingleLineMethods to Parser

--- lib/rubocop/cop/single_line_methods.rb
+++ lib/rubocop/cop/single_line_methods.rb
@@ -3,57 +3,31 @@
 module Rubocop
   module Cop
     class SingleLineMethods < Cop
-      ERROR_MESSAGE = 'Avoid single-line methods.'
+      ERROR_MESSAGE = 'Avoid single-line method definitions.'
 
-      def inspect(file, source, tokens, sexp)
-        if SingleLineMethods.config['AllowIfMethodIsEmpty']
-          is_empty = empty_methods(sexp)
-        end
-
-        lineno_of_def = nil
-        possible_offence = false
-
-        tokens.each_with_index do |token, ix|
-          if possible_offence
-            if token.pos.lineno > lineno_of_def
-              possible_offence = false
-            elsif [token.type, token.text] == [:on_kw, 'end']
-              add_offence(:convention, lineno_of_def, ERROR_MESSAGE)
-            end
-          end
-
-          if [token.type, token.text] == [:on_kw, 'def']
-            lineno_of_def = token.pos.lineno
-            name_token = tokens[ix..-1].find do |t|
-              [:on_ident, :on_const].include?(t.type)
-            end
-            possible_offence =
-              if SingleLineMethods.config['AllowIfMethodIsEmpty']
-                !is_empty[name_token.pos]
-              else
-                true
-              end
-          end
-        end
+      def self.portable?
+        true
       end
 
-      private
+      def inspect(file, source, tokens, sexp)
+        allow_empty = SingleLineMethods.config['AllowIfMethodIsEmpty']
 
-      # Returns a hash mapping positions of method names to booleans
-      # saying whether or not the method is empty.
-      def empty_methods(sexp)
-        is_empty = {}
-        # Since def is a keyword, def: can confuse the editor. Hence
-        # Ruby 1.8 hash syntax is used here.
-        # rubocop:disable HashSyntax
-        { :def => [1, 3], :defs => [3, 5] }.each do |key, offsets|
-          each(key, sexp) do |d|
-            method_name_pos = d[offsets.first][-1]
-            is_empty[method_name_pos] =
-              (d[offsets.last] == [:bodystmt, [[:void_stmt]], nil, nil, nil])
+        on_node([:def, :defs], sexp) do |s|
+          start_line = s.src.keyword.line
+          end_line = s.src.end.line
+
+          if s.type == :def
+            empty_body = s.children[2].type == :nil
+          else
+            empty_body = s.children[3].type == :nil
+          end
+
+          if start_line == end_line && !(allow_empty && empty_body)
+            add_offence(:convention,
+                        start_line,
+                        ERROR_MESSAGE)
           end
         end
-        is_empty
       end
     end
   end
