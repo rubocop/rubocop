a54c2ed82829811c476bba9cf4eb1e87b045cd66
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-13 16:59:21 +0300

    Port HashLiteral to Parser

--- lib/rubocop/cop/hash_literal.rb
+++ lib/rubocop/cop/hash_literal.rb
@@ -5,56 +5,24 @@ module Rubocop
     class HashLiteral < Cop
       ERROR_MESSAGE = 'Use hash literal {} instead of Hash.new.'
 
-      def inspect(file, source, tokens, sexp)
-        offences = preliminary_scan(sexp)
-
-        # find Hash.new()
-        each(:method_add_arg, sexp) do |s|
-          next if s[1][0] != :call
-
-          receiver = s[1][1][1]
-          method_name = s[1][3][1]
-
-          if receiver && receiver[1] == 'Hash' &&
-              method_name == 'new' && s[2] == [:arg_paren, nil]
-            offences.delete(Offence.new(:convention,
-                                        receiver[2].lineno,
-                                        ERROR_MESSAGE))
-            add_offence(:convention,
-                        receiver[2].lineno,
-                        ERROR_MESSAGE)
-          end
-
-          # check for false positives
-          if receiver && receiver[1] == 'Hash' &&
-              method_name == 'new' && s[2] != [:arg_paren, nil]
-            offences.delete(Offence.new(:convention,
-                                        receiver[2].lineno,
-                                        ERROR_MESSAGE))
-          end
-        end
-
-        offences.each { |o| add_offence(*o.explode) }
+      def self.portable?
+        true
       end
 
-      def preliminary_scan(sexp)
-        offences = []
+      def inspect(file, source, tokens, sexp)
+        on_node(:send, sexp, :block) do |s|
+          children = s.children
 
-        # find Hash.new
-        # there will be some false positives here, which
-        # we'll eliminate later on
-        each(:call, sexp) do |s|
-          receiver = s[1][1]
-
-          if receiver && receiver[1] == 'Hash' &&
-              s[3][1] == 'new'
-            offences << Offence.new(:convention,
-                                    receiver[2].lineno,
-                                    ERROR_MESSAGE)
+          # We're interested in the following AST:
+          # (send
+          #   (const nil :Hash) :new)
+          if children.size == 2 && children[0].type == :const &&
+              children[0].to_a[1].to_s == 'Hash' && children[1] == :new
+            add_offence(:convention,
+                        s.src.line,
+                        ERROR_MESSAGE)
           end
         end
-
-        offences
       end
     end
   end
