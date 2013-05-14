0897b24d58c7a66fea77de38f1574c62d860c03e
Jonas Arvidsson <jonas054@gmail.com>
2013-05-14 19:46:45 +0200

    Port CaseIndentation to Parser.

--- lib/rubocop/cop/case_indentation.rb
+++ lib/rubocop/cop/case_indentation.rb
@@ -5,38 +5,22 @@ module Rubocop
     class CaseIndentation < Cop
       ERROR_MESSAGE = 'Indent when as deep as case.'
 
+      def self.portable?
+        true
+      end
+
       def inspect(file, source, tokens, sexp)
-        case_tokens = find_keywords(tokens, 'case')
-        when_tokens = find_keywords(tokens, 'when')
-        each_when(sexp) do |case_ix|
-          when_pos = when_tokens.shift.pos
-          if when_pos.column != case_tokens[case_ix].pos.column
-            add_offence(:convention, when_pos.lineno, ERROR_MESSAGE)
+        on_node(:case, sexp) do |case_node|
+          _, *bodies = *case_node
+          when_nodes = bodies[0..-2]
+
+          case_column = case_node.source_map.keyword.column
+          when_nodes.each do |when_node|
+            pos = when_node.src.keyword
+            if pos.column != case_column
+              add_offence(:convention, pos.line, ERROR_MESSAGE)
+            end
           end
-        end
-      end
-
-      def find_keywords(tokens, keyword)
-        indexes = tokens.each_index.select do |ix|
-          keyword?(tokens, ix, keyword)
-        end
-        tokens.values_at(*indexes)
-      end
-
-      def keyword?(tokens, ix, keyword)
-        [tokens[ix].type, tokens[ix].text] == [:on_kw, keyword] &&
-          tokens[ix - 1].type != :on_symbeg
-      end
-
-      # Does a depth first search for :when, yielding the index of the
-      # corresponding :case for each one.
-      def each_when(sexp, case_ix = -1, &block)
-        if sexp[0] == :case
-          @total_case_ix = (@total_case_ix || -1) + 1
-          each_when(sexp[2], @total_case_ix, &block)
-        else
-          yield case_ix if sexp[0] == :when
-          sexp.grep(Array).each { |s| each_when(s, case_ix, &block) }
         end
       end
     end
