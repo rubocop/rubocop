d27e47047e3da33b3516e64626337ba88662b7c3
Jonas Arvidsson <jonas054@gmail.com>
2013-05-14 21:14:10 +0200

    Use underscore prefix to denote unused variable that's OK.
    Filter out messages like "Assigned but unused variable - _condition".

--- lib/rubocop/cop/case_indentation.rb
+++ lib/rubocop/cop/case_indentation.rb
@@ -11,11 +11,10 @@ module Rubocop
 
       def inspect(file, source, tokens, sexp)
         on_node(:case, sexp) do |case_node|
-          _, *bodies = *case_node
-          when_nodes = bodies[0..-2]
+          _condition, *whens, _else = *case_node
 
           case_column = case_node.source_map.keyword.column
-          when_nodes.each do |when_node|
+          whens.each do |when_node|
             pos = when_node.src.keyword
             if pos.column != case_column
               add_offence(:convention, pos.line, ERROR_MESSAGE)
