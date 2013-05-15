3d2d595fcbafec2f6286ef98ecf644ab52860294
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-15 17:55:05 +0300

    Port CollectionMethods to Parser

--- lib/rubocop/cop/collection_methods.rb
+++ lib/rubocop/cop/collection_methods.rb
@@ -4,23 +4,26 @@ module Rubocop
   module Cop
     class CollectionMethods < Cop
       PREFERRED_METHODS = {
-        'collect' => 'map',
-        'inject' => 'reduce',
-        'detect' => 'find',
-        'find_all' => 'select',
+        collect: 'map',
+        inject: 'reduce',
+        detect: 'find',
+        find_all: 'select'
       }
 
+      def self.portable?
+        true
+      end
+
       def inspect(file, source, tokens, sexp)
-        each(:call, sexp) do |s|
-          s.drop(2).each_slice(2) do |m|
-            method_name = m[1][1]
-            if PREFERRED_METHODS[method_name]
-              add_offence(
-                :convention,
-                m[1][2].lineno,
-                "Prefer #{PREFERRED_METHODS[method_name]} over #{method_name}."
-              )
-            end
+        on_node(:send, sexp) do |node|
+          _receiver, method_name, *_args = *node
+
+          if PREFERRED_METHODS[method_name]
+            add_offence(
+              :convention,
+              node.src.line,
+              "Prefer #{PREFERRED_METHODS[method_name]} over #{method_name}."
+            )
           end
         end
       end
