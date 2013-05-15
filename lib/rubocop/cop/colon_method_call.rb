01fda7253171e642c919c5b660ce6bb67bec532f
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-15 14:53:03 +0300

    Port ColonMethodCall to Parser

--- lib/rubocop/cop/colon_method_call.rb
+++ lib/rubocop/cop/colon_method_call.rb
@@ -5,38 +5,17 @@ module Rubocop
     class ColonMethodCall < Cop
       ERROR_MESSAGE = 'Do not use :: for method invocation.'
 
+      def self.portable?
+        true
+      end
+
       def inspect(file, source, tokens, sexp)
-        state = :outside
-        tokens.each do |t|
-          state = case [state, t.type]
-                  when [:outside, :on_const]
-                    :const
-
-                  when [:outside, :on_ident]
-                    :ident
-
-                  when [:const, :on_op]
-                    t.text == '::' ? :const_colon : :outside
-
-                  when [:ident, :on_op]
-                    t.text == '::' ? :ident_colon : :outside
-
-                  when [:ident_colon, :on_ident]
-                    add_offence(:convention, t.pos.lineno, ERROR_MESSAGE)
-                    :ident
-
-                  when [:const_colon, :on_ident]
-                    add_offence(:convention, t.pos.lineno, ERROR_MESSAGE)
-                    :ident
-
-                  when [:ident_colon, :on_const]
-                    :const
-
-                  when [:const_colon, :on_const]
-                    :const
-                  else
-                    :outside
-            end || state
+        on_node(:send, sexp) do |s|
+          if s.src.expression.to_source.include?('::')
+            add_offence(:convention,
+                        s.src.line,
+                        ERROR_MESSAGE)
+          end
         end
       end
     end
