d284739a920588c33c19897b6e61446e92c3b771
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-16 18:27:43 +0300

    Port EnsureReturn to Parser

--- lib/rubocop/cop/ensure_return.rb
+++ lib/rubocop/cop/ensure_return.rb
@@ -5,31 +5,20 @@ module Rubocop
     class EnsureReturn < Cop
       ERROR_MESSAGE = 'Never return from an ensure block.'
 
-      def inspect(file, source, tokens, sexp)
-        in_ensure = false
-        ensure_col = nil
+      def self.portable?
+        true
+      end
 
-        tokens.each_index do |ix|
-          t = tokens[ix]
-          if ensure_start?(t)
-            in_ensure = true
-            ensure_col = t.pos.column
-          elsif ensure_end?(t, ensure_col)
-            in_ensure = false
-          elsif in_ensure && t.type == :on_kw && t.text == 'return'
-            add_offence(:warning, t.pos.lineno, ERROR_MESSAGE)
+      def inspect(file, source, tokens, sexp)
+        on_node(:ensure, sexp) do |ensure_node|
+          _body, ensure_body = *ensure_node
+
+          on_node(:return, ensure_body) do |e|
+            add_offence(:warning,
+                        e.src.line,
+                        ERROR_MESSAGE)
           end
         end
-      end
-
-      private
-
-      def ensure_start?(t)
-        t.type == :on_kw && t.text == 'ensure'
-      end
-
-      def ensure_end?(t, column)
-        t.type == :on_kw && t.text == 'end' && t.pos.column == column
       end
     end
   end
