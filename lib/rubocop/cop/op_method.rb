ca067ac8a0d94120d94cfc3964ac186d7311a23f
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-16 13:45:00 +0300

    Port OpMethod to Parser

--- lib/rubocop/cop/op_method.rb
+++ lib/rubocop/cop/op_method.rb
@@ -5,30 +5,24 @@ module Rubocop
     class OpMethod < Cop
       ERROR_MESSAGE = 'When defining the %s operator, name its argument other.'
 
+      BLACKLISTED = [:+@, :-@, :[], :[]=, :<<]
+
+      TARGET_ARGS = s(:args, s(:arg, :other))
+
+      def self.portable?
+        true
+      end
+
       def inspect(file, source, tokens, sexp)
-        each(:def, sexp) do |s|
-          if binary_operator?(s) && !%w([] []= <<).include?(s[1][1])
-            params = parameters(s[2])
-            unless params[0][1] == 'other'
-              add_offence(:convention,
-                          params[0][2].lineno,
-                          sprintf(ERROR_MESSAGE, s[1][1]))
-            end
+        on_node(:def, sexp) do |s|
+          name, args, _body = *s
+
+          if name !~ /\A\w/ && !BLACKLISTED.include?(name) &&
+            args.children.size == 1 && args != TARGET_ARGS
+            add_offence(:convention,
+                        s.src.line,
+                        sprintf(ERROR_MESSAGE, name))
           end
-        end
-      end
-
-      private
-
-      def binary_operator?(def_sexp)
-        def_sexp[1][0] == :@op && parameters(def_sexp[2]).size == 1
-      end
-
-      def parameters(param_sexp)
-        if param_sexp[0] == :paren # param is surrounded by braces?
-          parameters(param_sexp[1])
-        else
-          param_sexp[1] || []
         end
       end
     end
