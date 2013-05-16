f903dcb4ebc49e020a652b487d6782405c2d4073
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-16 16:37:35 +0300

    Use regexps instead of Ripper's lexer
    Nothing we can do about checks like this with Parser.
    For now they'll be simple(and flawed) regexp checks.

--- lib/rubocop/cop/empty_lines.rb
+++ lib/rubocop/cop/empty_lines.rb
@@ -5,17 +5,15 @@ module Rubocop
     class EmptyLines < Cop
       ERROR_MESSAGE = 'Extra blank line detected.'
 
+      def self.portable?
+        true
+      end
+
       def inspect(file, source, tokens, sexp)
-        previous_token = Token.new(0, :init, '')
-
-        tokens.each do |t|
-          if t.type == :on_ignored_nl && previous_token.type == :on_ignored_nl
-            add_offence(:convention,
-                        t.pos.lineno,
-                        ERROR_MESSAGE)
+        source.each_with_index do |line, ix|
+          if ix > 0 && line.empty? && source[ix - 1].empty?
+            add_offence(:convention, ix, ERROR_MESSAGE)
           end
-
-          previous_token = t
         end
       end
     end
