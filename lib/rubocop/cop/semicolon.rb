7166702704048173580427c528414f98fd0b521c
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-16 15:38:31 +0300

    Add some comments

--- lib/rubocop/cop/semicolon.rb
+++ lib/rubocop/cop/semicolon.rb
@@ -15,9 +15,11 @@ module Rubocop
 
           next if exprs.size < 2
 
+          # create a map matching lines to the number of expressions on them
           exprs_lines = exprs.map { |e| e.src.expression.line }
           lines = exprs_lines.group_by { |i| i }
 
+          # every line with more than 1 expression on it is an offence
           lines.each do |line, expr_on_line|
             if expr_on_line.size > 1
               add_offence(:convention, line, ERROR_MESSAGE)
@@ -25,6 +27,7 @@ module Rubocop
           end
         end
 
+        # not pretty reliable, but the best we can do for now
         source.each_with_index do |line, index|
           add_offence(:convention, index, ERROR_MESSAGE) if line =~ /;\s*\z/
         end
