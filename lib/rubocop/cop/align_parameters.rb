87f31e1283f88665eb2ce40d5bbf27c22b899d3d
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-17 14:33:36 +0300

    Port AlignParameters to Parser

--- lib/rubocop/cop/align_parameters.rb
+++ lib/rubocop/cop/align_parameters.rb
@@ -6,107 +6,36 @@ module Rubocop
       ERROR_MESSAGE = 'Align the parameters of a method call if they span ' +
         'more than one line.'
 
+      def self.portable?
+        true
+      end
+
       def inspect(file, source, tokens, sexp)
-        @file = file
-        @tokens = tokens
-        @token_indexes = {}
-        @tokens.each_with_index { |t, ix| @token_indexes[t.pos] = ix }
+        on_node(:send, sexp) do |node|
+          _receiver, method, *args = *node
 
-        each(:method_add_arg, sexp) do |method_add_arg|
-          args = get_args(method_add_arg) or next
-          first_arg, rest_of_args = divide_args(args)
-          @first_lparen_ix = get_lparen_ix(method_add_arg)
-          pos_of_1st_arg = position_of(first_arg) or next # Give up.
-          rest_of_args.each do |arg|
-            pos = position_of(arg) or next # Give up if no position found.
-            if pos.lineno != pos_of_1st_arg.lineno
-              if pos.column != pos_of_1st_arg.column
-                add_offence(:convention, pos.lineno, ERROR_MESSAGE)
-              end
+          next if method == :[]=
+          next unless args.size > 1
+
+          first_arg_col = args.first.src.expression.column
+          prev_arg_line = args.first.src.expression.line
+          prev_arg_col = args.first.src.expression.column
+
+          args.each do |arg|
+            cur_arg_line = arg.src.expression.line
+            cur_arg_col = arg.src.expression.column
+
+            if cur_arg_line != prev_arg_line &&
+                cur_arg_col != first_arg_col
+              add_offence(:convetion,
+                          cur_arg_line,
+                          ERROR_MESSAGE)
             end
+
+            prev_arg_col = cur_arg_col
+            prev_arg_line = cur_arg_line
           end
         end
-      end
-
-      private
-
-      def get_args(method_add_arg)
-        fcall = method_add_arg[1]
-        return nil if fcall[0] != :fcall
-        return nil if fcall[1][0..1] == [:@ident, 'lambda']
-        arg_paren = method_add_arg[2..-1][0]
-        return nil if arg_paren[0] != :arg_paren || arg_paren[1].nil?
-
-        # A command (call wihtout parentheses) as first parameter
-        # means there's only one parameter.
-        return nil if [:command, :command_call].include?(arg_paren[1][0][0])
-
-        if arg_paren[1][0] == :args_add_block
-          args_add_block = arg_paren[1]
-          args_add_block[1].empty? ? [args_add_block[2]] : args_add_block[1]
-        else
-          arg_paren[1]
-        end
-      end
-
-      def divide_args(args)
-        if args[0] == :args_add_star
-          first_arg = args[1]
-          rest_of_args = args[2..-1]
-        else
-          first_arg = args[0]
-          rest_of_args = args[1..-1]
-        end
-        [first_arg, rest_of_args]
-      end
-
-      def get_lparen_ix(method_add_arg)
-        method_name_pos = method_add_arg[1][1][-1]
-        method_name_ix = @token_indexes[method_name_pos]
-        method_name_ix +
-          @tokens[method_name_ix..-1].map(&:type).index(:on_lparen)
-      end
-
-      def position_of(sexp)
-        # Indentation inside a string literal is irrelevant.
-        return nil if sexp[0] == :string_literal
-
-        pos = find_pos_in_sexp(sexp) or return nil # Nil means not found.
-        ix = find_first_non_whitespace_token(pos) or return nil
-        @tokens[ix].pos
-      end
-
-      def find_pos_in_sexp(sexp)
-        return sexp[2] if Position === sexp[2]
-        sexp.grep(Array).each do |s|
-          pos = find_pos_in_sexp(s) and return pos
-        end
-        nil
-      end
-
-      def find_first_non_whitespace_token(pos)
-        ix = @token_indexes[pos]
-        newline_found = false
-        start_ix = ix.downto(0) do |i|
-          case @tokens[i].text
-          when '('
-            break i + 1 if i == @first_lparen_ix
-          when "\n"
-            newline_found = true
-          when /\t/
-            # Bail out if tabs are used. Too difficult to calculate column.
-            return nil
-          when ','
-            if newline_found
-              break i + 1
-            else
-              # Bail out if there's a preceding comma on the same line.
-              return nil
-            end
-          end
-        end
-        offset = @tokens[start_ix..-1].index { |t| !whitespace?(t) }
-        start_ix + offset
       end
     end
   end
--- spec/rubocop/cops/align_parameters_spec.rb
+++ spec/rubocop/cops/align_parameters_spec.rb
@@ -10,17 +10,19 @@ module Rubocop
       it 'registers an offence for parameters with single indent' do
         inspect_source(align, 'file.rb', ['function(a,',
                                           '  if b then c else d end)'])
-        expect(align.offences.map(&:message)).to eq(
-          ['Align the parameters of a method call if they span more than ' +
-           'one line.'])
+        expect(align.offences.size).to eq(1)
       end
 
       it 'registers an offence for parameters with double indent' do
         inspect_source(align, 'file.rb', ['function(a,',
                                           '    if b then c else d end)'])
-        expect(align.offences.map(&:message)).to eq(
-          ['Align the parameters of a method call if they span more than ' +
-           'one line.'])
+        expect(align.offences.size).to eq(1)
+      end
+
+      it 'accepts multiline []= method call' do
+        inspect_source(align, 'file.rb', ['Test.config["something"] =',
+                                          ' true'])
+        expect(align.offences).to be_empty
       end
 
       it 'accepts correctly aligned parameters' do
@@ -28,19 +30,19 @@ module Rubocop
                                           '         0, 1,',
                                           '         (x + y),',
                                           '         if b then c else d end)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'accepts calls that only span one line' do
         inspect_source(align, 'file.rb', ['find(path, s, @special[sexp[0]])'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it "doesn't get confused by a symbol argument" do
         inspect_source(align, '',
                        ['add_offence(:convention, index,',
                         '            ERROR_MESSAGE % kind)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it "doesn't get confused by splat operator" do
@@ -71,40 +73,40 @@ module Rubocop
         inspect_source(align, '',
                        ['add_offence(:convention, x,',
                         '            a)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle a string literal as other argument' do
         inspect_source(align, '',
                        ['add_offence(:convention,',
                         '            "", a)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it "doesn't get confused by a line break inside a parameter" do
         inspect_source(align, '',
                        ['read(path, { headers:    true,',
                         '             converters: :numeric })'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it "doesn't get confused by symbols with embedded expressions" do
         inspect_source(align, '',
                        ['send(:"#{name}_comments_path")'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it "doesn't get confused by regexen with embedded expressions" do
         inspect_source(align, '',
                        ['a(/#{name}/)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'accepts braceless hashes' do
         inspect_source(align, '',
                        ['run(collection, :entry_name => label,',
                         '                :paginator  => paginator)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'accepts the first parameter being on a new row' do
@@ -113,7 +115,7 @@ module Rubocop
                         '    a,',
                         '    b',
                         '  )'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle heredoc strings' do
@@ -124,7 +126,7 @@ module Rubocop
                '            end',
                '            EOS']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle a method call within a method call' do
@@ -133,13 +135,13 @@ module Rubocop
                         '  b(b1,',
                         '    b2),',
                         '  a2)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle a call embedded in a string' do
         inspect_source(align, '',
                        ['model("#{index(name)}", child)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle do-end' do
@@ -147,7 +149,7 @@ module Rubocop
                        ['      run(lambda do |e|',
                         "        w = e['warden']",
                         '      end)'])
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle a call with a block inside another call' do
@@ -156,27 +158,19 @@ module Rubocop
                "      col['name']",
                '    })']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle a ternary condition with a block reference' do
         src = ['cond ? a : func(&b)']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle parentheses used with no parameters' do
         src = ['func()']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
-      end
-
-      it 'can handle a multiline hash as first parameter' do
-        src = ['assert_equal({',
-               '  :space_before => "",',
-               '}, state)']
-        inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle a multiline hash as second parameter' do
@@ -184,26 +178,19 @@ module Rubocop
                '  :value => value',
                '})']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle method calls without parentheses' do
         src = ['a(b c, d)']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
 
       it 'can handle other method calls without parentheses' do
         src = ['chars(Unicode.apply_mapping @wrapped_string, :uppercase)']
         inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
-      end
-
-      it "doesn't check alignment if tabs are used to indent" do
-        src = ['a(b,',
-               "\tc)"]
-        inspect_source(align, '', src)
-        expect(align.offences.map(&:message)).to be_empty
+        expect(align.offences).to be_empty
       end
     end
   end
