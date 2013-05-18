3639b52a9e547c3568bdd206f869a0d5fcd09496
Jonas Arvidsson <jonas054@gmail.com>
2013-05-18 07:33:11 +0200

    Port SpaceAfterComma, SpaceAfterSemicolon, SpaceAfterColon to Parser.

--- lib/rubocop/cop/space_after_comma_etc.rb
+++ lib/rubocop/cop/space_after_comma_etc.rb
@@ -1,35 +1,60 @@
 # encoding: utf-8
 
+# rubocop:disable SymbolName
+
 module Rubocop
   module Cop
     module SpaceAfterCommaEtc
       MSG = 'Space missing after %s.'
 
       def inspect(file, source, tokens, sexp)
-        # TODO
+        tokens.each_cons(2) do |t1, t2|
+          if kind(t1) && t1.pos.lineno == t2.pos.lineno &&
+              t2.pos.column == t1.pos.column + offset(t1)
+            add_offence(:convention, t1.pos.lineno, sprintf(MSG, kind(t1)))
+          end
+        end
+      end
+
+      # The normal offset, i.e., the distance from the punctuation
+      # token where a space should be, is 1.
+      def offset(token)
+        1
       end
     end
 
     class SpaceAfterComma < Cop
       include SpaceAfterCommaEtc
+
       def kind(token)
-        'comma' if token.type == :on_comma
+        'comma' if token.type == :tCOMMA
       end
     end
 
     class SpaceAfterSemicolon < Cop
       include SpaceAfterCommaEtc
+
       def kind(token)
-        'semicolon' if token.type == :on_semicolon
+        'semicolon' if token.type == :tSEMI
       end
     end
 
     class SpaceAfterColon < Cop
       include SpaceAfterCommaEtc
+
+      # The colon following a label will not appear in the token
+      # array. Instad we get a tLABEL token, whose length we use to
+      # calculate where we expect a space.
+      def offset(token)
+        case token.type
+        when :tLABEL then token.text.length + 1
+        when :tCOLON then 1
+        end
+      end
+
       def kind(token)
         case token.type
-        when :on_label then 'colon'
-        when :on_op    then 'colon' if token.text == ':'
+        when :tLABEL, :tCOLON then 'colon'
         end
       end
     end
--- spec/rubocop/cops/space_after_colon_spec.rb
+++ spec/rubocop/cops/space_after_colon_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 module Rubocop
   module Cop
-    describe SpaceAfterColon, broken: true do
+    describe SpaceAfterColon do
       let(:space) { SpaceAfterColon.new }
 
       it 'registers an offence for colon without space after it' do
