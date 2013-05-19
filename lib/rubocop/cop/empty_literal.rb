e1d609f694d8ca3a35ae53c1c5e5b4c9e012d87a
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-19 09:15:22 +0300

    Combined ArrayLiteral & HashLiteral into EmptyLiteral
    Performance is therefore improved since we don't have to walk the
    AST twice. I've also added a check for String.new invocations.

---
+++ lib/rubocop/cop/empty_literal.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+module Rubocop
+  module Cop
+    class EmptyLiteral < Cop
+      ARR_MSG = 'Use array literal [] instead of Array.new.'
+      HASH_MSG = 'Use hash literal {} instead of Hash.new.'
+      STR_MSG = "Use string literal '' instead of String.new."
+
+      # Empty array node
+      #
+      # (send
+      #   (const nil :Array) :new)
+      ARRAY_NODE = s(:send, s(:const, nil, :Array), :new)
+
+      # Empty hash node
+      #
+      # (send
+      #   (const nil :Hash) :new)
+      HASH_NODE = s(:send, s(:const, nil, :Hash), :new)
+
+      # Empty string node
+      #
+      # (send
+      #   (const nil :String) :new)
+      STR_NODE = s(:send, s(:const, nil, :String), :new)
+
+      def inspect(file, source, tokens, ast)
+        on_node(:send, ast, :block) do |node|
+          if node == ARRAY_NODE
+            add_offence(:convention,
+                        node.src.line,
+                        ARR_MSG)
+          elsif node == HASH_NODE
+            add_offence(:convention,
+                        node.src.line,
+                        HASH_MSG)
+          elsif node == STR_NODE
+            add_offence(:convention,
+                        node.src.line,
+                        STR_MSG)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cops/empty_literal_spec.rb
@@ -0,0 +1,100 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+module Rubocop
+  module Cop
+    describe EmptyLiteral do
+      let(:a) { EmptyLiteral.new }
+
+      describe 'Empty Array' do
+        it 'registers an offence for Array.new()' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Array.new()'])
+          expect(a.offences.size).to eq(1)
+          expect(a.offences.map(&:message))
+            .to eq([EmptyLiteral::ARR_MSG])
+        end
+
+        it 'registers an offence for Array.new' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Array.new'])
+          expect(a.offences.size).to eq(1)
+          expect(a.offences.map(&:message))
+            .to eq([EmptyLiteral::ARR_MSG])
+        end
+
+        it 'does not register an offence for Array.new(3)' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Array.new(3)'])
+          expect(a.offences).to be_empty
+        end
+      end
+
+      describe 'Empty Hash' do
+        it 'registers an offence for Hash.new()' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Hash.new()'])
+          expect(a.offences.size).to eq(1)
+          expect(a.offences.map(&:message))
+            .to eq([EmptyLiteral::HASH_MSG])
+        end
+
+        it 'registers an offence for Hash.new' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Hash.new'])
+          expect(a.offences.size).to eq(1)
+          expect(a.offences.map(&:message))
+            .to eq([EmptyLiteral::HASH_MSG])
+        end
+
+        it 'does not register an offence for Hash.new(3)' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Hash.new(3)'])
+          expect(a.offences).to be_empty
+        end
+
+        it 'does not register an offence for Hash.new { block }' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = Hash.new { block }'])
+          expect(a.offences).to be_empty
+        end
+      end
+
+      describe 'Empty String' do
+        it 'registers an offence for String.new()' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = String.new()'])
+          expect(a.offences.size).to eq(1)
+          expect(a.offences.map(&:message))
+            .to eq([EmptyLiteral::STR_MSG])
+        end
+
+        it 'registers an offence for String.new' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = String.new'])
+          expect(a.offences.size).to eq(1)
+          expect(a.offences.map(&:message))
+            .to eq([EmptyLiteral::STR_MSG])
+        end
+
+        it 'does not register an offence for String.new("top")' do
+          inspect_source(a,
+                         'file.rb',
+                         ['test = String.new("top")'])
+          expect(a.offences).to be_empty
+        end
+      end
+
+    end
+  end
+end
