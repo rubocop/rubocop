f7ab7c5a91e67a43607a3d00dfc526a0f8002ec2
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-22 14:02:13 +0300

    Port CaseIndentation cop to AST::Processor

--- lib/rubocop/cop/case_indentation.rb
+++ lib/rubocop/cop/case_indentation.rb
@@ -5,18 +5,17 @@ module Rubocop
     class CaseIndentation < Cop
       MSG = 'Indent when as deep as case.'
 
-      def inspect(file, source, tokens, ast)
-        on_node(:case, ast) do |case_node|
-          _condition, *whens, _else = *case_node
+      def on_case(case_node)
+        _condition, *whens, _else = *case_node
 
-          case_column = case_node.source_map.keyword.column
-          whens.each do |when_node|
-            pos = when_node.src.keyword
-            if pos.column != case_column
-              add_offence(:convention, pos.line, MSG)
-            end
-          end
+        case_column = case_node.source_map.keyword.column
+
+        whens.each do |when_node|
+          pos = when_node.src.keyword
+          add_offence(:convention, pos.line, MSG) if pos.column != case_column
         end
+
+        super
       end
     end
   end
