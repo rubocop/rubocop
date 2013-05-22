41e385d45a3b03cc9f169cf8f04a15547de48c2e
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-22 14:24:23 +0300

    Port ClassMethods cop to AST::Processor

--- lib/rubocop/cop/class_methods.rb
+++ lib/rubocop/cop/class_methods.rb
@@ -5,15 +5,10 @@ module Rubocop
     class ClassMethods < Cop
       MSG = 'Prefer self over class/module for class/module methods.'
 
-      def inspect(file, source, tokens, ast)
-        # defs nodes correspond to class & module methods
-        on_node(:defs, ast) do |s|
-          if s.children.first.type == :const
-            add_offence(:convention,
-                        s.src.line,
-                        MSG)
-          end
-        end
+      def on_defs(node)
+        definee, _name, _args, _body = *node
+
+        add_offence(:convention, node.src.line, MSG) if definee.type == :const
       end
     end
   end
