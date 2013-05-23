2e9aba0e66d764a3fce8a3832d0218eb43ef2e41
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-23 18:25:56 +0300

    Port ParameterLists to AST::Processor

--- lib/rubocop/cop/parameter_lists.rb
+++ lib/rubocop/cop/parameter_lists.rb
@@ -5,12 +5,12 @@ module Rubocop
     class ParameterLists < Cop
       MSG = 'Avoid parameter lists longer than four parameters.'
 
-      def inspect(file, source, tokens, ast)
-        on_node(:args, ast) do |node|
-          args_count = node.children.size
+      def on_args(node)
+        args_count = node.children.size
 
-          add_offence(:convention, node.src.line, MSG) if args_count > 4
-        end
+        add_offence(:convention, node.src.line, MSG) if args_count > 4
+
+        super
       end
     end
   end
