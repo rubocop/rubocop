78519a85f140b1a548007b124d0df8e2786fe003
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-23 17:21:22 +0300

    Port ConstantName to AST::Processor

--- lib/rubocop/cop/constant_name.rb
+++ lib/rubocop/cop/constant_name.rb
@@ -6,18 +6,15 @@ module Rubocop
       MSG = 'Use SCREAMING_SNAKE_CASE for constants.'
       SNAKE_CASE = /^[\dA-Z_]+$/
 
-      def inspect(file, source, tokens, ast)
-        on_node(:cdecl, ast) do |node|
-          _scope, const_name, value = *node
+      def on_cdecl(node)
+        _scope, const_name, value = *node
 
-          # we cannot know the result of method calls
-          next if value.type == :send
-
-          unless const_name =~ SNAKE_CASE
-            add_offence(:convention,
-                        node.src.line,
-                        MSG)
-          end
+        # we cannot know the result of method calls line
+        # NewClass = something_that_returns_a_class
+        if value.type != :send && const_name !~ SNAKE_CASE
+          add_offence(:convention,
+                      node.src.line,
+                      MSG)
         end
       end
     end
