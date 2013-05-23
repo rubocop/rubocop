16829f90fbd80310b0f3778f028bf0ae7471c9a8
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-23 18:47:53 +0300

    Port Not to AST::Processor

--- lib/rubocop/cop/not.rb
+++ lib/rubocop/cop/not.rb
@@ -5,17 +5,16 @@ module Rubocop
     class Not < Cop
       MSG = 'Use ! instead of not.'
 
-      def inspect(file, source, tokens, ast)
-        on_node(:send, ast) do |node|
-          _receiver, method_name, *args = *node
+      def on_send(node)
+        _receiver, method_name, *args = *node
 
-          # not does not take any arguments
-          next unless args.empty?
-
-          if method_name == :! && node.src.selector.to_source == 'not'
-            add_offence(:convention, node.src.line, MSG)
-          end
+        # not does not take any arguments
+        if args.empty? && method_name == :! &&
+            node.src.selector.to_source == 'not'
+          add_offence(:convention, node.src.line, MSG)
         end
+
+        super
       end
     end
   end
