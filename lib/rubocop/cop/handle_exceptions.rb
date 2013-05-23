78a5d1677814164953ae5fbce51075798737a76f
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-23 19:41:58 +0300

    Port HandleExceptions to AST::Processor

--- lib/rubocop/cop/handle_exceptions.rb
+++ lib/rubocop/cop/handle_exceptions.rb
@@ -5,14 +5,12 @@ module Rubocop
     class HandleExceptions < Cop
       MSG = 'Do not suppress exceptions.'
 
-      def inspect(file, source, tokens, ast)
-        on_node(:resbody, ast) do |node|
-          _exc_list_node, _exc_var_node, body_node = *node
+      def on_resbody(node)
+        _exc_list_node, _exc_var_node, body_node = *node
 
-          add_offence(:warning,
-                      node.src.line,
-                      MSG) if body_node.type == :nil
-        end
+        add_offence(:warning, node.src.line, MSG) if body_node.type == :nil
+
+        super
       end
     end
   end
