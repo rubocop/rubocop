89c86294e1efc168be500d3d8f7753d1a145fa20
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-23 18:45:35 +0300

    Port NumericLiterals to AST::Processor

--- lib/rubocop/cop/numeric_literals.rb
+++ lib/rubocop/cop/numeric_literals.rb
@@ -6,14 +6,16 @@ module Rubocop
       MSG = 'Add underscores to large numeric literals to ' +
         'improve their readability.'
 
-      def inspect(file, source, tokens, ast)
-        on_node([:int, :float], ast) do |s|
-          if s.to_a[0] > 10000 &&
-              s.src.expression.to_source.split('.').grep(/\d{6}/).any?
-            add_offence(:convention, s.src.expression.line, MSG)
-          end
+      def on_int(node)
+        value, = *node
+
+        if value > 10000 &&
+            node.src.expression.to_source.split('.').grep(/\d{6}/).any?
+          add_offence(:convention, node.src.expression.line, MSG)
         end
       end
+
+      alias_method :on_float, :on_int
     end
   end
 end
