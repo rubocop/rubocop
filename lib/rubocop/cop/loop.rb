37f83e67a600c8d7f416e9e3cb265a99743941bb
Bozhidar Batsov <bozhidar@batsov.com>
2013-05-24 09:50:16 +0300

    Port Loop to AST::Processor

--- lib/rubocop/cop/loop.rb
+++ lib/rubocop/cop/loop.rb
@@ -5,15 +5,23 @@ module Rubocop
     class Loop < Cop
       MSG = 'Use Kernel#loop with break rather than begin/end/until(or while).'
 
-      def inspect(file, source, tokens, ast)
-        on_node([:while, :until], ast) do |node|
-          _cond, body = *node
-          type = node.type.to_s
+      def on_while(node)
+        check(node)
+      end
 
-          if body.type == :begin &&
-              !node.src.expression.to_source.start_with?(type)
-            add_offence(:warning, node.src.keyword.line, MSG)
-          end
+      def on_until(node)
+        check(node)
+      end
+
+      private
+
+      def check(node)
+        _cond, body = *node
+        type = node.type.to_s
+
+        if body.type == :begin &&
+            !node.src.expression.to_source.start_with?(type)
+          add_offence(:warning, node.src.keyword.line, MSG)
         end
       end
     end
