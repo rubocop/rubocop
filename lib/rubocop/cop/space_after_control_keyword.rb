7260e8792a78cf64b5b076798a8d7a39b4ab937f
Jonas Arvidsson <jonas054@gmail.com>
2013-05-24 14:48:03 +0200

    Port SpaceAfterControlKeyword to Parser.

--- lib/rubocop/cop/space_after_control_keyword.rb
+++ lib/rubocop/cop/space_after_control_keyword.rb
@@ -4,11 +4,25 @@ module Rubocop
   module Cop
     class SpaceAfterControlKeyword < Cop
       MSG = 'Use space after control keywords.'
+      # elsif and unless are handled by on_if.
+      KEYWORDS = %w(if case when while until)
 
-      KEYWORDS = %w(if elsif case when while until unless)
+      def on_keyword(node)
+        return if node.src.is_a?(Parser::Source::Map::Ternary)
 
-      def inspect(file, source, tokens, ast)
-        # TODO
+        exp = node.src.expression
+        kw = node.src.keyword
+        kw_offset = kw.begin_pos - exp.begin_pos
+        if exp.to_source[kw_offset..-1].start_with?(kw.to_source + '(')
+          add_offence(:convention, kw.line, MSG)
+        end
+      end
+
+      KEYWORDS.each do |keyword|
+        define_method(:"on_#{keyword}") do |node|
+          on_keyword(node)
+          super(node)
+        end
       end
     end
   end
