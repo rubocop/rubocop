cab0ad7c5a0972a5e50fedba57c0e309ea21d198
Bozhidar Batsov <bozhidar@batsov.com>
2013-05-24 09:44:40 +0300

    Port SingleLineMethods to AST::Processor

--- lib/rubocop/cop/single_line_methods.rb
+++ lib/rubocop/cop/single_line_methods.rb
@@ -5,24 +5,34 @@ module Rubocop
     class SingleLineMethods < Cop
       MSG = 'Avoid single-line method definitions.'
 
-      def inspect(file, source, tokens, ast)
-        allow_empty = SingleLineMethods.config['AllowIfMethodIsEmpty']
+      def allow_empty?
+        SingleLineMethods.config['AllowIfMethodIsEmpty']
+      end
 
-        on_node([:def, :defs], ast) do |s|
-          start_line = s.src.keyword.line
-          end_line = s.src.end.line
+      def on_def(node)
+        check(node)
+      end
 
-          if s.type == :def
-            empty_body = s.children[2].type == :nil
-          else
-            empty_body = s.children[3].type == :nil
-          end
+      def on_defs(node)
+        check(node)
+      end
 
-          if start_line == end_line && !(allow_empty && empty_body)
-            add_offence(:convention,
-                        start_line,
-                        MSG)
-          end
+      private
+
+      def check(node)
+        start_line = node.src.keyword.line
+        end_line = node.src.end.line
+
+        if node.type == :def
+          empty_body = node.children[2].type == :nil
+        else
+          empty_body = node.children[3].type == :nil
+        end
+
+        if start_line == end_line && !(allow_empty? && empty_body)
+          add_offence(:convention,
+                      start_line,
+                      MSG)
         end
       end
     end
