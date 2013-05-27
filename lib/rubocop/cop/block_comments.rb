02813eadf3c049a6a2eb350981662d153fa0886f
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-27 12:03:26 +0300

    Port BlockComments to Parser 2.0

--- lib/rubocop/cop/block_comments.rb
+++ lib/rubocop/cop/block_comments.rb
@@ -5,9 +5,9 @@ module Rubocop
     class BlockComments < Cop
       MSG = 'Do not use block comments.'
 
-      def inspect(source, tokens, ast, comments)
-        source.each_with_index do |line, ix|
-          add_offence(:convention, ix, MSG) if line =~ /\A=begin\b/
+      def on_comment(comment)
+        if comment.text.start_with?('=begin')
+          add_offence(:convention, comment.loc.line, MSG)
         end
       end
     end
