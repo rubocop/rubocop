e370e431f783ad14c9db6381acd307918f4fb492
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-29 12:09:52 +0300

    Port StringLiterals to AST::Processor

--- lib/rubocop/cop/string_literals.rb
+++ lib/rubocop/cop/string_literals.rb
@@ -6,15 +6,16 @@ module Rubocop
       MSG = "Prefer single-quoted strings when you don't need " +
         'string interpolation or special symbols.'
 
-      def inspect(source, tokens, ast, comments)
-        on_node(:str, ast, [:dstr, :regexp]) do |node|
-          text, = *node
+      def on_str(node)
+        text, = *node
 
-          if text !~ /['\n\t\r]/ && node.loc.begin.source == '"'
-            add_offence(:convention, node.loc.line, MSG)
-          end
+        if text !~ /['\n\t\r]/ && node.loc.begin.source == '"'
+          add_offence(:convention, node.loc.line, MSG)
         end
       end
+
+      alias_method :on_dstr, :ignore_node
+      alias_method :on_regexp, :ignore_node
     end
   end
 end
