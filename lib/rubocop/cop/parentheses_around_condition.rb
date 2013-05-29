07d0f1bb457984498cf90234a9ccfc50297fcc92
Bozhidar Batsov <bozhidar@tradeo.com>
2013-05-29 12:47:06 +0300

    Port ParenthesesAroundCondition to AST::Processor

--- lib/rubocop/cop/parentheses_around_condition.rb
+++ lib/rubocop/cop/parentheses_around_condition.rb
@@ -6,17 +6,33 @@ module Rubocop
       MSG = "Don't use parentheses around the condition of an " +
         'if/unless/while/until, unless the condition contains an assignment.'
 
-      def inspect(source, tokens, ast, comments)
-        on_node([:if, :while, :until], ast) do |node|
-          cond, _body = *node
+      def on_if(node)
+        process_control_op(node)
 
-          cond_source = cond.loc.expression.source
+        super
+      end
 
-          if cond_source.start_with?('(') && cond_source.end_with?(')')
-            add_offence(:convetion,
-                        cond.loc.line,
-                        MSG)
-          end
+      def on_while(node)
+        process_control_op(node)
+
+        super
+      end
+
+      def on_until(node)
+        process_control_op(node)
+
+        super
+      end
+
+      private
+
+      def process_control_op(node)
+        cond, _body = *node
+
+        cond_source = cond.loc.expression.source
+
+        if cond_source.start_with?('(') && cond_source.end_with?(')')
+          add_offence(:convetion, cond.loc.line, MSG)
         end
       end
     end
