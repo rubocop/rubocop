bc09082b4ef8df8ef6cc1f2619a847b0a1ef934f
Bozhidar Batsov <bozhidar@tradeo.com>
2013-06-02 19:17:13 +0300

    Code style

--- spec/rubocop/cops/not_spec.rb
+++ spec/rubocop/cops/not_spec.rb
@@ -8,22 +8,17 @@ module Rubocop
       let(:a) { Not.new }
 
       it 'registers an offence for not' do
-        inspect_source(a,
-                       ['not test'])
+        inspect_source(a, ['not test'])
         expect(a.offences.size).to eq(1)
-        expect(a.offences.map(&:message))
-          .to eq([Not::MSG])
       end
 
       it 'does not register an offence for !' do
-        inspect_source(a,
-                       ['!test'])
+        inspect_source(a, ['!test'])
         expect(a.offences).to be_empty
       end
 
       it 'does not register an offence for :not' do
-        inspect_source(a,
-                       ['[:not, :if, :else]'])
+        inspect_source(a, ['[:not, :if, :else]'])
         expect(a.offences).to be_empty
       end
     end
