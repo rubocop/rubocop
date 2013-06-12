270b735d80a9fe9ba30f8f565c2b891dc1d56883
Yuji Nakayama <nkymyj@gmail.com>
2013-06-13 02:35:38 +0900

    Force JRuby not to select working directory as temporary directory
    JRuby selects a temporary directory from the candidate directories,
    which includes the ordinary /tmp and also the current working directory,
    by checking if they are world writable or not. If /tmp is world writable
    and the current working directory is not, the latter is chosen as a
    temporary directory. This affects to behaviors of Tempfile and
    Dir.mktmpdir. This is the reason that the spec context
    "isolated enviroment" was actually not isolated on JRuby.
    
    This is related to the following issue:
    https://github.com/jruby/jruby/issues/405

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 # encoding: utf-8
 
+if ENV['TRAVIS'] && RUBY_ENGINE == 'jruby'
+  # Force JRuby not to select working directory
+  # as temporary directory on Travis CI.
+  # https://github.com/jruby/jruby/issues/405
+  require 'fileutils'
+  tmp_dir = ENV['TMPDIR'] || ENV['TMP'] || ENV['TEMP'] ||
+            Etc.systmpdir || '/tmp'
+  non_world_writable_tmp_dir = File.join(tmp_dir, 'rubocop')
+  FileUtils.makedirs(non_world_writable_tmp_dir, mode: 0700)
+  ENV['TMPDIR'] = non_world_writable_tmp_dir
+end
+
 if ENV['TRAVIS'] || ENV['COVERAGE']
   require 'simplecov'
 
