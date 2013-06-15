ba288f17b4667dcc358ef24092996533bb8ece4d
Lee Dohm <lee@liftedstudios.com>
2013-06-15 08:36:40 -0700

    Migrate new cop to Style namespace

---
+++ spec/rubocop/cops/style/method_invocation_parentheses_spec.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+module Rubocop
+  module Cop
+    module Style
+      describe MethodInvocationParentheses do
+        let(:mip) { MethodInvocationParentheses.new }
+
+        it 'registers an offence for parens in method call without args' do
+          inspect_source(mip, ['top.test()'])
+        end
+
+        it 'it accepts no parens in method call without args' do
+          inspect_source(mip, ['top.test'])
+        end
+
+        it 'it accepts parens in method call with args' do
+          inspect_source(mip, ['top.test(a)'])
+        end
+      end
+    end
+  end
+end
