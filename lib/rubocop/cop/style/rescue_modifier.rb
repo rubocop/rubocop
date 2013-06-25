5c8c6bad3ce625259574d94d96519a16fb83e1e6
Yuji Nakayama <nkymyj@gmail.com>
2013-06-25 11:51:02 +0900

    Migrate RescueModifier cop to Parser 2.0.0.beta8
    Parser now generates `kwbegin` node for explicit `begin end` source
    to differentiate it with implicit begin.
    https://github.com/whitequark/parser/issues/70#issuecomment-19525402

--- lib/rubocop/cop/style/rescue_modifier.rb
+++ lib/rubocop/cop/style/rescue_modifier.rb
@@ -7,11 +7,25 @@ module Rubocop
       class RescueModifier < Cop
         MSG = 'Avoid using rescue in its modifier form.'
 
+        def on_def(node)
+          _method_name, _args, body = *node
+          # Skip processing child nodes
+          # if this method has rescue with implicit begin.
+          return if body && body.type == :rescue
+          super
+        end
+
+        def on_defs(node)
+          _receiver, _method_name, _args, body = *node
+          return if body && body.type == :rescue
+          super
+        end
+
         def on_rescue(node)
           add_offence(:convention, node.loc.expression, MSG)
         end
 
-        alias_method :on_begin, :ignore_node
+        alias_method :on_kwbegin, :ignore_node
       end
     end
   end
