54ea14cf4ddab9dc6ce754de511b052b7b5dee18
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2013-06-28 18:58:50 +0300

    Add checks for end alignment for blocks
    This fixes the pending spec about end alignment for blocks
    and adds additional checks for cases when the return value
    of the block is used in assignment.
    
    Cleaned the inline block in the spec_helper.rb file that didn't
    comply with the checks.

--- lib/rubocop/cop/lint/end_alignment.rb
+++ lib/rubocop/cop/lint/end_alignment.rb
@@ -10,6 +10,16 @@ module Rubocop
       class EndAlignment < Cop
         MSG = 'end at %d, %d is not aligned with %s at %d, %d'
 
+        def initialize
+          super
+          @inspected_blocks = []
+        end
+
+        def inspect(source_buffer, source, tokens, ast, comments)
+          @inspected_blocks = []
+          super
+        end
+
         def on_def(node)
           check(node)
           super
@@ -30,21 +40,6 @@ module Rubocop
           super
         end
 
-  # def on_block(node)
-  #   start_loc = node.loc.expression
-  #   end_loc = node.loc.end
-
-  #   if start_loc.line != end_loc.line && start_loc.column != end_loc.column
-  #     add_offence(:warning,
-  #                 end_loc.expression,
-  #                 sprintf(MSG, end_loc.line, end_loc.column,
-  #                         start_loc.source.lines.to_a.first.chomp,
-  #                         start_loc.line, start_loc.column))
-  #   end
-
-  #   super
-  # end
-
         def on_if(node)
           check(node) if node.loc.respond_to?(:end)
           super
@@ -60,7 +55,73 @@ module Rubocop
           super
         end
 
+        # Block related alignments
+
+        def on_block(node)
+          return if already_processed_node?(node)
+          check_block_alignment(node.loc.expression, node.loc)
+          super
+        end
+
+        def on_lvasgn(node)
+          _, children = *node
+          process_block_assignment(node, children)
+          super
+        end
+
+        alias_method :on_ivasgn,   :on_lvasgn
+        alias_method :on_cvasgn,   :on_lvasgn
+        alias_method :on_gvasgn,   :on_lvasgn
+        alias_method :on_and_asgn, :on_lvasgn
+        alias_method :on_or_asgn,  :on_lvasgn
+
+        def on_casgn(node)
+          _, _, children = *node
+          process_block_assignment(node, children)
+          super
+        end
+
+        def on_op_asgn(node)
+          variable, _op, args = *node
+          process_block_assignment(variable, args)
+          super
+        end
+
+        def on_send(node)
+          receiver, method, args = *node
+          if attribute_writer?(method)
+            process_block_assignment(receiver, args)
+          end
+          super
+        end
+
+        def on_masgn(node)
+          variables, args = *node
+          process_block_assignment(variables, args)
+          super
+        end
+
         private
+
+        def process_block_assignment(begin_node, block_node)
+          if block_node && block_node.type == :block
+            @inspected_blocks << block_node
+            check_block_alignment(begin_node.loc.expression, block_node.loc)
+          end
+        end
+
+        def check_block_alignment(start_loc, block_loc)
+          end_loc = block_loc.end
+
+          if block_loc.begin.line != end_loc.line &&
+               start_loc.column != end_loc.column
+            add_offence(:warning,
+                        end_loc,
+                        sprintf(MSG, end_loc.line, end_loc.column,
+                                start_loc.source.lines.to_a.first.chomp,
+                                start_loc.line, start_loc.column))
+          end
+        end
 
         def check(node)
           # discard modifier forms of if/while/until
@@ -75,6 +136,14 @@ module Rubocop
                         sprintf(MSG, end_loc.line, end_loc.column,
                                 kw_loc.source, kw_loc.line, kw_loc.column))
           end
+        end
+
+        def already_processed_node?(node)
+          @inspected_blocks.include?(node)
+        end
+
+        def attribute_writer?(method)
+          method.to_s[-1] == '='
         end
       end
     end
--- spec/rubocop/cops/lint/end_alignment_spec.rb
+++ spec/rubocop/cops/lint/end_alignment_spec.rb
@@ -65,12 +65,135 @@ module Rubocop
         end
 
         it 'registers an offence for mismatched block end' do
-          pending
           inspect_source(end_align,
                          ['test do |ala|',
                           '  end'
                          ])
           expect(end_align.offences.size).to eq(1)
+        end
+
+        context 'align the end to the beginning of the line when' do
+          it 'block is assigned to a variable' do
+            inspect_source(end_align,
+                           ['variable = test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['variable = test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned to an instance variable' do
+            inspect_source(end_align,
+                           ['@variable = test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['@variable = test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned to a class variable' do
+            inspect_source(end_align,
+                           ['@@variable = test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['@@variable = test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned to a global variable' do
+            inspect_source(end_align,
+                           ['$variable = test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['$variable = test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned to a constant' do
+            inspect_source(end_align,
+                           ['CONSTANT = test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['Module::CONSTANT = test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned to object method' do
+            inspect_source(end_align,
+                           ['parser.child.consumer = lambda do |token|',
+                            '  token << 1',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+
+            inspect_source(end_align,
+                           ['parser.child.consumer = lambda do |token|',
+                            '  token << 1',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is used with op-asgn (+=, etc)' do
+            inspect_source(end_align,
+                           ['rb += files.select do |file|',
+                            '  file << something',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['rb += files.select do |file|',
+                            '  file << something',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned with and-asgn (&&=)' do
+            inspect_source(end_align,
+                           ['variable &&= test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['variable &&= test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
+
+          it 'block is assigned with or-asgn (||=)' do
+            inspect_source(end_align,
+                           ['variable ||= test do |ala|',
+                            'end'
+                           ])
+            expect(end_align.offences.size).to eq(0)
+            inspect_source(end_align,
+                           ['variable ||= test do |ala|',
+                            '  end'
+                           ])
+            expect(end_align.offences.size).to eq(1)
+          end
         end
       end
     end
