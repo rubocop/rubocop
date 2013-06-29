36a59066c03a17b8836cfef3f6fce4004b430260
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2013-06-29 15:29:06 +0300

    Separate and improve the specs of EndAlignment cop

--- spec/rubocop/cops/lint/end_alignment_spec.rb
+++ spec/rubocop/cops/lint/end_alignment_spec.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+# rubocop:disable LineLength
 
 require 'spec_helper'
 
@@ -6,194 +7,218 @@ module Rubocop
   module Cop
     module Lint
       describe EndAlignment do
-        let(:end_align) { EndAlignment.new }
+        let(:cop) { EndAlignment.new }
 
         it 'registers an offence for mismatched class end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['class Test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched module end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['module Test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched def end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['def test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched defs end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['def Test.test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched if end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['if test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched while end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['while test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched until end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['until test',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
         it 'registers an offence for mismatched block end' do
-          inspect_source(end_align,
+          inspect_source(cop,
                          ['test do |ala|',
                           '  end'
                          ])
-          expect(end_align.offences.size).to eq(1)
+          expect(cop.offences.size).to eq(1)
         end
 
-        context 'align the end to the beginning of the line when' do
-          it 'block is assigned to a variable' do
-            inspect_source(end_align,
-                           ['variable = test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['variable = test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'accepts end aligned with a variable' do
+          inspect_source(cop,
+                         ['variable = test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
 
-          it 'block is assigned to an instance variable' do
-            inspect_source(end_align,
-                           ['@variable = test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['@variable = test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'registers an offence for mismatched block end with a variable' do
+          inspect_source(cop,
+                         ['variable = test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
 
-          it 'block is assigned to a class variable' do
-            inspect_source(end_align,
-                           ['@@variable = test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['@@variable = test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'accepts end aligned with an instance variable' do
+          inspect_source(cop,
+                         ['@variable = test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
 
-          it 'block is assigned to a global variable' do
-            inspect_source(end_align,
-                           ['$variable = test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['$variable = test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'registers an offence for mismatched block end with an instance variable' do
+          inspect_source(cop,
+                         ['@variable = test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
 
-          it 'block is assigned to a constant' do
-            inspect_source(end_align,
-                           ['CONSTANT = test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['Module::CONSTANT = test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'accepts end aligned with a class variable' do
+          inspect_source(cop,
+                         ['@@variable = test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
 
-          it 'block is assigned to object method' do
-            inspect_source(end_align,
-                           ['parser.child.consumer = lambda do |token|',
-                            '  token << 1',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
+        it 'registers an offence for mismatched block end with a class variable' do
+          inspect_source(cop,
+                         ['@@variable = test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
 
-            inspect_source(end_align,
-                           ['parser.child.consumer = lambda do |token|',
-                            '  token << 1',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'accepts end aligned with a global variable' do
+          inspect_source(cop,
+                         ['$variable = test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
 
-          it 'block is used with op-asgn (+=, etc)' do
-            inspect_source(end_align,
-                           ['rb += files.select do |file|',
-                            '  file << something',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['rb += files.select do |file|',
-                            '  file << something',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'registers an offence for mismatched block end with a global variable' do
+          inspect_source(cop,
+                         ['$variable = test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
 
-          it 'block is assigned with and-asgn (&&=)' do
-            inspect_source(end_align,
-                           ['variable &&= test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['variable &&= test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'accepts end aligned with a constant' do
+          inspect_source(cop,
+                         ['CONSTANT = test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
 
-          it 'block is assigned with or-asgn (||=)' do
-            inspect_source(end_align,
-                           ['variable ||= test do |ala|',
-                            'end'
-                           ])
-            expect(end_align.offences.size).to eq(0)
-            inspect_source(end_align,
-                           ['variable ||= test do |ala|',
-                            '  end'
-                           ])
-            expect(end_align.offences.size).to eq(1)
-          end
+        it 'registers an offence for mismatched block end with a constant' do
+          inspect_source(cop,
+                         ['Module::CONSTANT = test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
+
+        it 'accepts end aligned with an attribute writer method' do
+          inspect_source(cop,
+                         ['parser.child.consumer = lambda do |token|',
+                          '  token << 1',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'registers an offence for mismatched block end with an attribute writer method' do
+          inspect_source(cop,
+                         ['parser.child.consumer = lambda do |token|',
+                          '  token << 1',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
+
+        it 'accepts end aligned with an op-asgn (+=, -=)' do
+          inspect_source(cop,
+                         ['rb += files.select do |file|',
+                          '  file << something',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'registers an offence for mismatched block end with an op-asgn (+=, -=)' do
+          inspect_source(cop,
+                         ['rb += files.select do |file|',
+                          '  file << something',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
+
+        it 'accepts end aligned with an and-asgn (&&=)' do
+          inspect_source(cop,
+                         ['variable &&= test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'registers an offence for mismatched block end with an and-asgn (&&=)' do
+          inspect_source(cop,
+                         ['variable &&= test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
+        end
+
+        it 'accepts end aligned with an or-asgn (||=)' do
+          inspect_source(cop,
+                         ['variable ||= test do |ala|',
+                          'end'
+                         ])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'registers an offence for mismatched block end with an or-asgn (||=)' do
+          inspect_source(cop,
+                         ['variable ||= test do |ala|',
+                          '  end'
+                         ])
+          expect(cop.offences.size).to eq(1)
         end
       end
     end
