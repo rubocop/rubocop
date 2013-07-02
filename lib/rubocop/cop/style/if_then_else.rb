bd0388178c0fe91fe997763e91eea4fa5746dfb9
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2013-07-02 14:33:50 +0300

    Fix MultilineIfThen for postfix nested unless conditions
    This fixes #316

--- lib/rubocop/cop/style/if_then_else.rb
+++ lib/rubocop/cop/style/if_then_else.rb
@@ -38,14 +38,29 @@ module Rubocop
         end
       end
 
-      # Checks for uses of then multi-line if statements.
+      # Checks for uses of the `then` keyword in multi-line if statements.
+      #
+      # This is considered bad practice:
+      # @example
+      #
+      # if cond then
+      # end
+      #
+      # While if statements can contain `then` on the same line:
+      # @example
+      #
+      # if cond then a
+      # elsif cond then b
+      # end
       class MultilineIfThen < Cop
         include IfThenElse
 
         def offending_line(node)
-          condition, body = *node
+          condition, body, else_clause = *node
           next_thing = if body && body.loc.expression
                          body.loc.expression.begin
+                       elsif else_clause && else_clause.loc.expression
+                        else_clause.loc.expression.begin
                        else
                          node.loc.end # No body, use "end".
                        end
