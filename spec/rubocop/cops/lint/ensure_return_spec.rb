7d41e28cdd183aa87dc15ae6649117663639afbb
Bozhidar Batsov <bozhidar@tradeo.com>
2013-07-10 16:55:28 +0300

    Code style

--- spec/rubocop/cops/lint/ensure_return_spec.rb
+++ spec/rubocop/cops/lint/ensure_return_spec.rb
@@ -6,41 +6,38 @@ module Rubocop
   module Cop
     module Lint
       describe EnsureReturn do
-        let(:er) { EnsureReturn.new }
+        let(:cop) { EnsureReturn.new }
 
         it 'registers an offence for return in ensure' do
-          inspect_source(er,
+          inspect_source(cop,
                          ['begin',
                           '  something',
                           'ensure',
                           '  file.close',
                           '  return',
                           'end'])
-          expect(er.offences.size).to eq(1)
-          expect(er.offences.map(&:message))
-            .to eq([EnsureReturn::MSG])
+          expect(cop.offences).to have(1).item
         end
 
         it 'does not register an offence for return outside ensure' do
-          inspect_source(er,
+          inspect_source(cop,
                          ['begin',
                           '  something',
                           '  return',
                           'ensure',
                           '  file.close',
                           'end'])
-          expect(er.offences).to be_empty
+          expect(cop.offences).to be_empty
         end
 
         it 'does not check when ensure block has no body' do
           expect do
-            inspect_source(er,
+            inspect_source(cop,
                            ['begin',
                             '  something',
                             'ensure',
                             'end'])
           end.to_not raise_exception
-
         end
       end
     end
