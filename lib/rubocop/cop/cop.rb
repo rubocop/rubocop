b89ee35b42c6c4d4829474cbfe53daa098cce421
Yuji Nakayama <nkymyj@gmail.com>
2013-08-15 10:46:25 +0900

    Remove duplicated definition of Token

--- lib/rubocop/cop/cop.rb
+++ lib/rubocop/cop/cop.rb
@@ -2,19 +2,6 @@
 
 module Rubocop
   module Cop
-    # A basic wrapper around Parser's tokens.
-    class Token
-      attr_reader :pos, :type, :text
-
-      def initialize(pos, type, text)
-        @pos, @type, @text = pos, type, text
-      end
-
-      def to_s
-        "[[#{@pos.line}, #{@pos.column}], #{@type}, #{@text.inspect}]"
-      end
-    end
-
     # Store for all cops with helper functions
     class CopStore < ::Array
 
