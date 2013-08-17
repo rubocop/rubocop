05c801a2e0ab84a5fca7207c9ce6ad20d13c8a38
Jonas Arvidsson <jonas054@gmail.com>
2013-08-17 14:56:39 +0200

    New cop ParenthesesAsGroupedExpression.
    Keeps track of method calls with a space before the opening parenthesis.
    Second half of solution that fixes #435.

---
+++ spec/rubocop/cop/lint/parentheses_as_grouped_expression_spec.rb
@@ -0,0 +1,63 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+module Rubocop
+  module Cop
+    module Lint
+      describe ParenthesesAsGroupedExpression do
+        let(:cop) { ParenthesesAsGroupedExpression.new }
+
+        it 'registers an offence for method call with space before the ' +
+          'parenthesis' do
+          inspect_source(cop, ['a.func (x)'])
+          expect(cop.offences).to have(1).item
+        end
+
+        it 'registers an offence for predicate method call with space ' +
+          'before the parenthesis' do
+          inspect_source(cop, ['is? (x)'])
+          expect(cop.offences).to have(1).item
+        end
+
+        it 'registers an offence for math expression' do
+          inspect_source(cop, ['puts (2 + 3) * 4'])
+          expect(cop.offences).to have(1).item
+        end
+
+        it 'accepts a method call without arguments' do
+          inspect_source(cop, ['func'])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'accepts a method call with arguments but no parentheses' do
+          inspect_source(cop, ['puts x'])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'accepts a chain of method calls' do
+          inspect_source(cop, ['a.b',
+                               'a.b 1',
+                               'a.b(1)'])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'accepts method with parens as arg to method without' do
+          inspect_source(cop, ['a b(c)'])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'accepts an operator call with argument in parentheses' do
+          inspect_source(cop, ['a % (b + c)',
+                               'a.b = (c == d)'])
+          expect(cop.offences).to be_empty
+        end
+
+        it 'accepts a space inside opening paren followed by left paren' do
+          inspect_source(cop, ['a( (b) )'])
+          expect(cop.offences).to be_empty
+        end
+      end
+    end
+  end
+end
