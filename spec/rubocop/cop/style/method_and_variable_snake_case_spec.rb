c27b0cc56468d9d4d56b90e6727ac6c02894702a
Jonas Arvidsson <jonas054@gmail.com>
2013-08-19 23:40:50 +0200

    Refactor specs for MethodAndVariableSnakeCase.

--- spec/rubocop/cop/style/method_and_variable_snake_case_spec.rb
+++ spec/rubocop/cop/style/method_and_variable_snake_case_spec.rb
@@ -6,102 +6,79 @@ module Rubocop
   module Cop
     module Style
       describe MethodAndVariableSnakeCase do
-        let(:snake_case) { MethodAndVariableSnakeCase.new }
+        let(:cop) { described_class.new }
+        let(:highlights) { cop.offences.map { |o| o.location.source } }
 
         it 'registers an offence for camel case in instance method name' do
-          inspect_source(snake_case,
-                         ['def myMethod',
-                          '  # ...',
-                          'end',
-                         ])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['myMethod'])
+          inspect_source(cop, ['def myMethod',
+                               '  # ...',
+                               'end'])
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['myMethod'])
         end
 
         it 'registers an offence for camel case in singleton method name' do
-          inspect_source(snake_case,
-                         ['def self.myMethod',
-                          '  # ...',
-                          'end',
-                         ])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['myMethod'])
+          inspect_source(cop, ['def self.myMethod',
+                               '  # ...',
+                               'end'])
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['myMethod'])
         end
 
         it 'registers an offence for camel case in local variable name' do
-          inspect_source(snake_case, ['myLocal = 1'])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['myLocal'])
+          inspect_source(cop, 'myLocal = 1')
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['myLocal'])
         end
 
         it 'registers an offence for camel case in instance variable name' do
-          inspect_source(snake_case, ['@myAttribute = 3'])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['@myAttribute'])
+          inspect_source(cop, '@myAttribute = 3')
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['@myAttribute'])
         end
 
         it 'registers an offence for camel case in setter name' do
-          inspect_source(snake_case, ['self.mySetter = 2'])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['mySetter'])
+          inspect_source(cop, 'self.mySetter = 2')
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['mySetter'])
         end
 
         it 'registers an offence for capitalized camel case' do
-          inspect_source(snake_case,
-                         ['def MyMethod',
-                          'end',
-                         ])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['MyMethod'])
+          inspect_source(cop, ['def MyMethod',
+                               'end'])
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['MyMethod'])
         end
 
         it 'accepts snake case in names' do
-          inspect_source(snake_case,
-                         ['def my_method',
-                          '  my_local_html = 1',
-                          '  self.my_setter = 2',
-                          '  @my_attribute = 3',
-                          'end',
-                         ])
-          expect(snake_case.offences.map(&:message)).to be_empty
+          inspect_source(cop, ['def my_method',
+                               '  my_local_html = 1',
+                               '  self.my_setter = 2',
+                               '  @my_attribute = 3',
+                               'end'])
+          expect(cop.offences).to be_empty
         end
 
         it 'registers an offence for mixed snake case and camel case' do
-          inspect_source(snake_case,
-                         ['def visit_Arel_Nodes_SelectStatement',
-                          'end'])
-          expect(snake_case.offences.map(&:message)).to eq(
-            ['Use snake_case for methods and variables.'])
-          expect(snake_case.offences.map { |o| o.location.source })
-            .to eq(['visit_Arel_Nodes_SelectStatement'])
+          inspect_source(cop, ['def visit_Arel_Nodes_SelectStatement',
+                               'end'])
+          expect(cop.offences).to have(1).item
+          expect(highlights).to eq(['visit_Arel_Nodes_SelectStatement'])
         end
 
         it 'accepts screaming snake case globals' do
-          inspect_source(snake_case, ['$MY_GLOBAL = 0'])
-          expect(snake_case.offences.map(&:message)).to be_empty
+          inspect_source(cop, '$MY_GLOBAL = 0')
+          expect(cop.offences).to be_empty
         end
 
         it 'accepts screaming snake case constants' do
-          inspect_source(snake_case, ['MY_CONSTANT = 0'])
-          expect(snake_case.offences.map(&:message)).to be_empty
+          inspect_source(cop, 'MY_CONSTANT = 0')
+          expect(cop.offences).to be_empty
         end
 
         it 'accepts assigning to camel case constant' do
-          inspect_source(snake_case,
-                         ['Paren = Struct.new :left, :right, :kind'])
-          expect(snake_case.offences.map(&:message)).to be_empty
+          inspect_source(cop, 'Paren = Struct.new :left, :right, :kind')
+          expect(cop.offences).to be_empty
         end
       end
     end
