eeca9b7e627f3f7c37a02c67067fe05ee26edeae
Yuji Nakayama <nkymyj@gmail.com>
2013-09-13 01:03:30 +0900

    Fix an ad-hoc comment out

--- lib/rubocop/cop/variable_inspector/reference.rb
+++ lib/rubocop/cop/variable_inspector/reference.rb
@@ -7,14 +7,20 @@ module Rubocop
       class Reference
         include Locatable
 
+        VARIABLE_REFERENCE_TYPES = (
+          [VARIABLE_REFERENCE_TYPE] +
+          OPERATOR_ASSIGNMENT_TYPES +
+          [ZERO_ARITY_SUPER_TYPE]
+        ).freeze
+
         attr_reader :node, :scope
 
         def initialize(node, scope)
-          # unless VARIABLE_ASSIGNMENT_TYPES.include?(node.type)
-          #   fail ArgumentError,
-          #        "Node type must be any of #{VARIABLE_ASSIGNMENT_TYPES}, " +
-          #        "passed #{node.type}"
-          # end
+          unless VARIABLE_REFERENCE_TYPES.include?(node.type)
+            fail ArgumentError,
+                 "Node type must be any of #{VARIABLE_REFERENCE_TYPES}, " +
+                 "passed #{node.type}"
+          end
 
           @node = node
           @scope = scope
