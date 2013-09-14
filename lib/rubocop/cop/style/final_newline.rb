52a3a0a8901e76450e6183ddc6ea61cfd82cf99a
Jonas Arvidsson <jonas054@gmail.com>
2013-09-14 08:57:30 +0200

    Fix crash on empty input file in FinalNewline.

--- lib/rubocop/cop/style/final_newline.rb
+++ lib/rubocop/cop/style/final_newline.rb
@@ -5,12 +5,12 @@ module Rubocop
     module Style
       # This cop enforces the presence of a final newline in each source file.
       class FinalNewline < Cop
-        MSG = 'Source files should end with a newline(\n).'
+        MSG = 'Source files should end with a newline (\n).'
 
         def investigate(processed_source)
           final_line = processed_source.raw_lines.to_a.last
 
-          unless final_line.end_with?("\n")
+          unless final_line.nil? || final_line.end_with?("\n")
             convention(nil,
                        source_range(processed_source.buffer,
                                     processed_source[0...-1],
