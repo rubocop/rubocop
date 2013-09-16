218af638e5fddcd94713b7da08b728977203d722
Jonas Arvidsson <jonas054@gmail.com>
2013-09-16 15:23:38 +0200

    Fix duplication in align_ array/parameters and string/character _literal.
    Extract common code into new modules.

--- lib/rubocop/cop/style/align_parameters.rb
+++ lib/rubocop/cop/style/align_parameters.rb
@@ -6,6 +6,8 @@ module Rubocop
       # Here we check if the parameters on a multi-line method call are
       # aligned.
       class AlignParameters < Cop
+        include AutocorrectAlignment
+
         MSG = 'Align the parameters of a method call if they span ' +
           'more than one line.'
 
@@ -24,26 +26,6 @@ module Rubocop
                 current_pos.column != first_arg_column
               @column_delta = first_arg_column - current_pos.column
               convention(current, current_pos)
-            end
-          end
-        end
-
-        def autocorrect_action(node)
-          # We can't use the instance variable inside the lambda. That would
-          # just give each lambda the same reference and they would all get
-          # the last value of @column_delta. A local variable fixes the
-          # problem.
-          column_delta = @column_delta
-
-          @corrections << lambda do |corrector|
-            expr = node.loc.expression
-            if column_delta > 0
-              corrector.replace(expr, ' ' * column_delta + expr.source)
-            else
-              range = Parser::Source::Range.new(expr.source_buffer,
-                                                expr.begin_pos + column_delta,
-                                                expr.end_pos)
-              corrector.replace(range, expr.source)
             end
           end
         end
---
+++ lib/rubocop/cop/style/autocorrect_alignment.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+module Rubocop
+  module Cop
+    module Style
+      # This module does auto-correction of nodes that should just be moved to
+      # the left or to the right, amount being determined by the instance
+      # variable @column_delta.
+      module AutocorrectAlignment
+        def autocorrect_action(node)
+          # We can't use the instance variable inside the lambda. That would
+          # just give each lambda the same reference and they would all get
+          # the last value of @column_delta. A local variable fixes the
+          # problem.
+          column_delta = @column_delta
+
+          @corrections << lambda do |corrector|
+            expr = node.loc.expression
+            if column_delta > 0
+              corrector.replace(expr, ' ' * column_delta + expr.source)
+            else
+              range = Parser::Source::Range.new(expr.source_buffer,
+                                                expr.begin_pos + column_delta,
+                                                expr.end_pos)
+              corrector.replace(range, expr.source)
+            end
+          end
+        end
+      end
+    end
+  end
+end
