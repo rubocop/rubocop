a5fe5f97ee47f3d264a0ac9c7298481106035b19
Yuji Nakayama <nkymyj@gmail.com>
2013-09-25 03:04:34 +0900

    Remove duplicated #report_summary specs
    ClangStyleFormatter just uses inherited #report_summary.

--- spec/rubocop/formatter/clang_style_formatter_spec.rb
+++ spec/rubocop/formatter/clang_style_formatter_spec.rb
@@ -9,40 +9,6 @@ module Rubocop
       subject(:formatter) { ClangStyleFormatter.new(output) }
       let(:output) { StringIO.new }
 
-      describe '#report_summary' do
-        context 'when no files inspected' do
-          it 'handles pluralization correctly' do
-            formatter.report_summary(0, 0)
-            expect(output.string).to eq(
-              "\n0 files inspected, no offences detected\n")
-          end
-        end
-
-        context 'when a file inspected and no offences detected' do
-          it 'handles pluralization correctly' do
-            formatter.report_summary(1, 0)
-            expect(output.string).to eq(
-              "\n1 file inspected, no offences detected\n")
-          end
-        end
-
-        context 'when a offence detected' do
-          it 'handles pluralization correctly' do
-            formatter.report_summary(1, 1)
-            expect(output.string).to eq(
-              "\n1 file inspected, 1 offence detected\n")
-          end
-        end
-
-        context 'when 2 offences detected' do
-          it 'handles pluralization correctly' do
-            formatter.report_summary(2, 2)
-            expect(output.string).to eq(
-              "\n2 files inspected, 2 offences detected\n")
-          end
-        end
-      end
-
       describe '#report_file' do
         it 'displays text containing the offending source line' do
           cop = Cop::Cop.new
