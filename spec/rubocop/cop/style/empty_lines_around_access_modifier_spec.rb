339f50ccf17559ccbf70601b0802cc5d4fec10e9
Bozhidar Batsov <bozhidar@tradeo.com>
2013-11-04 18:35:24 +0200

    Improve EmptyLinesAroundAccessModifier spec

--- spec/rubocop/cop/style/empty_lines_around_access_modifier_spec.rb
+++ spec/rubocop/cop/style/empty_lines_around_access_modifier_spec.rb
@@ -5,50 +5,52 @@ require 'spec_helper'
 describe Rubocop::Cop::Style::EmptyLinesAroundAccessModifier do
   subject(:cop) { described_class.new }
 
-  it 'requires blank line before private/protected' do
-    inspect_source(cop,
-                   ['class Test',
-                    '  something',
-                    '  protected',
-                    '',
-                    '  def test; end',
-                    'end'])
-    expect(cop.offences.size).to eq(1)
-    expect(cop.messages)
-      .to eq(['Keep a blank line before and after protected.'])
-  end
+  %w(private protected public).each do |access_modifier|
+    it "requires blank line before #{access_modifier}" do
+      inspect_source(cop,
+                     ['class Test',
+                      '  something',
+                      "  #{access_modifier}",
+                      '',
+                      '  def test; end',
+                      'end'])
+      expect(cop.offences.size).to eq(1)
+      expect(cop.messages)
+        .to eq(["Keep a blank line before and after #{access_modifier}."])
+    end
 
-  it 'requires blank line after private/protected' do
-    inspect_source(cop,
-                   ['class Test',
-                    '  something',
-                    '',
-                    '  protected',
-                    '  def test; end',
-                    'end'])
-    expect(cop.offences.size).to eq(1)
-    expect(cop.messages)
-      .to eq(['Keep a blank line before and after protected.'])
-  end
+    it 'requires blank line after #{access_modifier}' do
+      inspect_source(cop,
+                     ['class Test',
+                      '  something',
+                      '',
+                      "  #{access_modifier}",
+                      '  def test; end',
+                      'end'])
+      expect(cop.offences.size).to eq(1)
+      expect(cop.messages)
+        .to eq(["Keep a blank line before and after #{access_modifier}."])
+    end
 
-  it 'accepts missing blank line when at the beginning of class/module' do
-    inspect_source(cop,
-                   ['class Test',
-                    '  protected',
-                    '',
-                    '  def test; end',
-                    'end'])
-    expect(cop.offences).to be_empty
-  end
+    it 'accepts missing blank line when at the beginning of class/module' do
+      inspect_source(cop,
+                     ['class Test',
+                      "  #{access_modifier}",
+                      '',
+                      '  def test; end',
+                      'end'])
+      expect(cop.offences).to be_empty
+    end
 
-  it 'recognizes blank lines with DOS style line endings' do
-    inspect_source(cop,
-                   ["class Test\r",
-                    "\r",
-                    "  protected\r",
-                    "\r",
-                    "  def test; end\r",
-                    "end\r"])
-    expect(cop.offences.size).to eq(0)
+    it 'recognizes blank lines with DOS style line endings' do
+      inspect_source(cop,
+                     ["class Test\r",
+                      "\r",
+                      "  #{access_modifier}\r",
+                      "\r",
+                      "  def test; end\r",
+                      "end\r"])
+      expect(cop.offences.size).to eq(0)
+    end
   end
 end
