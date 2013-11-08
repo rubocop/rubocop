rubocop/rubocop

commit e4f1d427937a993bf1c894afe0782a4fae785455

Make Options class more single responsibility.
Let it deal with option parsing and validation, and then a minimum of other things.

lib/rubocop/cli.rb
@@ -24,9 +24,10 @@ module Rubocop
     def run(args = ARGV)
       trap_interrupt
 
-      @options, target_files = Options.new(@config_store).parse(args)
+      @options, remaining_args = Options.new.parse(args)
+      target_files = target_finder.find(remaining_args)
 
-      ConfigLoader.debug = @options[:debug]
+      act_on_options(remaining_args)
 
       any_failed = process_files(target_files)
 
@@ -85,6 +86,33 @@ module Rubocop
       any_failed
     end
 
+    def act_on_options(args)
+      if @options[:show_cops]
+        print_available_cops
+        exit(0)
+      end
+
+      @config_store.set_options_config(@options[:config]) if @options[:config]
+
+      Sickill::Rainbow.enabled = false if @options[:no_color]
+
+      puts Rubocop::Version.version(false) if @options[:version]
+      puts Rubocop::Version.version(true) if @options[:verbose_version]
+      exit(0) if @options[:version] || @options[:verbose_version]
+
+      ConfigLoader.debug = @options[:debug]
+
+      if @options[:auto_gen_config]
+        target_finder.find(args).each do |file|
+          config = @config_store.for(file)
+          if config.contains_auto_generated_config
+            fail "Remove #{ConfigLoader::AUTO_GENERATED_FILE} from the " +
+              'current configuration before generating it again.'
+          end
+        end
+      end
+    end
+
     def mobilized_cop_classes(config)
       @mobilized_cop_classes ||= {}
       @mobilized_cop_classes[config.object_id] ||= begin
@@ -110,6 +138,33 @@ module Rubocop
       offences = team.inspect_file(file)
       @errors.concat(team.errors)
       offences
+    end
+
+    def print_available_cops
+      cops = Cop::Cop.all
+      puts "Available cops (#{cops.length}) + config for #{Dir.pwd.to_s}: "
+      dirconf = @config_store.for(Dir.pwd.to_s)
+      cops.types.sort!.each do |type|
+        coptypes = cops.with_type(type).sort_by!(&:cop_name)
+        puts "Type '#{type.to_s.capitalize}' (#{coptypes.size}):"
+        coptypes.each do |cop|
+          puts " - #{cop.cop_name}"
+          cnf = dirconf.for_cop(cop).dup
+          print_conf_option('Description',
+                            cnf.delete('Description') { 'None' })
+          cnf.each { |k, v| print_conf_option(k, v) }
+          print_conf_option('SupportsAutoCorrection',
+                            cop.new.support_autocorrect?.to_s)
+        end
+      end
+    end
+
+    def print_conf_option(option, value)
+      puts  "    - #{option}: #{value}"
+    end
+
+    def target_finder
+      @target_finder ||= TargetFinder.new(@config_store, @options[:debug])
     end
 
     def run_rails_cops?(config)

spec/rubocop/cli_spec.rb
@@ -25,6 +25,14 @@ describe Rubocop::CLI, :isolated_environment do
   end
 
   describe 'option' do
+    describe '--version' do
+      it 'exits cleanly' do
+        expect { cli.run ['-v'] }.to exit_with_code(0)
+        expect { cli.run ['--version'] }.to exit_with_code(0)
+        expect($stdout.string).to eq((Rubocop::Version::STRING + "\n") * 2)
+      end
+    end
+
     describe '--auto-gen-config' do
       it 'exits with error if asked to re-generate a todo list that is in ' +
         'use' do
@@ -168,6 +176,88 @@ describe Rubocop::CLI, :isolated_environment do
         expect($stdout.string.lines[-1])
           .to eq(["#{abs('example1.rb')}:1:1: C: Tab: Tab detected.",
                   ''].join("\n"))
+      end
+    end
+
+    describe '--show-cops' do
+      let(:cops) { Rubocop::Cop::Cop.all }
+
+      let(:global_conf) do
+        config_path =
+          Rubocop::ConfigLoader.configuration_file_for(Dir.pwd.to_s)
+        Rubocop::ConfigLoader.configuration_from_file(config_path)
+      end
+
+      let(:stdout) { $stdout.string }
+
+      before do
+        expect { cli.run ['--show-cops'] }.to exit_with_code(0)
+      end
+
+      # Extracts the first line out of the description
+      def short_description_of_cop(cop)
+        desc = full_description_of_cop(cop)
+        desc ? desc.lines.first.strip : ''
+      end
+
+      # Gets the full description of the cop or nil if no description is set.
+      def full_description_of_cop(cop)
+        cop_config = global_conf.for_cop(cop)
+        cop_config['Description']
+      end
+
+      it 'prints all available cops and their description' do
+        cops.each do |cop|
+          expect(stdout).to include cop.cop_name
+          expect(stdout).to include short_description_of_cop(cop)
+        end
+      end
+
+      it 'prints all types' do
+        cops
+          .types
+          .map(&:to_s)
+          .map(&:capitalize)
+          .each { |type| expect(stdout).to include(type) }
+      end
+
+      it 'prints all cops in their right type listing' do
+        lines = stdout.lines
+        lines.slice_before(/Type /).each do |slice|
+          types = cops.types.map(&:to_s).map(&:capitalize)
+          current = types.delete(slice.shift[/Type '(?<c>[^'']+)'/, 'c'])
+          # all cops in their type listing
+          cops.with_type(current).each do |cop|
+            expect(slice.any? { |l| l.include? cop.cop_name }).to be_true
+          end
+
+          # no cop in wrong type listing
+          types.each do |type|
+            cops.with_type(type).each do |cop|
+              expect(slice.any? { |l| l.include? cop.cop_name }).to be_false
+            end
+          end
+        end
+      end
+
+      it 'prints the current configuration' do
+        out = stdout.lines.to_a
+        cops.each do |cop|
+          conf = global_conf[cop.cop_name].dup
+          confstrt =
+            out.find_index { |i| i.include?("- #{cop.cop_name}") } + 1
+          c = out[confstrt, conf.keys.size].to_s
+          conf.delete('Description')
+          expect(c).to include(short_description_of_cop(cop))
+          conf.each do |k, v|
+            # ugly hack to get hash/array content tested
+            if v.kind_of?(Hash) || v.kind_of?(Array)
+              expect(c).to include "#{k}: #{v.to_s.dump[2, -2]}"
+            else
+              expect(c).to include "#{k}: #{v}"
+            end
+          end
+        end
       end
     end
 

.rubocop.yml
@@ -6,7 +6,7 @@ MethodLength:
   Max: 22
 
 ClassLength:
-  Max: 160
+  Max: 140
 
 CyclomaticComplexity:
   Max: 12

lib/rubocop/cop/cop.rb
@@ -91,7 +91,7 @@ module Rubocop
 
       def initialize(config = nil, options = nil)
         @config = config || Config.new
-        @options = options || { autocorrect: false, debug: false }
+        @options = options || { auto_correct: false, debug: false }
 
         @offences = []
         @corrections = []
@@ -103,7 +103,7 @@ module Rubocop
       end
 
       def autocorrect?
-        @options[:autocorrect] && support_autocorrect?
+        @options[:auto_correct] && support_autocorrect?
       end
 
       def debug?

lib/rubocop/cop/team.rb
@@ -9,12 +9,12 @@ module Rubocop
       def initialize(cop_classes, config, options = nil)
         @cop_classes = cop_classes
         @config = config
-        @options = options || { autocorrect: false, debug: false }
+        @options = options || { auto_correct: false, debug: false }
         @errors = []
       end
 
       def autocorrect?
-        @options[:autocorrect]
+        @options[:auto_correct]
       end
 
       def debug?

lib/rubocop/options.rb
@@ -7,8 +7,7 @@ module Rubocop
   class Options
     DEFAULT_FORMATTER = 'progress'
 
-    def initialize(config_store)
-      @config_store = config_store
+    def initialize
       @options = {}
     end
 
@@ -20,88 +19,84 @@ module Rubocop
       OptionParser.new do |opts|
         opts.banner = 'Usage: rubocop [options] [file1, file2, ...]'
 
-        opts.on('-d', '--debug', 'Display debug info.') do |d|
-          @options[:debug] = d
-        end
-        opts.on('-c', '--config FILE', 'Specify configuration file.') do |f|
-          @options[:config] = f
-          @config_store.set_options_config(@options[:config])
-        end
-        opts.on('--only COP', 'Run just one cop.') do |s|
-          @options[:only] = s
+        option(opts, '-d', '--debug', 'Display debug info.')
+        option(opts, '-c', '--config FILE', 'Specify configuration file.')
+
+        option(opts, '--only COP', 'Run just one cop.') do
           validate_only_option
         end
-        opts.on('--auto-gen-config',
-                'Generate a configuration file acting as a',
-                'TODO list.') do
-          @options[:auto_gen_config] = true
-          @options[:formatters] = [
-                                   [DEFAULT_FORMATTER],
-                                   [Formatter::DisabledConfigFormatter,
-                                    ConfigLoader::AUTO_GENERATED_FILE]
-                                  ]
+
+        option(opts, '--auto-gen-config',
+               'Generate a configuration file acting as a',
+               'TODO list.') do
           validate_auto_gen_config_option(args)
+          @options[:formatters] = [[DEFAULT_FORMATTER],
+                                   [Formatter::DisabledConfigFormatter,
+                                    ConfigLoader::AUTO_GENERATED_FILE]]
         end
-        opts.on('--show-cops',
-                'Shows cops and their config for the',
-                'current directory.') do
-          print_available_cops
-          exit(0)
-        end
-        opts.on('-f', '--format FORMATTER',
-                'Choose an output formatter. This option',
-                'can be specified multiple times to enable',
-                'multiple formatters at the same time.',
-                '  [p]rogress (default)',
-                '  [s]imple',
-                '  [c]lang',
-                '  [e]macs',
-                '  [j]son',
-                '  [f]iles',
-                '  [o]ffences',
-                '  custom formatter class name') do |key|
+
+        option(opts, '--show-cops',
+               'Shows cops and their config for the',
+               'current directory.')
+
+        option(opts, '-f', '--format FORMATTER',
+               'Choose an output formatter. This option',
+               'can be specified multiple times to enable',
+               'multiple formatters at the same time.',
+               '  [p]rogress (default)',
+               '  [s]imple',
+               '  [c]lang',
+               '  [e]macs',
+               '  [j]son',
+               '  [f]iles',
+               '  [o]ffences',
+               '  custom formatter class name') do |key|
           @options[:formatters] ||= []
           @options[:formatters] << [key]
         end
-        opts.on('-o', '--out FILE',
-                'Write output to a file instead of STDOUT.',
-                'This option applies to the previously',
-                'specified --format, or the default format',
-                'if no format is specified.') do |path|
+
+        option(opts, '-o', '--out FILE',
+               'Write output to a file instead of STDOUT.',
+               'This option applies to the previously',
+               'specified --format, or the default format',
+               'if no format is specified.') do |path|
           @options[:formatters] ||= [[DEFAULT_FORMATTER]]
           @options[:formatters].last << path
         end
-        opts.on('-r', '--require FILE', 'Require Ruby file.') do |f|
+
+        option(opts, '-r', '--require FILE', 'Require Ruby file.') do |f|
           require f
         end
-        opts.on('-R', '--rails', 'Run extra Rails cops.') do |r|
-          @options[:rails] = r
-        end
-        opts.on('-l', '--lint', 'Run only lint cops.') do |l|
-          @options[:lint] = l
-        end
-        opts.on('-a', '--auto-correct', 'Auto-correct offences.') do |a|
-          @options[:autocorrect] = a
-        end
-        opts.on('-n', '--no-color', 'Disable color output.') do |s|
-          Sickill::Rainbow.enabled = false
-        end
-        opts.on('-v', '--version', 'Display version.') do
-          puts Rubocop::Version.version(false)
-          exit(0)
-        end
-        opts.on('-V', '--verbose-version', 'Display verbose version.') do
-          puts Rubocop::Version.version(true)
-          exit(0)
-        end
+
+        option(opts, '-R', '--rails', 'Run extra Rails cops.')
+        option(opts, '-l', '--lint', 'Run only lint cops.')
+        option(opts, '-a', '--auto-correct', 'Auto-correct offences.')
+        option(opts, '-n', '--no-color', 'Disable color output.')
+        option(opts, '-v', '--version', 'Display version.')
+        option(opts, '-V', '--verbose-version', 'Display verbose version.')
       end.parse!(args)
 
-      target_files = target_finder.find(args)
-      [@options, target_files]
+      [@options, args]
     end
     # rubocop:enable MethodLength
 
     private
+
+    # Sets a value in the @options hash, based on the given long option and its
+    # value, in addition to calling the block if a block is given.
+    def option(opts, *args)
+      opts.on(*args) do |arg|
+        @options[long_opt_symbol(args)] = arg
+        yield arg if block_given?
+      end
+    end
+
+    # Finds the option in `args` starting with -- and converts it to a symbol,
+    # e.g. [..., '--auto-correct', ...] to :auto_correct.
+    def long_opt_symbol(args)
+      long_opt = args.find { |arg| arg.start_with?('--') }
+      long_opt[2..-1].sub(/ .*/, '').gsub(/-/, '_').to_sym
+    end
 
     def ignore_dropped_options(args)
       # Currently we don't make -s/--silent option raise error
@@ -144,41 +139,6 @@ module Rubocop
         fail ArgumentError,
              '--auto-gen-config can not be combined with any other arguments.'
       end
-
-      target_finder.find(args).each do |file|
-        config = @config_store.for(file)
-        if @options[:auto_gen_config] && config.contains_auto_generated_config
-          fail "Remove #{ConfigLoader::AUTO_GENERATED_FILE} from the " +
-            'current configuration before generating it again.'
-        end
-      end
-    end
-
-    def target_finder
-      @target_finder ||= TargetFinder.new(@config_store, @options[:debug])
-    end
-
-    def print_available_cops
-      cops = Cop::Cop.all
-      puts "Available cops (#{cops.length}) + config for #{Dir.pwd.to_s}: "
-      dirconf = @config_store.for(Dir.pwd.to_s)
-      cops.types.sort!.each do |type|
-        coptypes = cops.with_type(type).sort_by!(&:cop_name)
-        puts "Type '#{type.to_s.capitalize}' (#{coptypes.size}):"
-        coptypes.each do |cop|
-          puts " - #{cop.cop_name}"
-          cnf = dirconf.for_cop(cop).dup
-          print_conf_option('Description',
-                            cnf.delete('Description') { 'None' })
-          cnf.each { |k, v| print_conf_option(k, v) }
-          print_conf_option('SupportsAutoCorrection',
-                            cop.new.support_autocorrect?.to_s)
-        end
-      end
-    end
-
-    def print_conf_option(option, value)
-      puts  "    - #{option}: #{value}"
     end
   end
 end

spec/rubocop/cop/team_spec.rb
@@ -16,8 +16,8 @@ describe Rubocop::Cop::Team do
       it { should be_false }
     end
 
-    context 'when { autocorrect: true } is passed to .new' do
-      let(:options) { { autocorrect: true } }
+    context 'when { auto_correct: true } is passed to .new' do
+      let(:options) { { auto_correct: true } }
       it { should be_true }
     end
   end
@@ -91,7 +91,7 @@ describe Rubocop::Cop::Team do
     end
 
     context 'when autocorrection is enabled' do
-      let(:options) { { autocorrect: true } }
+      let(:options) { { auto_correct: true } }
 
       before do
         create_file(file_path, [

spec/rubocop/options_spec.rb
@@ -5,8 +5,7 @@ require 'spec_helper'
 describe Rubocop::Options, :isolated_environment do
   include FileHelper
 
-  subject(:options) { described_class.new(config_store) }
-  let(:config_store) { Rubocop::ConfigStore.new }
+  subject(:options) { described_class.new }
 
   before(:each) do
     $stdout = StringIO.new
@@ -97,100 +96,10 @@ Usage: rubocop [options] [file1, file2, ...]
       end
     end
 
-    describe '--version' do
-      it 'exits cleanly' do
-        expect { options.parse ['-v'] }.to exit_with_code(0)
-        expect { options.parse ['--version'] }.to exit_with_code(0)
-        expect($stdout.string).to eq((Rubocop::Version::STRING + "\n") * 2)
-      end
-    end
-
     describe '--only' do
       it 'exits with error if an incorrect cop name is passed' do
         expect { options.parse(%w(--only 123)) }
           .to raise_error(ArgumentError, /Unrecognized cop name: 123./)
-      end
-    end
-
-    describe '--show-cops' do
-      let(:cops) { Rubocop::Cop::Cop.all }
-
-      let(:global_conf) do
-        config_path =
-          Rubocop::ConfigLoader.configuration_file_for(Dir.pwd.to_s)
-        Rubocop::ConfigLoader.configuration_from_file(config_path)
-      end
-
-      let(:stdout) { $stdout.string }
-
-      before do
-        expect { options.parse ['--show-cops'] }.to exit_with_code(0)
-      end
-
-      # Extracts the first line out of the description
-      def short_description_of_cop(cop)
-        desc = full_description_of_cop(cop)
-        desc ? desc.lines.first.strip : ''
-      end
-
-      # Gets the full description of the cop or nil if no description is set.
-      def full_description_of_cop(cop)
-        cop_config = global_conf.for_cop(cop)
-        cop_config['Description']
-      end
-
-      it 'prints all available cops and their description' do
-        cops.each do |cop|
-          expect(stdout).to include cop.cop_name
-          expect(stdout).to include short_description_of_cop(cop)
-        end
-      end
-
-      it 'prints all types' do
-        cops
-          .types
-          .map(&:to_s)
-          .map(&:capitalize)
-          .each { |type| expect(stdout).to include(type) }
-      end
-
-      it 'prints all cops in their right type listing' do
-        lines = stdout.lines
-        lines.slice_before(/Type /).each do |slice|
-          types = cops.types.map(&:to_s).map(&:capitalize)
-          current = types.delete(slice.shift[/Type '(?<c>[^'']+)'/, 'c'])
-          # all cops in their type listing
-          cops.with_type(current).each do |cop|
-            expect(slice.any? { |l| l.include? cop.cop_name }).to be_true
-          end
-
-          # no cop in wrong type listing
-          types.each do |type|
-            cops.with_type(type).each do |cop|
-              expect(slice.any? { |l| l.include? cop.cop_name }).to be_false
-            end
-          end
-        end
-      end
-
-      it 'prints the current configuration' do
-        out = stdout.lines.to_a
-        cops.each do |cop|
-          conf = global_conf[cop.cop_name].dup
-          confstrt =
-            out.find_index { |i| i.include?("- #{cop.cop_name}") } + 1
-          c = out[confstrt, conf.keys.size].to_s
-          conf.delete('Description')
-          expect(c).to include(short_description_of_cop(cop))
-          conf.each do |k, v|
-            # ugly hack to get hash/array content tested
-            if v.kind_of?(Hash) || v.kind_of?(Array)
-              expect(c).to include "#{k}: #{v.to_s.dump[2, -2]}"
-            else
-              expect(c).to include "#{k}: #{v}"
-            end
-          end
-        end
       end
     end
 

commit 33ea0a061b3b1414b645cb97f24aac988d3657a5

Properly handle file inclusion/exclusion rules when using -c
A regression introduced in e4f1d427 caused file inclusion/exclusion
rules (like ['AllCops']['Excludes']) to be ignored when specified in
a configuration file passed via -c. This fixes that by correcting the
order of operations.

Fixes #620.

lib/rubocop/cli.rb
@@ -25,9 +25,8 @@ module Rubocop
       trap_interrupt
 
       @options, remaining_args = Options.new.parse(args)
-      target_files = target_finder.find(remaining_args)
-
       act_on_options(remaining_args)
+      target_files = target_finder.find(remaining_args)
 
       any_failed = process_files(target_files)
 

spec/rubocop/cli_spec.rb
@@ -1114,6 +1114,24 @@ describe Rubocop::CLI, :isolated_environment do
                 abs('example/.rubocop.yml'),
                 ''].join("\n"))
     end
+
+    it 'works when a configuration file passed by -c specifies Excludes' do
+      create_file('example/example1.rb', [
+                                          '# encoding: utf-8',
+                                          '#' * 90
+                                         ])
+
+      create_file('rubocop.yml', [
+                                           'AllCops:',
+                                           '  Excludes:',
+                                           '    - !ruby/regexp /example1\.rb$/'
+                                          ])
+
+      cli.run(%w(--format simple -c rubocop.yml))
+      expect($stdout.string)
+        .to eq(['', '0 files inspected, no offences detected',
+                ''].join("\n"))
+    end
   end
 
   describe '#display_error_summary' do