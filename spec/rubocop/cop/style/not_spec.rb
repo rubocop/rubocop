591d45f1fada6df29d77976a735606f7e682d71a
Bozhidar Batsov <bozhidar@batsov.com>
2014-03-13 11:03:58 +0200

    Simplify a spec

--- spec/rubocop/cop/style/not_spec.rb
+++ spec/rubocop/cop/style/not_spec.rb
@@ -6,17 +6,12 @@ describe Rubocop::Cop::Style::Not do
   subject(:cop) { described_class.new }
 
   it 'registers an offense for not' do
-    inspect_source(cop, ['not test'])
+    inspect_source(cop, 'not test')
     expect(cop.offenses.size).to eq(1)
   end
 
   it 'does not register an offense for !' do
-    inspect_source(cop, ['!test'])
-    expect(cop.offenses).to be_empty
-  end
-
-  it 'does not register an offense for :not' do
-    inspect_source(cop, ['[:not, :if, :else]'])
+    inspect_source(cop, '!test')
     expect(cop.offenses).to be_empty
   end
 end
