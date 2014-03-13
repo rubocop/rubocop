c2b3fb5e073824302e92678c598e43edf11e3d72
Yuji Nakayama <nkymyj@gmail.com>
2014-03-13 23:32:18 +0900

    Accept non-rb extensions and file names specified in AllCops/Includes
    https://github.com/bbatsov/rubocop/commit/a0064f7ddd46573e8f59deda317403e880239f53#commitcomment-5664681

--- spec/rubocop/cop/style/file_name_spec.rb
+++ spec/rubocop/cop/style/file_name_spec.rb
@@ -3,7 +3,13 @@
 require 'spec_helper'
 
 describe Rubocop::Cop::Style::FileName do
-  subject(:cop) { described_class.new }
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) do
+    Rubocop::Config.new('AllCops' => { 'Includes' => includes })
+  end
+
+  let(:includes) { [] }
 
   it 'reports offense for camelCase file names ending in .rb' do
     source = ['print 1']
@@ -39,5 +45,27 @@ describe Rubocop::Cop::Style::FileName do
       .to receive(:name).and_return('/some/dir/test_case')
     _investigate(cop, processed_source)
     expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts offense for snake_case file names with non-rb extension' do
+    source = ['print 1']
+    processed_source = parse_source(source)
+    allow(processed_source.buffer)
+      .to receive(:name).and_return('/some/dir/some_task.rake')
+    _investigate(cop, processed_source)
+    expect(cop.offenses).to be_empty
+  end
+
+  context 'when the file is specified in AllCops/Includes' do
+    let(:includes) { ['**/Gemfile'] }
+
+    it 'accepts the file name even if it is not snake_case' do
+      source = ['print 1']
+      processed_source = parse_source(source)
+      allow(processed_source.buffer)
+        .to receive(:name).and_return('/some/dir/Gemfile')
+      _investigate(cop, processed_source)
+      expect(cop.offenses).to be_empty
+    end
   end
 end
