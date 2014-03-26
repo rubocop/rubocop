a667250dc2e332f16498ac213d7c1ad5ea43ad69
Bozhidar Batsov <bozhidar@tradeo.com>
2014-03-26 13:33:46 +0200

    Refine a couple of specs

--- spec/rubocop/cop/style/nil_comparison_spec.rb
+++ spec/rubocop/cop/style/nil_comparison_spec.rb
@@ -6,19 +6,19 @@ describe Rubocop::Cop::Style::NilComparison do
   subject(:cop) { described_class.new }
 
   it 'registers an offense for == nil' do
-    inspect_source(cop,
-                   ['x == nil'])
+    inspect_source(cop, 'x == nil')
     expect(cop.offenses.size).to eq(1)
+    expect(cop.highlights).to eq(['=='])
   end
 
   it 'registers an offense for === nil' do
-    inspect_source(cop,
-                   ['x === nil'])
+    inspect_source(cop, 'x === nil')
     expect(cop.offenses.size).to eq(1)
+    expect(cop.highlights).to eq(['==='])
   end
 
   it 'autocorrects by replacing == nil with .nil?' do
-    corrected = autocorrect_source(cop, ['x == nil'])
+    corrected = autocorrect_source(cop, 'x == nil')
     expect(corrected).to eq 'x.nil?'
   end
 end
--- spec/rubocop/cop/style/non_nil_check_spec.rb
+++ spec/rubocop/cop/style/non_nil_check_spec.rb
@@ -6,20 +6,20 @@ describe Rubocop::Cop::Style::NonNilCheck do
   subject(:cop) { described_class.new }
 
   it 'registers an offense for != nil' do
-    inspect_source(cop,
-                   ['x != nil'])
+    inspect_source(cop, 'x != nil')
     expect(cop.offenses.size).to eq(1)
+    expect(cop.highlights).to eq(['!='])
   end
 
   it 'registers an offense for !x.nil?' do
-    inspect_source(cop,
-                   ['!x.nil?'])
+    inspect_source(cop, '!x.nil?')
     expect(cop.offenses.size).to eq(1)
+    expect(cop.highlights).to eq(['!x.nil?'])
   end
 
   it 'registers an offense for not x.nil?' do
-    inspect_source(cop,
-                   ['not x.nil?'])
+    inspect_source(cop, 'not x.nil?')
     expect(cop.offenses.size).to eq(1)
+    expect(cop.highlights).to eq(['not x.nil?'])
   end
 end
