ddef20cf62d8d99ffff696ffa6ad7c821969386e
Yuji Nakayama <nkymyj@gmail.com>
2014-03-28 23:07:25 +0900

    Fall back to Ruby 2.1 parser if current Ruby runtime is not supported

--- lib/rubocop.rb
+++ lib/rubocop.rb
@@ -1,12 +1,22 @@
 # encoding: utf-8
 
+# Try to load the latest parser and fall back to the latest supported version,
+# if parser does not yet support the current Ruby version.
+# This should only be problem when using development Ruby builds.
+begin
+  require 'parser/current'
+rescue NotImplementedError => error
+  warn "Falling back to Ruby 2.1 parser: #{error.message}"
+  require 'parser/ruby21'
+  Parser::CurrentRuby = Parser::Ruby21
+end
+
 require 'rainbow'
 # Rainbow 2.0 does not load the monkey-patch for String by default.
 require 'rainbow/ext/string' unless String.method_defined?(:color)
 
 require 'English'
 require 'set'
-require 'parser/current'
 require 'ast/sexp'
 require 'powerpack/enumerable/drop_last'
 require 'powerpack/hash/symbolize_keys'
