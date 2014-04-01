78bb2efa98834ee415b5480df14fbed402a02d67
Bozhidar Batsov <bozhidar@tradeo.com>
2014-04-01 18:13:24 +0300

    Improve WhileUntilModifier's message

--- lib/rubocop/cop/style/while_until_modifier.rb
+++ lib/rubocop/cop/style/while_until_modifier.rb
@@ -25,7 +25,7 @@ module Rubocop
         private
 
         def message(keyword)
-          "Favor modifier #{keyword} usage when you have a single-line body."
+          "Favor modifier `#{keyword}` usage when having a single-line body."
         end
       end
     end
