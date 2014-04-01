d93723f0aa79015bdc93d92b61582dfb1045a05a
Bozhidar Batsov <bozhidar@tradeo.com>
2014-04-01 16:53:21 +0300

    Improve Not's message

--- lib/rubocop/cop/style/not.rb
+++ lib/rubocop/cop/style/not.rb
@@ -5,7 +5,7 @@ module Rubocop
     module Style
       # This cop checks for uses if the keyword *not* instead of !.
       class Not < Cop
-        MSG = 'Use ! instead of not.'
+        MSG = 'Use `!` instead of `not`.'
 
         def on_send(node)
           _receiver, method_name, *args = *node
