d30f57ae8f1876a5f2ac7ff408ffdb5348c9d4f0
Bozhidar Batsov <bozhidar@tradeo.com>
2014-04-01 15:03:18 +0300

    Improve EndBlock's message

--- lib/rubocop/cop/style/end_block.rb
+++ lib/rubocop/cop/style/end_block.rb
@@ -5,7 +5,7 @@ module Rubocop
     module Style
       # This cop checks for END blocks.
       class EndBlock < Cop
-        MSG = 'Avoid the use of END blocks. Use `Kernel#at_exit` instead.'
+        MSG = 'Avoid the use of `END` blocks. Use `Kernel#at_exit` instead.'
 
         def on_postexe(node)
           add_offense(node, :keyword)
