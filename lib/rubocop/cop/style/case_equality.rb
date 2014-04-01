ff249200774fa4818d477583409e6857babf0f12
Bozhidar Batsov <bozhidar@tradeo.com>
2014-04-01 12:47:37 +0300

    Improve CaseEquality's message a bit

--- lib/rubocop/cop/style/case_equality.rb
+++ lib/rubocop/cop/style/case_equality.rb
@@ -5,7 +5,7 @@ module Rubocop
     module Style
       # This cop checks for uses of the case equality operator(===).
       class CaseEquality < Cop
-        MSG = 'Avoid the use of the case equality operator(===).'
+        MSG = 'Avoid the use of the case equality operator `===`.'
 
         def on_send(node)
           _receiver, method_name, *_args = *node
