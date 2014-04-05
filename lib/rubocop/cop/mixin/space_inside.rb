924cfdd15cf9c114f21cc01c2e76388ea53ee8ff
Jonas Arvidsson <jonas054@gmail.com>
2014-04-05 12:01:03 +0200

    [Fix #957] Allow space + comment inside ( { and [

--- lib/rubocop/cop/mixin/space_inside.rb
+++ lib/rubocop/cop/mixin/space_inside.rb
@@ -13,6 +13,10 @@ module Rubocop
         left, right, kind = specifics
         processed_source.tokens.each_cons(2) do |t1, t2|
           if t1.type == left || t2.type == right
+            # If the second token is a comment, that means that a line break
+            # follows, and that the rules for space inside don't apply.
+            next if t2.type == :tCOMMENT
+
             if t2.pos.line == t1.pos.line && space_between?(t1, t2)
               range = Parser::Source::Range.new(processed_source.buffer,
                                                 t1.pos.end_pos,
