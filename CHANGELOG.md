4342d30f717422a89faeefb9ed9be6380a95069a
Jonas Arvidsson <jonas054@gmail.com>
2014-04-10 22:36:46 +0200

    [Fix #490] Add EnforcedStyle config option to TrailingBlankLines

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@
 * [#963](https://github.com/bbatsov/rubocop/issues/963): Add `AllowDSLWriters` options to `TrivialAccessors`. ([@tamird][])
 * [#969](https://github.com/bbatsov/rubocop/issues/969): Let the `Debugger` cop check for forgotten calls to byebug. ([@bquorning][])
 * [#971](https://github.com/bbatsov/rubocop/issues/971): Configuration format deprecation warnings include the path to the problematic config file. ([@bcobb][])
+* [#490](https://github.com/bbatsov/rubocop/issues/490): Add EnforcedStyle config option to TrailingBlankLines. ([@jonas054][])
 
 ### Bugs fixed
 
--- spec/rubocop/cop/style/trailing_blank_lines_spec.rb
+++ spec/rubocop/cop/style/trailing_blank_lines_spec.rb
@@ -5,39 +5,91 @@ require 'spec_helper'
 describe Rubocop::Cop::Style::TrailingBlankLines do
   subject(:cop) { described_class.new(config) }
   let(:config) do
-    Rubocop::Config.new('TrailingWhitespace' => { 'Enabled' => true })
+    Rubocop::Config.new('TrailingWhitespace' => trailing_ws_config,
+                        'TrailingBlankLines' =>
+                        cop_config.merge('SupportedStyles' =>
+                                         %w(final_newline final_blank_line)))
   end
+  let(:trailing_ws_config) { { 'Enabled' => true } }
 
-  it 'accepts final newline' do
-    inspect_source(cop, ['x = 0', ''])
-    expect(cop.offenses).to be_empty
-  end
+  context 'when EnforcedStyle is final_newline' do
+    let(:cop_config) { { 'EnforcedStyle' => 'final_newline' } }
 
-  it 'registers an offense for multiple trailing blank lines' do
-    inspect_source(cop, ['x = 0', '', '', '', ''])
-    expect(cop.offenses.size).to eq(1)
-    expect(cop.messages).to eq(['3 trailing blank lines detected.'])
-  end
-
-  it 'auto-corrects unwanted blank lines' do
-    new_source = autocorrect_source(cop, ['x = 0', '', '', '', ''])
-    expect(new_source).to eq(['x = 0', ''].join("\n"))
-  end
-
-  it 'does not auto-correct if it interferes with TrailingWhitespace' do
-    original = ['x = 0', '', '  ', '', '']
-    new_source = autocorrect_source(cop, original)
-    expect(new_source).to eq(original.join("\n"))
-  end
-
-  context 'with TrailingWhitespace disabled' do
-    let(:config) do
-      Rubocop::Config.new('TrailingWhitespace' => { 'Enabled' => false })
+    it 'accepts final newline' do
+      inspect_source(cop, ['x = 0', ''])
+      expect(cop.offenses).to be_empty
     end
 
-    it 'auto-corrects even if some lines have space' do
-      new_source = autocorrect_source(cop, ['x = 0', '', '  ', '', ''])
+    it 'registers an offense for multiple trailing blank lines' do
+      inspect_source(cop, ['x = 0', '', '', '', ''])
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.messages).to eq(['3 trailing blank lines detected.'])
+    end
+
+    it 'registers an offense for no final newline' do
+      inspect_source(cop, ['x = 0'])
+      expect(cop.messages).to eq(['Final newline missing.'])
+    end
+
+    it 'auto-corrects unwanted blank lines' do
+      new_source = autocorrect_source(cop, ['x = 0', '', '', '', ''])
       expect(new_source).to eq(['x = 0', ''].join("\n"))
+    end
+
+    it 'does not auto-correct if it interferes with TrailingWhitespace' do
+      original = ['x = 0', '', '  ', '', '']
+      new_source = autocorrect_source(cop, original)
+      expect(new_source).to eq(original.join("\n"))
+    end
+
+    context 'with TrailingWhitespace disabled' do
+      let(:trailing_ws_config) { { 'Enabled' => false } }
+
+      it 'auto-corrects even if some lines have space' do
+        new_source = autocorrect_source(cop, ['x = 0', '', '  ', '', ''])
+        expect(new_source).to eq(['x = 0', ''].join("\n"))
+      end
+    end
+  end
+
+  context 'when EnforcedStyle is final_blank_line' do
+    let(:cop_config) { { 'EnforcedStyle' => 'final_blank_line' } }
+
+    it 'registers an offense for final newline' do
+      inspect_source(cop, ['x = 0', ''])
+      expect(cop.messages).to eq(['Trailing blank line missing.'])
+    end
+
+    it 'registers an offense for multiple trailing blank lines' do
+      inspect_source(cop, ['x = 0', '', '', '', ''])
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.messages)
+        .to eq(['3 trailing blank lines instead of 1 detected.'])
+    end
+
+    it 'registers an offense for no final newline' do
+      inspect_source(cop, ['x = 0'])
+      expect(cop.messages).to eq(['Final newline missing.'])
+    end
+
+    it 'accepts final blank line' do
+      inspect_source(cop, ['x = 0', '', ''])
+      expect(cop.offenses).to be_empty
+    end
+
+    it 'auto-corrects unwanted blank lines' do
+      new_source = autocorrect_source(cop, ['x = 0', '', '', '', ''])
+      expect(new_source).to eq(['x = 0', '', ''].join("\n"))
+    end
+
+    it 'auto-corrects missing blank line' do
+      new_source = autocorrect_source(cop, ['x = 0', ''])
+      expect(new_source).to eq(['x = 0', '', ''].join("\n"))
+    end
+
+    it 'auto-corrects missing newline' do
+      new_source = autocorrect_source(cop, ['x = 0'])
+      expect(new_source).to eq(['x = 0', '', ''].join("\n"))
     end
   end
 end
