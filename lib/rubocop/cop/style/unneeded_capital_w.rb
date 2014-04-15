ea9314c005f3e740b08d1984e313c8810c217dfd
Steve Feldon <steve@feldon.org>
2014-04-15 15:21:22 -0700

    Add `UnneededCapitalW` cop
    Add new cop `UnneededCapitalW` which detects usage of %W when
    interpolation is not needed and %w would so.

---
+++ lib/rubocop/cop/style/unneeded_capital_w.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+module Rubocop
+  module Cop
+    module Style
+      # This cop checks for usage of the %W() syntax when %w() would do.
+      class UnneededCapitalW < Cop
+        include PercentLiteral
+
+        MSG =
+          'Do not use `%W` unless interpolation is needed.  If not, use `%w`.'
+
+        def on_array(node)
+          process(node, '%W')
+        end
+
+        private
+
+        def on_percent_literal(node, types)
+          type = type(node)
+          if types.include?(type) &&
+            node.children.none? { |x| x.type == :dstr }
+            add_offense(node, :expression)
+          end
+        end
+      end
+    end
+  end
+end
