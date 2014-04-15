f607337fb2effdfb114a5321f7cedcb06b5ee12f
Jonas Arvidsson <jonas054@gmail.com>
2014-04-15 21:28:39 +0200

    [Fix #1000] Support modifier and def on same line in IndentationWidth
    Enforce an indentation style for Ruby >= 2.1 when "private def" etc
    is used.

--- lib/rubocop/cop/mixin/check_methods.rb
+++ lib/rubocop/cop/mixin/check_methods.rb
@@ -13,6 +13,18 @@ module Rubocop
         _scope, method_name, args, body = *node
         check(node, method_name, args, body)
       end
+
+      private
+
+      # Returns true for constructs such as
+      # private def my_method
+      # which are allowed in Ruby 2.1 and later.
+      def visibility_and_def_on_same_line?(receiver, method_name, args)
+        !receiver &&
+          [:public, :protected, :private,
+           :module_function].include?(method_name) &&
+          args.size == 1 && [:def, :defs].include?(args.first.type)
+      end
     end
   end
 end
