ea0489d6fc18b08ee8b089fb54de45384633eb81
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2014-04-17 12:42:15 +0200

    New Rails cop `Delegate` - checks for delegations
    Prefer delegate method for delegations

--- lib/rubocop.rb
+++ lib/rubocop.rb
@@ -235,6 +235,7 @@ require 'rubocop/cop/style/word_array'
 
 require 'rubocop/cop/rails/action_filter'
 require 'rubocop/cop/rails/default_scope'
+require 'rubocop/cop/rails/delegate'
 require 'rubocop/cop/rails/has_and_belongs_to_many'
 require 'rubocop/cop/rails/output'
 require 'rubocop/cop/rails/read_write_attribute'
