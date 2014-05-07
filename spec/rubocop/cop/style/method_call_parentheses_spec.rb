1de41bea8bf2be87678186d2460352aa54e2ae5f
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2014-05-07 12:42:50 +0200

    Extend MethodCallParentheses for DSL methods
    Omit parentheses around parameters for methods that are part of an
    internal DSL (e.g. Rake, Rails, RSpec), methods that have "keyword"
    status in Ruby (e.g. attr_reader, puts) and attribute access methods.

--- spec/rubocop/cop/style/method_call_parentheses_spec.rb
+++ spec/rubocop/cop/style/method_call_parentheses_spec.rb
@@ -5,8 +5,12 @@ require 'spec_helper'
 describe Rubocop::Cop::Style::MethodCallParentheses, :config do
   subject(:cop) { described_class.new(config) }
   let(:config) do
-    Rubocop::Config.new('EmptyLiteral' => { 'Enabled' => true })
+    Rubocop::Config.new(
+      'EmptyLiteral' => { 'Enabled' => empty_literal_enabled },
+      'MethodCallParentheses' => { 'RequireNoParentheses' => ['include'] }
+    )
   end
+  let(:empty_literal_enabled) { true }
 
   it 'registers an offense for parens in method call without args' do
     inspect_source(cop, ['top.test()'])
@@ -33,6 +37,35 @@ describe Rubocop::Cop::Style::MethodCallParentheses, :config do
     expect(new_source).to eq('test')
   end
 
+  it 'detects DSL methods with parentheses' do
+    inspect_source(cop, ['  include("bar")']) # spaces are on purpose
+    expect(cop.offenses.size).to eq(1)
+    expect(cop.offenses.map(&:line).sort).to eq([1])
+    expect(cop.messages) .to eq(['Omit parentheses for DSL method calls.'])
+    expect(cop.highlights).to eq(['include("bar")'])
+  end
+
+  it 'ignores DSL methods without parentheses' do
+    inspect_source(cop, ['include "bar"'])
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'ignores custom methods with names like DSL methods' do
+    inspect_source(cop, ['include = foo.include("bar")',
+                         'should include("bar")'])
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'ignores method calls with operators' do
+    inspect_source(cop, ['foo = "bar"', 'foo * 100', 'foo << bar'])
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'auto-corrects by removing braces from DSL methods' do
+    new_source = autocorrect_source(cop, 'include("foo")')
+    expect(new_source).to eq('include "foo"')
+  end
+
   it 'does not auto-correct calls that will be changed to empty literals' do
     original = ['Hash.new()',
                 'Array.new()',
@@ -42,9 +75,7 @@ describe Rubocop::Cop::Style::MethodCallParentheses, :config do
   end
 
   context 'when EmptyLiteral is disabled' do
-    let(:config) do
-      Rubocop::Config.new('EmptyLiteral' => { 'Enabled' => false })
-    end
+    let(:empty_literal_enabled) { false }
 
     it 'auto-corrects calls that could be empty literals' do
       original = ['Hash.new()',
