d093a94d3c680e7e66632c0469ca24ba5803cd26
Scott Albertson <scott@thoughtbot.com>
2014-05-30 14:19:07 -0700

    Use leading dot

--- spec/rubocop/cop/style/inline_comment_spec.rb
+++ spec/rubocop/cop/style/inline_comment_spec.rb
@@ -6,7 +6,7 @@ describe RuboCop::Cop::Style::InlineComment do
   it 'registers an offense for a inline comment' do
     code = 'two = 1 + 1 # An inline comment'
 
-    expect(subject).to find_offenses_in(code).
-      with_highlight('# An inline comment')
+    expect(subject).to find_offenses_in(code)
+      .with_highlight('# An inline comment')
   end
 end
