3db7e6485fef6530a40ff13d36a6e008a78b31b1
Scott Albertson <scott@thoughtbot.com>
2014-05-30 14:31:00 -0700

    Add chainable message matcher

--- spec/rubocop/cop/style/inline_comment_spec.rb
+++ spec/rubocop/cop/style/inline_comment_spec.rb
@@ -7,6 +7,7 @@ describe RuboCop::Cop::Style::InlineComment do
     code = 'two = 1 + 1 # An inline comment'
 
     expect(subject).to find_offenses_in(code)
+      .with_message('Avoid inline comments.')
       .with_highlight('# An inline comment')
   end
 end
--- spec/support/offenses_matcher.rb
+++ spec/support/offenses_matcher.rb
@@ -3,15 +3,28 @@
 RSpec::Matchers.define :find_offenses_in do |code|
   match do |cop|
     inspect_source(cop, [code])
-
-    if @highlight
-      cop.highlights.include?(@highlight)
-    else
+    includes_highlight(cop) &&
+      includes_message(cop) &&
       cop.offenses.any?
-    end
   end
 
   chain :with_highlight do |highlight|
     @highlight = highlight
+  end
+
+  chain :with_message do |message|
+    @message = message
+  end
+
+  def includes_highlight(cop)
+    return true unless @highlight
+
+    cop.highlights.include?(@highlight)
+  end
+
+  def includes_message(cop)
+    return true unless @message
+
+    cop.offenses.map(&:message).include?(@message)
   end
 end
