865de9e391461b6e0c304884416a03b475c6245a
Yuji Nakayama <nkymyj@gmail.com>
2014-06-06 09:17:33 +0900

    Convert specs to RSpec 3.0.0 syntax with Transpec
    This conversion is done by Transpec 2.2.1 with the following command:
        transpec --keep oneliner
    
    * 1 conversion
        from: failure_message_for_should { }
          to: failure_message { }
    
    * 1 conversion
        from: failure_message_for_should_not { }
          to: failure_message_when_negated { }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,11 +47,11 @@ module ExitCodeMatchers
       end
       actual && actual == code
     end
-    failure_message_for_should do
+    failure_message do
       "expected block to call exit(#{code}) but exit" +
         (actual.nil? ? ' not called' : "(#{actual}) was called")
     end
-    failure_message_for_should_not do
+    failure_message_when_negated do
       "expected block not to call exit(#{code})"
     end
     description do
