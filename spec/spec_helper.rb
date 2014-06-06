7f4a58391b708680dba5e55bc3d6b2886263d0e0
Yuji Nakayama <nkymyj@gmail.com>
2014-06-06 09:18:26 +0900

    Remove obsolete RSpec config option
    It's always enabled in RSpec 3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,8 +61,6 @@ module ExitCodeMatchers
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
