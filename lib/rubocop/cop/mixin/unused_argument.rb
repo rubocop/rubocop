03f342abf6b4822fd995ed110f1d290772847842
Yuji Nakayama <nkymyj@gmail.com>
2014-06-27 01:40:07 +0900

    Correct with source location info rather than regexp in UnusedArgument

--- lib/rubocop/cop/mixin/unused_argument.rb
+++ lib/rubocop/cop/mixin/unused_argument.rb
@@ -9,13 +9,6 @@ module RuboCop
           force_class == VariableForce
         end
 
-        def autocorrect(node)
-          new_name = node.loc.expression.source.sub(/(\W?)(\w+)/, '\\1_\\2')
-          @corrections << lambda do |corrector|
-            corrector.replace(node.loc.expression, new_name)
-          end
-        end
-
         def after_leaving_scope(scope, _variable_table)
           scope.variables.each_value do |variable|
             check_argument(variable)
@@ -28,6 +21,12 @@ module RuboCop
 
           message = message(variable)
           add_offense(variable.declaration_node, :name, message)
+        end
+
+        def autocorrect(node)
+          @corrections << lambda do |corrector|
+            corrector.insert_before(node.loc.name, '_')
+          end
         end
       end
     end
