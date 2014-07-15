d0ef1311beaf8ede7c48c13e29d300b2137c651b
Maarten Claes <maartencls@gmail.com>
2014-07-15 22:05:08 +0200

    Refactor bracket checking in SpaceInside mixin

--- lib/rubocop/cop/mixin/space_inside.rb
+++ lib/rubocop/cop/mixin/space_inside.rb
@@ -3,16 +3,16 @@
 module RuboCop
   module Cop
     # Common functionality for checking for spaces inside various
-    # kinds of parentheses.
+    # kinds of brackets.
     module SpaceInside
       include SurroundingSpace
       MSG = 'Space inside %s detected.'
 
       def investigate(processed_source)
         @processed_source = processed_source
-        _, right, kind = specifics
+        brackets = Brackets.new(*specifics)
         processed_source.tokens.each_cons(2) do |t1, t2|
-          next unless left?(t1.type) || t2.type == right
+          next unless brackets.left_side?(t1) || brackets.right_side?(t2)
 
           # If the second token is a comment, that means that a line break
           # follows, and that the rules for space inside don't apply.
@@ -22,7 +22,7 @@ module RuboCop
           range = Parser::Source::Range.new(processed_source.buffer,
                                             t1.pos.end_pos,
                                             t2.pos.begin_pos)
-          add_offense(range, range, format(MSG, kind))
+          add_offense(range, range, format(MSG, brackets.kind))
         end
       end
 
@@ -30,11 +30,33 @@ module RuboCop
         @corrections << ->(corrector) { corrector.remove(range) }
       end
 
-      private
+      # Wraps info about the brackets. Makes it easy to check whether a token
+      # is one of the brackets.
+      #
+      # @example Parentheses `()`
+      #   Brackets.new(:tLPAREN, :tRPAREN, 'parentheses')
+      #
+      # @example Square brackets `[]`
+      #   Brackets.new([:tLBRACK, :tLBRACK2], :tRBRACK, 'square brackets')
+      #
+      class Brackets
+        attr_reader :kind
 
-      def left?(token_type)
-        @left_types ||= [specifics.first].flatten
-        @left_types.include?(token_type)
+        def initialize(left, right, kind)
+          @left_side_types = [left].flatten
+          @right_side_type = right
+          @kind = kind
+        end
+
+        def left_side?(token)
+          # Left side bracket has to be able to match multiple types
+          # (e.g. :tLBRACK and :tLBRACK2)
+          @left_side_types.include?(token.type)
+        end
+
+        def right_side?(token)
+          @right_side_type == token.type
+        end
       end
     end
   end
