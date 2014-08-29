845add3813820b5c1f2b8ff72370fab8d3adf235
Yuji Nakayama <nkymyj@gmail.com>
2014-08-29 09:00:12 +0900

    Clean up copied & pasted code in HTMLFormatter and the spec
    They were just copied & pasted from JSONFormatter and the HTML output
    was actually not tested.

--- lib/rubocop/formatter/html_formatter.rb
+++ lib/rubocop/formatter/html_formatter.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 require 'erb'
-require 'pathname'
+require 'ostruct'
 
 module RuboCop
   module Formatter
@@ -9,81 +9,48 @@ module RuboCop
     class HTMLFormatter < BaseFormatter
       include PathUtil
 
-      attr_reader :output_hash
+      TEMPLATE_PATH =
+        File.expand_path('../../../../assets/output.html.erb', __FILE__)
+
+      attr_reader :files, :summary
 
       def initialize(output)
         super
-        @output_hash = {
-          metadata: metadata_hash,
-          files:    [],
-          summary:  { offense_count: 0 }
-        }
+        @files = []
+        @summary = OpenStruct.new(offense_count: 0)
       end
 
       def started(target_files)
-        output_hash[:summary][:target_file_count] = target_files.count
+        summary.target_files = target_files
       end
 
       def file_finished(file, offenses)
-        output_hash[:files] << hash_for_file(file, offenses)
-        output_hash[:summary][:offense_count] += offenses.count
+        files << OpenStruct.new(path: file, offenses: offenses)
+        summary.offense_count += offenses.count
       end
 
       def finished(inspected_files)
-        output_hash[:summary][:inspected_file_count] = inspected_files.count
-        template = File.read(File
-          .expand_path('../../../../assets/output.html.erb', __FILE__))
-        erb = ERB.new(template)
-        html_content = erb.result(binding)
-        output.write html_content
+        summary.inspected_files = inspected_files
+
+        render_html
       end
 
-      def metadata_hash
-        {
+      def render_html
+        template = File.read(TEMPLATE_PATH)
+        erb = ERB.new(template)
+        html = erb.result(binding)
+
+        output.write html
+      end
+
+      def metadata
+        OpenStruct.new(
           rubocop_version: RuboCop::Version::STRING,
           ruby_engine:     RUBY_ENGINE,
           ruby_version:    RUBY_VERSION,
           ruby_patchlevel: RUBY_PATCHLEVEL.to_s,
           ruby_platform:   RUBY_PLATFORM
-        }
-      end
-
-      def hash_for_file(file, offenses)
-        {
-          path:     relative_path(file),
-          offenses: offenses.map { |o| hash_for_offense(o) }
-        }
-      end
-
-      def hash_for_offense(offense)
-        {
-          severity: offense.severity.name,
-          message:  offense.message,
-          cop_name: offense.cop_name,
-          corrected: offense.corrected?,
-          location: hash_for_location(offense)
-        }
-      end
-
-      # TODO: Consider better solution for Offense#real_column.
-      def hash_for_location(offense)
-        {
-          line:   offense.line,
-          column: offense.real_column,
-          length: offense.location.length,
-          source_line: offense.location.source_line,
-          highlight: highlight_line(offense.location)
-        }
-      end
-
-      def highlight_line(location)
-        column_length = if location.begin.line == location.end.line
-                          location.column_range.count
-                        else
-                          location.source_line.length - location.column
-                        end
-
-        ' ' * location.column + '^' * column_length
+        )
       end
     end
   end
