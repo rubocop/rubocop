2435565180137e5e7bd23dab89b6376135f910eb
Jonas Arvidsson <jonas054@gmail.com>
2014-10-18 08:10:28 +0200

    [Fix #1181] New cop StringLiteralsInInterpolation

--- lib/rubocop/cop/style/string_literals_in_interpolation.rb
+++ lib/rubocop/cop/style/string_literals_in_interpolation.rb
@@ -4,34 +4,27 @@ module RuboCop
   module Cop
     module Style
       # Checks if uses of quotes match the configured preference.
-      class StringLiterals < Cop
+      class StringLiteralsInInterpolation < Cop
         include ConfigurableEnforcedStyle
         include StringHelp
-
-        def on_dstr(node)
-          # A dstr node with dstr and str children is a concatenated
-          # string. Don't ignore the whole thing.
-          return if node.children.find { |child| child.type == :str }
-
-          # Dynamic strings can not use single quotes, and quotes inside
-          # interpolation expressions are checked by the
-          # StringLiteralsInInterpolation cop, so ignore.
-          ignore_node(node)
-        end
 
         private
 
         def message(*)
-          if style == :single_quotes
-            "Prefer single-quoted strings when you don't need string " \
-            'interpolation or special symbols.'
-          else
-            'Prefer double-quoted strings unless you need single quotes to ' \
-            'avoid extra backslashes for escaping.'
-          end
+          # single_quotes -> single-quoted
+          kind = style.to_s.sub(/_(.*)s/, '-\1d')
+
+          "Prefer #{kind} strings inside interpolations."
         end
 
         def offense?(node)
+          # If it's not a string within a dynamic string, i.e. part of an
+          # expression in an interpolation, then it's not an offense for this
+          # cop.
+          return false unless node.each_ancestor.find do |a|
+            a.type == :dstr && within_node?(node, a)
+          end
+
           src = node.loc.expression.source
           return false if src.start_with?('%') || src.start_with?('?')
           if style == :single_quotes
@@ -39,6 +32,11 @@ module RuboCop
           else
             src !~ /" | \\/x
           end
+        end
+
+        def within_node?(inner, outer)
+          o, i = outer.loc.expression, inner.loc.expression
+          i.begin_pos >= o.begin_pos && i.end_pos <= o.end_pos
         end
 
         def autocorrect(node)
---
+++ spec/rubocop/cop/style/string_literals_in_interpolation_spec.rb
@@ -0,0 +1,63 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Style::StringLiteralsInInterpolation, :config do
+  subject(:cop) { described_class.new(config) }
+
+  context 'configured with single quotes preferred' do
+    let(:cop_config) { { 'EnforcedStyle' => 'single_quotes' } }
+
+    it 'detects unneeded double quotes within embedded expression' do
+      src = ['"#{"A"}"']
+      inspect_source(cop, src)
+      expect(cop.messages)
+        .to eq(['Prefer single-quoted strings inside interpolations.'])
+    end
+
+    it 'accepts double quotes on a static string' do
+      src = ['"A"']
+      inspect_source(cop, src)
+      expect(cop.offenses).to be_empty
+    end
+
+    it 'can handle a built-in constant parsed as string' do
+      # Parser will produce str nodes for constants such as __FILE__.
+      src = ['if __FILE__ == $PROGRAM_NAME',
+             'end']
+      inspect_source(cop, src)
+      expect(cop.offenses).to be_empty
+    end
+
+    it 'can handle character literals' do
+      src = 'a = ?/'
+      inspect_source(cop, src)
+      expect(cop.offenses).to be_empty
+    end
+
+    it 'auto-corrects " with \'' do
+      new_source = autocorrect_source(cop, 's = "#{"abc"}"')
+      expect(new_source).to eq(%q(s = "#{'abc'}"))
+    end
+  end
+
+  context 'configured with double quotes preferred' do
+    let(:cop_config) { { 'EnforcedStyle' => 'double_quotes' } }
+
+    it 'registers an offense for single quotes within embedded expression' do
+      src = %q("#{'A'}")
+      inspect_source(cop, src)
+      expect(cop.messages)
+        .to eq(['Prefer double-quoted strings inside interpolations.'])
+    end
+  end
+
+  context 'when configured with a bad value' do
+    let(:cop_config) { { 'EnforcedStyle' => 'other' } }
+
+    it 'fails' do
+      expect { inspect_source(cop, ['a = "#{"b"}"']) }
+        .to raise_error(RuntimeError)
+    end
+  end
+end
