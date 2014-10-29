073a92b716f997efeb2f179c7a012e3650a13dc8
Bozhidar Batsov <bozhidar@tradeo.com>
2014-10-29 11:52:42 +0200

    [Fix #1387] Expand the documentation of the DoubleNegation cop

--- lib/rubocop/cop/style/double_negation.rb
+++ lib/rubocop/cop/style/double_negation.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Cop
     module Style
       # This cop checks for uses of double negation (!!) to convert something
-      # to a boolean value. As this is both cryptic and usually redundant it
+      # to a boolean value. As this is both cryptic and usually redundant, it
       # should be avoided.
       #
       # @example
@@ -14,6 +14,11 @@ module RuboCop
       #
       #   # good
       #   !something.nil?
+      #
+      # Please, note that when something is a boolean value
+      # !!something and !something.nil? are not the same thing.
+      # As you're unlikely to write code that can accept values of any type
+      # this is rarely a problem in practice.
       class DoubleNegation < Cop
         MSG = 'Avoid the use of double negation (`!!`).'
 
