53d6f4f64d69cad4434a6ba5f2ceee8c054e106a
Bozhidar Batsov <bozhidar@tradeo.com>
2014-11-07 14:39:35 +0200

    Tiny doc fix

--- lib/rubocop/cop/mixin/on_normal_if_unless.rb
+++ lib/rubocop/cop/mixin/on_normal_if_unless.rb
@@ -2,7 +2,7 @@
 
 module RuboCop
   module Cop
-    # Common functionality for cops checking if and unless statements.
+    # Common functionality for cops checking if and unless expressions.
     module OnNormalIfUnless
       include IfNode
 
