40fcf7b38945b156112f5dbb8dc44ff5f5f5363e
Jeff Carbonella <jeff@contactually.com>
2014-11-25 10:21:13 -0500

    Add EmptyLinesAroundBlockBody cop
    There were a few existing cops for preventing/enforcing newlines
    around the body of classes, methods, and modules. There wasn't one
    for blocks (especially relevant for rspec!) so this adds one.

---
+++ lib/rubocop/cop/style/empty_lines_around_block_body.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+module RuboCop
+  module Cop
+    module Style
+      # This cops checks if empty lines around the bodies of blocks match
+      # the configuration.
+      #
+      # @example
+      #
+      #   something do
+      #
+      #     ...
+      #   end
+      #
+      class EmptyLinesAroundBlockBody < Cop
+        include EmptyLinesAroundBody
+
+        KIND = 'block'
+
+        def on_block(node)
+          check(node)
+        end
+
+        private
+
+        def check(node)
+          start_line = node.loc.begin.line
+          end_line = node.loc.end.line
+
+          return if start_line == end_line
+
+          check_source(start_line, end_line)
+        end
+      end
+    end
+  end
+end
