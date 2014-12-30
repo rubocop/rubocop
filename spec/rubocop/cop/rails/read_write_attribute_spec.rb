8d82b5142c43646129c9af89141fd0bfbd000dad
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-12-30 10:46:18 +0100

    Implement autocorrection for Rails/ReadWriteAttribute cop.

--- spec/rubocop/cop/rails/read_write_attribute_spec.rb
+++ spec/rubocop/cop/rails/read_write_attribute_spec.rb
@@ -11,9 +11,116 @@ describe RuboCop::Cop::Rails::ReadWriteAttribute do
     expect(cop.highlights).to eq(['read_attribute'])
   end
 
+  it 'registers no offense for read_attribute with explicit receiver' do
+    inspect_source(cop, 'res = object.read_attribute(:test)')
+    expect(cop.offenses.size).to eq(0)
+  end
+
   it 'registers an offense for write_attribute' do
     inspect_source(cop, 'write_attribute(:test, val)')
     expect(cop.offenses.size).to eq(1)
     expect(cop.highlights).to eq(['write_attribute'])
+  end
+
+  it 'registers no offense for write_attribute with explicit receiver' do
+    inspect_source(cop, 'object.write_attribute(:test, val)')
+    expect(cop.offenses.size).to eq(0)
+  end
+
+  describe '#autocorrect' do
+    context 'write_attribute' do
+      it 'autocorrects symbol' do
+        source = 'write_attribute(:attr, var)'
+        corrected_source = 'self[:attr] = var'
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects string' do
+        source = "write_attribute('attr', 'test')"
+        corrected_source = "self['attr'] = 'test'"
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects without parentheses' do
+        source = "write_attribute 'attr', 'test'"
+        corrected_source = "self['attr'] = 'test'"
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects expression' do
+        source = "write_attribute(:attr, 'test_' + postfix)"
+        corrected_source = "self[:attr] = 'test_' + postfix"
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects multiline' do
+        source = [
+          'write_attribute(',
+          ':attr, ',
+          '(',
+          "'test_' + postfix",
+          ').to_sym',
+          ')'
+        ]
+        corrected_source = [
+          'self[:attr] = (',
+          "'test_' + postfix",
+          ').to_sym'
+        ].join("\n")
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+    end
+
+    context 'read_attribute' do
+      it 'autocorrects symbol' do
+        source = 'res = read_attribute(:test)'
+        corrected_source = 'res = self[:test]'
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects string' do
+        source = "res = read_attribute('test')"
+        corrected_source = "res = self['test']"
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects without parentheses' do
+        source = "res = read_attribute 'test'"
+        corrected_source = "res = self['test']"
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects expression' do
+        source = "res = read_attribute('test_' + postfix)"
+        corrected_source = "res = self['test_' + postfix]"
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+
+      it 'autocorrects multiline' do
+        source = [
+          'res = read_attribute(',
+          '(',
+          "'test_' + postfix",
+          ').to_sym',
+          ')'
+        ]
+        corrected_source = [
+          'res = self[(',
+          "'test_' + postfix",
+          ').to_sym]'
+        ].join("\n")
+
+        expect(autocorrect_source(cop, source)).to eq(corrected_source)
+      end
+    end
   end
 end
