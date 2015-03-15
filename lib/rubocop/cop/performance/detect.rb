abc359c31496ecad493c7eb98c7c7598b6684eef
palkan <dementiev.vm@gmail.com>
2015-03-15 11:02:20 +0300

    Improve perfomance/detect code

--- lib/rubocop/cop/performance/detect.rb
+++ lib/rubocop/cop/performance/detect.rb
@@ -28,13 +28,7 @@ module RuboCop
           _, first_method = *receiver
           return unless SELECT_METHODS.include?(first_method)
 
-          source_buffer = node.loc.expression.source_buffer
-          location_of_select = receiver.loc.selector.begin_pos
-          end_location = node.loc.selector.end_pos
-
-          range = Parser::Source::Range.new(source_buffer,
-                                            location_of_select,
-                                            end_location)
+          range = receiver.loc.selector.join(node.loc.selector)
 
           add_offense(node, range, format(MSG,
                                           preferred_method,
@@ -44,32 +38,20 @@ module RuboCop
         def autocorrect(node)
           receiver, _first_method = *node
 
-          first_range = Parser::Source::Range.new(
-            node.loc.expression.source_buffer,
-            node.loc.dot.begin_pos,
-            node.loc.selector.end_pos)
+          first_range = node.loc.dot.join(node.loc.selector)
 
           receiver, _args, _body = *receiver if receiver.block_type?
 
-          select_range = Parser::Source::Range.new(
-            node.loc.expression.source_buffer,
-            receiver.loc.selector.begin_pos,
-            receiver.loc.selector.end_pos)
-
           @corrections << lambda do |corrector|
             corrector.remove(first_range)
-            corrector.replace(select_range, preferred_method)
+            corrector.replace(receiver.loc.selector, preferred_method)
           end
         end
 
         private
 
         def preferred_method
-          cop_config['PreferredMethod'] || default_cop_config['PreferredMethod']
-        end
-
-        def default_cop_config
-          ConfigLoader.default_configuration[cop_name]
+          cop_config['PreferredMethod']
         end
       end
     end
