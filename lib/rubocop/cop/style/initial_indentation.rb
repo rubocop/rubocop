509a4971ee6951d95e5d76f9ea24ecb7666fa3c8
Jonas Arvidsson <jonas054@gmail.com>
2015-07-04 20:35:24 +0200

    [Fix #2001] New cop Style/InitialIndentation

---
+++ lib/rubocop/cop/style/initial_indentation.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+module RuboCop
+  module Cop
+    module Style
+      # This cops checks for indentation of the first non-blank non-comment
+      # line in a file.
+      class InitialIndentation < Cop
+        MSG = 'Indentation of first line in file detected.'
+
+        def investigate(processed_source)
+          first_token = processed_source.tokens.find do |t|
+            !t.text.start_with?('#')
+          end
+          return unless first_token
+          return if first_token.pos.column == 0
+
+          with_space = range_with_surrounding_space(first_token.pos, :left,
+                                                    nil, !:with_newline)
+          space = Parser::Source::Range.new(processed_source.buffer,
+                                            with_space.begin_pos,
+                                            first_token.pos.begin_pos)
+          add_offense(space, first_token.pos)
+        end
+
+        def autocorrect(range)
+          ->(corrector) { corrector.remove(range) }
+        end
+      end
+    end
+  end
+end
