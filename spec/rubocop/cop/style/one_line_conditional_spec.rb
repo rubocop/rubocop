8028b1237747d5bf61e2b377ea52665293ba38f0
Stephanie Liu <sliu14@outlook.com>
2015-08-01 23:03:40 -0400

    [Fix #2093] if/then/end generates an OLC offense

--- spec/rubocop/cop/style/one_line_conditional_spec.rb
+++ spec/rubocop/cop/style/one_line_conditional_spec.rb
@@ -5,9 +5,25 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::OneLineConditional do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense for one line if/then/end' do
+  it 'registers an offense for one line if/then/else/end' do
     inspect_source(cop, 'if cond then run else dont end')
     expect(cop.messages).to eq(['Favor the ternary operator (?:)' \
                                 ' over if/then/else/end constructs.'])
+  end
+
+  it 'does not register an offense for if/then/end' do
+    inspect_source(cop, 'if cond then run end')
+    expect(cop.messages).to be_empty
+  end
+
+  it 'does register an offense for one line unless/then/else/end' do
+    inspect_source(cop, 'unless cond then run else dont end')
+    expect(cop.messages).to eq(['Favor the ternary operator (?:)' \
+                                ' over unless/then/else/end constructs.'])
+  end
+
+  it 'does not register an offense for one line unless/then/end' do
+    inspect_source(cop, 'unless cond then run end')
+    expect(cop.messages).to be_empty
   end
 end
