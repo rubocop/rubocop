ac4f351b46807018e71c058d622595d72a18381e
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-08-05 18:00:18 -0400

    Add auto-correction to Style/RescueModifier

--- lib/rubocop/cop/style/rescue_modifier.rb
+++ lib/rubocop/cop/style/rescue_modifier.rb
@@ -5,13 +5,52 @@ module RuboCop
     module Style
       # This cop checks for uses of rescue in its modifier form.
       class RescueModifier < Cop
+        include AutocorrectAlignment
+
         MSG = 'Avoid using `rescue` in its modifier form.'
 
         def investigate(processed_source)
-          processed_source.tokens.each do |t|
-            next unless t.type == :kRESCUE_MOD
-            add_offense(nil, t.pos)
+          @modifier_locations = processed_source.tokens
+                                .select { |t| t.type == :kRESCUE_MOD }
+                                .map(&:pos)
+        end
+
+        def on_resbody(node)
+          return unless modifier?(node)
+          add_offense(node.parent, :expression)
+        end
+
+        def autocorrect(node)
+          operation, rescue_modifier, = *node
+          *_, rescue_args = *rescue_modifier
+
+          correction =
+            "begin\n" <<
+            indentation(node) << operation.loc.expression.source <<
+            "\n#{offset(node)}rescue\n" <<
+            indentation(node) << rescue_args.loc.expression.source <<
+            "\n#{offset(node)}end"
+          range = Parser::Source::Range.new(node.loc.expression.source_buffer,
+                                            node.loc.expression.begin_pos,
+                                            node.loc.expression.end_pos)
+
+          lambda do |corrector|
+            corrector.replace(range, correction)
           end
+        end
+
+        private
+
+        def modifier?(node)
+          @modifier_locations.include?(node.loc.keyword)
+        end
+
+        def indentation(node)
+          offset(node) + (' ' * configured_indentation_width)
+        end
+
+        def offset(node)
+          ' ' * node.loc.column
         end
       end
     end
