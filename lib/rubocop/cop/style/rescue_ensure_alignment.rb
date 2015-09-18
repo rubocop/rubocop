fa363dec54e0d3ff1a164138d4e05375f56f4ec8
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-09-17 21:46:43 -0400

    Use each_with_object instead of select...map

--- lib/rubocop/cop/style/rescue_ensure_alignment.rb
+++ lib/rubocop/cop/style/rescue_ensure_alignment.rb
@@ -33,9 +33,11 @@ module RuboCop
         end
 
         def investigate(processed_source)
-          @modifier_locations = processed_source.tokens
-                                .select { |t| t.type == :kRESCUE_MOD }
-                                .map(&:pos)
+          @modifier_locations =
+            processed_source.tokens.each_with_object([]) do |token, locations|
+              next unless token.type == :kRESCUE_MOD
+              locations << token.pos
+            end
         end
 
         def autocorrect(node)
