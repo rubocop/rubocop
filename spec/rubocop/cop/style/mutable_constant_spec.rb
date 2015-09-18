ba8eb6b40272992c27fcf4777155468096cb767a
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-09-17 22:31:36 -0400

    Remove redundant constants from tests

--- spec/rubocop/cop/style/mutable_constant_spec.rb
+++ spec/rubocop/cop/style/mutable_constant_spec.rb
@@ -5,14 +5,7 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::MutableConstant do
   subject(:cop) { described_class.new }
 
-  MUTABLE_OBJECTS = [
-    '[1, 2, 3]',
-    '{ a: 1, b: 2 }',
-    "'str'",
-    '"top#{1 + 2}"'
-  ]
-
-  MUTABLE_OBJECTS.each do |o|
+  shared_examples :mutable_objects do |o|
     it "registers an offense for #{o} assigned to a constant" do
       inspect_source(cop, "CONST = #{o}")
       expect(cop.offenses.size).to eq(1)
@@ -24,18 +17,21 @@ describe RuboCop::Cop::Style::MutableConstant do
     end
   end
 
-  IMMUTABLE_OBJECTS = [
-    '1',
-    '1.5',
-    ':sym'
-  ]
+  it_behaves_like :mutable_objects, '[1, 2, 3]'
+  it_behaves_like :mutable_objects, '{ a: 1, b: 2 }'
+  it_behaves_like :mutable_objects, "'str'"
+  it_behaves_like :mutable_objects, '"top#{1 + 2}"'
 
-  IMMUTABLE_OBJECTS.each do |o|
+  shared_examples :immutable_objects do |o|
     it "allows #{o} to be assigned to a constant" do
       inspect_source(cop, "CONST = #{o}")
       expect(cop.offenses).to be_empty
     end
   end
+
+  it_behaves_like :immutable_objects, '1'
+  it_behaves_like :immutable_objects, '1.5'
+  it_behaves_like :immutable_objects, ':sym'
 
   it 'allows method call assignments' do
     inspect_source(cop, 'TOP_TEST = Something.new')
