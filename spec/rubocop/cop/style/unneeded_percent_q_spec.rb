a2e21876bed6d0041ee2bfcf43e2b7d8b9ac68d1
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-10-01 14:52:31 -0400

    Refactor UnneededPercentQ tests

--- spec/rubocop/cop/style/unneeded_percent_q_spec.rb
+++ spec/rubocop/cop/style/unneeded_percent_q_spec.rb
@@ -6,114 +6,136 @@ describe RuboCop::Cop::Style::UnneededPercentQ do
   subject(:cop) { described_class.new }
 
   context 'with %q strings' do
-    let(:source) do
-      <<-END.strip_indent
-        %q('hi') # line 1
-        %q("hi")
-        %q(hi)
-        %q('"hi"')
-        %q('hi\\t') # line 5
-      END
-    end
-    let(:corrected) do
-      <<-END.strip_indent
-        "'hi'" # line 1
-        '"hi"'
-        'hi'
-        %q('"hi"')
-        %q('hi\\t') # line 5
-      END
-    end
-    before { inspect_source(cop, source) }
-
     it 'registers an offense for only single quotes' do
-      expect(cop.offenses.map(&:line)).to include(1)
+      inspect_source(cop, "%q('hi')")
+
       expect(cop.messages).to eq(['Use `%q` only for strings that contain ' \
-                                  'both single quotes and double quotes.'] * 3)
+                                  'both single quotes and double quotes.'])
     end
 
     it 'registers an offense for only double quotes' do
-      expect(cop.offenses.map(&:line)).to include(2)
+      inspect_source(cop, '%q("hi")')
+
+      expect(cop.messages).to eq(['Use `%q` only for strings that contain ' \
+                                  'both single quotes and double quotes.'])
     end
 
     it 'registers an offense for no quotes' do
-      expect(cop.offenses.map(&:line)).to include(3)
+      inspect_source(cop, '%q(hi)')
+
+      expect(cop.messages).to eq(['Use `%q` only for strings that contain ' \
+                                  'both single quotes and double quotes.'])
     end
 
     it 'accepts a string with single quotes and double quotes' do
-      expect(cop.offenses.map(&:line)).not_to include(4)
+      inspect_source(cop, %q(%q('"hi"')))
+
+      expect(cop.messages).to be_empty
     end
 
     it 'accepts a string with a tab character' do
-      expect(cop.offenses.map(&:line)).not_to include(5)
+      inspect_source(cop, "%q('hi\\t')")
+
+      expect(cop.messages).to be_empty
     end
 
-    it 'auto-corrects' do
-      new_source = autocorrect_source(cop, source)
-      expect(new_source).to eq(corrected)
+    it 'accepts regular expressions starting with %q' do
+      inspect_source(cop, '/%q?/')
+
+      expect(cop.messages).to be_empty
+    end
+
+    context 'auto-correct' do
+      it 'registers an offense for only single quotes' do
+        new_source = autocorrect_source(cop, "%q('hi')")
+
+        expect(new_source).to eq(%q("'hi'"))
+      end
+
+      it 'registers an offense for only double quotes' do
+        new_source = autocorrect_source(cop, '%q("hi")')
+
+        expect(new_source).to eq(%q('"hi"'))
+      end
+
+      it 'registers an offense for no quotes' do
+        new_source = autocorrect_source(cop, '%q(hi)')
+
+        expect(new_source).to eq("'hi'")
+      end
     end
   end
 
   context 'with %Q strings' do
-    let(:source) do
-      <<-END.strip_indent
-        %Q(hi) # line 1
-        %Q("hi")
-        %Q(hi\#{4})
-        %Q('"hi"')
-        %Q("\\thi")
-        %Q("hi\#{4}")
-        /%Q?/ # line 7
-      END
-    end
-    let(:corrected) do
-      <<-END.strip_indent
-        "hi" # line 1
-        '"hi"'
-        "hi\#{4}"
-        %Q('"hi"')
-        %Q("\\thi")
-        %Q("hi\#{4}")
-        /%Q?/ # line 7
-      END
-    end
-    before { inspect_source(cop, source) }
-
     it 'registers an offense for static string without quotes' do
-      expect(cop.offenses.map(&:line)).to include(1)
+      inspect_source(cop, '%Q(hi)')
+
       expect(cop.messages).to eq(['Use `%Q` only for strings that contain ' \
                                   'both single quotes and double quotes, or ' \
                                   'for dynamic strings that contain double ' \
-                                  'quotes.'] * 3)
+                                  'quotes.'])
     end
 
     it 'registers an offense for static string with only double quotes' do
-      expect(cop.offenses.map(&:line)).to include(2)
+      inspect_source(cop, '%Q("hi")')
+
+      expect(cop.messages).to eq(['Use `%Q` only for strings that contain ' \
+                                  'both single quotes and double quotes, or ' \
+                                  'for dynamic strings that contain double ' \
+                                  'quotes.'])
     end
 
     it 'registers an offense for dynamic string without quotes' do
-      expect(cop.offenses.map(&:line)).to include(3)
+      inspect_source(cop, "%Q(hi\#{4})")
+
+      expect(cop.messages).to eq(['Use `%Q` only for strings that contain ' \
+                                  'both single quotes and double quotes, or ' \
+                                  'for dynamic strings that contain double ' \
+                                  'quotes.'])
     end
 
     it 'accepts a string with single quotes and double quotes' do
-      expect(cop.offenses.map(&:line)).not_to include(4)
+      inspect_source(cop, %q(%Q('"hi"')))
+
+      expect(cop.messages).to be_empty
     end
 
     it 'accepts a string with double quotes and tab character' do
-      expect(cop.offenses.map(&:line)).not_to include(5)
+      inspect_source(cop, '%Q("\\thi")')
+
+      expect(cop.messages).to be_empty
     end
 
     it 'accepts a dynamic %Q string with double quotes' do
-      expect(cop.offenses.map(&:line)).not_to include(6)
+      inspect_source(cop, '%Q("hi\#{4}")')
+
+      expect(cop.messages).to be_empty
     end
 
     it 'accepts regular expressions starting with %Q' do
-      expect(cop.offenses.map(&:line)).not_to include(7)
+      inspect_source(cop, '/%Q?/')
+
+      expect(cop.messages).to be_empty
     end
 
-    it 'auto-corrects' do
-      new_source = autocorrect_source(cop, source)
-      expect(new_source).to eq(corrected)
+    context 'auto-correct' do
+      it 'corrects a static string without quotes' do
+        new_source = autocorrect_source(cop, '%Q(hi)')
+
+        expect(new_source).to eq('"hi"')
+      end
+
+      it 'corrects a static string with only double quotes' do
+        new_source = autocorrect_source(cop, '%Q("hi")')
+
+        expect(new_source).to eq(%q('"hi"'))
+      end
+
+      it 'corrects a dynamic string without quotes' do
+        new_source = autocorrect_source(cop, "%Q(hi\#{4})")
+
+        expect(new_source).to eq(%Q("hi\#{4}"))
+      end
     end
   end
 
