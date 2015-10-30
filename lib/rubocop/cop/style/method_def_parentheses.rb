6c5d8fb3133cb025821fe0da007fbcc0606619f1
Alex Dowad <alexinbeijing@gmail.com>
2015-10-31 00:04:53 +0200

    [Fix #1729] Add require_no_parentheses_except_multiline style for Style/MethodDefParentheses

--- lib/rubocop/cop/style/method_def_parentheses.rb
+++ lib/rubocop/cop/style/method_def_parentheses.rb
@@ -10,7 +10,9 @@ module RuboCop
         include ConfigurableEnforcedStyle
 
         def on_method_def(node, _method_name, args, _body)
-          if style == :require_parentheses
+          if style == :require_parentheses ||
+             (style == :require_no_parentheses_except_multiline &&
+              args.multiline?)
             if arguments?(args) && !parentheses?(args)
               missing_parentheses(node, args)
             else
@@ -25,7 +27,11 @@ module RuboCop
 
         def autocorrect(node)
           lambda do |corrector|
-            if style == :require_parentheses
+            if node.args_type?
+              # offense is registered on args node when parentheses are unwanted
+              corrector.replace(node.loc.begin, ' ')
+              corrector.remove(node.loc.end)
+            else
               args_expr = args_node(node).loc.expression
               args_with_space = range_with_surrounding_space(args_expr, :left)
               just_space = Parser::Source::Range.new(args_expr.source_buffer,
@@ -33,9 +39,6 @@ module RuboCop
                                                      args_expr.begin_pos)
               corrector.replace(just_space, '(')
               corrector.insert_after(args_expr, ')')
-            elsif style == :require_no_parentheses
-              corrector.replace(node.loc.begin, ' ')
-              corrector.remove(node.loc.end)
             end
           end
         end
@@ -45,13 +48,13 @@ module RuboCop
         def missing_parentheses(node, args)
           add_offense(node, args.loc.expression,
                       'Use def with parentheses when there are parameters.') do
-            opposite_style_detected
+            unexpected_style_detected(:require_no_parentheses)
           end
         end
 
         def unwanted_parentheses(args)
           add_offense(args, :expression, 'Use def without parentheses.') do
-            opposite_style_detected
+            unexpected_style_detected(:require_parentheses)
           end
         end
 
--- spec/rubocop/cop/style/method_def_parentheses_spec.rb
+++ spec/rubocop/cop/style/method_def_parentheses_spec.rb
@@ -59,9 +59,9 @@ describe RuboCop::Cop::Style::MethodDefParentheses, :config do
     end
   end
 
-  context 'require_no_parentheses' do
-    let(:cop_config) { { 'EnforcedStyle' => 'require_no_parentheses' } }
-
+  shared_examples :no_parentheses do
+    # common to require_no_parentheses and
+    # require_no_parentheses_except_multiline
     it 'reports an offense for def with parameters with parens' do
       src = ['def func(a, b)',
              'end']
@@ -69,6 +69,13 @@ describe RuboCop::Cop::Style::MethodDefParentheses, :config do
       expect(cop.offenses.size).to eq(1)
       expect(cop.config_to_allow_offenses).to eq('EnforcedStyle' =>
                                                  'require_parentheses')
+    end
+
+    it 'accepts a def with parameters but no parens' do
+      src = ['def func a, b',
+             'end']
+      inspect_source(cop, src)
+      expect(cop.offenses).to be_empty
     end
 
     it 'reports an offense for opposite + correct' do
@@ -88,11 +95,25 @@ describe RuboCop::Cop::Style::MethodDefParentheses, :config do
       expect(cop.offenses.size).to eq(1)
     end
 
+    it 'accepts a class def with parameters with parens' do
+      src = ['def Test.func a, b',
+             'end']
+      inspect_source(cop, src)
+      expect(cop.offenses).to be_empty
+    end
+
     it 'reports an offense for def with no args and parens' do
       src = ['def func()',
              'end']
       inspect_source(cop, src)
       expect(cop.offenses.size).to eq(1)
+    end
+
+    it 'accepts def with no args and no parens' do
+      src = ['def func',
+             'end']
+      inspect_source(cop, src)
+      expect(cop.offenses).to be_empty
     end
 
     it 'auto-removes the parens' do
@@ -103,6 +124,43 @@ describe RuboCop::Cop::Style::MethodDefParentheses, :config do
     it 'auto-removes the parens for defs' do
       new_source = autocorrect_source(cop, 'def self.test(param); end')
       expect(new_source).to eq('def self.test param; end')
+    end
+  end
+
+  context 'require_no_parentheses' do
+    let(:cop_config) { { 'EnforcedStyle' => 'require_no_parentheses' } }
+
+    it_behaves_like :no_parentheses
+  end
+
+  context 'require_no_parentheses_except_multiline' do
+    let(:cop_config) do
+      { 'EnforcedStyle' => 'require_no_parentheses_except_multiline' }
+    end
+
+    context 'when args are all on a single line' do
+      it_behaves_like :no_parentheses
+    end
+
+    context 'when args span multiple lines' do
+      it 'reports an offense for correct + opposite' do
+        src = ['def func(a,',
+               '         b)',
+               'end',
+               'def func a,',
+               '         b',
+               'end']
+        inspect_source(cop, src)
+        expect(cop.offenses.size).to eq(1)
+        expect(cop.config_to_allow_offenses).to eq('Enabled' => false)
+      end
+
+      it 'auto-adds required parens to argument lists on multiple lines' do
+        new_source = autocorrect_source(cop, ['def test one,', 'two', 'end'])
+        expect(new_source).to eq(['def test(one,',
+                                  'two)',
+                                  'end'].join("\n"))
+      end
     end
   end
 end
