0fce62aaa631e7a7e06d23a6d8c20bc690171ca4
Alex Dowad <alexinbeijing@gmail.com>
2015-11-02 08:00:06 +0200

    Correct nesting (and inaccurate context description) in Style/PredicateName specs

--- spec/rubocop/cop/style/predicate_name_spec.rb
+++ spec/rubocop/cop/style/predicate_name_spec.rb
@@ -28,30 +28,30 @@ describe RuboCop::Cop::Style::PredicateName, :config do
                            'end'])
       expect(cop.offenses).to be_empty
     end
+  end
 
-    context 'with blacklisted prefixes' do
-      let(:cop_config) do
-        { 'NamePrefix' => %w(has_ is_), 'NamePrefixBlacklist' => [] }
-      end
+  context 'without blacklisted prefixes' do
+    let(:cop_config) do
+      { 'NamePrefix' => %w(has_ is_), 'NamePrefixBlacklist' => [] }
+    end
 
-      %w(has is).each do |prefix|
-        it 'registers an offense when method name starts with known prefix' do
-          inspect_source(cop, ["def #{prefix}_attr",
-                               '  # ...',
-                               'end'])
-          expect(cop.offenses.size).to eq(1)
-          expect(cop.messages)
-            .to eq(["Rename `#{prefix}_attr` to `#{prefix}_attr?`."])
-          expect(cop.highlights).to eq(["#{prefix}_attr"])
-        end
-      end
-
-      it 'accepts method name that starts with unknown prefix' do
-        inspect_source(cop, ['def have_attr',
+    %w(has is).each do |prefix|
+      it 'registers an offense when method name starts with known prefix' do
+        inspect_source(cop, ["def #{prefix}_attr",
                              '  # ...',
                              'end'])
-        expect(cop.offenses).to be_empty
+        expect(cop.offenses.size).to eq(1)
+        expect(cop.messages)
+          .to eq(["Rename `#{prefix}_attr` to `#{prefix}_attr?`."])
+        expect(cop.highlights).to eq(["#{prefix}_attr"])
       end
+    end
+
+    it 'accepts method name that starts with unknown prefix' do
+      inspect_source(cop, ['def have_attr',
+                           '  # ...',
+                           'end'])
+      expect(cop.offenses).to be_empty
     end
   end
 end
