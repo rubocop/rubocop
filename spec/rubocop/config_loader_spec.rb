27a35ba4d4c065beb960ddf3d8215fc71fee6af5
Jonas Arvidsson <jonas054@gmail.com>
2015-11-11 07:51:40 +0100

    Make inherit_from override inherit_gem
    The problem was discovered in #2411, but this change does not
    fix the problem reported there.
    
    Make configuration loading work as described in the README regarding
    precedence of `.rubocop.yml`, `inherit_from`, and `inherit_gem`.
    
    Remove `rubocop: config/default.yml` from the example in the
    README as it makes no sense. All configurations are based on the
    `default.yml` of the `rubocop` being run.

--- spec/rubocop/config_loader_spec.rb
+++ spec/rubocop/config_loader_spec.rb
@@ -282,23 +282,31 @@ describe RuboCop::ConfigLoader do
         create_file('somegemname/config/rubocop.yml',
                     ['Metrics/MethodLength:',
                      '  Enabled: false',
-                     '  Max: 200'
+                     '  Max: 200',
+                     '  CountComments: false'
+                    ])
+        create_file('local.yml',
+                    ['Metrics/MethodLength:',
+                     '  CountComments: true'
                     ])
         create_file(file_path,
                     ['inherit_gem:',
                      '  somegemname: config/rubocop.yml',
+                     '',
+                     'inherit_from: local.yml',
                      '',
                      'Metrics/MethodLength:',
                      '  Enabled: true'
                     ])
       end
 
-      it 'returns values from the gem config' do
+      it 'returns values from the gem config with local overrides' do
         mock_spec = Struct.new(:gem_dir).new('somegemname')
         expect(Gem::Specification).to receive(:find_by_name)
           .at_least(:once).with('somegemname').and_return(mock_spec)
 
         expected = { 'Enabled' => true,        # overridden in .rubocop.yml
+                     'CountComments' => true,  # overridden in local.yml
                      'Max' => 200 }            # inherited from somegem
         expect(configuration_from_file['Metrics/MethodLength'].to_set)
           .to be_superset(expected.to_set)
