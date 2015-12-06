0a6e99202273e9e7d5b531ad78506adfcafde7b1
Teemu <teemu@aikais.in>
2015-12-06 20:24:20 +0200

    [Fix #2449] Fix StabbyLambdaParentheses with lambda
    `Style/StabbyLambdaParentheses` does not check lambdas that use the
    `lambda` method instead of `->`.

--- spec/rubocop/cop/style/stabby_lambda_parentheses_spec.rb
+++ spec/rubocop/cop/style/stabby_lambda_parentheses_spec.rb
@@ -4,8 +4,27 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::StabbyLambdaParentheses, :config do
   subject(:cop) { described_class.new(config) }
 
+  shared_examples 'common' do
+    it 'does not check the old lambda syntax' do
+      inspect_source(cop, 'lambda(&:nil?)')
+      expect(cop.offenses).to be_empty
+    end
+
+    it 'does not check a stabby lambda without arguments' do
+      inspect_source(cop, '-> { true }')
+      expect(cop.offenses).to be_empty
+    end
+
+    it 'does not check a method call named lambda' do
+      inspect_source(cop, 'o.lambda')
+      expect(cop.offenses).to be_empty
+    end
+  end
+
   context 'require_parentheses' do
     let(:cop_config) { { 'EnforcedStyle' => 'require_parentheses' } }
+
+    it_behaves_like 'common'
 
     it 'registers an offense for a stabby lambda without parentheses' do
       inspect_source(cop, '->a,b,c { a + b + c }')
@@ -23,20 +42,12 @@ describe RuboCop::Cop::Style::StabbyLambdaParentheses, :config do
       corrected = autocorrect_source(cop, ['->a,b,c { a + b + c }'])
       expect(corrected).to eq '->(a,b,c) { a + b + c }'
     end
-
-    it 'does not check a stabby lambda without arguments' do
-      inspect_source(cop, '-> { true }')
-      expect(cop.offenses).to be_empty
-    end
-
-    it 'does not check a method call named lambda' do
-      inspect_source(cop, 'o.lambda')
-      expect(cop.offenses).to be_empty
-    end
   end
 
   context 'require_no_parentheses' do
     let(:cop_config) { { 'EnforcedStyle' => 'require_no_parentheses' } }
+
+    it_behaves_like 'common'
 
     it 'registers an offense for a stabby lambda with parentheses' do
       inspect_source(cop, '->(a,b,c) { a + b + c }')
@@ -48,11 +59,6 @@ describe RuboCop::Cop::Style::StabbyLambdaParentheses, :config do
     it 'autocorrects when a stabby lambda does not parentheses' do
       corrected = autocorrect_source(cop, ['->(a,b,c) { a + b + c }'])
       expect(corrected).to eq '->a,b,c { a + b + c }'
-    end
-
-    it 'does not check a stabby lambda without arguments' do
-      inspect_source(cop, '-> { true }')
-      expect(cop.offenses).to be_empty
     end
   end
 end
