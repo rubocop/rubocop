rubocop/rubocop

commit 5673c6f3ba42816bfbfe1f77b026c5ad39a13d90

ConfigurableEnforcedStyle doesn't write arrays into auto-genned config
ConfigurableEnforcedStyle recently received an upgrade which allows it to work
with 3 or more supported styles, and intelligently select the style which
matches EVERYTHING found in the code (if there is such a style).

When there is more than one style which could potentially be a match, it does
this by maintaining an array of all the candidates which match everything seen
so far.

However, when the end of all the files to be scanned is reached, there may still
be 2 or more candidates remaining. If so, make sure that only one of them is
written into .rubocop.yml. (The choice is arbitrary.)

lib/rubocop/cop/mixin/configurable_enforced_style.rb
@@ -48,23 +48,24 @@ module RuboCop
       end
 
       def no_acceptable_style!
+        @detected_style = []
         self.config_to_allow_offenses = { 'Enabled' => false }
       end
 
       def detected_style
-        config_to_allow_offenses[parameter_name]
+        @detected_style ||= nil
       end
 
       def detected_style=(style)
+        @detected_style = style
+
         if style.nil?
           no_acceptable_style!
         elsif style.is_a?(Array)
           if style.empty?
             no_acceptable_style!
-          elsif style.one?
-            config_to_allow_offenses[parameter_name] = style[0]
           else
-            config_to_allow_offenses[parameter_name] = style
+            config_to_allow_offenses[parameter_name] = style[0]
           end
         else
           config_to_allow_offenses[parameter_name] = style

spec/rubocop/cop/style/indent_array_spec.rb
@@ -272,8 +272,7 @@ describe RuboCop::Cop::Style::IndentArray do
                     'Indent the right bracket the same as the start of the ' \
                     'line where the left bracket is.'])
           expect(cop.config_to_allow_offenses)
-            .to eq('EnforcedStyle' => %w(special_inside_parentheses
-                                         align_brackets))
+            .to eq('EnforcedStyle' => 'special_inside_parentheses')
         end
 
         it 'accepts normal indentation for second argument' do

spec/rubocop/cop/style/indent_hash_spec.rb
@@ -334,8 +334,7 @@ describe RuboCop::Cop::Style::IndentHash do
                     'Indent the right brace the same as the start of the ' \
                     'line where the left brace is.'])
           expect(cop.config_to_allow_offenses)
-            .to eq('EnforcedStyle' => %w(special_inside_parentheses
-                                         align_braces))
+            .to eq('EnforcedStyle' => 'special_inside_parentheses')
         end
 
         it 'accepts normal indentation for second argument' do

commit 3ffd5069d5e489ba0801aa988cd2b7085696ce36

Generate --auto-gen-config properly when styles are spread across files
5673c6f broke auto-generation of config, in cases where some files use one style
and other files use another style.

After inspection of each file, RC would effectively "forget" which styles it
had already seen, so if the correct style was seen in one file and an incorrect
style in another file, it would not recognize the conflicting style and
configure the cop as `Enabled: false`.

lib/rubocop/cop/mixin/configurable_enforced_style.rb
@@ -48,16 +48,16 @@ module RuboCop
       end
 
       def no_acceptable_style!
-        @detected_style = []
         self.config_to_allow_offenses = { 'Enabled' => false }
+        Formatter::DisabledConfigFormatter.detected_styles[cop_name] = []
       end
 
       def detected_style
-        @detected_style ||= nil
+        Formatter::DisabledConfigFormatter.detected_styles[cop_name] ||= nil
       end
 
       def detected_style=(style)
-        @detected_style = style
+        Formatter::DisabledConfigFormatter.detected_styles[cop_name] = style
 
         if style.nil?
           no_acceptable_style!

lib/rubocop/formatter/disabled_config_formatter.rb
@@ -16,11 +16,12 @@ module RuboCop
         .join("\n")
 
       @config_to_allow_offenses = {}
+      @detected_styles = {}
 
       COPS = Cop::Cop.all.group_by(&:cop_name)
 
       class << self
-        attr_accessor :config_to_allow_offenses
+        attr_accessor :config_to_allow_offenses, :detected_styles
       end
 
       def file_started(_file, file_info)

spec/rubocop/cli/cli_auto_gen_config_spec.rb
@@ -475,5 +475,19 @@ describe RuboCop::CLI, :isolated_environment do
         end
       end
     end
+
+    it 'disables cop if different styles appear in different files' do
+      create_file('example1.rb', ['$!'])
+      create_file('example2.rb', ['$ERROR_INFO'])
+      expect(cli.run(['--auto-gen-config'])).to eq(1)
+      expect(IO.readlines('.rubocop_todo.yml')[8..-1].join)
+        .to eq(['# Offense count: 1',
+                '# Cop supports --auto-correct.',
+                '# Configuration parameters: EnforcedStyle, SupportedStyles.',
+                '# SupportedStyles: use_perl_names, use_english_names',
+                'Style/SpecialGlobalVars:',
+                '  Enabled: false',
+                ''].join("\n"))
+    end
   end
 end

spec/support/cop_helper.rb
@@ -12,6 +12,7 @@ module CopHelper
       fail "Don't use an array for a single line of code: #{source}"
     end
     RuboCop::Formatter::DisabledConfigFormatter.config_to_allow_offenses = {}
+    RuboCop::Formatter::DisabledConfigFormatter.detected_styles = {}
     processed_source = parse_source(source, file)
     fail 'Error parsing example code' unless processed_source.valid_syntax?
     _investigate(cop, processed_source)