e3385ea9c50aa5f6d4e324fe2d069211de77612c
Samuel Giddins <segiddins@segiddins.me>
2016-01-09 18:16:02 -0600

    [Fix #2616] Only warn when there is a receiver to the match call

--- lib/rubocop/cop/performance/redundant_match.rb
+++ lib/rubocop/cop/performance/redundant_match.rb
@@ -24,7 +24,7 @@ module RuboCop
         # a string or regexp literal on one side or the other
         def_node_matcher :match_call?, <<-END
           {(send {str regexp} :match _)
-           (send _ :match {str regexp})}
+           (send !nil :match {str regexp})}
         END
 
         def_node_matcher :only_truthiness_matters?, <<-END
