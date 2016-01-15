1f0bd2ef57d51b3a38e0b36e0a071860a886e90f
Steven De Coeyer <git@stevendecoeyer.be>
2016-01-15 14:16:47 +0100

    Fix typo in each_with_object.rb

--- lib/rubocop/cop/style/each_with_object.rb
+++ lib/rubocop/cop/style/each_with_object.rb
@@ -5,7 +5,7 @@ module RuboCop
   module Cop
     module Style
       # This cop looks for inject / reduce calls where the passed in object is
-      # returned at the end and so could be replace by each_with_object without
+      # returned at the end and so could be replaced by each_with_object without
       # the need to return the object at the end.
       #
       # However, we can't replace with each_with_object if the accumulator
