rubocop/rubocop

commit eeab10efca3b6f26a8bf9ba8621b8d3cc3d6cfd1

Include check for Hash#update in Performance/RedundantMerge cop
Previously, `Performance/RedundantMerge` did only check for `Hash#merge!`
and not the alias `Hash#update`.

Example of the new behaviour:

```
hash.update(foo: 'bar')
```

renders the suggestion:

```
hash[:foo] = 'bar'
```

config/enabled.yml
@@ -1238,7 +1238,7 @@ Performance/RedundantMatch:
   Enabled: true
 
 Performance/RedundantMerge:
-  Description: 'Use Hash#[]=, rather than Hash#merge! with a single key-value pair.'
+  Description: 'Use Hash#[]=, rather than Hash#merge! or Hash#update with a single key-value pair.'
   Reference: 'https://github.com/JuanitoFatas/fast-ruby#hashmerge-vs-hash-code'
   Enabled: true
 

lib/rubocop/cop/performance/redundant_merge.rb
@@ -15,7 +15,8 @@ module RuboCop
         AREF_ASGN = '%s[%s] = %s'.freeze
         MSG = 'Use `%s` instead of `%s`.'.freeze
 
-        def_node_matcher :redundant_merge, '(send $_ :merge! (hash $...))'
+        def_node_matcher :redundant_merge,
+                         '(send $_ {:merge! :update} (hash $...))'
         def_node_matcher :modifier_flow_control, '[{if while until} #modifier?]'
 
         def on_send(node)

spec/rubocop/cop/performance/redundant_merge_spec.rb
@@ -6,58 +6,63 @@ require 'spec_helper'
 describe RuboCop::Cop::Performance::RedundantMerge do
   subject(:cop) { described_class.new }
 
-  it 'autocorrects hash.merge!(a: 1)' do
-    new_source = autocorrect_source(cop, 'hash.merge!(a: 1)')
-    expect(new_source).to eq 'hash[:a] = 1'
-  end
-
-  it 'autocorrects hash.merge!("abc" => "value")' do
-    new_source = autocorrect_source(cop, 'hash.merge!("abc" => "value")')
-    expect(new_source).to eq 'hash["abc"] = "value"'
-  end
-
-  context 'when receiver is a local variable' do
-    it 'autocorrects hash.merge!(a: 1, b: 2)' do
-      new_source = autocorrect_source(cop, ['hash = {}',
-                                            'hash.merge!(a: 1, b: 2)'])
-      expect(new_source).to eq(['hash = {}',
-                                'hash[:a] = 1',
-                                'hash[:b] = 2'].join("\n"))
+  shared_examples 'redundant_merge' do |method|
+    it "autocorrects hash.#{method}(a: 1)" do
+      new_source = autocorrect_source(cop, "hash.#{method}(a: 1)")
+      expect(new_source).to eq 'hash[:a] = 1'
     end
-  end
 
-  context 'when receiver is a method call' do
-    it "doesn't autocorrect hash.merge!(a: 1, b: 2)" do
-      new_source = autocorrect_source(cop, 'hash.merge!(a: 1, b: 2)')
-      expect(new_source).to eq('hash.merge!(a: 1, b: 2)')
+    it "autocorrects hash.#{method}('abc' => 'value')" do
+      new_source = autocorrect_source(cop, "hash.#{method}('abc' => 'value')")
+      expect(new_source).to eq "hash['abc'] = 'value'"
     end
-  end
 
-  %w(if unless while until).each do |kw|
-    context "when there is a modifier #{kw}, and more than 1 pair" do
-      it "autocorrects it to an #{kw} block" do
-        new_source = autocorrect_source(
-          cop,
-          ['hash = {}',
-           "hash.merge!(a: 1, b: 2) #{kw} condition1 && condition2"])
+    context 'when receiver is a local variable' do
+      it "autocorrects hash.#{method}(a: 1, b: 2)" do
+        new_source = autocorrect_source(cop, ['hash = {}',
+                                              "hash.#{method}(a: 1, b: 2)"])
         expect(new_source).to eq(['hash = {}',
-                                  "#{kw} condition1 && condition2",
-                                  '  hash[:a] = 1',
-                                  '  hash[:b] = 2',
-                                  'end'].join("\n"))
+                                  'hash[:a] = 1',
+                                  'hash[:b] = 2'].join("\n"))
       end
     end
+
+    context 'when receiver is a method call' do
+      it "doesn't autocorrect hash.#{method}(a: 1, b: 2)" do
+        new_source = autocorrect_source(cop, "hash.#{method}(a: 1, b: 2)")
+        expect(new_source).to eq("hash.#{method}(a: 1, b: 2)")
+      end
+    end
+
+    %w(if unless while until).each do |kw|
+      context "when there is a modifier #{kw}, and more than 1 pair" do
+        it "autocorrects it to an #{kw} block" do
+          new_source = autocorrect_source(
+            cop,
+            ['hash = {}',
+             "hash.#{method}(a: 1, b: 2) #{kw} condition1 && condition2"])
+          expect(new_source).to eq(['hash = {}',
+                                    "#{kw} condition1 && condition2",
+                                    '  hash[:a] = 1',
+                                    '  hash[:b] = 2',
+                                    'end'].join("\n"))
+        end
+      end
+    end
+
+    it "doesn't register an error when return value is used" do
+      inspect_source(cop, ["variable = hash.#{method}(a: 1)",
+                           'puts variable'])
+      expect(cop.offenses).to be_empty
+    end
+
+    it "formats the error message correctly for hash.#{method}(a: 1)" do
+      inspect_source(cop, "hash.#{method}(a: 1)")
+      expect(cop.messages).to eq(
+        ["Use `hash[:a] = 1` instead of `hash.#{method}(a: 1)`."])
+    end
   end
 
-  it "doesn't register an error when return value is used" do
-    inspect_source(cop, ['variable = hash.merge!(a: 1)',
-                         'puts variable'])
-    expect(cop.offenses).to be_empty
-  end
-
-  it 'formats the error message correctly for hash.merge!(a: 1)' do
-    inspect_source(cop, 'hash.merge!(a: 1)')
-    expect(cop.messages).to eq(
-      ['Use `hash[:a] = 1` instead of `hash.merge!(a: 1)`.'])
-  end
+  it_behaves_like('redundant_merge', 'merge!')
+  it_behaves_like('redundant_merge', 'update')
 end

spec/rubocop/cop/rails/action_filter_spec.rb
@@ -49,7 +49,7 @@ describe RuboCop::Cop::Rails::ActionFilter, :config do
 
   context 'when style is action' do
     before do
-      cop_config.update('EnforcedStyle' => 'action')
+      cop_config['EnforcedStyle'] = 'action'
     end
 
     described_class::FILTER_METHODS.each do |method|
@@ -79,7 +79,7 @@ describe RuboCop::Cop::Rails::ActionFilter, :config do
 
   context 'when style is filter' do
     before do
-      cop_config.update('EnforcedStyle' => 'filter')
+      cop_config['EnforcedStyle'] = 'filter'
     end
 
     described_class::ACTION_METHODS.each do |method|

commit 108acca6c0b273eb6ab991db40f433334cc9ca65

[Fix #2781] Revert "Include check for Hash#update in Performance/RedundantMerge cop"
This reverts commit eeab10efca3b6f26a8bf9ba8621b8d3cc3d6cfd1, which broke
projects which use Active Record by converting `record.update(key: val)` to
`record[:key] = val`.

config/enabled.yml
@@ -1239,7 +1239,7 @@ Performance/RedundantMatch:
   Enabled: true
 
 Performance/RedundantMerge:
-  Description: 'Use Hash#[]=, rather than Hash#merge! or Hash#update with a single key-value pair.'
+  Description: 'Use Hash#[]=, rather than Hash#merge! with a single key-value pair.'
   Reference: 'https://github.com/JuanitoFatas/fast-ruby#hashmerge-vs-hash-code'
   Enabled: true
 

lib/rubocop/cop/performance/redundant_merge.rb
@@ -15,8 +15,7 @@ module RuboCop
         AREF_ASGN = '%s[%s] = %s'.freeze
         MSG = 'Use `%s` instead of `%s`.'.freeze
 
-        def_node_matcher :redundant_merge,
-                         '(send $_ {:merge! :update} (hash $...))'
+        def_node_matcher :redundant_merge, '(send $_ :merge! (hash $...))'
         def_node_matcher :modifier_flow_control, '[{if while until} #modifier?]'
 
         def on_send(node)

spec/rubocop/cop/performance/redundant_merge_spec.rb
@@ -10,75 +10,70 @@ describe RuboCop::Cop::Performance::RedundantMerge, :config do
     { 'MaxKeyValuePairs' => 2 }
   end
 
-  shared_examples 'redundant_merge' do |method|
-    it "autocorrects hash.#{method}(a: 1)" do
-      new_source = autocorrect_source(cop, "hash.#{method}(a: 1)")
-      expect(new_source).to eq 'hash[:a] = 1'
-    end
+  it 'autocorrects hash.merge!(a: 1)' do
+    new_source = autocorrect_source(cop, 'hash.merge!(a: 1)')
+    expect(new_source).to eq 'hash[:a] = 1'
+  end
 
-    it "autocorrects hash.#{method}('abc' => 'value')" do
-      new_source = autocorrect_source(cop, "hash.#{method}('abc' => 'value')")
-      expect(new_source).to eq "hash['abc'] = 'value'"
-    end
+  it 'autocorrects hash.merge!("abc" => "value")' do
+    new_source = autocorrect_source(cop, 'hash.merge!("abc" => "value")')
+    expect(new_source).to eq 'hash["abc"] = "value"'
+  end
 
-    context 'when receiver is a local variable' do
-      it "autocorrects hash.#{method}(a: 1, b: 2)" do
-        new_source = autocorrect_source(cop, ['hash = {}',
-                                              "hash.#{method}(a: 1, b: 2)"])
+  context 'when receiver is a local variable' do
+    it 'autocorrects hash.merge!(a: 1, b: 2)' do
+      new_source = autocorrect_source(cop, ['hash = {}',
+                                            'hash.merge!(a: 1, b: 2)'])
+      expect(new_source).to eq(['hash = {}',
+                                'hash[:a] = 1',
+                                'hash[:b] = 2'].join("\n"))
+    end
+  end
+
+  context 'when receiver is a method call' do
+    it "doesn't autocorrect hash.merge!(a: 1, b: 2)" do
+      new_source = autocorrect_source(cop, 'hash.merge!(a: 1, b: 2)')
+      expect(new_source).to eq('hash.merge!(a: 1, b: 2)')
+    end
+  end
+
+  %w(if unless while until).each do |kw|
+    context "when there is a modifier #{kw}, and more than 1 pair" do
+      it "autocorrects it to an #{kw} block" do
+        new_source = autocorrect_source(
+          cop,
+          ['hash = {}',
+           "hash.merge!(a: 1, b: 2) #{kw} condition1 && condition2"])
         expect(new_source).to eq(['hash = {}',
-                                  'hash[:a] = 1',
-                                  'hash[:b] = 2'].join("\n"))
-      end
-    end
-
-    context 'when receiver is a method call' do
-      it "doesn't autocorrect hash.#{method}(a: 1, b: 2)" do
-        new_source = autocorrect_source(cop, "hash.#{method}(a: 1, b: 2)")
-        expect(new_source).to eq("hash.#{method}(a: 1, b: 2)")
-      end
-    end
-
-    %w(if unless while until).each do |kw|
-      context "when there is a modifier #{kw}, and more than 1 pair" do
-        it "autocorrects it to an #{kw} block" do
-          new_source = autocorrect_source(
-            cop,
-            ['hash = {}',
-             "hash.#{method}(a: 1, b: 2) #{kw} condition1 && condition2"])
-          expect(new_source).to eq(['hash = {}',
-                                    "#{kw} condition1 && condition2",
-                                    '  hash[:a] = 1',
-                                    '  hash[:b] = 2',
-                                    'end'].join("\n"))
-        end
-      end
-    end
-
-    it "doesn't register an error when return value is used" do
-      inspect_source(cop, ["variable = hash.#{method}(a: 1)",
-                           'puts variable'])
-      expect(cop.offenses).to be_empty
-    end
-
-    it "formats the error message correctly for hash.#{method}(a: 1)" do
-      inspect_source(cop, "hash.#{method}(a: 1)")
-      expect(cop.messages).to eq(
-        ["Use `hash[:a] = 1` instead of `hash.#{method}(a: 1)`."])
-    end
-
-    context 'with MaxKeyValuePairs of 1' do
-      let(:cop_config) do
-        { 'MaxKeyValuePairs' => 1 }
-      end
-
-      it "doesn't register errors for multi-value hash merges" do
-        inspect_source(cop, ['hash = {}',
-                             "hash.#{method}(a: 1, b: 2)"])
-        expect(cop.offenses).to be_empty
+                                  "#{kw} condition1 && condition2",
+                                  '  hash[:a] = 1',
+                                  '  hash[:b] = 2',
+                                  'end'].join("\n"))
       end
     end
   end
 
-  it_behaves_like('redundant_merge', 'merge!')
-  it_behaves_like('redundant_merge', 'update')
+  it "doesn't register an error when return value is used" do
+    inspect_source(cop, ['variable = hash.merge!(a: 1)',
+                         'puts variable'])
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'formats the error message correctly for hash.merge!(a: 1)' do
+    inspect_source(cop, 'hash.merge!(a: 1)')
+    expect(cop.messages).to eq(
+      ['Use `hash[:a] = 1` instead of `hash.merge!(a: 1)`.'])
+  end
+
+  context 'with MaxKeyValuePairs of 1' do
+    let(:cop_config) do
+      { 'MaxKeyValuePairs' => 1 }
+    end
+
+    it "doesn't register errors for multi-value hash merges" do
+      inspect_source(cop, ['hash = {}',
+                           'hash.merge!(a: 1, b: 2)'])
+      expect(cop.offenses).to be_empty
+    end
+  end
 end