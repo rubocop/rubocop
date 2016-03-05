fe91582a527757ce8ccf4a40fd491d0a18b756c7
Alex Dowad <alexinbeijing@gmail.com>
2016-03-05 06:15:53 +0200

    De-duplicate ZeroLengthPredicate specs using RSpec shared examples

--- spec/rubocop/cop/style/zero_length_predicate_spec.rb
+++ spec/rubocop/cop/style/zero_length_predicate_spec.rb
@@ -10,167 +10,42 @@ describe RuboCop::Cop::Style::ZeroLengthPredicate do
     inspect_source(cop, source)
   end
 
-  context 'when checking for 0 length' do
-    context 'when receiver is 0' do
-      context 'when sender is a length' do
-        let(:source) { '[1, 2, 3].length == 0' }
+  shared_examples 'registers offense' do |code, message|
+    context "when checking #{code}" do
+      let(:source) { code }
 
-        it 'registers an offense' do
-          expect(cop.offenses.size).to eq(1)
-          expect(cop.offenses.first.message).to eq(
-            'Use `empty?` instead of `length == 0`.'
-          )
-          expect(cop.highlights).to eq(['[1, 2, 3].length == 0'])
-        end
-      end
-
-      context 'when sender is a size' do
-        let(:source) { '[1, 2, 3].size == 0' }
-
-        it 'registers an offense' do
-          expect(cop.offenses.size).to eq(1)
-          expect(cop.offenses.first.message).to eq(
-            'Use `empty?` instead of `size == 0`.'
-          )
-          expect(cop.highlights).to eq(['[1, 2, 3].size == 0'])
-        end
-      end
-    end
-
-    context 'when sender is 0' do
-      context 'when receiver is a length' do
-        let(:source) { '0 == [1, 2, 3].length' }
-
-        it 'registers an offense' do
-          expect(cop.offenses.size).to eq(1)
-          expect(cop.offenses.first.message).to eq(
-            'Use `empty?` instead of `0 == length`.'
-          )
-          expect(cop.highlights).to eq(['0 == [1, 2, 3].length'])
-        end
-      end
-
-      context 'when receiver is a size' do
-        let(:source) { '0 == [1, 2, 3].size' }
-
-        it 'registers an offense' do
-          expect(cop.offenses.size).to eq(1)
-          expect(cop.offenses.first.message).to eq(
-            'Use `empty?` instead of `0 == size`.'
-          )
-          expect(cop.highlights).to eq(['0 == [1, 2, 3].size'])
-        end
+      it 'registers an offense' do
+        expect(cop.offenses.size).to eq(1)
+        expect(cop.offenses.first.message).to eq(message)
+        expect(cop.highlights).to eq([code])
       end
     end
   end
 
-  context 'when checking for non-0 length' do
-    context 'when receiver is 0' do
-      context 'when sender is a length' do
-        context 'when comparing using >' do
-          let(:source) { '[1, 2, 3].length > 0' }
+  it_behaves_like 'registers offense', '[1, 2, 3].length == 0',
+                  'Use `empty?` instead of `length == 0`.'
+  it_behaves_like 'registers offense', '[1, 2, 3].size == 0',
+                  'Use `empty?` instead of `size == 0`.'
+  it_behaves_like 'registers offense', '0 == [1, 2, 3].length',
+                  'Use `empty?` instead of `0 == length`.'
+  it_behaves_like 'registers offense', '0 == [1, 2, 3].size',
+                  'Use `empty?` instead of `0 == size`.'
 
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `length > 0`.'
-            )
-            expect(cop.highlights).to eq(['[1, 2, 3].length > 0'])
-          end
-        end
+  it_behaves_like 'registers offense', '[1, 2, 3].length > 0',
+                  'Use `!empty?` instead of `length > 0`.'
+  it_behaves_like 'registers offense', '[1, 2, 3].size > 0',
+                  'Use `!empty?` instead of `size > 0`.'
+  it_behaves_like 'registers offense', '[1, 2, 3].length != 0',
+                  'Use `!empty?` instead of `length != 0`.'
+  it_behaves_like 'registers offense', '[1, 2, 3].size != 0',
+                  'Use `!empty?` instead of `size != 0`.'
 
-        context 'when comparing using !=' do
-          let(:source) { '[1, 2, 3].length != 0' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `length != 0`.'
-            )
-            expect(cop.highlights).to eq(['[1, 2, 3].length != 0'])
-          end
-        end
-      end
-
-      context 'when sender is a size' do
-        context 'when comparing using >' do
-          let(:source) { '[1, 2, 3].size > 0' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `size > 0`.'
-            )
-            expect(cop.highlights).to eq(['[1, 2, 3].size > 0'])
-          end
-        end
-
-        context 'when comparing using !=' do
-          let(:source) { '[1, 2, 3].size != 0' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `size != 0`.'
-            )
-            expect(cop.highlights).to eq(['[1, 2, 3].size != 0'])
-          end
-        end
-      end
-    end
-
-    context 'when sender is 0' do
-      context 'when receiver is a length' do
-        context 'when comparing using <' do
-          let(:source) { ' 0 < [1, 2, 3].length' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `0 < length`.'
-            )
-            expect(cop.highlights).to eq(['0 < [1, 2, 3].length'])
-          end
-        end
-
-        context 'when comparing using !=' do
-          let(:source) { '0 != [1, 2, 3].length' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `0 != length`.'
-            )
-            expect(cop.highlights).to eq(['0 != [1, 2, 3].length'])
-          end
-        end
-      end
-
-      context 'when receiver is a size' do
-        context 'when comparing using <' do
-          let(:source) { '0 < [1, 2, 3].size' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `0 < size`.'
-            )
-            expect(cop.highlights).to eq(['0 < [1, 2, 3].size'])
-          end
-        end
-
-        context 'when comparing using !=' do
-          let(:source) { '0 != [1, 2, 3].size' }
-
-          it 'registers an offense' do
-            expect(cop.offenses.size).to eq(1)
-            expect(cop.offenses.first.message).to eq(
-              'Use `!empty?` instead of `0 != size`.'
-            )
-            expect(cop.highlights).to eq(['0 != [1, 2, 3].size'])
-          end
-        end
-      end
-    end
-  end
+  it_behaves_like 'registers offense', '0 < [1, 2, 3].length',
+                  'Use `!empty?` instead of `0 < length`.'
+  it_behaves_like 'registers offense', '0 < [1, 2, 3].size',
+                  'Use `!empty?` instead of `0 < size`.'
+  it_behaves_like 'registers offense', '0 != [1, 2, 3].length',
+                  'Use `!empty?` instead of `0 != length`.'
+  it_behaves_like 'registers offense', '0 != [1, 2, 3].size',
+                  'Use `!empty?` instead of `0 != size`.'
 end
