57f41b702637f59d9f99be20a3ecc3a848a94fd2
Andy Waite <andyw8@users.noreply.github.com>
2016-05-02 13:58:04 +0100

    Fix grammar in comment for MethodLength cop

--- lib/rubocop/cop/metrics/method_length.rb
+++ lib/rubocop/cop/metrics/method_length.rb
@@ -4,7 +4,7 @@
 module RuboCop
   module Cop
     module Metrics
-      # This cop checks if the length a method exceeds some maximum value.
+      # This cop checks if the length of a method exceeds some maximum value.
       # Comment lines can optionally be ignored.
       # The maximum allowed length is configurable.
       class MethodLength < Cop
