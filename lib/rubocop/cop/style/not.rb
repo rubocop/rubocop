d490d8216f9aa7c31cb9ab8efde33d335647a9a9
Teemu <teemu@aikais.in>
2016-05-08 21:13:30 +0300

    Reduce complexity in Not
    Split `autocorrect` into smaller methods.

--- lib/rubocop/cop/style/not.rb
+++ lib/rubocop/cop/style/not.rb
@@ -31,21 +31,41 @@ module RuboCop
           range = range_with_surrounding_space(node.loc.selector, :right)
           child = node.children.first
 
-          if child.send_type? && OPPOSITE_METHODS.key?(child.method_name)
-            lambda do |corrector|
-              corrector.remove(range)
-              corrector.replace(child.loc.selector,
-                                OPPOSITE_METHODS[child.method_name].to_s)
-            end
-          elsif child.and_type? || child.or_type? || child.binary_operation? ||
-                ternary?(child)
-            lambda do |corrector|
-              corrector.replace(range, '!(')
-              corrector.insert_after(node.source_range, ')')
-            end
+          if opposite_method?(child)
+            correct_opposite_method(range, child)
+          elsif requires_parens?(child)
+            correct_with_parens(range, node)
           else
-            ->(corrector) { corrector.replace(range, '!') }
+            correct_without_parens(range)
           end
+        end
+
+        def opposite_method?(child)
+          child.send_type? && OPPOSITE_METHODS.key?(child.method_name)
+        end
+
+        def requires_parens?(child)
+          child.and_type? || child.or_type? || child.binary_operation? ||
+            ternary?(child)
+        end
+
+        def correct_opposite_method(range, child)
+          lambda do |corrector|
+            corrector.remove(range)
+            corrector.replace(child.loc.selector,
+                              OPPOSITE_METHODS[child.method_name].to_s)
+          end
+        end
+
+        def correct_with_parens(range, node)
+          lambda do |corrector|
+            corrector.replace(range, '!(')
+            corrector.insert_after(node.source_range, ')')
+          end
+        end
+
+        def correct_without_parens(range)
+          ->(corrector) { corrector.replace(range, '!') }
         end
       end
     end
