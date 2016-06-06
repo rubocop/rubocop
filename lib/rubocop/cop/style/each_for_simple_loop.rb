2fa34d8b25228f97a0fd9d51c96716ab92931f8a
Tejas Bubane <tejasbubane@gmail.com>
2016-06-06 19:21:50 +0530

    Add autocorrect to `Style/EachForSimpleLoop` cop

--- lib/rubocop/cop/style/each_for_simple_loop.rb
+++ lib/rubocop/cop/style/each_for_simple_loop.rb
@@ -17,18 +17,25 @@ module RuboCop
       #   @good
       #   10.times { }
       class EachForSimpleLoop < Cop
-        def_node_matcher :bad_each_loop, <<-PATTERN
-          (block (send (begin ({irange erange} int int)) :each) (args) ...)
-        PATTERN
-
         def on_block(node)
-          if bad_each_loop(node)
+          if bad_each_count(node)
             send_node, = *node
             range = send_node.receiver.source_range.join(send_node.loc.selector)
             add_offense(node, range, 'Use `Integer#times` for a simple loop ' \
                                      'which iterates a fixed number of times.')
           end
         end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.replace(node.children.first.source_range,
+                              "#{bad_each_count(node)}.times")
+          end
+        end
+
+        def_node_matcher :bad_each_count, <<-PATTERN
+          (block (send (begin ({irange erange} int (int $_))) :each) (args) ...)
+        PATTERN
       end
     end
   end
--- spec/rubocop/cop/style/each_for_simple_loop_spec.rb
+++ spec/rubocop/cop/style/each_for_simple_loop_spec.rb
@@ -6,34 +6,65 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::EachForSimpleLoop do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense for (0..10).each {}' do
+  OFFENSE_MSG = 'Use `Integer#times` for a simple loop ' \
+                'which iterates a fixed number of times.'.freeze
+
+  it 'registers offense for exclusive end range' do
     inspect_source(cop, '(0..10).each {}')
     expect(cop.offenses.size).to eq 1
-    expect(cop.messages).to eq(['Use `Integer#times` for a simple loop which ' \
-                                'iterates a fixed number of times.'])
+    expect(cop.messages).to eq([OFFENSE_MSG])
     expect(cop.highlights).to eq(['(0..10).each'])
   end
 
-  it 'registers an offense for (0...10).each {}' do
+  it 'registers offense for inclusive end range' do
     inspect_source(cop, '(0...10).each {}')
     expect(cop.offenses.size).to eq 1
-    expect(cop.messages).to eq(['Use `Integer#times` for a simple loop which ' \
-                                'iterates a fixed number of times.'])
+    expect(cop.messages).to eq([OFFENSE_MSG])
     expect(cop.highlights).to eq(['(0...10).each'])
   end
 
-  it "doesn't register an offense if range startpoint is not constant" do
+  it 'registers offense for inclusive end range' do
+    inspect_source(cop, ['(0...10).each do',
+                         'end'])
+    expect(cop.offenses.size).to eq 1
+    expect(cop.messages).to eq([OFFENSE_MSG])
+    expect(cop.highlights).to eq(['(0...10).each'])
+  end
+
+  it 'does not register offense if range startpoint is not constant' do
     inspect_source(cop, '(a..10).each {}')
     expect(cop.offenses).to be_empty
   end
 
-  it "doesn't register an offense if range endpoint is not constant" do
+  it 'does not register offense if range startpoint is not constant' do
     inspect_source(cop, '(0..b).each {}')
     expect(cop.offenses).to be_empty
   end
 
-  it "doesn't register an offense if block takes parameters" do
+  it 'does not register offense for inline block with parameters' do
     inspect_source(cop, '(0..10).each { |n| puts n }')
     expect(cop.offenses).to be_empty
+  end
+
+  it 'does not register offense for multiline block with parameters' do
+    inspect_source(cop, ['(0..10).each do |n|',
+                         'end'])
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'does not register offense for character range' do
+    inspect_source(cop, "('a'..'b').each {}")
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'autocorrects the source with inline block' do
+    corrected = autocorrect_source(cop, '(0..10).each {}')
+    expect(corrected).to eq '10.times {}'
+  end
+
+  it 'autocorrects the source with multiline block' do
+    corrected = autocorrect_source(cop, ['(0..10).each do',
+                                         'end'])
+    expect(corrected).to eq "10.times do\nend"
   end
 end
