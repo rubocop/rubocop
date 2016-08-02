5e8b50d2ee22530338741ac2a495aa17d61430bc
Teemu <teemu@aikais.in>
2016-08-02 09:49:55 +0300

    Reduce complexity in AlignHash
    Split `autocorrect` into smaller methods.

--- lib/rubocop/cop/style/align_hash.rb
+++ lib/rubocop/cop/style/align_hash.rb
@@ -207,23 +207,36 @@ module RuboCop
         def autocorrect(node)
           # We can't use the instance variable inside the lambda. That would
           # just give each lambda the same reference and they would all get the
+          # last value of each. A local variable fixes the problem.
+          key_delta = @column_deltas[:key] || 0
+          key, value = *node
+
+          if value.nil?
+            correct_no_value(key_delta, node.source_range)
+          else
+            correct_key_value(key_delta, key.source_range, value.source_range,
+                              node.loc.operator)
+          end
+        end
+
+        def correct_no_value(key_delta, key)
+          ->(corrector) { adjust(corrector, key_delta, key) }
+        end
+
+        def correct_key_value(key_delta, key, value, separator)
+          # We can't use the instance variable inside the lambda. That would
+          # just give each lambda the same reference and they would all get the
           # last value of each. Some local variables fix the problem.
-          key_delta       = @column_deltas[:key] || 0
           separator_delta = @column_deltas[:separator] || 0
           value_delta     = @column_deltas[:value] || 0
 
-          key, value = *node
-          key_column = key.source_range.column
+          key_column = key.column
           key_delta = -key_column if key_delta < -key_column
 
           lambda do |corrector|
-            if value.nil?
-              adjust(corrector, key_delta, node.source_range)
-            else
-              adjust(corrector, key_delta, key.source_range)
-              adjust(corrector, separator_delta, node.loc.operator)
-              adjust(corrector, value_delta, value.source_range)
-            end
+            adjust(corrector, key_delta, key)
+            adjust(corrector, separator_delta, separator)
+            adjust(corrector, value_delta, value)
           end
         end
 
