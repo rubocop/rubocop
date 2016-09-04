a168ab53babc1c35cb3b089316f45668f1813038
Teemu <teemu@aikais.in>
2016-09-04 09:16:04 +0300

    Reduce complexity in OptionalArguments
    Split `on_def` into smaller methods.

--- lib/rubocop/cop/style/optional_arguments.rb
+++ lib/rubocop/cop/style/optional_arguments.rb
@@ -25,6 +25,28 @@ module RuboCop
         def on_def(node)
           _method, arguments, = *node
           arguments = *arguments
+
+          each_misplaced_optional_arg(arguments) do |argument|
+            arg, = *argument
+            add_offense(argument, :expression, format(MSG, arg))
+          end
+        end
+
+        private
+
+        def each_misplaced_optional_arg(arguments)
+          optarg_positions, arg_positions = argument_positions(arguments)
+          return if optarg_positions.empty? || arg_positions.empty?
+
+          optarg_positions.each do |optarg_position|
+            # there can only be one group of optional arguments
+            break if optarg_position > arg_positions.max
+
+            yield arguments[optarg_position]
+          end
+        end
+
+        def argument_positions(arguments)
           optarg_positions = []
           arg_positions = []
 
@@ -33,16 +55,7 @@ module RuboCop
             arg_positions << index if argument.arg_type?
           end
 
-          return if optarg_positions.empty? || arg_positions.empty?
-
-          optarg_positions.each do |optarg_position|
-            # there can only be one group of optional arguments
-            break if optarg_position > arg_positions.max
-            argument = arguments[optarg_position]
-            arg, = *argument
-
-            add_offense(argument, :expression, format(MSG, arg))
-          end
+          [optarg_positions, arg_positions]
         end
       end
     end
