e758256686a32db9bc0147309c7fdd772965c04a
Tejas Bubane <tejasbubane@gmail.com>
2016-09-12 01:46:43 +0530

    [Fix #3485] Make OneLineConditional cop ignore empty else
    Also refactor specs for the cop

--- spec/rubocop/cop/style/one_line_conditional_spec.rb
+++ spec/rubocop/cop/style/one_line_conditional_spec.rb
@@ -6,44 +6,65 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::OneLineConditional do
   subject(:cop) { described_class.new }
 
-  context 'one line if/then/else/end' do
-    let(:source) { 'if cond then run else dont end' }
-
+  shared_examples 'offense' do |condition|
     it 'registers an offense' do
       inspect_source(cop, source)
-      expect(cop.messages).to eq(['Favor the ternary operator (`?:`)' \
-                                  ' over `if/then/else/end` constructs.'])
-    end
-
-    it 'auto-corrects' do
-      corrected = autocorrect_source(cop, source)
-      expect(corrected).to eq('cond ? run : dont')
+      expect(cop.messages)
+        .to eq(['Favor the ternary operator (`?:`)' \
+                " over `#{condition}/then/else/end` constructs."])
     end
   end
 
-  it 'does not register an offense for if/then/end' do
-    inspect_source(cop, 'if cond then run end')
-    expect(cop.messages).to be_empty
+  shared_examples 'no offense' do
+    it 'does not register an offense' do
+      inspect_source(cop, source)
+      expect(cop.messages).to be_empty
+    end
+  end
+
+  shared_examples 'autocorrect' do |correct_code|
+    it 'auto-corrects' do
+      corrected = autocorrect_source(cop, source)
+      expect(corrected).to eq(correct_code)
+    end
+  end
+
+  context 'one line if/then/else/end' do
+    let(:source) { 'if cond then run else dont end' }
+
+    include_examples 'offense', 'if'
+    include_examples 'autocorrect', 'cond ? run : dont'
+
+    context 'empty else' do
+      let(:source) { 'if cond then run else end' }
+
+      include_examples 'no offense'
+    end
+  end
+
+  context 'one line if/then/end' do
+    let(:source) { 'if cond then run end' }
+
+    include_examples 'no offense'
   end
 
   context 'one line unless/then/else/end' do
     let(:source) { 'unless cond then run else dont end' }
 
-    it 'does register an offense for ' do
-      inspect_source(cop, source)
-      expect(cop.messages).to eq(['Favor the ternary operator (`?:`)' \
-                                  ' over `unless/then/else/end` constructs.'])
-    end
+    include_examples 'offense', 'unless'
+    include_examples 'autocorrect', 'cond ? dont : run'
 
-    it 'auto-corrects' do
-      corrected = autocorrect_source(cop, source)
-      expect(corrected).to eq('cond ? dont : run')
+    context 'empty else' do
+      let(:source) { 'unless cond then run else end' }
+
+      include_examples 'no offense'
     end
   end
 
-  it 'does not register an offense for one line unless/then/end' do
-    inspect_source(cop, 'unless cond then run end')
-    expect(cop.messages).to be_empty
+  context 'one line unless/then/end' do
+    let(:source) { 'unless cond then run end' }
+
+    include_examples 'no offense'
   end
 
   %w(| ^ & <=> == === =~ > >= < <= << >> + - * / % ** ~ ! != !~
