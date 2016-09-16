b9da390bfe0955ad89be9cb8beee3c0ce3090ddd
Ted Johansson <drenmi@gmail.com>
2016-09-16 22:26:34 +0800

    [Fix #3460] Fix false positives in `Style/InlineComment` cop
    This cop's check was based on a misunderstanding of how
    `Parser::Source::Comment#inline?` works, and was missing a test
    case for the negative case.
    
    This change fixes the cop and adds a test for the future.

--- lib/rubocop/cop/style/inline_comment.rb
+++ lib/rubocop/cop/style/inline_comment.rb
@@ -4,13 +4,26 @@
 module RuboCop
   module Cop
     module Style
-      # This cop checks for inline comments.
+      # This cop checks for trailing inline comments.
+      #
+      # @example
+      #
+      #   # good
+      #   foo.each do |f|
+      #     # Standalone comment
+      #     f.bar
+      #   end
+      #
+      #   # bad
+      #   foo.each do |f|
+      #     f.bar # Trailing inline comment
+      #   end
       class InlineComment < Cop
-        MSG = 'Avoid inline comments.'.freeze
+        MSG = 'Avoid trailing inline comments.'.freeze
 
         def investigate(processed_source)
           processed_source.comments.each do |comment|
-            next unless comment.inline?
+            next if comment_line?(processed_source[comment.loc.line - 1])
             add_offense(comment, :expression)
           end
         end
--- spec/rubocop/cop/style/inline_comment_spec.rb
+++ spec/rubocop/cop/style/inline_comment_spec.rb
@@ -6,10 +6,17 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::InlineComment do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense for a inline comment' do
-    inspect_source(cop, 'two = 1 + 1 # An inline comment')
+  it 'registers an offense for a trailing inline comment' do
+    inspect_source(cop, 'two = 1 + 1 # A trailing inline comment')
 
-    expect(cop.messages).to eq(['Avoid inline comments.'])
-    expect(cop.highlights).to eq(['# An inline comment'])
+    expect(cop.offenses.size).to eq(1)
+    expect(cop.messages).to eq(['Avoid trailing inline comments.'])
+    expect(cop.highlights).to eq(['# A trailing inline comment'])
+  end
+
+  it 'does not register an offense for a standalone comment' do
+    inspect_source(cop, '# A standalone comment')
+
+    expect(cop.offenses).to be_empty
   end
 end
