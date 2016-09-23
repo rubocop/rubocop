rubocop/rubocop

commit b0d8e0cf76d0ae9943a572e4bcb45bfc1adcf5d3

Refactor `Lint/AssignmentInCondition` cop
This change refactors `Lint/AssignmentInCondition` by extracting:

 - `#allowed_construct?`
 - `#conditional_assignment?`
 - `#skip_children?`

lib/rubocop/cop/lint/assignment_in_condition.rb
@@ -29,31 +29,34 @@ module RuboCop
         def check(node)
           condition, = *node
 
-          # assignments inside blocks are not what we're looking for
           return if condition.block_type?
+
           traverse_node(condition, ASGN_TYPES) do |asgn_node|
-            if asgn_node.send_type?
-              _receiver, method_name, *_args = *asgn_node
-              next :skip_children if method_name !~ /=\z/
-            end
+            next :skip_children if skip_children?(asgn_node)
+            next if allowed_construct?(asgn_node)
 
-            # skip safe assignment nodes if safe assignment is allowed
-            if safe_assignment_allowed? && safe_assignment?(asgn_node)
-              next :skip_children
-            end
-
-            # assignment nodes from shorthand ops like ||= don't have operator
-            if !asgn_node.begin_type? && asgn_node.loc.operator
-              add_offense(asgn_node, :operator)
-            end
+            add_offense(asgn_node, :operator)
           end
+        end
+
+        def allowed_construct?(asgn_node)
+          asgn_node.begin_type? || conditional_assignment?(asgn_node)
+        end
+
+        def conditional_assignment?(asgn_node)
+          !asgn_node.loc.operator
+        end
+
+        def skip_children?(asgn_node)
+          safe_assignment_allowed? && safe_assignment?(asgn_node)
         end
 
         # each_node/visit_descendants_with_types with :skip_children
         def traverse_node(node, types, &block)
           result = yield node if types.include?(node.type)
-          # return to skip all descendant nodes
+
           return if result == :skip_children
+
           node.each_child_node { |child| traverse_node(child, types, &block) }
         end
       end

commit ab35f329e5f17efa620f0c322a363999280c03fe

Fix false positive in `Lint/AssignmentInCondition` (#3520)
This was broken in b0d8e0cf which was simply a refactoring commit. It looks like part of the code was omitted accidentally when porting to the new block. Specifically, the part which was dropped was: https://github.com/bbatsov/rubocop/blob/78de463f33992bce5b1ecf40e311958ad773bad5/lib/rubocop/cop/lint/assignment_in_condition.rb#L35-L38

I've ported the missing checks to the `#skip_children?` method, and added a test which would've caught the regression.

lib/rubocop/cop/lint/assignment_in_condition.rb
@@ -48,7 +48,8 @@ module RuboCop
         end
 
         def skip_children?(asgn_node)
-          safe_assignment_allowed? && safe_assignment?(asgn_node)
+          (asgn_node.send_type? && asgn_node.method_name !~ /=\z/) ||
+            (safe_assignment_allowed? && safe_assignment?(asgn_node))
         end
 
         # each_node/visit_descendants_with_types with :skip_children

spec/rubocop/cop/lint/assignment_in_condition_spec.rb
@@ -83,6 +83,12 @@ describe RuboCop::Cop::Lint::AssignmentInCondition, :config do
     expect(cop.offenses).to be_empty
   end
 
+  it 'accepts = in a block followed by method call' do
+    inspect_source(cop,
+                   'return 1 if any_errors? { o = file }.present?')
+    expect(cop.offenses).to be_empty
+  end
+
   it 'accepts ||= in condition' do
     inspect_source(cop,
                    'raise StandardError unless foo ||= bar')