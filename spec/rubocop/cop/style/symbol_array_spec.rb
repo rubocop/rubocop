5414239380217f34a1d2db0fdd737a8286156257
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-28 21:29:50 -0300

    A few grammar fixes

--- spec/rubocop/cop/style/symbol_array_spec.rb
+++ spec/rubocop/cop/style/symbol_array_spec.rb
@@ -30,22 +30,22 @@ describe RuboCop::Cop::Style::SymbolArray, :config do
       expect(new_source).to eq('%i(one \\) three)')
     end
 
-    it 'does not reg an offense for array with non-syms' do
+    it 'does not register an offense for array with non-syms' do
       inspect_source(cop, '[:one, :two, "three"]')
       expect(cop.offenses).to be_empty
     end
 
-    it 'does not reg an offense for array starting with %i' do
+    it 'does not register an offense for array starting with %i' do
       inspect_source(cop, '%i(one two three)')
       expect(cop.offenses).to be_empty
     end
 
-    it 'does not reg an offense for array with one element' do
+    it 'does not register an offense for array with one element' do
       inspect_source(cop, '[:three]')
       expect(cop.offenses).to be_empty
     end
 
-    it 'does not reg an offense if symbol contains whitespace' do
+    it 'does not register an offense if symbol contains whitespace' do
       inspect_source(cop, '[:one, :two, :"space here"]')
       expect(cop.offenses).to be_empty
     end
@@ -54,7 +54,7 @@ describe RuboCop::Cop::Style::SymbolArray, :config do
   context 'when EnforcedStyle is array' do
     let(:cop_config) { { 'EnforcedStyle' => 'brackets' } }
 
-    it 'does not registers an offense for arrays of symbols' do
+    it 'does not register an offense for arrays of symbols' do
       inspect_source(cop, '[:one, :two, :three]')
       expect(cop.offenses).to be_empty
     end
