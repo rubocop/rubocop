4bbfc78b2332a42f9990922af491d709f292eb9f
Corey Osman <corey@logicminds.biz>
2016-10-06 22:58:40 -0700

    Add the ability to inspect Rails 5 test process method (#3370) (#3365)
    Rails 5 introduces the ability to pass keyword arguments
    to the http method calls for test classes.
    Any Rails 5 upgrade would likely see
    hundreds if not thousands of messages like:
    
         DEPRECATION WARNING: ActionDispatch::IntegrationTest HTTP request
         methods will accept
         only the following keyword arguments in future Rails versions:
         params, headers, env.
    
    This commit adds a new Rails/HttpPositionalArguments cop that detects
     and fixes those deprecated methods in tests by replacing
     with keyword args like params and headers.

---
+++ lib/rubocop/cop/rails/http_positional_arguments.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Rails
+      # This cop is used to identify usages of `http methods
+      # like get,post,put,path` without the usage of keyword arguments
+      # in your tests and change them to use keyword arguments.
+      #
+      # @example
+      #   # bad
+      #   get :new, { user_id: 1}
+      #
+      #   # good
+      #   get :new, params: { user_id: 1 }
+      class HttpPositionalArguments < Cop
+        MSG = 'Use `keyword arguments` instead of ' \
+              'positional arguments for http call: %s.'.freeze
+        KEYWORD_ARGS = [:headers, :env, :params, :body, :flash, :as].freeze
+        HTTP_METHODS = [:get, :post, :put, :patch, :delete, :head].freeze
+
+        def on_send(node)
+          receiver, http_method, http_path, data = *node
+          # if the first word on the line is not an http method, then skip
+          return unless HTTP_METHODS.include?(http_method)
+          # if the data is nil then we don't need to add keyword arguments
+          # because there is no data to put in params or headers, so skip
+          return if data.nil?
+          return unless needs_conversion?(data)
+          # ensures this is the first method on the line
+          # there is an edge case here where sometimes the http method is
+          # wrapped into another method, but its just safer to skip those
+          # cases and process manually
+          return unless receiver.nil?
+          # a http_method without a path?, must be something else
+          return if http_path.nil?
+          add_offense(node, node.loc.selector, format(MSG, node.method_name))
+        end
+
+        # @return [Boolean] true if the line needs to be converted
+        def needs_conversion?(data)
+          # if the line has already been converted to use keyword args
+          # then skip
+          # ie. get :new, params: { user_id: 1 }  (looking for keyword arg)
+          value = data.descendants.find do |d|
+            KEYWORD_ARGS.include?(d.children.first) if d.type == :sym
+          end
+          value.nil?
+        end
+
+        def convert_hash_data(data, type)
+          # empty hash or no hash return empty string
+          return '' if data.nil? || data.children.count < 1
+          hash_data = if data.type == :send
+                        # user supplies an object,
+                        # no need to surround with braces
+                        data.source
+                      else
+                        format('{ %s }', data.children.map(&:source).join(', '))
+                      end
+          format(', %s: %s', type, hash_data)
+        end
+
+        # given a pre Rails 5 method: get :new, user_id: @user.id, {}
+        #
+        # @return lambda of auto correct procedure
+        # the result should look like:
+        #     get :new, params: { user_id: @user.id }, headers: {}
+        # the http_method is the method use to call the controller
+        # the controller node can be a symbol, method, object or string
+        # that represents the path/action on the Rails controller
+        # the data is the http parameters and environment sent in
+        # the Rails 5 http call
+        def autocorrect(node)
+          _receiver, http_method, http_path, *data = *node
+          controller_action = http_path.source
+          params = convert_hash_data(data.first, 'params')
+          headers = convert_hash_data(data.last, 'headers') if data.count > 1
+          # the range of the text to replace, which is the whole line
+          code_to_replace = node.loc.expression
+          # what to replace with
+          new_code = format('%s %s%s%s', http_method, controller_action,
+                            params, headers)
+          ->(corrector) { corrector.replace(code_to_replace, new_code) }
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/rails/http_positional_arguments_spec.rb
@@ -0,0 +1,218 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe RuboCop::Cop::Rails::HttpPositionalArguments do
+  subject(:cop) { RuboCop::Cop::Rails::HttpPositionalArguments.new }
+
+  it 'registers an offense for post method' do
+    source = 'post :create, user_id: @user.id'
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'registers an offense for patch method' do
+    source = 'patch :update, user_id: @user.id'
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  it 'registers an offense for delete method' do
+    source = 'delete :destroy, id: @user.id'
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(1)
+  end
+
+  describe 'when using process' do
+    let(:source) do
+      'process :new, method: :get, params: { user_id: @user.id }'
+    end
+
+    it 'does not register an offense' do
+      inspect_source(cop, source)
+      expect(cop.messages).to be_empty
+    end
+  end
+
+  describe 'when using keyword args' do
+    let(:source) do
+      'get :new, params: { user_id: @user.id }'
+    end
+
+    it 'does not register an offense' do
+      inspect_source(cop, source)
+      expect(cop.messages).to be_empty
+    end
+  end
+
+  describe :get do
+    let(:source) do
+      'get :new, user_id: @user.id'
+    end
+
+    let(:corrected_result) do
+      'get :new, params: { user_id: @user.id }'
+    end
+
+    it 'registers an offense' do
+      inspect_source(cop, source)
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it 'autocorrects offense' do
+      new_source = autocorrect_source(cop, source)
+      expect(new_source).to eq(corrected_result)
+    end
+
+    describe 'no params' do
+      let(:source) do
+        'get :new'
+      end
+
+      it 'does not register an offense' do
+        inspect_source(cop, source)
+        expect(cop.offenses.size).to eq(0)
+      end
+    end
+  end
+
+  describe :patch do
+    let(:source) do
+      <<-EOS
+patch :update,
+          id: @user.id,
+          ac: {
+            article_id: @article1.id,
+            profile_id: @profile1.id,
+            content: 'Some Text'
+          }
+EOS
+    end
+
+    let(:corrected_result) do
+      <<-EOS
+patch :update, params: { id: @user.id, ac: {
+            article_id: @article1.id,
+            profile_id: @profile1.id,
+            content: 'Some Text'
+          } }
+EOS
+    end
+
+    it 'registers an offense' do
+      inspect_source(cop, source)
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it 'autocorrects offense' do
+      new_source = autocorrect_source(cop, source)
+      expect(new_source).to eq(corrected_result)
+    end
+  end
+
+  describe :post do
+    let(:source) do
+      <<-EOS
+post :create,
+          id: @user.id,
+          ac: {
+            article_id: @article1.id,
+            profile_id: @profile1.id,
+            content: 'Some Text'
+          }
+      EOS
+    end
+
+    let(:corrected_result) do
+      <<-EOS
+post :create, params: { id: @user.id, ac: {
+            article_id: @article1.id,
+            profile_id: @profile1.id,
+            content: 'Some Text'
+          } }
+      EOS
+    end
+
+    it 'registers an offense' do
+      inspect_source(cop, source)
+      expect(cop.offenses.size).to eq(1)
+    end
+
+    it 'autocorrects offense' do
+      new_source = autocorrect_source(cop, source)
+      expect(new_source).to eq(corrected_result)
+    end
+  end
+
+  %w(post get patch put delete).each do |keyword|
+    it 'does not register an offense when keyword' do
+      source = "@user.#{keyword}.id = ''"
+      inspect_source(cop, source)
+      expect(cop.offenses.size).to eq(0)
+    end
+  end
+
+  it 'auto-corrects http action when method' do
+    source = 'post user_attrs, id: 1'
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(1)
+    new_source = autocorrect_source(cop, source)
+    expected = 'post user_attrs, params: { id: 1 }'
+    expect(new_source).to eq(expected)
+  end
+
+  it 'auto-corrects http action when symbol' do
+    source = 'post :user_attrs, id: 1'
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(1)
+    new_source = autocorrect_source(cop, source)
+    expected = 'post :user_attrs, params: { id: 1 }'
+    expect(new_source).to eq(expected)
+  end
+
+  it 'does not register when post is found' do
+    source = "if post.stint_title.present? \n true\n end"
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(0)
+  end
+
+  it 'does not remove quotes when single quoted' do
+    source = "get '/auth/linkedin/callback'"
+    new_source = autocorrect_source(cop, source)
+    expect(new_source).to eq("get '/auth/linkedin/callback'")
+  end
+
+  it 'does not remove quotes when double quoted' do
+    source = 'get "/auth/linkedin/callback"'
+    new_source = autocorrect_source(cop, source)
+    expect(new_source).to eq('get "/auth/linkedin/callback"')
+  end
+
+  it 'does add headers keyword when env or headers are used' do
+    source = 'get some_path(profile.id), {},'
+    source += " 'HTTP_REFERER' => p_url(p.id).to_s"
+    new_source = autocorrect_source(cop, source)
+    output = 'get some_path(profile.id),'
+    output += " headers: { 'HTTP_REFERER' => p_url(p.id).to_s }"
+    expect(new_source).to eq(output)
+  end
+
+  it 'does not duplicate brackets when hash is already supplied' do
+    source = 'get some_path(profile.id), '
+    source += '{ user_id: @user.id, profile_id: p.id },'
+    source += " 'HTTP_REFERER' => p_url(p.id).to_s"
+    new_source = autocorrect_source(cop, source)
+    output = 'get some_path(profile.id), params:'
+    output += ' { user_id: @user.id, profile_id: p.id },'
+    output += " headers: { 'HTTP_REFERER' => p_url(p.id).to_s }"
+    expect(new_source).to eq(output)
+  end
+
+  it 'does not register an offense for process method' do
+    source = 'post :create, confirmation_data'
+    inspect_source(cop, source)
+    expect(cop.offenses.size).to eq(1)
+    new_source = autocorrect_source(cop, source)
+    output = 'post :create, params: confirmation_data'
+    expect(new_source).to eq(output)
+  end
+end
