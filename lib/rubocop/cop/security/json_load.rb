745f0774df94167be37c85414c1591a59b162b28
savef <savef@users.noreply.github.com>
2016-10-08 00:10:40 +0100

    [Fix #3521] Disable autocorrect on `Security/JSONLoad` by default (#3584)
    The autocorrect on this cop is dangerous for at least two reasons depending on the value being passed to the `JSON` class method, so this disabled it by default. One known reason is where we pass in a stream such as `JSON.load(open('file'))`, this cannot be swapped out for `JSON.parse` without calling `#read` on the stream. Another known reason is where the JSON string is a single value, rather than a full JSON object, such as `JSON.load('false')`, this cannot be swapped out for `JSON.parse` without adding the `quirks_mode: true` option.
    
    Also slightly improve the description in the cop and `enabled.yml`.
    
    Also fix the offence message.

--- lib/rubocop/cop/security/json_load.rb
+++ lib/rubocop/cop/security/json_load.rb
@@ -3,7 +3,16 @@
 module RuboCop
   module Cop
     module Security
-      # This cop checks for the use of unsecure JSON methods.
+      # This cop checks for the use of JSON class methods which have potential
+      # security issues.
+      #
+      # Autocorrect is disabled by default because it's potentially dangerous.
+      # If using a stream, like `JSON.load(open('file'))`, it will need to call
+      # `#read` manually, like `JSON.parse(open('file').read)`.
+      # If reading single values (rather than proper JSON objects), like
+      # `JSON.load('false')`, it will need to pass the `quirks_mode: true`
+      # option, like `JSON.parse('false', quirks_mode: true)`.
+      # Other similar issues may apply.
       #
       # @example
       #   # always offense
@@ -14,7 +23,7 @@ module RuboCop
       #   JSON.parse("{}")
       #
       class JSONLoad < Cop
-        MSG = 'Prefer `JSON.parse` instead of `JSON#%s`.'.freeze
+        MSG = 'Prefer `JSON.parse` over `JSON.%s`.'.freeze
 
         def_node_matcher :json_load, <<-END
           (send (const nil :JSON) ${:load :restore} ...)
