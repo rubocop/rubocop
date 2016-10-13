efa7ed5bbe8870d729e90c9349bef743552d585a
Teemu <teemu@aikais.in>
2016-10-13 20:35:19 +0300

    [Fix #3291] Improve Rails safety method detection
    Only assume `raw` and `html_safe` to be a Rails helpers if calls have
    one and zero arguments respectively.
    
    Also unify related test descriptions.

--- spec/rubocop/cop/rails/output_safety_spec.rb
+++ spec/rubocop/cop/rails/output_safety_spec.rb
@@ -5,35 +5,54 @@ require 'spec_helper'
 describe RuboCop::Cop::Rails::OutputSafety do
   subject(:cop) { described_class.new }
 
-  it 'records an offense for html_safe methods with a receiver' do
+  it 'registers an offense for html_safe methods with a receiver and no ' \
+     'arguments' do
     source = ['foo.html_safe',
               '"foo".html_safe']
     inspect_source(cop, source)
     expect(cop.offenses.size).to eq(2)
   end
 
-  it 'does not record an offense for html_safe methods without a receiver' do
-    source = ['html_safe foo',
-              'html_safe "foo"']
+  it 'accepts html_safe methods without a receiver' do
+    source = 'html_safe'
     inspect_source(cop, source)
     expect(cop.offenses).to be_empty
   end
 
-  it 'records an offense for raw methods without a receiver' do
+  it 'accepts html_safe methods with arguments' do
+    source = ['foo.html_safe one',
+              '"foo".html_safe two']
+    inspect_source(cop, source)
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'registers an offense for raw methods without a receiver' do
     source = ['raw(foo)',
               'raw "foo"']
     inspect_source(cop, source)
     expect(cop.offenses.size).to eq(2)
   end
 
-  it 'does not record an offense for raw methods with a receiver' do
-    source = ['foo.raw',
-              '"foo".raw']
+  it 'accepts raw methods with a receiver' do
+    source = ['foo.raw(foo)',
+              '"foo".raw "foo"']
     inspect_source(cop, source)
     expect(cop.offenses).to be_empty
   end
 
-  it 'does not record an offense for comments' do
+  it 'accepts raw methods without arguments' do
+    source = 'raw'
+    inspect_source(cop, source)
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts raw methods with more than one arguments' do
+    source = 'raw one, two'
+    inspect_source(cop, source)
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts comments' do
     source = ['# foo.html_safe',
               '# raw foo']
     inspect_source(cop, source)
