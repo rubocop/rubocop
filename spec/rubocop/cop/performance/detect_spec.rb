b1f33ec51499455f97009c4c2ab511a6d225f1ca
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2016-10-21 21:57:11 -0400

    Modify Performance/Detect to use NodePattern

--- spec/rubocop/cop/performance/detect_spec.rb
+++ spec/rubocop/cop/performance/detect_spec.rb
@@ -6,7 +6,6 @@ describe RuboCop::Cop::Performance::Detect do
   subject(:cop) { described_class.new(config) }
 
   let(:collection_method) { nil }
-
   let(:config) do
     RuboCop::Config.new(
       'Style/CollectionMethods' => {
@@ -17,7 +16,11 @@ describe RuboCop::Cop::Performance::Detect do
     )
   end
 
-  described_class::SELECT_METHODS.each do |method|
+  # rspec will not let you use a variable assigned using let outside
+  # of `it`
+  select_methods = [:select, :find_all].freeze
+
+  select_methods.each do |method|
     it "registers an offense when first is called on #{method}" do
       inspect_source(cop, "[1, 2, 3].#{method} { |i| i % 2 == 0 }.first")
 
@@ -43,24 +46,17 @@ describe RuboCop::Cop::Performance::Detect do
     end
 
     it "registers an offense when first is called on multiline #{method}" do
-      inspect_source(
-        cop, %(
-        [1, 2, 3].#{method} do
-          |i| i % 2 == 0
-        end.first)
-      )
+      inspect_source(cop, ["[1, 2, 3].#{method} do |i|",
+                           '  i % 2 == 0',
+                           'end.first'])
 
-      expect(cop.messages)
-        .to eq(["Use `detect` instead of `#{method}.first`."])
+      expect(cop.messages).to eq(["Use `detect` instead of `#{method}.first`."])
     end
 
     it "registers an offense when first is called on multiline #{method}" do
-      inspect_source(
-        cop, %(
-        [1, 2, 3].#{method} do
-          |i| i % 2 == 0
-        end.last)
-      )
+      inspect_source(cop, ["[1, 2, 3].#{method} do |i|",
+                           '  i % 2 == 0',
+                           'end.last'])
 
       expect(cop.messages)
         .to eq(["Use `reverse.detect` instead of `#{method}.last`."])
@@ -69,8 +65,7 @@ describe RuboCop::Cop::Performance::Detect do
     it "registers an offense when first is called on #{method} short syntax" do
       inspect_source(cop, "[1, 2, 3].#{method}(&:even?).first")
 
-      expect(cop.messages)
-        .to eq(["Use `detect` instead of `#{method}.first`."])
+      expect(cop.messages).to eq(["Use `detect` instead of `#{method}.first`."])
     end
 
     it "registers an offense when last is called on #{method} short syntax" do
@@ -84,8 +79,7 @@ describe RuboCop::Cop::Performance::Detect do
        'on `lazy` without receiver' do
       inspect_source(cop, "lazy.#{method}(&:even?).first")
 
-      expect(cop.messages)
-        .to eq(["Use `detect` instead of `#{method}.first`."])
+      expect(cop.messages).to eq(["Use `detect` instead of `#{method}.first`."])
     end
 
     it "does not register an offense when #{method} is used " \
@@ -127,105 +121,87 @@ describe RuboCop::Cop::Performance::Detect do
     shared_examples 'detect_autocorrect' do |preferred_method|
       context "with #{preferred_method}" do
         let(:collection_method) { preferred_method }
-        described_class::SELECT_METHODS.each do |method|
-          it "corrects #{method}.first to #{preferred_method} (with block)" do
-            new_source = autocorrect_source(
-              cop,
-              "[1, 2, 3].#{method} { |i| i % 2 == 0 }.first"
-            )
 
-            expect(new_source).to eq(
-              "[1, 2, 3].#{preferred_method} { |i| i % 2 == 0 }"
-            )
+        select_methods.each do |method|
+          it "corrects #{method}.first to #{preferred_method} (with block)" do
+            source = "[1, 2, 3].#{method} { |i| i % 2 == 0 }.first"
+
+            new_source = autocorrect_source(cop, source)
+
+            expect(new_source)
+              .to eq("[1, 2, 3].#{preferred_method} { |i| i % 2 == 0 }")
           end
 
           it "corrects #{method}.last to reverse.#{preferred_method} " \
              '(with block)' do
-            new_source = autocorrect_source(
-              cop,
-              "[1, 2, 3].#{method} { |i| i % 2 == 0 }.last"
-            )
+            source = "[1, 2, 3].#{method} { |i| i % 2 == 0 }.last"
 
-            expect(new_source).to eq(
-              "[1, 2, 3].reverse.#{preferred_method} { |i| i % 2 == 0 }"
-            )
+            new_source = autocorrect_source(cop, source)
+
+            expect(new_source)
+              .to eq("[1, 2, 3].reverse.#{preferred_method} { |i| i % 2 == 0 }")
           end
 
           it "corrects #{method}.first to #{preferred_method} (short syntax)" do
-            new_source = autocorrect_source(
-              cop,
-              "[1, 2, 3].#{method}(&:even?).first"
-            )
+            source = "[1, 2, 3].#{method}(&:even?).first"
+
+            new_source = autocorrect_source(cop, source)
 
             expect(new_source).to eq("[1, 2, 3].#{preferred_method}(&:even?)")
           end
 
           it "corrects #{method}.last to reverse.#{preferred_method} " \
              '(short syntax)' do
-            new_source = autocorrect_source(
-              cop,
-              "[1, 2, 3].#{method}(&:even?).last"
-            )
+            source = "[1, 2, 3].#{method}(&:even?).last"
+
+            new_source = autocorrect_source(cop, source)
 
             expect(new_source)
               .to eq("[1, 2, 3].reverse.#{preferred_method}(&:even?)")
           end
 
           it "corrects #{method}.first to #{preferred_method} (multiline)" do
-            new_source = autocorrect_source(
-              cop,
-              %([1, 2, 3].#{method} do
-                  |i| i % 2 == 0
-                end.first)
-            )
+            source = ["[1, 2, 3].#{method} do |i|",
+                      '  i % 2 == 0',
+                      'end.first']
+            new_source = autocorrect_source(cop, source)
 
-            expect(new_source).to eq(
-              %([1, 2, 3].#{preferred_method} do
-                  |i| i % 2 == 0
-                end)
-            )
+            expect(new_source).to eq(["[1, 2, 3].#{preferred_method} do |i|",
+                                      '  i % 2 == 0',
+                                      'end'].join("\n"))
           end
 
           it "corrects #{method}.last to reverse.#{preferred_method} " \
              '(multiline)' do
-            new_source = autocorrect_source(
-              cop,
-              %([1, 2, 3].#{method} do
-                  |i| i % 2 == 0
-                end.last)
-            )
+            source = ["[1, 2, 3].#{method} do |i|",
+                      '  i % 2 == 0',
+                      'end.last']
+            new_source = autocorrect_source(cop, source)
 
-            expect(new_source).to eq(
-              %([1, 2, 3].reverse.#{preferred_method} do
-                  |i| i % 2 == 0
-                end)
-            )
+            expect(new_source)
+              .to eq(["[1, 2, 3].reverse.#{preferred_method} do |i|",
+                      '  i % 2 == 0',
+                      'end'].join("\n"))
           end
 
           it "corrects multiline #{method} to #{preferred_method} " \
-             'with \'first\' on the last line' do
-            new_source = autocorrect_source(
-              cop,
-              %([1, 2, 3].#{method} { true }
-                         .first['x'])
-            )
+             "with 'first' on the last line" do
+            source = ["[1, 2, 3].#{method} { true }",
+                      ".first['x']"]
+            new_source = autocorrect_source(cop, source)
 
-            expect(new_source).to eq(
-              %([1, 2, 3].#{preferred_method} { true }['x'])
-            )
+            expect(new_source)
+              .to eq("[1, 2, 3].#{preferred_method} { true }['x']")
           end
 
           it "corrects multiline #{method} to #{preferred_method} " \
-             'with \'first\' on the last line (short syntax)' do
-            new_source = autocorrect_source(
-              cop,
-              %([1, 2, 3].#{method}(&:blank?)
-                         .first['x'])
-            )
+             "with 'first' on the last line (short syntax)" do
+            source = ["[1, 2, 3].#{method}(&:blank?)",
+                      ".first['x']"]
+            new_source = autocorrect_source(cop, source)
 
-            expect(new_source).to eq(
-              %([1, 2, 3].#{preferred_method}(&:blank?)['x'])
-            )
+            expect(new_source)
+              .to eq("[1, 2, 3].#{preferred_method}(&:blank?)['x']")
           end
         end
       end
@@ -252,7 +228,7 @@ describe RuboCop::Cop::Performance::Detect do
       )
     end
 
-    described_class::SELECT_METHODS.each do |method|
+    select_methods.each do |method|
       it "doesn't register an offense when first is called on #{method}" do
         inspect_source(cop, "[1, 2, 3].#{method} { |i| i % 2 == 0 }.first")
 
