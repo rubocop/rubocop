9bb55ef3e93c2d23add4270c86796dcb78552b4d
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2016-10-27 13:53:02 -0400

    Modify Performance/Sample to use NodePattern

--- lib/rubocop/cop/performance/sample.rb
+++ lib/rubocop/cop/performance/sample.rb
@@ -27,110 +27,112 @@ module RuboCop
       class Sample < Cop
         MSG = 'Use `%<correct>s` instead of `%<incorrect>s`.'.freeze
 
+        def_node_matcher :sample_candidate?, <<-PATTERN
+          (send $(send _ :shuffle $...) ${:first :last :[]} $...)
+        PATTERN
+
         def on_send(node)
-          analyzer = ShuffleAnalyzer.new(node)
-          return unless analyzer.offensive?
-          add_offense(node, analyzer.source_range, analyzer.message)
+          sample_candidate?(node) do |shuffle, shuffle_arg, method, method_args|
+            return unless offensive?(method, method_args)
+
+            range = source_range(shuffle, node)
+            message = message(shuffle_arg, method, method_args, range)
+            add_offense(node, range, message)
+          end
         end
 
         def autocorrect(node)
-          ShuffleAnalyzer.new(node).autocorrect
+          shuffle_node, shuffle_arg, method, method_args =
+            sample_candidate?(node)
+
+          lambda do |corrector|
+            corrector.replace(source_range(shuffle_node, node),
+                              correction(shuffle_arg, method, method_args))
+          end
         end
 
-        # An internal class for representing a shuffle + method node analyzer.
-        class ShuffleAnalyzer
-          def initialize(shuffle_node)
-            @shuffle_node = shuffle_node
-            @method_node  = shuffle_node.parent
+        private
+
+        def offensive?(method, method_args)
+          case method
+          when :first, :last
+            true
+          when :[]
+            sample_size(method_args) != :unknown
+          else
+            false
           end
+        end
 
-          def autocorrect
-            ->(corrector) { corrector.replace(source_range, correct) }
+        def sample_size(method_args)
+          case method_args.size
+          when 1
+            sample_size_for_one_arg(method_args.first)
+          when 2
+            sample_size_for_two_args(*method_args)
           end
+        end
 
-          def message
-            format(MSG, correct: correct, incorrect: source_range.source)
+        def sample_size_for_one_arg(arg)
+          case arg.type
+          when :erange, :irange
+            range_size(arg)
+          when :int
+            arg.to_a.first.zero? ? nil : :unknown
+          else
+            :unknown
           end
+        end
 
-          def offensive?
-            shuffle_node.to_a[1] == :shuffle && corrigible?
+        def sample_size_for_two_args(first, second)
+          return :unknown unless first.int_type? && first.to_a.first.zero?
+          second.int_type? ? second.to_a.first : :unknown
+        end
+
+        def range_size(range_node)
+          vals = range_node.to_a
+          return :unknown unless vals.all?(&:int_type?)
+          low, high = vals.map { |val| val.children[0] }
+          return :unknown unless low.zero? && high >= 0
+
+          case range_node.type
+          when :erange
+            (low...high).size
+          when :irange
+            (low..high).size
           end
+        end
 
-          def source_range
-            Parser::Source::Range.new(shuffle_node.source_range.source_buffer,
-                                      shuffle_node.loc.selector.begin_pos,
-                                      method_node.source_range.end_pos)
+        def source_range(shuffle_node, node)
+          Parser::Source::Range.new(shuffle_node.source_range.source_buffer,
+                                    shuffle_node.loc.selector.begin_pos,
+                                    node.source_range.end_pos)
+        end
+
+        def message(shuffle_arg, method, method_args, range)
+          format(MSG,
+                 correct: correction(shuffle_arg, method, method_args),
+                 incorrect: range.source)
+        end
+
+        def correction(shuffle_arg, method, method_args)
+          shuffle_arg = extract_source(shuffle_arg)
+          sample_arg = sample_arg(method, method_args)
+          args = [sample_arg, shuffle_arg].compact.join(', ')
+          args.empty? ? 'sample' : "sample(#{args})"
+        end
+
+        def sample_arg(method, method_args)
+          case method
+          when :first, :last
+            extract_source(method_args)
+          when :[]
+            sample_size(method_args)
           end
+        end
 
-          private
-
-          attr_reader :method_node, :shuffle_node
-
-          def correct
-            args = [sample_arg, shuffle_arg].compact.join(', ')
-            args.empty? ? 'sample' : "sample(#{args})"
-          end
-
-          def corrigible?
-            case method
-            when :first, :last then true
-            when :[]           then sample_size != :unknown
-            else false
-            end
-          end
-
-          def method
-            method_node.to_a[1]
-          end
-
-          def method_arg
-            _, _, arg = *method_node
-            arg.source if arg
-          end
-
-          def range_size(range_node)
-            vals = *range_node
-            return :unknown unless vals.all?(&:int_type?)
-            low, high = *vals.map { |val| val.to_a.first }
-            return :unknown unless low.zero? && high >= 0
-            case range_node.type
-            when :erange then high - low
-            when :irange then high - low + 1
-            end
-          end
-
-          def sample_arg
-            case method
-            when :first, :last then method_arg
-            when :[]           then sample_size
-            end
-          end
-
-          def sample_size
-            _, _, *args = *method_node
-            case args.size
-            when 1 then sample_size_for_one_arg(args.first)
-            when 2 then sample_size_for_two_args(*args)
-            end
-          end
-
-          def sample_size_for_one_arg(arg)
-            case arg.type
-            when :erange, :irange then range_size(arg)
-            when :int             then arg.to_a.first.zero? ? nil : :unknown
-            else :unknown
-            end
-          end
-
-          def sample_size_for_two_args(first, second)
-            return :unknown unless first.int_type? && first.to_a.first.zero?
-            second.int_type? ? second.to_a.first : :unknown
-          end
-
-          def shuffle_arg
-            _, _, arg = *shuffle_node
-            arg.source if arg
-          end
+        def extract_source(args)
+          args.empty? ? nil : args.first.source
         end
       end
     end
