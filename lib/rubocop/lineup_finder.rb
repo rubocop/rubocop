15a7e0205b234fd46de6d0f18b7fc35cb6533654
melch <melch@procore.com>
2016-11-07 18:08:24 -0800

    Extend LineupFinder to report changed lines.

--- lib/rubocop/lineup_finder.rb
+++ lib/rubocop/lineup_finder.rb
@@ -15,17 +15,32 @@ module RuboCop
       .map { |file| File.absolute_path(file) }
     end
 
+    def changed_line_ranges(file)
+      git_diff_zero_unified(file)
+      .each_line
+      .grep(/@@ -(\d+)(?:,)?(\d+)? \+(\d+)(?:,)?(\d+)? @@/) {
+        [
+          Regexp.last_match[3].to_i,
+          (Regexp.last_match[4] || 1).to_i
+        ]
+      }
+    end
+
     def changed_files_and_lines
       @diff_info ||= Hash[
         changed_files.collect do |file|
-          [file, line_change_info(file)]
+          [file, changed_line_ranges(file)]
         end
       ]
 
       @changes ||= Hash[
-        diff_info.collect do |filename, line_change_info|
-          mask = line_change_info.collect do |changed_line_number, number_of_changed_lines|
-            Array(changed_line_number .. (changed_line_number + number_of_changed_lines))
+        diff_info.collect do |filename, changed_line_ranges|
+          mask = changed_line_ranges.collect do |changed_line_number, number_of_changed_lines|
+            if number_of_changed_lines == 0
+              []
+            else
+              Array(changed_line_number .. (changed_line_number + number_of_changed_lines - 1))
+            end
           end.flatten
 
           [filename, mask]
@@ -37,6 +52,10 @@ module RuboCop
 
     def git_diff_name_only
       `git diff --diff-filter=AM --name-only HEAD`
+    end
+
+    def git_diff_zero_unified(file)
+      `git diff -U0 HEAD #{file}`
     end
   end
 end
--- spec/rubocop/lineup_finder_spec.rb
+++ spec/rubocop/lineup_finder_spec.rb
@@ -10,11 +10,70 @@ describe RuboCop::LineupFinder, :isolated_environment do
     allow(subj).to receive(:git_diff_name_only) {
       <<-END
 .gitignore
-lib/rubocop.rb
-lib/rubocop/options.rb
-spec/rubocop/options_spec.rb
+lib/rubocop/cop/cop.rb
+lib/rubocop/lineup_finder.rb
       END
     }
+    allow(subj).to receive(:git_diff_zero_unified).with('.gitignore').and_return (
+      <<-END
+diff --git a/.gitignore b/.gitignore
+index 2f63bfd..7a9a461 100644
+--- a/.gitignore
++++ b/.gitignore
+@@ -36 +36 @@ TAGS
+-#.DS_Store
++.DS_Store
+      END
+    )
+    allow(subj).to receive(:git_diff_zero_unified).with('lib/rubocop/cop/cop.rb').and_return (
+      <<-END
+diff --git a/lib/rubocop/cop/cop.rb b/lib/rubocop/cop/cop.rb
+index 16ed2d3..0b0b8a8 100644
+--- a/lib/rubocop/cop/cop.rb
++++ b/lib/rubocop/cop/cop.rb
+@@ -169 +168,0 @@ module RuboCop
+-
+@@ -171,0 +171,3 @@ module RuboCop
++        intersection = RuboCop::LineupFinder.new.changed_files_and_lines[location.source_buffer.name] & Array(location.first_line..location.last_line)
++        return if intersection && intersection.empty?
++
+      END
+    )
+    allow(subj).to receive(:git_diff_zero_unified).with('lib/rubocop/lineup_finder.rb').and_return (
+      <<-END
+diff --git a/lib/rubocop/lineup_finder.rb b/lib/rubocop/lineup_finder.rb
+index f53131e..58eb6f1 100644
+--- a/lib/rubocop/lineup_finder.rb
++++ b/lib/rubocop/lineup_finder.rb
+@@ -7 +6,0 @@ module RuboCop
+-  class LineupFinder
+@@ -17,0 +17,11 @@ module RuboCop
++    def changed_line_ranges(file)
++      git_diff_zero_unified(file)
++      .each_line
++      .grep(/@@ -(\d+)(?:,)?(\d+)? \+(\d+)(?:,)?(\d+)? @@/) {
++        [
++          Regexp.last_match[3].to_i,
++          (Regexp.last_match[4] || 1).to_i
++        ]
++      }
++    end
++
+@@ -21 +31 @@ module RuboCop
+-          [file, line_change_info(file)]
++          [file, changed_line_ranges(file)]
+@@ -26,2 +36,2 @@ module RuboCop
+-        diff_info.collect do |filename, line_change_info|
+-          mask = line_change_info.collect do |changed_line_number, number_of_changed_lines|
++        diff_info.collect do |filename, changed_line_ranges|
++          mask = changed_line_ranges.collect do |changed_line_number, number_of_changed_lines|
+@@ -40,0 +51,4 @@ module RuboCop
++
++    def git_diff_zero_unified(file)
++      `git diff -U0 HEAD file`
++    end
+      END
+    )
     subj
   end
 
@@ -24,5 +83,24 @@ spec/rubocop/options_spec.rb
     changed_files.each do |file|
       expect(file.sub(/^[A-Z]:/, '')).to start_with('/')
     end
+  end
+
+  it "should parse changes, additions, and deletions" do
+    allow(lineup_finder).to receive(:changed_files).and_return ([
+      '.gitignore',
+      'lib/rubocop/cop/cop.rb',
+      'lib/rubocop/lineup_finder.rb',
+    ])
+    expect(lineup_finder.changed_line_ranges('lib/rubocop/lineup_finder.rb')).to eq(
+      [
+        [6, 0],
+        [17, 11],
+        [31, 1],
+        [36, 2],
+        [51, 4],
+      ]
+    )
+    changed_lines = (17..27).to_a + [31, 36, 37, 51, 52, 53, 54]
+    expect(lineup_finder.changed_files_and_lines['lib/rubocop/lineup_finder.rb'].sort).to eq(changed_lines.sort)
   end
 end
