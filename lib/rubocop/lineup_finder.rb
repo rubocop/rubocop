d9f443483724c2031810235a761f6e75e305d270
melch <melch@procore.com>
2016-11-07 17:26:18 -0800

    Check in some style violations for messing around locally.

--- lib/rubocop/lineup_finder.rb
+++ lib/rubocop/lineup_finder.rb
@@ -5,12 +5,32 @@
 module RuboCop
   # This class finds changed files by parsing git changes
   class LineupFinder
+    attr_reader :diff_info
+
     def changed_files
       @changed_files ||= git_diff_name_only
       .lines
       .map(&:chomp)
       .grep(/\.rb$/)
       .map { |file| File.absolute_path(file) }
+    end
+
+    def changed_files_and_lines
+      @diff_info ||= Hash[
+        changed_files.collect do |file|
+          [file, line_change_info(file)]
+        end
+      ]
+
+      @changes ||= Hash[
+        diff_info.collect do |filename, line_change_info|
+          mask = line_change_info.collect do |changed_line_number, number_of_changed_lines|
+            Array(changed_line_number .. (changed_line_number + number_of_changed_lines))
+          end.flatten
+
+          [filename, mask]
+        end
+      ]
     end
 
     private
