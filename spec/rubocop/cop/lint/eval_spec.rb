ee7f2cf6a0ae55552b39a5ba66bffd50d7bd4c4d
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2016-11-09 18:16:27 -0500

    Modify Lint/Eval to use NodePattern

--- spec/rubocop/cop/lint/eval_spec.rb
+++ spec/rubocop/cop/lint/eval_spec.rb
@@ -6,28 +6,24 @@ describe RuboCop::Cop::Lint::Eval do
   subject(:cop) { described_class.new }
 
   it 'registers an offense for eval as function' do
-    inspect_source(cop,
-                   'eval(something)')
+    inspect_source(cop, 'eval(something)')
     expect(cop.offenses.size).to eq(1)
     expect(cop.highlights) .to eq(['eval'])
   end
 
   it 'registers an offense for eval as command' do
-    inspect_source(cop,
-                   'eval something')
+    inspect_source(cop, 'eval something')
     expect(cop.offenses.size).to eq(1)
     expect(cop.highlights).to eq(['eval'])
   end
 
   it 'does not register an offense for eval as variable' do
-    inspect_source(cop,
-                   'eval = something')
+    inspect_source(cop, 'eval = something')
     expect(cop.offenses).to be_empty
   end
 
   it 'does not register an offense for eval as method' do
-    inspect_source(cop,
-                   'something.eval')
+    inspect_source(cop, 'something.eval')
     expect(cop.offenses).to be_empty
   end
 
