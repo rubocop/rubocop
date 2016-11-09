729937a275cdfbb5d2afbb757170f540977518ea
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2016-11-09 17:43:22 -0500

    Modify Rails/ScopeArgs to use NodePattern

--- spec/rubocop/cop/rails/scope_args_spec.rb
+++ spec/rubocop/cop/rails/scope_args_spec.rb
@@ -6,20 +6,53 @@ describe RuboCop::Cop::Rails::ScopeArgs do
   subject(:cop) { described_class.new }
 
   it 'registers an offense a scope with a method arg' do
-    inspect_source(cop,
-                   'scope :active, where(active: true)')
+    inspect_source(cop, 'scope :active, where(active: true)')
+
     expect(cop.offenses.size).to eq(1)
   end
 
-  it 'accepts a lambda arg' do
-    inspect_source(cop,
-                   'scope :active, -> { where(active: true) }')
+  it 'accepts a non send argument' do
+    inspect_source(cop, 'scope :active, "adsf"')
+
     expect(cop.offenses).to be_empty
   end
 
-  it 'accepts a proc arg' do
+  it 'accepts a stabby lambda' do
+    inspect_source(cop, 'scope :active, -> { where(active: true) }')
+
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts a stabby lambda with arguments' do
+    inspect_source(cop, 'scope :active, ->(active) { where(active: active) }')
+
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts a lambda' do
+    inspect_source(cop, 'scope :active, lambda { where(active: true) }')
+
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts a lambda' do
     inspect_source(cop,
-                   'scope :active, proc { where(active: true) }')
+                   'scope :active, lambda { |active| where(active: active) }')
+
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts a lambda with a multiline block' do
+    inspect_source(cop, ['scope :active, (lambda do |active|',
+                         '                 where(active: active)',
+                         '               end)'])
+
+    expect(cop.offenses).to be_empty
+  end
+
+  it 'accepts a proc' do
+    inspect_source(cop, 'scope :active, proc { where(active: true) }')
+
     expect(cop.offenses).to be_empty
   end
 end
