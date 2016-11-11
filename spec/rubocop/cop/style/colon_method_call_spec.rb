dd7babec63a06cc03e05f286d9e58e13c2f8eb21
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2016-11-11 17:15:43 -0500

    Modify Style/ColonMethodCall to use NodePattern

--- spec/rubocop/cop/style/colon_method_call_spec.rb
+++ spec/rubocop/cop/style/colon_method_call_spec.rb
@@ -6,50 +6,42 @@ describe RuboCop::Cop::Style::ColonMethodCall do
   subject(:cop) { described_class.new }
 
   it 'registers an offense for instance method call' do
-    inspect_source(cop,
-                   'test::method_name')
+    inspect_source(cop, 'test::method_name')
     expect(cop.offenses.size).to eq(1)
   end
 
   it 'registers an offense for instance method call with arg' do
-    inspect_source(cop,
-                   'test::method_name(arg)')
+    inspect_source(cop, 'test::method_name(arg)')
     expect(cop.offenses.size).to eq(1)
   end
 
   it 'registers an offense for class method call' do
-    inspect_source(cop,
-                   'Class::method_name')
+    inspect_source(cop, 'Class::method_name')
     expect(cop.offenses.size).to eq(1)
   end
 
   it 'registers an offense for class method call with arg' do
-    inspect_source(cop,
-                   'Class::method_name(arg, arg2)')
+    inspect_source(cop, 'Class::method_name(arg, arg2)')
     expect(cop.offenses.size).to eq(1)
   end
 
   it 'does not register an offense for constant access' do
-    inspect_source(cop,
-                   'Tip::Top::SOME_CONST')
+    inspect_source(cop, 'Tip::Top::SOME_CONST')
     expect(cop.offenses).to be_empty
   end
 
   it 'does not register an offense for nested class' do
-    inspect_source(cop,
-                   'Tip::Top.some_method')
+    inspect_source(cop, 'Tip::Top.some_method')
     expect(cop.offenses).to be_empty
   end
 
   it 'does not register an offense for op methods' do
-    inspect_source(cop,
-                   'Tip::Top.some_method[3]')
+    inspect_source(cop, 'Tip::Top.some_method[3]')
     expect(cop.offenses).to be_empty
   end
 
   it 'does not register an offense when for constructor methods' do
-    inspect_source(cop,
-                   'Tip::Top(some_arg)')
+    inspect_source(cop, 'Tip::Top(some_arg)')
     expect(cop.offenses).to be_empty
   end
 
