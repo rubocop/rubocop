e4cb787e5428f73b0a2be89380f7e6aa421f5f35
melch <melch@procore.com>
2016-11-22 23:19:17 -0800

    Move logic to annotate offense message. Out of Cop into its own class.
    The Cop class was too large and failed style checks.
    The moved logic pertains only to annotating the message
    passed into an offense and not any other part of Cop.
    
    Message annotation was not 100% tested, so attempted to move safely by:
    1. Create loose scaffold spec in cop_spec.
    2. Move logic into new class.
    3. Move existing specs into new class's spec file.
    4. Move scaffold specs into new class's spec file.

--- lib/rubocop/cop/cop.rb
+++ lib/rubocop/cop/cop.rb
@@ -141,25 +141,6 @@ module RuboCop
         @cop_config ||= @config.for_cop(self)
       end
 
-      def debug?
-        @options[:debug]
-      end
-
-      def display_cop_names?
-        debug? || @options[:display_cop_names] ||
-          @config.for_all_cops['DisplayCopNames']
-      end
-
-      def display_style_guide?
-        (style_guide_url || reference_url) &&
-          (@options[:display_style_guide] ||
-            config.for_all_cops['DisplayStyleGuide'])
-      end
-
-      def extra_details?
-        @options[:extra_details] || config.for_all_cops['ExtraDetails']
-      end
-
       def changed_lines_only?
         @options[:changed_lines_only]
       end
@@ -240,36 +221,12 @@ module RuboCop
         !relevant_file?(file)
       end
 
-      def style_guide_url
-        url = cop_config['StyleGuide']
-        return nil if url.nil? || url.empty?
-
-        base_url = config.for_all_cops['StyleGuideBaseURL']
-        return url if base_url.nil? || base_url.empty?
-
-        URI.join(base_url, url).to_s
-      end
-
-      def reference_url
-        url = cop_config['Reference']
-        url.nil? || url.empty? ? nil : url
-      end
-
-      def details
-        details = cop_config && cop_config['Details']
-        details.nil? || details.empty? ? nil : details
-      end
-
       private
 
       def annotate_message(message)
-        message = "#{name}: #{message}" if display_cop_names?
-        message += " #{details}" if extra_details?
-        if display_style_guide?
-          links = [style_guide_url, reference_url].compact.join(', ')
-          message = "#{message} (#{links})"
-        end
-        message
+        RuboCop::Cop::OffenseMessageAnnotater.new(
+          config, cop_config, @options
+        ).annotate_message(message, name)
       end
 
       def file_name_matches_any?(file, parameter, default_result)
