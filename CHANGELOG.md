164a488ce603f540d8b9345b85f4312490f0834b
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-12-18 12:32:48 +0900

    Add `Lint/MultipleCompare` cop
    In Ruby, the following code raises an error.
    
    ```ruby
    x = 20
    0 < x < 100 # => NoMethodError: undefined method `<' for true:TrueClass
    ```
    
    The error is confusing for beginner and Python programmer.
    
    This cop suggests and autocorrects the following code instead.
    
    ```ruby
    x = 20
    0 < x && x < 100
    ```

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 
 * [#3757](https://github.com/bbatsov/rubocop/pull/3757): Add Auto-Correct for `Bundler/OrderedGems` cop. ([@pocke][])
 * `Style/FrozenStringLiteralComment` now supports the style `never` that will remove the `frozen_string_literal` comment. ([@rrosenblum][])
+* [#3795](https://github.com/bbatsov/rubocop/pull/3795): Add `Lint/MultipleCompare` cop. ([@pocke][])
 
 ### Changes
 
---
+++ spec/rubocop/cop/lint/multiple_compare_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Lint::MultipleCompare do
+  let(:config) { RuboCop::Config.new }
+  subject(:cop) { described_class.new(config) }
+
+  shared_examples 'Check to use two comparison operator' do |op1, op2|
+    bad_source = "x #{op1} y #{op2} z"
+    good_source = "x #{op1} y && y #{op2} z"
+
+    it "registers an offense for #{bad_source}" do
+      inspect_source(cop, bad_source)
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.messages)
+        .to eq(['Use `&&` operator to compare multiple value.'])
+    end
+
+    it 'autocorrects' do
+      new_source = autocorrect_source(cop, bad_source)
+      expect(new_source).to eq(good_source)
+    end
+
+    it "accepts for #{good_source}" do
+      inspect_source(cop, good_source)
+      expect(cop.offenses).to be_empty
+    end
+  end
+
+  %w(< > <= >=).repeated_permutation(2) do |op1, op2|
+    include_examples 'Check to use two comparison operator', op1, op2
+  end
+
+  it 'accepts to use one compare operator' do
+    inspect_source(cop, 'x < 1')
+    expect(cop.offenses).to be_empty
+  end
+end
