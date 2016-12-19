964643513e7729aa0574e15cd27a0bd2b2042033
Timothée Peignier <timothee.peignier@tryphon.org>
2016-12-18 21:14:10 -0800

    Add cop documentation

--- lib/rubocop/cop/style/round.rb
+++ lib/rubocop/cop/style/round.rb
@@ -4,6 +4,18 @@ module RuboCop
   module Cop
     module Style
       # This cop checks for the use of the round method.
+      #
+      # @example
+      #
+      #   @bad
+      #   6.75.round
+      #   6.75.round(0)
+      #
+      #   @good
+      #   6.75.floor
+      #   6.75.ceil
+      #   6.75.round(2)
+      #
       class Round < Cop
         MSG = 'Prefer `Float#floor` or `Float#ceil` over `Float#round`.'.freeze
 
