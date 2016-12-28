810d1bb3a23b0c4ca80517599cf33266e4437102
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-12-28 08:12:42 +0100

    Fix typo in EmptyMethod cop documentation

--- lib/rubocop/cop/style/empty_method.rb
+++ lib/rubocop/cop/style/empty_method.rb
@@ -5,7 +5,7 @@ module RuboCop
     module Style
       # This cop checks for the formatting of empty method definitions.
       # By default it enforces empty method definitions to go on a single
-      # line (compact style), but it cah be configured to enforce the `end`
+      # line (compact style), but it can be configured to enforce the `end`
       # to go on its own line (expanded style.)
       #
       # Note: A method definition is not considered empty if it contains
