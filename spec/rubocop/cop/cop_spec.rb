dbfe88e1d62b018ccecd987a00864446343daad9
John Backus <johncbackus@gmail.com>
2017-01-04 23:59:21 -0800

    Refactor specs by applying RSpec/InstanceVariable

--- spec/rubocop/cop/cop_spec.rb
+++ spec/rubocop/cop/cop_spec.rb
@@ -108,42 +108,40 @@ describe RuboCop::Cop::Cop do
     end
 
     context 'when cop supports autocorrection' do
-      before do
-        @cop = RuboCop::Cop::Style::Alias.new
-      end
+      let(:cop) { RuboCop::Cop::Style::Alias.new }
 
       context 'when offense was corrected' do
         before do
-          allow(@cop).to receive(:autocorrect?).and_return(true)
-          allow(@cop).to receive(:autocorrect).and_return(->(_corrector) {})
+          allow(cop).to receive(:autocorrect?).and_return(true)
+          allow(cop).to receive(:autocorrect).and_return(->(_corrector) {})
         end
 
         it 'is set to true' do
-          @cop.add_offense(nil, location, 'message')
-          expect(@cop.offenses.first.corrected?).to eq(true)
+          cop.add_offense(nil, location, 'message')
+          expect(cop.offenses.first.corrected?).to eq(true)
         end
       end
 
       context 'when autocorrection is not needed' do
         before do
-          allow(@cop).to receive(:autocorrect?).and_return(false)
+          allow(cop).to receive(:autocorrect?).and_return(false)
         end
 
         it 'is set to false' do
-          @cop.add_offense(nil, location, 'message')
-          expect(@cop.offenses.first.corrected?).to eq(false)
+          cop.add_offense(nil, location, 'message')
+          expect(cop.offenses.first.corrected?).to eq(false)
         end
       end
 
       context 'when offense was not corrected because of an error' do
         before do
-          allow(@cop).to receive(:autocorrect?).and_return(true)
-          allow(@cop).to receive(:autocorrect).and_return(false)
+          allow(cop).to receive(:autocorrect?).and_return(true)
+          allow(cop).to receive(:autocorrect).and_return(false)
         end
 
         it 'is set to false' do
-          @cop.add_offense(nil, location, 'message')
-          expect(@cop.offenses.first.corrected?).to eq(false)
+          cop.add_offense(nil, location, 'message')
+          expect(cop.offenses.first.corrected?).to eq(false)
         end
       end
     end
