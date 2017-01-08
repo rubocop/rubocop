1777712715f57c5ddf991eec450fe312d87c4723
John Backus <johncbackus@gmail.com>
2017-01-08 14:47:15 -0800

    Add specs for enabled.yml and disabled.yml
    Intended to help avoid issues like @pocke fixed in #3725

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,20 +3,25 @@
 require 'spec_helper'
 
 describe 'RuboCop Project' do
-  describe 'default configuration file' do
-    let(:cop_names) { RuboCop::Cop::Cop.all.map(&:cop_name) }
+  let(:cop_names) { RuboCop::Cop::Cop.all.map(&:cop_name) }
 
-    subject(:default_config) do
-      RuboCop::ConfigLoader.load_file('config/default.yml')
-    end
+  shared_context 'configuration file' do |config_path|
+    subject(:config) { RuboCop::ConfigLoader.load_file(config_path) }
+
+    let(:configuration_keys) { config.keys }
+    let(:raw_configuration) { config.to_h.values }
+  end
+
+  describe 'default configuration file' do
+    include_context 'configuration file', 'config/default.yml'
 
     it 'has configuration for all cops' do
-      expect(default_config.keys).to match_array(%w(AllCops Rails) + cop_names)
+      expect(configuration_keys).to match_array(%w(AllCops Rails) + cop_names)
     end
 
     it 'has a nicely formatted description for all cops' do
       cop_names.each do |name|
-        description = default_config[name]['Description']
+        description = config[name]['Description']
         expect(description).not_to be_nil
         expect(description).not_to include("\n")
       end
@@ -26,11 +31,11 @@ describe 'RuboCop Project' do
       'and EnforcedStyle is valid' do
       errors = []
       cop_names.each do |name|
-        enforced_styles = default_config[name]
+        enforced_styles = config[name]
                           .select { |key, _| key.start_with?('Enforced') }
         enforced_styles.each do |style_name, _style|
           supported_key = RuboCop::Cop::Util.to_supported_styles(style_name)
-          valid = default_config[name][supported_key]
+          valid = config[name][supported_key]
           errors.push("#{supported_key} is missing for #{name}") unless valid
         end
       end
@@ -51,6 +56,24 @@ describe 'RuboCop Project' do
         end
         expect(msg).to match(/(?:[.?]|(?:\[.+\])|%s)$/)
       end
+    end
+  end
+
+  describe 'config/disabled.yml' do
+    include_context 'configuration file', 'config/disabled.yml'
+
+    it 'disables all cops in the file' do
+      expect(raw_configuration)
+        .to all(match(hash_including('Enabled' => false)))
+    end
+  end
+
+  describe 'config/enabled.yml' do
+    include_context 'configuration file', 'config/enabled.yml'
+
+    it 'enables all cops in the file' do
+      expect(raw_configuration)
+        .to all(match(hash_including('Enabled' => true)))
     end
   end
 
