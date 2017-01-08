49c32a803e1610df7b6e196e5e9b62d56e6ce7c3
Ted Johansson <drenmi@gmail.com>
2017-01-08 16:14:46 +0800

    Add `ForNode` node extension
    This change adds a node extension for `for` nodes. It gives access to
    some `for` related methods from within RuboCop.

---
+++ lib/rubocop/ast/node/for_node.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module AST
+    # A node extension for `for` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `for` nodes within RuboCop.
+    class ForNode < Node
+      # Returns the keyword of the `for` statement as a string.
+      #
+      # @return [String] the keyword of the `until` statement
+      def keyword
+        'for'
+      end
+
+      # Checks whether the `for` node has a `do` keyword.
+      #
+      # @return [Boolean] whether the `for` node has a `do` keyword
+      def do?
+        loc.begin && loc.begin.is?('do')
+      end
+
+      # Returns the iteration variable of the `for` loop.
+      #
+      # @return [Node] The iteration variable of the `for` loop
+      def variable
+        node_parts[0]
+      end
+
+      # Returns the collection the `for` loop is iterating over.
+      #
+      # @return [Node] The collection the `for` loop is iterating over
+      def collection
+        node_parts[1]
+      end
+
+      # Returns the body of the `for` loop.
+      #
+      # @return [Node, nil] The body of the `for` loop.
+      def body
+        node_parts[2]
+      end
+
+      # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # @return [Array<Node>] the different parts of the `until` statement
+      def node_parts
+        [*self]
+      end
+    end
+  end
+end
---
+++ spec/rubocop/ast/for_node_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::AST::ForNode do
+  let(:for_node) { parse_source(source).ast }
+
+  describe '.new' do
+    let(:source) { 'for foo in bar; baz; end' }
+
+    it { expect(for_node).to be_a(described_class) }
+  end
+
+  describe '#keyword' do
+    let(:source) { 'for foo in bar; baz; end' }
+
+    it { expect(for_node.keyword).to eq('for') }
+  end
+
+  describe '#do?' do
+    context 'with a do keyword' do
+      let(:source) { 'for foo in bar do baz; end' }
+
+      it { expect(for_node.do?).to be_truthy }
+    end
+
+    context 'without a do keyword' do
+      let(:source) { 'for foo in bar; baz; end' }
+
+      it { expect(for_node.do?).to be_falsey }
+    end
+  end
+
+  describe '#variable' do
+    let(:source) { 'for foo in :bar; :baz; end' }
+
+    it { expect(for_node.variable).to be_lvasgn_type }
+  end
+
+  describe '#collection' do
+    let(:source) { 'for foo in :bar; baz; end' }
+
+    it { expect(for_node.collection).to be_sym_type }
+  end
+
+  describe '#body' do
+    let(:source) { 'for foo in bar; :baz; end' }
+
+    it { expect(for_node.body).to be_sym_type }
+  end
+end
