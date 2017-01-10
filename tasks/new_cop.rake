rubocop/rubocop

commit e2ce5d8dc238153d4cae75298d30abe4d6b3da1a

[Fix #3838] Add RuboCop::Cop::Badge
 - Unify the convention of `Department/CopName` and centralize parsing
   of these tokens

 - Remove the unnecessary internal convention of downcasing departments
   and calling `#capitalize` later which breaks for departments like
   `RSpec` (see #3838)

tasks/new_cop.rake
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rubocop'
+
 desc 'Generate a new cop template'
 task :new_cop, [:cop] do |_task, args|
   def to_snake(str)
@@ -10,11 +12,13 @@ task :new_cop, [:cop] do |_task, args|
       .downcase
   end
 
-  cop_name = args[:cop] # Category/Name
+  cop_name = args[:cop] # Department/Name
   raise ArgumentError, 'One argument is required' unless cop_name
-  category, name = *cop_name.split('/', 2)
-  unless category && name
-    raise ArgumentError, 'Specify a cop name with Category/Name style'
+
+  badge = RuboCop::Cop::Badge.parse(cop_name)
+
+  unless badge.qualified?
+    raise ArgumentError, 'Specify a cop name with Department/Name style'
   end
 
   cop_code = <<-END
@@ -23,7 +27,7 @@ task :new_cop, [:cop] do |_task, args|
 # TODO: when finished, run `rake generate_cops_documentation` to update the docs
 module RuboCop
   module Cop
-    module #{category}
+    module #{badge.department}
       # TODO: Write cop description and example of bad / good code.
       #
       # @example
@@ -38,14 +42,14 @@ module RuboCop
       #
       #   # good
       #   good_method(args)
-      class #{name} < Cop
+      class #{badge.cop_name} < Cop
         # TODO: Implement the cop into here.
         #
         # In many cases, you can use a node matcher for matching node pattern.
         # See. https://github.com/bbatsov/rubocop/blob/master/lib/rubocop/node_pattern.rb
         #
         # For example
-        MSG = 'Message of #{cop_name}'.freeze
+        MSG = 'Message of #{badge.cop_name}'.freeze
 
         def_node_matcher :bad_method?, <<-PATTERN
           (:send nil :bad_method ...)
@@ -61,7 +65,7 @@ module RuboCop
 end
   END
 
-  cop_path = "lib/rubocop/cop/#{to_snake(cop_name)}.rb"
+  cop_path = "lib/rubocop/cop/#{to_snake(badge.cop_name)}.rb"
   File.write(cop_path, cop_code)
 
   spec_code = <<-END
@@ -69,7 +73,7 @@ end
 
 require 'spec_helper'
 
-describe RuboCop::Cop::#{category}::#{name} do
+describe RuboCop::Cop::#{badge.department}::#{badge.cop_name} do
   let(:config) { RuboCop::Config.new }
   subject(:cop) { described_class.new(config) }
 
@@ -80,7 +84,7 @@ describe RuboCop::Cop::#{category}::#{name} do
     inspect_source(cop, 'bad_method')
     expect(cop.offenses.size).to eq(1)
     expect(cop.messages)
-      .to eq(['Message of #{cop_name}'])
+      .to eq(['Message of #{badge.cop_name}'])
   end
 
   it 'accepts' do
@@ -100,7 +104,7 @@ created
 
 Do 4 steps
 - Add an entry to `New feature` section in CHANGELOG.md
-  - e.g. Add new `#{cop_name}` cop. ([@your_id][])
+  - e.g. Add new `#{badge.cop_name}` cop. ([@your_id][])
 - Add `require '#{cop_path.gsub(/\.rb$/, '')}'` into lib/rubocop.rb
 - Add an entry into config/enabled.yml or config/disabled.yml
 - Implement a new cop to the generated file!

lib/rubocop.rb
@@ -31,6 +31,7 @@ require 'rubocop/cop/offense'
 require 'rubocop/cop/message_annotator'
 require 'rubocop/cop/ignored_node'
 require 'rubocop/cop/autocorrect_logic'
+require 'rubocop/cop/badge'
 require 'rubocop/cop/cop'
 require 'rubocop/cop/commissioner'
 require 'rubocop/cop/corrector'

lib/rubocop/cli.rb
@@ -123,10 +123,7 @@ module RuboCop
                         selected_cops_of_department(cops, department)
                       end
 
-      if show_all
-        puts "# Department '#{department.to_s.capitalize}' " \
-             "(#{selected_cops.size}):"
-      end
+      puts "# Department '#{department}' (#{selected_cops.size}):" if show_all
 
       print_cop_details(selected_cops)
     end

lib/rubocop/config.rb
@@ -142,9 +142,7 @@ module RuboCop
     end
 
     def cop_enabled?(cop)
-      department = cop.department.to_s.capitalize!
-
-      if (dept_config = self[department])
+      if (dept_config = self[cop.department.to_s])
         return false if dept_config['Enabled'] == false
       end
 
@@ -323,7 +321,7 @@ module RuboCop
 
     def reject_obsolete_cops
       OBSOLETE_COPS.each do |cop_name, message|
-        next unless key?(cop_name) || key?(cop_name.split('/').last)
+        next unless key?(cop_name) || key?(Cop::Badge.parse(cop_name).cop_name)
         message += "\n(obsolete configuration found in #{loaded_path}, please" \
                    ' update it)'
         raise ValidationError, message

lib/rubocop/cop/badge.rb
@@ -1,0 +1,64 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    # Identifier of all cops containing a department and cop name.
+    #
+    # All cops are identified by their badge. For example, the badge
+    # for `RuboCop::Cop::Style::Tab` is `Style/Tab`. Badges can be
+    # parsed as either `Department/CopName` or just `CopName` to allow
+    # for badge references in source files that omit the department
+    # for RuboCop to infer.
+    class Badge
+      # Error raised when a badge parse fails.
+      class InvalidBadge < Error
+        MSG = 'Invalid badge %<badge>p. ' \
+              'Expected `Department/CopName` or `CopName`.'.freeze
+
+        def initialize(token)
+          super(format(MSG, badge: token))
+        end
+      end
+
+      attr_reader :department, :cop_name
+
+      def self.for(class_name)
+        new(*class_name.split('::').last(2))
+      end
+
+      def self.parse(identifier)
+        parts = identifier.split('/', 2)
+
+        raise InvalidBadge, identifier if parts.size > 2
+
+        if parts.one?
+          new(nil, *parts)
+        else
+          new(*parts)
+        end
+      end
+
+      def initialize(department, cop_name)
+        @department = department.to_sym if department
+        @cop_name   = cop_name
+      end
+
+      def ==(other)
+        hash == other.hash
+      end
+      alias eql? ==
+
+      def hash
+        [department, cop_name].hash
+      end
+
+      def to_s
+        qualified? ? "#{department}/#{cop_name}" : cop_name
+      end
+
+      def qualified?
+        !department.nil?
+      end
+    end
+  end
+end

lib/rubocop/cop/cop.rb
@@ -26,7 +26,7 @@ module RuboCop
         return name if cop_names.include?(name)
 
         basename = File.basename(name)
-        found_ns = departments.map(&:capitalize).select do |ns|
+        found_ns = departments.select do |ns|
           cop_names.include?("#{ns}/#{basename}")
         end
 
@@ -88,7 +88,7 @@ module RuboCop
       @all = CopStore.new
 
       def self.all
-        @all.without_department(:test)
+        @all.without_department(:Test)
       end
 
       def self.qualified_cop_name(name, origin)
@@ -96,23 +96,27 @@ module RuboCop
       end
 
       def self.non_rails
-        all.without_department(:rails)
+        all.without_department(:Rails)
       end
 
       def self.inherited(subclass)
         @all << subclass
       end
 
+      def self.badge
+        @badge ||= Badge.for(name)
+      end
+
       def self.cop_name
-        @cop_name ||= name.split('::').last(2).join('/')
+        badge.to_s
       end
 
       def self.department
-        @department ||= name.split('::')[-2].downcase.to_sym
+        badge.department
       end
 
       def self.lint?
-        department == :lint
+        department == :Lint
       end
 
       # Returns true if the cop name or the cop namespace matches any of the
@@ -121,7 +125,7 @@ module RuboCop
         return false unless given_names
 
         given_names.include?(cop_name) ||
-          given_names.include?(department.to_s.capitalize)
+          given_names.include?(department.to_s)
       end
 
       def initialize(config = nil, options = nil)

lib/rubocop/cop/lint/unneeded_disable.rb
@@ -159,7 +159,7 @@ module RuboCop
 
         def cop_range(comment, cop)
           matching_range(comment.loc.expression, cop) ||
-            matching_range(comment.loc.expression, cop.split('/').last) ||
+            matching_range(comment.loc.expression, Badge.parse(cop).cop_name) ||
             raise("Couldn't find #{cop} in comment: #{comment.text}")
         end
 

lib/rubocop/options.rb
@@ -179,7 +179,7 @@ module RuboCop
     def self.validate_cop_list(names)
       return unless names
 
-      departments = Cop::Cop.all.departments.map { |t| t.to_s.capitalize }
+      departments = Cop::Cop.all.departments.map(&:to_s)
 
       names.each do |name|
         next if Cop::Cop.all.any? { |c| c.cop_name == name }

commit 56c2c0183246610a3a5b6fab70be3ee43ffbafa6

Fix regression in new_cop task
Fixes regression I accidentally introduced in e2ce5d8. Resulted
in placing the cop in the wrong directory.

tasks/new_cop.rake
@@ -65,7 +65,7 @@ module RuboCop
 end
   END
 
-  cop_path = "lib/rubocop/cop/#{to_snake(badge.cop_name)}.rb"
+  cop_path = "lib/rubocop/cop/#{to_snake(badge.to_s)}.rb"
   File.write(cop_path, cop_code)
 
   spec_code = <<-END