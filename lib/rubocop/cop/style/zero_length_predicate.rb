3b6252e287763770b681fce73f6004b7154bee85
Ana María Martínez Gómez <ammartinez@suse.de>
2017-01-18 10:36:44 +0100

    Improve the documentation of ZeroLengthPredicate
    The ZeroLengthPredicate cop  not only checks for `receiver.length == 0`,
    `receiver.length > 0` and `receiver.length != 0`, but also things like
    `receiver.length < 1` and `receiver.size == 0`. Documentation updated to
    include these cases.

--- lib/rubocop/cop/style/zero_length_predicate.rb
+++ lib/rubocop/cop/style/zero_length_predicate.rb
@@ -3,21 +3,28 @@
 module RuboCop
   module Cop
     module Style
-      # This cop checks for receiver.length == 0 predicates and the
-      # negated versions receiver.length > 0 and receiver.length != 0.
-      # These can be replaced with receiver.empty? and
-      # !receiver.empty? respectively.
+      # This cop checks for numeric comparisons that can be replaced
+      # by a predicate method, such as receiver.length == 0,
+      # receiver.length > 0, receiver.length != 0,
+      # receiver.length < 1 and receiver.size == 0 that can be
+      # replaced by receiver.empty? and !receiver.empty
       #
       # @example
       #
       #   @bad
       #   [1, 2, 3].length == 0
       #   0 == "foobar".length
+      #   array.length < 1
+      #   {a: 1, b: 2}.length != 0
+      #   string.length > 0
       #   hash.size > 0
       #
       #   @good
       #   [1, 2, 3].empty?
       #   "foobar".empty?
+      #   array.empty?
+      #   !{a: 1, b: 2}.empty?
+      #   !string.empty?
       #   !hash.empty?
       class ZeroLengthPredicate < Cop
         ZERO_MSG = 'Use `empty?` instead of `%s %s %s`.'.freeze
--- manual/cops_style.md
+++ manual/cops_style.md
@@ -5554,10 +5554,11 @@ Enabled by default | Supports autocorrection
 --- | ---
 Enabled | Yes
 
-This cop checks for receiver.length == 0 predicates and the
-negated versions receiver.length > 0 and receiver.length != 0.
-These can be replaced with receiver.empty? and
-!receiver.empty? respectively.
+This cop checks for numeric comparisons that can be replaced
+by a predicate method, such as receiver.length == 0,
+receiver.length > 0, receiver.length != 0,
+receiver.length < 1 and receiver.size == 0 that can be
+replaced by receiver.empty? and !receiver.empty
 
 ### Example
 
@@ -5565,10 +5566,16 @@ These can be replaced with receiver.empty? and
 # bad
 [1, 2, 3].length == 0
 0 == "foobar".length
+array.length < 1
+{a: 1, b: 2}.length != 0
+string.length > 0
 hash.size > 0
 
 # good
 [1, 2, 3].empty?
 "foobar".empty?
+array.empty?
+!{a: 1, b: 2}.empty?
+!string.empty?
 !hash.empty?
 ```
