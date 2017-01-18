65fc85283d083f6e179ed99b635ef3a9fb09d505
Corey Osman <corey@logicminds.biz>
2017-01-17 19:32:07 -0800

    Improve the documentation of Rails/HttpPositionalArguments (#3925)

--- lib/rubocop/cop/rails/http_positional_arguments.rb
+++ lib/rubocop/cop/rails/http_positional_arguments.rb
@@ -5,7 +5,9 @@ module RuboCop
     module Rails
       # This cop is used to identify usages of http methods like `get`, `post`,
       # `put`, `patch` without the usage of keyword arguments in your tests and
-      # change them to use keyword arguments.
+      # change them to use keyword args.  This cop only applies to Rails >= 5
+      # If you are not running Rails >=5 you should disable
+      # the Rails/HttpPositionalArguments cop.
       #
       # @example
       #   # bad
