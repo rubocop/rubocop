4fab123f7bacd3292cedcb07d9bbdc3ccaed9eaa
Jonas Arvidsson <jonas054@gmail.com>
2017-01-22 08:10:33 +0100

    Add support for crlf and lf styles in Style/EndOfLine

--- lib/rubocop/cop/style/end_of_line.rb
+++ lib/rubocop/cop/style/end_of_line.rb
@@ -5,7 +5,10 @@ module RuboCop
     module Style
       # This cop checks for Windows-style line endings in the source code.
       class EndOfLine < Cop
-        MSG = 'Carriage return character detected.'.freeze
+        include ConfigurableEnforcedStyle
+
+        MSG_DETECTED = 'Carriage return character detected.'.freeze
+        MSG_MISSING = 'Carriage return character missing.'.freeze
 
         def investigate(processed_source)
           last_token = processed_source.tokens.last
@@ -14,14 +17,28 @@ module RuboCop
 
           processed_source.raw_source.each_line.with_index do |line, index|
             break if index >= last_line
-            next unless line =~ /\r$/
+            msg = offense_message(line)
+            next unless msg
+            next if unimportant_missing_cr?(index, last_line, line)
 
             range =
               source_range(processed_source.buffer, index + 1, 0, line.length)
-            add_offense(nil, range, MSG)
+            add_offense(nil, range, msg)
             # Usually there will be carriage return characters on all or none
             # of the lines in a file, so we report only one offense.
             break
+          end
+        end
+
+        # If there is no LF on the last line, we don't care if there's no CR.
+        def unimportant_missing_cr?(index, last_line, line)
+          style == :crlf && index == last_line - 1 && line !~ /\n$/
+        end
+
+        def offense_message(line)
+          case style
+          when :lf then MSG_DETECTED if line =~ /\r$/
+          else MSG_MISSING if line !~ /\r$/
           end
         end
       end
--- spec/rubocop/cop/style/end_of_line_spec.rb
+++ spec/rubocop/cop/style/end_of_line_spec.rb
@@ -3,78 +3,152 @@
 
 require 'spec_helper'
 
-describe RuboCop::Cop::Style::EndOfLine do
-  subject(:cop) { described_class.new }
+describe RuboCop::Cop::Style::EndOfLine, :config do
+  subject(:cop) { described_class.new(config) }
 
-  it 'accepts an empty file' do
-    inspect_source_file(cop, '')
-    expect(cop.offenses).to be_empty
+  shared_examples 'all configurations' do
+    it 'accepts an empty file' do
+      inspect_source_file(cop, '')
+      expect(cop.offenses).to be_empty
+    end
   end
 
-  it 'registers an offense for CR+LF' do
-    inspect_source_file(cop, ['x=0', '', "y=1\r"])
-    expect(cop.messages).to eq(['Carriage return character detected.'])
-  end
-
-  it 'highlights the whole offending line' do
-    inspect_source_file(cop, ['x=0', '', "y=1\r"])
-    expect(cop.highlights).to eq(["y=1\r"])
-  end
-
-  it 'registers an offense for CR at end of file' do
-    inspect_source_file(cop, "x=0\r")
-    expect(cop.messages).to eq(['Carriage return character detected.'])
-  end
-
-  it 'does not register offenses after __END__' do
-    inspect_source(cop, ['x=0',
-                         '__END__',
-                         "x=0\r"])
-    expect(cop.offenses).to be_empty
-  end
-
-  shared_examples 'iso-8859-15' do
+  shared_examples 'iso-8859-15' do |eol|
     it 'can inspect non-UTF-8 encoded source with proper encoding comment' do
-      inspect_source_file(cop, ['# coding: ISO-8859-15\r',
-                                "# Euro symbol: \xa4\r"])
+      inspect_source_file(cop, ["# coding: ISO-8859-15#{eol}",
+                                "# Euro symbol: \xa4#{eol}"])
       expect(cop.offenses.size).to eq(1)
     end
   end
 
-  context 'when there are many lines ending with CR+LF' do
-    it 'registers only one offense' do
-      inspect_source_file(cop, ['x=0', '', 'y=1'].join("\r\n"))
-      expect(cop.messages.size).to eq(1)
+  context 'when EnforcedStyle is crlf' do
+    let(:cop_config) { { 'EnforcedStyle' => 'crlf' } }
+    let(:messages) { ['Carriage return character missing.'] }
+    include_examples 'all configurations'
+
+    it 'registers an offense for CR+LF' do
+      inspect_source_file(cop, ['x=0', '', "y=1\r"])
+      expect(cop.messages).to eq(messages)
+      expect(cop.offenses.map(&:line)).to eq([1])
     end
 
-    include_examples 'iso-8859-15'
-  end
-
-  context 'when the default external encoding is US_ASCII' do
-    around do |example|
-      orig_encoding = Encoding.default_external
-      Encoding.default_external = Encoding::US_ASCII
-      example.run
-      Encoding.default_external = orig_encoding
+    it 'highlights the whole offending line' do
+      inspect_source_file(cop, ['x=0', '', "y=1\r"])
+      expect(cop.highlights).to eq(["x=0\n"])
     end
 
-    it 'does not crash on UTF-8 encoded non-ascii characters' do
-      inspect_source_file(cop,
-                          ['# encoding: UTF-8',
-                           'class Epd::ReportsController < EpdAreaController',
-                           "  'terecht bij uw ROM-coördinator.'",
-                           'end'].join("\n"))
+    it 'does not register offense for no CR at end of file' do
+      inspect_source_file(cop, 'x=0')
       expect(cop.offenses).to be_empty
     end
 
-    include_examples 'iso-8859-15'
+    it 'does not register offenses after __END__' do
+      inspect_source(cop, ["x=0\r",
+                           '__END__',
+                           'x=0'])
+      expect(cop.offenses).to be_empty
+    end
+
+    context 'and there are many lines ending with LF' do
+      it 'registers only one offense' do
+        inspect_source_file(cop, ['x=0', '', 'y=1'].join("\n"))
+        expect(cop.messages.size).to eq(1)
+      end
+
+      include_examples 'iso-8859-15', ''
+    end
+
+    context 'and the default external encoding is US_ASCII' do
+      around do |example|
+        orig_encoding = Encoding.default_external
+        Encoding.default_external = Encoding::US_ASCII
+        example.run
+        Encoding.default_external = orig_encoding
+      end
+
+      it 'does not crash on UTF-8 encoded non-ascii characters' do
+        source = ['# encoding: UTF-8',
+                  'class Epd::ReportsController < EpdAreaController',
+                  "  'terecht bij uw ROM-coördinator.'",
+                  'end'].join("\r\n")
+        inspect_source_file(cop, source)
+        expect(cop.offenses).to be_empty
+      end
+
+      include_examples 'iso-8859-15', ''
+    end
+
+    context 'and source is a string' do
+      it 'registers an offense' do
+        inspect_source(cop, "x=0\ny=1")
+
+        expect(cop.messages).to eq(['Carriage return character missing.'])
+      end
+    end
   end
 
-  context 'when source is a string' do
-    it 'registers an offense' do
-      inspect_source(cop, "x=0\r")
+  context 'when EnforcedStyle is lf' do
+    let(:cop_config) { { 'EnforcedStyle' => 'lf' } }
+    include_examples 'all configurations'
 
+    it 'registers an offense for CR+LF' do
+      inspect_source_file(cop, ['x=0', '', "y=1\r"])
       expect(cop.messages).to eq(['Carriage return character detected.'])
+      expect(cop.offenses.map(&:line)).to eq([3])
+    end
+
+    it 'highlights the whole offending line' do
+      inspect_source_file(cop, ['x=0', '', "y=1\r"])
+      expect(cop.highlights).to eq(["y=1\r"])
+    end
+
+    it 'registers an offense for CR at end of file' do
+      inspect_source_file(cop, "x=0\r")
+      expect(cop.messages).to eq(['Carriage return character detected.'])
+    end
+
+    it 'does not register offenses after __END__' do
+      inspect_source(cop, ['x=0',
+                           '__END__',
+                           "x=0\r"])
+      expect(cop.offenses).to be_empty
+    end
+
+    context 'and there are many lines ending with CR+LF' do
+      it 'registers only one offense' do
+        inspect_source_file(cop, ['x=0', '', 'y=1'].join("\r\n"))
+        expect(cop.messages.size).to eq(1)
+      end
+
+      include_examples 'iso-8859-15', "\r"
+    end
+
+    context 'and the default external encoding is US_ASCII' do
+      around do |example|
+        orig_encoding = Encoding.default_external
+        Encoding.default_external = Encoding::US_ASCII
+        example.run
+        Encoding.default_external = orig_encoding
+      end
+
+      it 'does not crash on UTF-8 encoded non-ascii characters' do
+        source = ['# encoding: UTF-8',
+                  'class Epd::ReportsController < EpdAreaController',
+                  "  'terecht bij uw ROM-coördinator.'",
+                  'end'].join("\n")
+        inspect_source_file(cop, source)
+        expect(cop.offenses).to be_empty
+      end
+
+      include_examples 'iso-8859-15', "\r"
+    end
+
+    context 'and source is a string' do
+      it 'registers an offense' do
+        inspect_source(cop, "x=0\r")
+
+        expect(cop.messages).to eq(['Carriage return character detected.'])
+      end
     end
   end
 end
