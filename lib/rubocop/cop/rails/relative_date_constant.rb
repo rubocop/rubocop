be120fbcd8ac018a0bcb83540f8dddb3a81c5e30
sinsoku <sinsoku.listy@gmail.com>
2017-02-09 21:29:25 +0900

    Simplify code

--- lib/rubocop/cop/rails/relative_date_constant.rb
+++ lib/rubocop/cop/rails/relative_date_constant.rb
@@ -21,7 +21,7 @@ module RuboCop
         MSG = 'Do not use `%s` in constant variable, because they ' \
               'are evaluated only once at system running.'.freeze
 
-        BAD_METHODS = %i(ago from_now since until).freeze
+        RELATIVE_DATE_METHODS = %i(ago from_now since until).freeze
 
         def on_casgn(node)
           value = node.child_nodes.last
@@ -31,16 +31,16 @@ module RuboCop
         private
 
         def check(node)
-          if bad_method_without_args?(node)
+          if bad_method?(node)
             add_offense(node, :expression, format(MSG, node.method_name))
           else
             node.each_child_node { |n| check(n) }
           end
         end
 
-        def bad_method_without_args?(node)
+        def bad_method?(node)
           node.send_type? &&
-            BAD_METHODS.include?(node.method_name) &&
+            RELATIVE_DATE_METHODS.include?(node.method_name) &&
             node.method_args.empty?
         end
       end
