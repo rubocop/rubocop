380a8077d2ab3bcd6e26c138a118b935012fe276
Masataka Kuwabara <kuwabara@pocke.me>
2017-02-21 21:28:20 +0900

    Add spec for `ensure_node` and `resbody_node`

---
+++ spec/rubocop/ast/resbody_node_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::AST::ResbodyNode do
+  let(:resbody_node) do
+    begin_node = parse_source(source).ast
+    rescue_node, = *begin_node
+    rescue_node.children[1]
+  end
+
+  describe '.new' do
+    let(:source) { 'begin; beginbody; rescue; rescuebody; end' }
+
+    it { expect(resbody_node).to be_a(described_class) }
+  end
+
+  describe '#body' do
+    let(:source) { 'begin; beginbody; rescue Error => ex; :rescuebody; end' }
+
+    it { expect(resbody_node.body).to be_sym_type }
+  end
+end
