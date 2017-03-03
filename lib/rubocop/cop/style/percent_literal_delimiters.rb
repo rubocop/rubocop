1ad9a403a17baa3865089d078ce16b379cb639eb
Kevin Deisz <kevin.deisz@gmail.com>
2017-03-03 11:53:43 -0500

    More documentation

--- lib/rubocop/cop/style/percent_literal_delimiters.rb
+++ lib/rubocop/cop/style/percent_literal_delimiters.rb
@@ -4,9 +4,25 @@ module RuboCop
   module Cop
     module Style
       # This cop enforces the consistent usage of `%`-literal delimiters.
+      #
       # Specify the 'default' key to set all preferred delimiters at once. You
       # can continue to specify individual preferred delimiters to override the
       # default.
+      #
+      # @example
+      #   # Style/PercentLiteralDelimiters:
+      #   #   PreferredDelimiters:
+      #   #     default: ()
+      #   #     %i:      []
+      #
+      #   # good
+      #   %w(alpha beta) + %i[gamma delta]
+      #
+      #   # bad
+      #   %W[alpha #{beta}]
+      #
+      #   # bad
+      #   %I[alpha beta]
       class PercentLiteralDelimiters < Cop
         include PercentLiteral
 
--- manual/cops_style.md
+++ manual/cops_style.md
@@ -4131,9 +4131,28 @@ Enabled by default | Supports autocorrection
 Enabled | Yes
 
 This cop enforces the consistent usage of `%`-literal delimiters.
+
 Specify the 'default' key to set all preferred delimiters at once. You
 can continue to specify individual preferred delimiters to override the
 default.
+
+### Example
+
+```ruby
+# Style/PercentLiteralDelimiters:
+#   PreferredDelimiters:
+#     default: ()
+#     %i:      []
+
+# good
+%w(alpha beta) + %i[gamma delta]
+
+# bad
+%W[alpha #{beta}]
+
+# bad
+%I[alpha beta]
+```
 
 ### Important attributes
 
