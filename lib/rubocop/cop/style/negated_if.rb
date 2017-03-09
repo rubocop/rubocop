2331d0f5539e6760550505afc9407a827f7368b8
Brandon Weiss <brandon@anti-pattern.com>
2017-03-09 11:37:24 -0800

    Adding “prefix” and “postfix” styles to Style/NegatedIf
    [Fixes bbatsov/rubocop#3961]
    
    Some people have a strong preference against using unless with an `if`
    statement in multi-line/block form:
    
    ```ruby
    unless foo
    bar
    end
    ```
    
    But they think it still makes sense to use it in single-line/modifier
    form:
    
    ```ruby
    bar unless foo
    ```
    
    I’ve added three supported styles:
    
    * `prefix` will only enforce using `unless` in the multi-line/block form
    * `postfix` will only enforce using `unless` in the
    single-line/modifier form
    * `both` will enforce using `unless` in both the `prefix` and `postfix`
    forms

--- lib/rubocop/cop/style/negated_if.rb
+++ lib/rubocop/cop/style/negated_if.rb
@@ -4,14 +4,75 @@ module RuboCop
   module Cop
     module Style
       # Checks for uses of if with a negated condition. Only ifs
-      # without else are considered.
+      # without else are considered. There are three different styles:
+      #
+      # @example
+      #
+      # both - enforces `unless` for `prefix` and `postfix` conditionals
+      #
+      #   # good
+      #
+      #   unless foo
+      #     bar
+      #   end
+      #
+      #   # bad
+      #
+      #   if !foo
+      #     bar
+      #   end
+      #
+      #   # good
+      #
+      #   bar unless foo
+      #
+      #   # bad
+      #
+      #   bar if !foo
+      #
+      # prefix - enforces `unless` for just `prefix` conditionals
+      #
+      #   # good
+      #
+      #   unless foo
+      #     bar
+      #   end
+      #
+      #   # bad
+      #
+      #   if !foo
+      #     bar
+      #   end
+      #
+      #   # good
+      #
+      #   bar if !foo
+      #
+      # postfix - enforces `unless` for just `postfix` conditionals
+      #
+      #   # good
+      #
+      #   bar unless foo
+      #
+      #   # bad
+      #
+      #   bar if !foo
+      #
+      #   # good
+      #
+      #   if !foo
+      #     bar
+      #   end
       class NegatedIf < Cop
+        include ConfigurableEnforcedStyle
         include NegativeConditional
 
         MSG = 'Favor `%s` over `%s` for negative conditions.'.freeze
 
         def on_if(node)
           return if node.elsif? || node.ternary?
+          return if style == :prefix && node.modifier_form?
+          return if style == :postfix && !node.modifier_form?
 
           check_negative_conditional(node)
         end
