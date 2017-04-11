70a2dc07b7742cb939a9c3822959a9b4083e0256
Scott Matthewman <scott@altmetric.com>
2017-04-11 16:27:21 +0100

    Add `MinSize` option to Style/SymbolArray
    In line with `Style/WordArray`, allow a `MinSize` setting to determine
    the number of items to allow per array before the cop kicks in.
    
    The minimum size handling is extracted into a new mixin, and some
    other similar functionality between the two cops has been better
    reflected with some method renaming and reordering.

--- lib/rubocop/cop/style/symbol_array.rb
+++ lib/rubocop/cop/style/symbol_array.rb
@@ -10,6 +10,11 @@ module RuboCop
       # projects which do not want to use that syntax, perhaps because they
       # support a version of Ruby lower than 2.0.
       #
+      # Configuration option: MinSize
+      # If set, arrays with fewer elements than this value will not trigger the
+      # cop. For example, a `MinSize of `3` will not enforce a style on an array
+      # of 2 or fewer elements.
+      #
       # @example
       #   EnforcedStyle: percent (default)
       #
@@ -28,8 +33,9 @@ module RuboCop
       #   # bad
       #   %i[foo bar baz]
       class SymbolArray < Cop
-        include ConfigurableEnforcedStyle
+        include ArrayMinSize
         include ArraySyntax
+        include ConfigurableEnforcedStyle
         include PercentLiteral
         extend TargetRubyVersion
 
@@ -37,6 +43,10 @@ module RuboCop
 
         PERCENT_MSG = 'Use `%i` or `%I` for an array of symbols.'.freeze
         ARRAY_MSG = 'Use `[]` for an array of symbols.'.freeze
+
+        class << self
+          attr_accessor :largest_brackets
+        end
 
         def on_array(node)
           if bracketed_array_of?(:sym, node)
@@ -46,18 +56,27 @@ module RuboCop
           end
         end
 
+        def autocorrect(node)
+          if style == :percent
+            correct_percent(node, 'i')
+          else
+            correct_bracketed(node)
+          end
+        end
+
         private
 
         def check_bracketed_array(node)
-          return if comments_in_array?(node)
-          return if symbols_contain_spaces?(node)
+          return if comments_in_array?(node) ||
+                    symbols_contain_spaces?(node) ||
+                    below_array_length?(node)
 
-          style_detected(:brackets)
+          array_style_detected(:brackets, node.values.size)
           add_offense(node, :expression, PERCENT_MSG) if style == :percent
         end
 
         def check_percent_array(node)
-          style_detected(:percent)
+          array_style_detected(:percent, node.values.size)
           add_offense(node, :expression, ARRAY_MSG) if style == :brackets
         end
 
@@ -74,14 +93,6 @@ module RuboCop
           node.children.any? do |sym|
             content, = *sym
             content =~ / /
-          end
-        end
-
-        def autocorrect(node)
-          if style == :percent
-            correct_percent(node, 'i')
-          else
-            correct_bracketed(node)
           end
         end
 
