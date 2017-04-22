953bdd98b41fc108f498eba9a8a9955a73fb1aee
ryu39 <dev.ryu39@gmail.com>
2017-04-23 02:58:57 +0900

    Fix generate_cops_documentation warnings
    * Fix parameter name 'options' in lib/rubocop/cop/message_annotator.rb
    * Indent multiline codes in lib/rubocop/style/copyright.rb and
      lib/rubocop/note_pattern.rb.
    * Use `` for codes in sentense in lib/rubocop/style/hash_syntax.rb.

--- lib/rubocop/cop/style/copyright.rb
+++ lib/rubocop/cop/style/copyright.rb
@@ -8,8 +8,8 @@ module RuboCop
       # The default regexp for an acceptable copyright notice can be found in
       # config/default.yml.  The default can be changed as follows:
       #
-      # Style/Copyright:
-      #   Notice: '^Copyright (\(c\) )?2\d{3} Acme Inc'
+      #     Style/Copyright:
+      #       Notice: '^Copyright (\(c\) )?2\d{3} Acme Inc'
       #
       # This regex string is treated as an unanchored regex.  For each file
       # that RuboCop scans, a comment that matches this regex must be found or
