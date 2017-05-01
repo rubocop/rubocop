98819788550838802b3ee39c795f409cf66041ab
John Backus <johncbackus@gmail.com>
2017-04-30 21:00:48 -0400

    [Fix #3905] Add `expect_offense` test sugar
    Declarative, clear, and powerful testing sugar from rubocop-rspec!
    
    Usage:
    
    ```ruby
    expect_offense(<<-RUBY)
    foo(bar)
    ^^^ This is some violation
        ^^^ This is another violation
    RUBY
    ```
    
    This is equivalent to writing a test that asserts:
    
    1. Inspecting this source generates two offenses
    2. The first and secon violation are on line 1
    3. The first offense line range is 0..3, the second is 4..7
    4. The first offense has the message "This is some violation"
    5. The second offense has the message "This is another violation"
    6. There are no other violations from this cop in this example source
    
    If you want to assert there no offenses then use `expect_no_offenses`:
    
    ```ruby
    expect_no_offenses(<<-RUBY)
    foo(bar)
    RUBY
    ```
    
    Also, writing the following
    
    ```ruby
    expect_offenses(<<-RUBY)
    foo(bar)
    RUBY
    ```
    
    will raise an error saying to use `expect_no_offenses` instead.

---
+++ lib/rubocop/rspec/expect_offense.rb
@@ -0,0 +1,167 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module RSpec
+    # Mixin for `expect_offense` and `expect_no_offenses`
+    #
+    # This mixin makes it easier to specify strict offense expectations
+    # and a declarative and visual fashion. Just type out the code that
+    # should generate a offense, annotate code by writing '^'s
+    # underneath each character that should be highlighted, and follow
+    # the carets with a string (separated by a space) that is the
+    # message of the offense. You can include multiple offenses in
+    # one code snippet.
+    #
+    # @example Usage
+    #
+    #     expect_offense(<<-RUBY.strip_indent)
+    #       a do
+    #         b
+    #       end.c
+    #       ^^^^^ Avoid chaining a method call on a do...end block.
+    #     RUBY
+    #
+    # @example Equivalent assertion without `expect_offense`
+    #
+    #     inspect_source(cop, <<-RUBY.strip_indent)
+    #       a do
+    #         b
+    #       end.c
+    #     RUBY
+    #
+    #     expect(cop.offenses.size).to be(1)
+    #
+    #     offense = cop.offenses.first
+    #     expect(offense.line).to be(3)
+    #     expect(offense.column_range).to be(0...5)
+    #     expect(offense.message).to eql(
+    #       'Avoid chaining a method call on a do...end block.'
+    #     )
+    #
+    # If you do not want to specify an offense then use the
+    # companion method `expect_no_offenses`. This method is a much
+    # simpler assertion since it just inspects the source and checks
+    # that there were no offenses. The `expect_offenses` method has
+    # to do more work by parsing out lines that contain carets.
+    module ExpectOffense
+      DEFAULT_FILENAME = 'example.rb'.freeze
+
+      def expect_offense(source, filename = DEFAULT_FILENAME)
+        expected_annotations = AnnotatedSource.parse(source)
+
+        if expected_annotations.plain_source == source
+          raise 'Use expect_no_offenses to assert that no offenses are found'
+        end
+
+        inspect_source(cop, expected_annotations.plain_source, filename)
+        actual_annotations =
+          expected_annotations.with_offense_annotations(cop.offenses)
+        expect(expected_annotations.to_s).to eq(actual_annotations.to_s)
+      end
+
+      def expect_no_offenses(source, filename = DEFAULT_FILENAME)
+        inspect_source(cop, source, filename)
+
+        expect(cop.offenses).to be_empty
+      end
+
+      # Parsed representation of code annotated with the `^^^ Message` style
+      class AnnotatedSource
+        ANNOTATION_PATTERN = /\A\s*\^+ /
+
+        # @param annotated_source [String] string passed to the matchers
+        #
+        # Separates annotation lines from source lines. Tracks the real
+        # source line number that each annotation corresponds to.
+        #
+        # @return [AnnotatedSource]
+        def self.parse(annotated_source)
+          source      = []
+          annotations = []
+
+          annotated_source.each_line do |source_line|
+            if source_line =~ ANNOTATION_PATTERN
+              annotations << [source.size, source_line]
+            else
+              source << source_line
+            end
+          end
+
+          new(source, annotations)
+        end
+
+        # @param lines [Array<String>]
+        # @param annotations [Array<(Integer, String)>]
+        #   each entry is the annotated line number and the annotation text
+        #
+        # @note annotations are sorted so that reconstructing the annotation
+        #   text via {#to_s} is deterministic
+        def initialize(lines, annotations)
+          @lines       = lines.freeze
+          @annotations = annotations.sort.freeze
+        end
+
+        # Construct annotated source string (like what we parse)
+        #
+        # Reconstruct a deterministic annotated source string. This is
+        # useful for eliminating semantically irrelevant annotation
+        # ordering differences.
+        #
+        # @example standardization
+        #
+        #     source1 = AnnotatedSource.parse(<<-RUBY)
+        #     line1
+        #     ^ Annotation 1
+        #      ^^ Annotation 2
+        #     RUBY
+        #
+        #     source2 = AnnotatedSource.parse(<<-RUBY)
+        #     line1
+        #      ^^ Annotation 2
+        #     ^ Annotation 1
+        #     RUBY
+        #
+        #     source1.to_s == source2.to_s # => true
+        #
+        # @return [String]
+        def to_s
+          reconstructed = lines.dup
+
+          annotations.reverse_each do |line_number, annotation|
+            reconstructed.insert(line_number, annotation)
+          end
+
+          reconstructed.join
+        end
+
+        # Return the plain source code without annotations
+        #
+        # @return [String]
+        def plain_source
+          lines.join
+        end
+
+        # Annotate the source code with the RuboCop offenses provided
+        #
+        # @param offenses [Array<RuboCop::Cop::Offense>]
+        #
+        # @return [self]
+        def with_offense_annotations(offenses)
+          offense_annotations =
+            offenses.map do |offense|
+              indent     = ' ' * offense.column
+              carets     = '^' * offense.column_length
+
+              [offense.line, "#{indent}#{carets} #{offense.message}\n"]
+            end
+
+          self.class.new(lines, offense_annotations)
+        end
+
+        private
+
+        attr_reader :lines, :annotations
+      end
+    end
+  end
+end
