555c6a2e9988526b2d3bd121f654a2458b6f4fa3
John Backus <johncbackus@gmail.com>
2017-05-06 22:39:05 -0700

    Use expect_offense for MethodCallOnDoEndBlock

--- spec/rubocop/cop/style/method_called_on_do_end_block_spec.rb
+++ spec/rubocop/cop/style/method_called_on_do_end_block_spec.rb
@@ -5,62 +5,57 @@ describe RuboCop::Cop::Style::MethodCalledOnDoEndBlock do
 
   context 'with a multi-line do..end block' do
     it 'registers an offense for a chained call' do
-      inspect_source(cop, <<-END.strip_indent)
+      expect_offense(<<-RUBY.strip_indent)
         a do
           b
         end.c
-      END
-      expect(cop.offenses.size).to eq(1)
-      expect(cop.highlights).to eq(['end.c'])
+        ^^^^^ Avoid chaining a method call on a do...end block.
+      RUBY
     end
 
     it 'accepts it if there is no chained call' do
-      inspect_source(cop, <<-END.strip_indent)
+      expect_no_offenses(<<-RUBY.strip_indent)
         a do
           b
         end
-      END
-      expect(cop.offenses).to be_empty
+      RUBY
     end
 
     it 'accepts a chained block' do
-      inspect_source(cop, <<-END.strip_indent)
+      expect_no_offenses(<<-RUBY.strip_indent)
         a do
           b
         end.c do
           d
         end
-      END
-      expect(cop.offenses).to be_empty
+      RUBY
     end
   end
 
   context 'with a single-line do..end block' do
     it 'registers an offense for a chained call' do
-      inspect_source(cop, 'a do b end.c')
-      expect(cop.offenses.size).to eq(1)
-      expect(cop.highlights).to eq(['end.c'])
+      expect_offense(<<-RUBY.strip_indent)
+        a do b end.c
+               ^^^^^ Avoid chaining a method call on a do...end block.
+      RUBY
     end
 
     it 'accepts a single-line do..end block with a chained block' do
-      inspect_source(cop, 'a do b end.c do d end')
-      expect(cop.offenses).to be_empty
+      expect_no_offenses('a do b end.c do d end')
     end
   end
 
   context 'with a {} block' do
     it 'accepts a multi-line block with a chained call' do
-      inspect_source(cop, <<-END.strip_indent)
+      expect_no_offenses(<<-RUBY.strip_indent)
         a {
           b
         }.c
-      END
-      expect(cop.offenses).to be_empty
+      RUBY
     end
 
     it 'accepts a single-line block with a chained call' do
-      inspect_source(cop, 'a { b }.c')
-      expect(cop.offenses).to be_empty
+      expect_no_offenses('a { b }.c')
     end
   end
 end
