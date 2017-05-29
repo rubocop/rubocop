0d524814278bbf32149915c869432bd249240ec3
Ted Johansson <drenmi@gmail.com>
2017-05-29 14:41:08 +0800

    Bring down ABC-size in `Layout/LeadingCommentSpace` cop

--- lib/rubocop/cop/layout/leading_comment_space.rb
+++ lib/rubocop/cop/layout/leading_comment_space.rb
@@ -3,39 +3,54 @@
 module RuboCop
   module Cop
     module Layout
-      # This cop checks whether comments have a leading space
-      # after the # denoting the start of the comment. The
-      # leading space is not required for some RDoc special syntax,
-      # like #++, #--, #:nodoc, etc. Neither is it required for
-      # =begin/=end comments.
+      # This cop checks whether comments have a leading space after the
+      # `#` denoting the start of the comment. The leading space is not
+      # required for some RDoc special syntax, like `#++`, `#--`,
+      # `#:nodoc`, `=begin`- and `=end` comments, "shebang" directives,
+      # or rackup options.
+      #
+      # @example
+      #
+      #   # bad
+      #   #Some comment
+      #
+      #   # good
+      #   # Some comment
       class LeadingCommentSpace < Cop
-        MSG = 'Missing space after #.'.freeze
+        MSG = 'Missing space after `#`.'.freeze
 
         def investigate(processed_source)
           processed_source.comments.each do |comment|
             next unless comment.text =~ /\A#+[^#\s=:+-]/
-            next if comment.text.start_with?('#!') && comment.loc.line == 1
-
-            # in config.ru files, if the first line starts with #\ it is treated
-            # as options (e.g. `#\ -p 8765` sets the request port to 8765)
-            next if comment.text.start_with?('#\\') && comment.loc.line == 1 &&
-                    config_ru?(processed_source.buffer.name)
+            next if comment.loc.line == 1 && allowed_on_first_line?(comment)
 
             add_offense(comment, :expression)
           end
         end
 
+        private
+
         def autocorrect(comment)
           expr = comment.loc.expression
-          b = expr.begin_pos
-          hash_mark = range_between(b, b + 1)
+          hash_mark = range_between(expr.begin_pos, expr.begin_pos + 1)
+
           ->(corrector) { corrector.insert_after(hash_mark, ' ') }
         end
 
-        private
+        def allowed_on_first_line?(comment)
+          shebang?(comment) || rackup_config_file? && rackup_options?(comment)
+        end
 
-        def config_ru?(file_path)
-          File.basename(file_path).eql?('config.ru')
+        def shebang?(comment)
+          comment.text.start_with?('#!')
+        end
+
+        def rackup_options?(comment)
+          comment.text.start_with?('#\\')
+        end
+
+        def rackup_config_file?
+          File.basename(processed_source.buffer.name).eql?('config.ru')
         end
       end
     end
