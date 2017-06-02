6cfcb8ef1677c8e4ac5ec6bd96da98105e7aac98
Ted Johansson <drenmi@gmail.com>
2017-06-02 16:16:05 +0800

    [Fix #4451] Make `Style/AndOr` cop aware of comparison methods
    This cop would behave badly when a comparison method was chained with an
    `and` or an `or`. E.g.:
    
    ```
    foo==1 or bar
    ```
    
    would autocorrect to:
    
    ```
    foo == () or bar
    ```
    
    This change fixes that, and adds documentation to the cop.

--- lib/rubocop/cop/style/and_or.rb
+++ lib/rubocop/cop/style/and_or.rb
@@ -3,60 +3,72 @@
 module RuboCop
   module Cop
     module Style
-      # This cop checks for uses of *and* and *or*.
+      # This cop checks for uses of `and` and `or`, and suggests using `&&` and
+      # `|| instead`. It can be configured to check only in conditions, or in
+      # all contexts.
+      #
+      # @example
+      #
+      #   # EnforcedStyle: always (default)
+      #
+      #   # good
+      #   foo.save && return
+      #   if foo && bar
+      #
+      #   # bad
+      #   foo.save and return
+      #   if foo and bar
+      #
+      # @example
+      #
+      #   # EnforcedStyle: conditionals
+      #
+      #   # good
+      #   foo.save && return
+      #   foo.save and return
+      #   if foo && bar
+      #
+      #   # bad
+      #   if foo and bar
       class AndOr < Cop
         include ConfigurableEnforcedStyle
 
         MSG = 'Use `%s` instead of `%s`.'.freeze
 
-        OPS = { 'and' => '&&', 'or' => '||' }.freeze
-
         def on_and(node)
-          process_logical_op(node) if style == :always
+          process_logical_operator(node) if style == :always
         end
-
-        def on_or(node)
-          process_logical_op(node) if style == :always
-        end
+        alias on_or on_and
 
         def on_if(node)
           on_conditionals(node) if style == :conditionals
         end
-
-        def on_while(node)
-          on_conditionals(node) if style == :conditionals
-        end
-
-        def on_while_post(node)
-          on_conditionals(node) if style == :conditionals
-        end
-
-        def on_until(node)
-          on_conditionals(node) if style == :conditionals
-        end
-
-        def on_until_post(node)
-          on_conditionals(node) if style == :conditionals
-        end
+        alias on_while      on_if
+        alias on_while_post on_if
+        alias on_until      on_if
+        alias on_until_post on_if
 
         private
 
         def on_conditionals(node)
           node.condition.each_node(*LOGICAL_OPERATOR_NODES) do |logical_node|
-            process_logical_op(logical_node)
+            process_logical_operator(logical_node)
           end
         end
 
-        def process_logical_op(node)
+        def process_logical_operator(node)
           return if node.logical_operator?
 
-          add_offense(node, :operator,
-                      format(MSG, node.alternate_operator, node.operator))
+          add_offense(node, :operator)
+        end
+
+        def message(node)
+          format(MSG, node.alternate_operator, node.operator)
         end
 
         def autocorrect(node)
           lambda do |corrector|
-            [*node].each do |expr|
+            node.each_child_node do |expr|
               if expr.send_type?
                 correct_send(expr, corrector)
               elsif expr.return_type?
@@ -73,15 +85,17 @@ module RuboCop
         def correct_send(node, corrector)
           return correct_not(node, node.receiver, corrector) if node.method?(:!)
           return correct_setter(node, corrector) if node.setter_method?
+          return correct_other(node, corrector) if node.comparison_method?
+
           return unless correctable_send?(node)
 
-          corrector.replace(whitespace_before_arg(node), '('.freeze)
-          corrector.insert_after(node.last_argument.source_range, ')'.freeze)
+          corrector.replace(whitespace_before_arg(node), '(')
+          corrector.insert_after(node.last_argument.source_range, ')')
         end
 
         def correct_setter(node, corrector)
-          corrector.insert_before(node.receiver.source_range, '('.freeze)
-          corrector.insert_after(node.last_argument.source_range, ')'.freeze)
+          corrector.insert_before(node.receiver.source_range, '(')
+          corrector.insert_after(node.last_argument.source_range, ')')
         end
 
         # ! is a special case:
@@ -108,7 +122,7 @@ module RuboCop
         end
 
         def correctable_send?(node)
-          !node.parenthesized? && !node.method?(:[]) && node.arguments?
+          !node.parenthesized? && node.arguments? && !node.method?(:[])
         end
 
         def whitespace_before_arg(node)
