04cde601fc3104da24c43cc4d9e16b0b7b7929f0
Ted Johansson <drenmi@gmail.com>
2017-06-07 11:43:19 +0800

    Make `Style/HeredocDelimiters` aware of "naked" heredocs
    This cop would not register offenses for "naked" heredocs with bad
    delimiters, e.g.:
    
    ```
    <<END
      foo
    END
    ```
    
    This change fixes that, and also improves test coverage for the cop to
    include tests for multiple heredocs starting on the same line.

--- spec/rubocop/cop/style/heredoc_delimiters_spec.rb
+++ spec/rubocop/cop/style/heredoc_delimiters_spec.rb
@@ -67,4 +67,75 @@ describe RuboCop::Cop::Style::HeredocDelimiters, :config do
       RUBY
     end
   end
+
+  context 'with a naked heredoc', :ruby23 do
+    it 'registers an offense with a non-meaningful delimiter' do
+      expect_offense(<<-RUBY.strip_indent)
+        <<END
+          foo
+        END
+        ^^^ Use meaningful heredoc delimiters.
+      RUBY
+    end
+
+    it 'does not register an offense with a meaningful delimiter' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        <<SQL
+          foo
+        SQL
+      RUBY
+    end
+  end
+
+  context 'when the delimiter contains non-letter characters' do
+    it 'does not register an offense when delimiter contains an underscore' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        <<-SQL_CODE
+          foo
+        SQL_CODE
+      RUBY
+    end
+
+    it 'does not register an offense when delimiter contains a number' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        <<-BASE64
+          foo
+        BASE64
+      RUBY
+    end
+  end
+
+  context 'with multiple heredocs starting on the same line' do
+    it 'registers an offense with a leading non-meaningful delimiter' do
+      expect_offense(<<-RUBY.strip_indent)
+        foo(<<-END, <<-SQL)
+          foo
+        END
+        ^^^ Use meaningful heredoc delimiters.
+          bar
+        SQL
+      RUBY
+    end
+
+    it 'registers an offense with a trailing non-meaningful delimiter' do
+      expect_offense(<<-RUBY.strip_indent)
+        foo(<<-SQL, <<-END)
+          foo
+        SQL
+          bar
+        END
+        ^^^ Use meaningful heredoc delimiters.
+      RUBY
+    end
+
+    it 'does not register an offense with meaningful delimiters' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        foo(<<-SQL, <<-JS)
+          foo
+        SQL
+          bar
+        JS
+      RUBY
+    end
+  end
 end
