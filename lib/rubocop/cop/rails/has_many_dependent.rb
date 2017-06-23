fdf47aacd01fdacde5ae09d94f216a965a711cec
Diego Algorta <diego@oboxodo.com>
2017-06-23 09:42:13 -0300

    Improve documentation

--- lib/rubocop/cop/rails/has_many_dependent.rb
+++ lib/rubocop/cop/rails/has_many_dependent.rb
@@ -3,18 +3,20 @@
 module RuboCop
   module Cop
     module Rails
-      # This cop looks for has_many or has_one associations that don't specify a
-      # :dependent option.
+      # This cop looks for `has_many` or `has_one` associations that don't
+      # specify a `:dependent` option.
       #
       # @example
       #   # bad
-      #   class Post < ActiveRecord::Base
+      #   class User < ActiveRecord::Base
       #     has_many :comments
+      #     has_one :avatar
       #   end
       #
       #   # good
-      #   class Post < ActiveRecord::Base
-      #     has_many :comments, dependent: :destroy
+      #   class User < ActiveRecord::Base
+      #     has_many :comments, dependent: :restrict_with_exception
+      #     has_one :avatar, dependent: :destroy
       #   end
       class HasManyDependent < Cop
         MSG = 'Specify a `:dependent` option.'.freeze
--- manual/cops_rails.md
+++ manual/cops_rails.md
@@ -471,20 +471,22 @@ Enabled by default | Supports autocorrection
 --- | ---
 Enabled | No
 
-This cop looks for has_many or has_one associations that don't specify a
-:dependent option.
+This cop looks for `has_many` or `has_one` associations that don't
+specify a `:dependent` option.
 
 ### Example
 
 ```ruby
 # bad
-class Post < ActiveRecord::Base
+class User < ActiveRecord::Base
   has_many :comments
+  has_one :avatar
 end
 
 # good
-class Post < ActiveRecord::Base
-  has_many :comments, dependent: :destroy
+class User < ActiveRecord::Base
+  has_many :comments, dependent: :restrict_with_exception
+  has_one :avatar, dependent: :destroy
 end
 ```
 
