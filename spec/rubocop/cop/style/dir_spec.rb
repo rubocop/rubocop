4a1faa9e25e2c276c9644d4246568e89cf5c20e9
Ted Johansson <drenmi@gmail.com>
2017-06-25 23:41:03 +0800

    Add new `Style/Dir` cop
    This cop checks for places where the `#__dir__` method can replace more
    complex constructs to retrieve a canonicalized absolute path to the
    current file.

---
+++ spec/rubocop/cop/style/dir_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+describe RuboCop::Cop::Style::Dir, :config do
+  subject(:cop) { described_class.new(config) }
+
+  shared_examples 'auto-correct' do |original, expected|
+    it 'auto-corrects' do
+      new_source = autocorrect_source(original)
+
+      expect(new_source).to eq(expected)
+    end
+  end
+
+  it 'registers an offense when using `#expand_path` and `#dirname`' do
+    expect_offense(<<-RUBY.strip_indent)
+      File.expand_path(File.dirname(__FILE__))
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `__dir__` to get an absolute path to the current file's directory.
+    RUBY
+  end
+
+  it_behaves_like 'auto-correct',
+                  'File.expand_path(File.dirname(__FILE__))',
+                  '__dir__'
+
+  it 'registers an offense when using `#dirname` and `#realpath`' do
+    expect_offense(<<-RUBY.strip_indent)
+      File.dirname(File.realpath(__FILE__))
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `__dir__` to get an absolute path to the current file's directory.
+    RUBY
+  end
+
+  it_behaves_like 'auto-correct',
+                  'File.dirname(File.realpath(__FILE__))',
+                  '__dir__'
+
+  context 'when target ruby version is 1.9', :ruby19 do
+    it 'does not register an offense when using `#expand_path` and ' \
+       '`#dirname`' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        File.expand_path(File.dirname(__FILE__))
+      RUBY
+    end
+
+    it 'does not register an offense when using `#dirname` and `#realpath`' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+      File.dirname(File.realpath(__FILE__))
+      RUBY
+    end
+  end
+end
