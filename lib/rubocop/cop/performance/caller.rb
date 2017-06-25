ecb6abce68da1e7a5019b602a988c6239085e17b
Masataka Kuwabara <kuwabara@pocke.me>
2017-06-25 17:06:44 +0900

    Make `Performance/Caller` aware of `caller_locations`
    `caller` and `caller_locations` are almost the same. They have difference in a type of return value. `caller` returns an `Array<String>`, but `caller_locations` returns an `Array<Thread::Backtrace::Location>`.
    The difference is not problematic for this cop. So, I'll make this cop aware of `caller_locations` by this change.
    
    Benchmark
    ============
    
    ```ruby
    require 'benchmark'
    
    def method_for_backtrace
      tap do
        tap do
          yield
        end
      end
    end
    
    def assert(a, b)
      raise "#{a} != #{b}" if a != b
    end
    
    method_for_backtrace do
      assert(caller, caller_locations.map(&:to_s))
      assert(caller_locations.first.to_s, caller_locations(1..1).first.to_s)
      assert(caller_locations[1].to_s, caller_locations(2..2).first.to_s)
    
      Benchmark.bm(30) do |x|
        x.report('caller_locations.first')      {1000000.times{caller_locations.first}}
        x.report('caller_locations(1..1).first'){1000000.times{caller_locations(1..1).first}}
    
        x.report('caller_locations[1]')         {1000000.times{caller_locations[1]}}
        x.report('caller_locations(2..2).first'){1000000.times{caller_locations(2..2).first}}
      end
    end
    ```
    
    ```
                                         user     system      total        real
    caller_locations.first           1.660000   0.000000   1.660000 (  1.656021)
    caller_locations(1..1).first     0.390000   0.000000   0.390000 (  0.392523)
    caller_locations[1]              1.660000   0.000000   1.660000 (  1.661755)
    caller_locations(2..2).first     0.460000   0.000000   0.460000 (  0.462888)
    ```

--- lib/rubocop/cop/performance/caller.rb
+++ lib/rubocop/cop/performance/caller.rb
@@ -10,33 +10,36 @@ module RuboCop
       #   # bad
       #   caller[1]
       #   caller.first
+      #   caller_locations[1]
+      #   caller_locations.first
       #
       #   # good
       #   caller(2..2).first
       #   caller(1..1).first
+      #   caller_locations(2..2).first
+      #   caller_locations(1..1).first
       class Caller < Cop
-        MSG_BRACE =
-          'Use `caller(%<n>d..%<n>d).first` instead of `caller[%<m>d]`.'.freeze
-        MSG_FIRST =
-          'Use `caller(%<n>d..%<n>d).first` instead of `caller.first`.'.freeze
+        MSG_BRACE = 'Use `%<method>s(%<n>d..%<n>d).first`' \
+                    ' instead of `%<method>s[%<m>d]`.'.freeze
+        MSG_FIRST = 'Use `%<method>s(%<n>d..%<n>d).first`' \
+                    ' instead of `%<method>s.first`.'.freeze
 
         def_node_matcher :slow_caller?, <<-PATTERN
           {
-            (send nil :caller)
-            (send nil :caller int)
+            (send nil {:caller :caller_locations})
+            (send nil {:caller :caller_locations} int)
           }
         PATTERN
 
         def_node_matcher :caller_with_scope_method?, <<-PATTERN
           {
-            (send $_recv :first)
-            (send $_recv :[] int)
+            (send #slow_caller? :first)
+            (send #slow_caller? :[] int)
           }
         PATTERN
 
         def on_send(node)
-          recv = caller_with_scope_method?(node)
-          return unless slow_caller?(recv)
+          return unless caller_with_scope_method?(node)
 
           add_offense(node)
         end
@@ -44,15 +47,16 @@ module RuboCop
         private
 
         def message(node)
-          caller_arg = node.receiver.arguments[0]
+          method_name = node.receiver.method_name
+          caller_arg = node.receiver.first_argument
           n = caller_arg ? int_value(caller_arg) : 1
 
           if node.method_name == :[]
-            m = int_value(node.arguments[0])
+            m = int_value(node.first_argument)
             n += m
-            format(MSG_BRACE, n: n, m: m)
+            format(MSG_BRACE, n: n, m: m, method: method_name)
           else
-            format(MSG_FIRST, n: n)
+            format(MSG_FIRST, n: n, method: method_name)
           end
         end
 
--- manual/cops_performance.md
+++ manual/cops_performance.md
@@ -15,10 +15,14 @@ can be replaced by `caller(n..n).first`.
 # bad
 caller[1]
 caller.first
+caller_locations[1]
+caller_locations.first
 
 # good
 caller(2..2).first
 caller(1..1).first
+caller_locations(2..2).first
+caller_locations(1..1).first
 ```
 
 ## Performance/CaseWhenSplat
