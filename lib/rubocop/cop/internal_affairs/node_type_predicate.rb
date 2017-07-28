264663b2037a7b1bb66dfe458828172f98773e7a
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2017-07-28 17:19:49 -0400

    Add autocorrect to NodeTypePredicate

--- lib/rubocop/cop/internal_affairs/node_type_predicate.rb
+++ lib/rubocop/cop/internal_affairs/node_type_predicate.rb
@@ -16,15 +16,26 @@ module RuboCop
       class NodeTypePredicate < Cop
         MSG = 'Use `#%s_type?` to check node type.'.freeze
 
-        def_node_search :node_type_check, <<-PATTERN
-          (send (send _ :type) :== (sym $_))
+        def_node_matcher :node_type_check, <<-PATTERN
+          (send (send $_ :type) :== (sym $_))
         PATTERN
 
         def on_send(node)
-          node_type_check(node) do |node_type|
+          node_type_check(node) do |_receiver, node_type|
             return unless Parser::Meta::NODE_TYPES.include?(node_type)
 
             add_offense(node, :expression, format(MSG, node_type))
+          end
+        end
+
+        def autocorrect(node)
+          receiver, node_type = node_type_check(node)
+          range = Parser::Source::Range.new(node.source_range.source_buffer,
+                                            receiver.loc.expression.end_pos + 1,
+                                            node.loc.expression.end_pos)
+
+          lambda do |corrector|
+            corrector.replace(range, "#{node_type}_type?")
           end
         end
       end
