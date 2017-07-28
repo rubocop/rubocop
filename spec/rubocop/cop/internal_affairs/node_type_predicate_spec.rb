70be62e9b74f9f73702a15005f8f2c32eadc61d5
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2017-07-28 17:19:49 -0400

    Add autocorrect to NodeTypePredicate

--- spec/rubocop/cop/internal_affairs/node_type_predicate_spec.rb
+++ spec/rubocop/cop/internal_affairs/node_type_predicate_spec.rb
@@ -3,11 +3,19 @@
 describe RuboCop::Cop::InternalAffairs::NodeTypePredicate do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense for a comparison node type check' do
-    expect_offense(<<-RUBY, 'example_cop.rb')
+  context 'comparison node type check' do
+    it 'registers an offense' do
+      expect_offense(<<-RUBY, 'example_cop.rb')
       node.type == :send
       ^^^^^^^^^^^^^^^^^^ Use `#send_type?` to check node type.
-    RUBY
+      RUBY
+    end
+
+    it 'auto-corrects' do
+      new_source = autocorrect_source('node.type == :send')
+
+      expect(new_source).to eq('node.send_type?')
+    end
   end
 
   it 'does not register an offense for a predicate node type check' do
