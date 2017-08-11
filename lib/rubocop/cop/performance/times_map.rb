dcee8ef6346c1750127fe57e19c79535847ab1ca
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-08-11 20:56:44 +0900

    [Fix #4658] Disable auto-correction for `Performance/TimesMap` by default
    See #4658
    
    This change has three changes.
    
    - Disable auto-correction by default
    - Improve the offense message
    - Refactoring
    
    Problem
    =====
    
    `Integer#times` does nothing if receiver is 0 or less.
    However, `Array.new` raises an error if argument is less than 0.
    
    For example:
    
    ```ruby
    -1.times{}    # does nothing
    Array.new(-1) # ArgumentError: negative array size
    ```
    
    Solution
    =====
    
    Disable the non-safe auto-correction by default. And Improve the offense message.

--- lib/rubocop/cop/performance/times_map.rb
+++ lib/rubocop/cop/performance/times_map.rb
@@ -19,7 +19,9 @@ module RuboCop
       #     i.to_s
       #   end
       class TimesMap < Cop
-        MSG = 'Use `Array.new` with a block instead of `.times.%s`.'.freeze
+        MESSAGE = 'Use `Array.new(%<count>s)` with a block ' \
+                  'instead of `.times.%<map_or_collect>s`'.freeze
+        MESSAGE_ONLY_IF = 'only if `%<count>s` is always 0 or more'.freeze
 
         def on_send(node)
           check(node)
@@ -32,27 +34,36 @@ module RuboCop
         private
 
         def check(node)
-          times_map_call(node) do |map_or_collect|
-            add_offense(node, :expression, format(MSG, map_or_collect))
+          times_map_call(node) do |map_or_collect, count|
+            add_offense(node, :expression, message(map_or_collect, count))
           end
         end
 
+        def message(map_or_collect, count)
+          template = if count.literal?
+                       MESSAGE + '.'
+                     else
+                       "#{MESSAGE} #{MESSAGE_ONLY_IF}."
+                     end
+          format(template,
+                 count: count.source,
+                 map_or_collect: map_or_collect.method_name)
+        end
+
         def_node_matcher :times_map_call, <<-PATTERN
-          {(block (send (send !nil :times) ${:map :collect}) ...)
-           (send (send !nil :times) ${:map :collect} (block_pass ...))}
+          {(block $(send (send $!nil :times) {:map :collect}) ...)
+           $(send (send $!nil :times) {:map :collect} (block_pass ...))}
         PATTERN
 
         def autocorrect(node)
-          send_node = node.send_type? ? node : node.each_descendant(:send).first
-
-          count, = *send_node.receiver
+          map_or_collect, count = times_map_call(node)
 
           replacement =
             "Array.new(#{count.source}" \
-            "#{send_node.arguments.map { |arg| ", #{arg.source}" }.join})"
+            "#{map_or_collect.arguments.map { |arg| ", #{arg.source}" }.join})"
 
           lambda do |corrector|
-            corrector.replace(send_node.loc.expression, replacement)
+            corrector.replace(map_or_collect.loc.expression, replacement)
           end
         end
       end
