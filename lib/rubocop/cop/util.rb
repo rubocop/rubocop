71d1ae528b8928aaa0954f9c06a4c1540693b33c
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2017-09-18 08:58:50 -0400

    Dup strings in jRuby before modifying encoding

--- lib/rubocop/cop/util.rb
+++ lib/rubocop/cop/util.rb
@@ -295,7 +295,7 @@ module RuboCop
       end
 
       def compatible_external_encoding_for?(src)
-        src = src.dup if RUBY_VERSION < '2.3'
+        src = src.dup if RUBY_VERSION < '2.3' || RUBY_ENGINE == 'jruby'
         src.force_encoding(Encoding.default_external).valid_encoding?
       end
 
