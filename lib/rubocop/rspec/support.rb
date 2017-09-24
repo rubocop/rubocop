f3812f16c05c461751c59a18d80d3de4e0f3daf8
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-09-24 21:10:54 +0900

    Use `require_relative` for own source code to improve performance
    `require_relative` is faster than `require`. Because, `require` looks `LOAD_PATH`, but `require_relative` doesn't look.
    
    This change especially effective for `rubocop --version`, `rubocop --help` or something. Because This change does not speed up the analysis.
    
    Benchmark
    ======
    
    ```ruby
    require 'benchmark'
    
    puts(Benchmark.realtime do
      10.times do
        system('rubocop --version')
      end
    end)
    ```
    
    before: 6.542355578014394
    after: 4.933600370990462
    
    Note: This benchmark relies number of installed gems. 401 gems are installed in my environment.
    It is effective for environments where more gems are installed.

--- lib/rubocop/rspec/support.rb
+++ lib/rubocop/rspec/support.rb
@@ -2,8 +2,8 @@
 
 # Require this file to load code that supports testing using RSpec.
 
-require 'rubocop/rspec/cop_helper'
-require 'rubocop/rspec/host_environment_simulation_helper'
-require 'rubocop/rspec/shared_contexts'
-require 'rubocop/rspec/shared_examples'
-require 'rubocop/rspec/expect_offense'
+require_relative 'cop_helper'
+require_relative 'host_environment_simulation_helper'
+require_relative 'shared_contexts'
+require_relative 'shared_examples'
+require_relative 'expect_offense'
