95399729d41d2442d259b736d38d0189da9814c0
Koichi ITO <koic.ito@gmail.com>
2017-09-25 11:08:15 +0900

    Use `minimum_target_ruby_version`
    `RUBY_VERSION` is a runtime ruby version.

--- spec/rubocop/cop/lint/unneeded_require_statement_spec.rb
+++ spec/rubocop/cop/lint/unneeded_require_statement_spec.rb
@@ -4,33 +4,31 @@ describe RuboCop::Cop::Lint::UnneededRequireStatement do
   let(:config) { RuboCop::Config.new }
   subject(:cop) { described_class.new(config) }
 
-  if RUBY_VERSION < '2.2'
-    context 'target ruby version < 2.2', :ruby21 do
-      it "does not registers an offense when using `require 'enumerator'`" do
-        expect_no_offenses(<<-RUBY.strip_indent)
-          require 'enumerator'
-        RUBY
-      end
+  context 'target ruby version < 2.2', :ruby21 do
+    it "does not registers an offense when using `require 'enumerator'`" do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        require 'enumerator'
+      RUBY
     end
-  else
-    context 'target ruby version >= 2.2', :ruby22 do
-      it "registers an offense when using `require 'enumerator'`" do
-        expect_offense(<<-RUBY.strip_indent)
-          require 'enumerator'
-          ^^^^^^^^^^^^^^^^^^^^ Remove unnecessary `require` statement.
-        RUBY
-      end
+  end
 
-      it 'autocorrects remove unnecessary require statement' do
-        new_source = autocorrect_source(<<-RUBY.strip_indent)
-          require 'enumerator'
-          require 'uri'
-        RUBY
+  context 'target ruby version >= 2.2', :ruby22 do
+    it "registers an offense when using `require 'enumerator'`" do
+      expect_offense(<<-RUBY.strip_indent)
+        require 'enumerator'
+        ^^^^^^^^^^^^^^^^^^^^ Remove unnecessary `require` statement.
+      RUBY
+    end
 
-        expect(new_source).to eq(<<-RUBY.strip_indent)
-          require 'uri'
-        RUBY
-      end
+    it 'autocorrects remove unnecessary require statement' do
+      new_source = autocorrect_source(<<-RUBY.strip_indent)
+        require 'enumerator'
+        require 'uri'
+      RUBY
+
+      expect(new_source).to eq(<<-RUBY.strip_indent)
+        require 'uri'
+      RUBY
     end
   end
 end
