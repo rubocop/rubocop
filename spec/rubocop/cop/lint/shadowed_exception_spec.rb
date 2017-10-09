a94b6fbedf50125459028ff326ce142d2263eb69
Koichi ITO <koic.ito@gmail.com>
2017-10-09 17:08:39 +0900

    Fix a typos

--- spec/rubocop/cop/lint/shadowed_exception_spec.rb
+++ spec/rubocop/cop/lint/shadowed_exception_spec.rb
@@ -60,7 +60,7 @@ describe RuboCop::Cop::Lint::ShadowedException do
       RUBY
     end
 
-    it 'accepts rescuing contain multiple same error code exceptions' do
+    it 'accepts rescue containing multiple same error code exceptions' do
       # System dependent error code depends on runtime environment.
       stub_const('Errno::EAGAIN::Errno', 35)
       stub_const('Errno::EWOULDBLOCK::Errno', 35)
