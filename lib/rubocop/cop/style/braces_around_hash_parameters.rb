450763a3518a962d750844a21987aec74bc64bf0
Koichi ITO <koic.ito@gmail.com>
2017-10-19 12:14:53 +0900

    Fix broken doc style of `Style/BracesAroundHashParameters` cop

--- lib/rubocop/cop/style/braces_around_hash_parameters.rb
+++ lib/rubocop/cop/style/braces_around_hash_parameters.rb
@@ -5,43 +5,40 @@ module RuboCop
     module Style
       # This cop checks for braces around the last parameter in a method call
       # if the last parameter is a hash.
-      # It supports 3 styles:
+      # It supports 3 styles.
       #
-      # * The `braces` style enforces braces around all method
-      # parameters that are hashes.
+      # @example
+      #   # 1. The `braces` style enforces braces around all method
+      #   #    parameters that are hashes.
       #
-      #     ```
-      #     # bad
-      #     some_method(x, y, a: 1, b: 2)
+      #   # bad
+      #   some_method(x, y, a: 1, b: 2)
       #
-      #     # good
-      #     some_method(x, y, {a: 1, b: 2})
-      #     ```
+      #   # good
+      #   some_method(x, y, {a: 1, b: 2})
       #
-      # * The `no_braces` style checks that the last parameter doesn't
-      # have braces around it.
+      # @example
+      #   # 2.  The `no_braces` style checks that the last parameter doesn't
+      #   #     have braces around it.
       #
-      #     ```
-      #     # bad
-      #     some_method(x, y, {a: 1, b: 2})
+      #   # bad
+      #   some_method(x, y, {a: 1, b: 2})
       #
-      #     # good
-      #     some_method(x, y, a: 1, b: 2)
-      #     ```
+      #   # good
+      #   some_method(x, y, a: 1, b: 2)
       #
-      # * The `context_dependent` style checks that the last parameter
-      # doesn't have braces around it, but requires braces if the
-      # second to last parameter is also a hash literal.
+      # @example
+      #   # 3. The `context_dependent` style checks that the last parameter
+      #   #    doesn't have braces around it, but requires braces if the
+      #   #    second to last parameter is also a hash literal.
       #
-      #     ```
-      #     # bad
-      #     some_method(x, y, {a: 1, b: 2})
-      #     some_method(x, y, {a: 1, b: 2}, a: 1, b: 2)
+      #   # bad
+      #   some_method(x, y, {a: 1, b: 2})
+      #   some_method(x, y, {a: 1, b: 2}, a: 1, b: 2)
       #
-      #     # good
-      #     some_method(x, y, a: 1, b: 2)
-      #     some_method(x, y, {a: 1, b: 2}, {a: 1, b: 2})
-      #     ```
+      #   # good
+      #   some_method(x, y, a: 1, b: 2)
+      #   some_method(x, y, {a: 1, b: 2}, {a: 1, b: 2})
       class BracesAroundHashParameters < Cop
         include ConfigurableEnforcedStyle
 
