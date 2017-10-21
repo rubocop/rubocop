3c70269da1eb27c0952707889a688c0eb34c17cf
John Backus <johncbackus@gmail.com>
2017-10-21 15:00:49 -0700

    Fix RSpec/RepeatedExample

--- spec/rubocop/cop/style/self_assignment_spec.rb
+++ spec/rubocop/cop/style/self_assignment_spec.rb
@@ -3,26 +3,7 @@
 describe RuboCop::Cop::Style::SelfAssignment do
   subject(:cop) { described_class.new }
 
-  %i[+ - * ** / | &].product(['x', '@x', '@@x']).each do |op, var|
-    it "registers an offense for non-shorthand assignment #{op} and #{var}" do
-      inspect_source("#{var} = #{var} #{op} y")
-      expect(cop.offenses.size).to eq(1)
-      expect(cop.messages)
-        .to eq(["Use self-assignment shorthand `#{op}=`."])
-    end
-
-    it "accepts shorthand assignment for #{op} and #{var}" do
-      inspect_source("#{var} #{op}= y")
-      expect(cop.offenses.empty?).to be(true)
-    end
-
-    it "auto-corrects a non-shorthand assignment #{op} and #{var}" do
-      new_source = autocorrect_source("#{var} = #{var} #{op} y")
-      expect(new_source).to eq("#{var} #{op}= y")
-    end
-  end
-
-  ['||', '&&'].product(['x', '@x', '@@x']).each do |op, var|
+  %i[+ - * ** / | & || &&].product(['x', '@x', '@@x']).each do |op, var|
     it "registers an offense for non-shorthand assignment #{op} and #{var}" do
       inspect_source("#{var} = #{var} #{op} y")
       expect(cop.offenses.size).to eq(1)
