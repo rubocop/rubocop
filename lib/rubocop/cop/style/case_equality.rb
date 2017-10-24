50e9b4a3ade71b309a56a0e29935db0c4a33615d
Patrik Wehrli <patrik.wehrli@gmail.com>
2017-10-24 22:46:17 +0200

    [#4910] Add example for Style/CaseEquality

--- lib/rubocop/cop/style/case_equality.rb
+++ lib/rubocop/cop/style/case_equality.rb
@@ -4,6 +4,18 @@ module RuboCop
   module Cop
     module Style
       # This cop checks for uses of the case equality operator(===).
+      #
+      # @example
+      #   # bad
+      #   Array === something
+      #   (1..100) === 7
+      #   /something/ === some_string
+      #
+      #   # good
+      #   something.is_a?(Array)
+      #   (1..100).include?(7)
+      #   some_string =~ /something/
+      #
       class CaseEquality < Cop
         MSG = 'Avoid the use of the case equality operator `===`.'.freeze
 
--- manual/cops_style.md
+++ manual/cops_style.md
@@ -279,6 +279,20 @@ Enabled | No
 
 This cop checks for uses of the case equality operator(===).
 
+### Example
+
+```ruby
+# bad
+Array === something
+(1..100) === 7
+/something/ === some_string
+
+# good
+something.is_a?(Array)
+(1..100).include?(7)
+some_string =~ /something/
+```
+
 ### References
 
 * [https://github.com/bbatsov/ruby-style-guide#no-case-equality](https://github.com/bbatsov/ruby-style-guide#no-case-equality)
