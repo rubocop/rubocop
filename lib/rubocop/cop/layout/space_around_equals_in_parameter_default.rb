8f2994943e496320dd942888e20aa317c4ae6d18
Jess Harrelson <jess.harrelson@betterment.com>
2017-10-23 20:23:54 -0400

    [Fix #4910] added example to space around equals in parameter default cop

--- lib/rubocop/cop/layout/space_around_equals_in_parameter_default.rb
+++ lib/rubocop/cop/layout/space_around_equals_in_parameter_default.rb
@@ -5,6 +5,16 @@ module RuboCop
     module Layout
       # Checks that the equals signs in parameter default assignments
       # have or don't have surrounding space depending on configuration.
+      # @example
+      #   # bad
+      #   def some_method(arg1=:default, arg2=nil, arg3=[])
+      #     # do something...
+      #   end
+      #
+      #   # good
+      #   def some_method(arg1 = :default, arg2 = nil, arg3 = [])
+      #     # do something...
+      #   end
       class SpaceAroundEqualsInParameterDefault < Cop
         include SurroundingSpace
         include ConfigurableEnforcedStyle
