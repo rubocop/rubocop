2be15199dff0bfe4d7f7b301d55e505fd64dfcac
Joe Jansen <jansen@betterment.com>
2017-10-23 20:56:28 -0400

    remove trailing whitespace

--- lib/rubocop/cop/naming/file_name.rb
+++ lib/rubocop/cop/naming/file_name.rb
@@ -8,7 +8,7 @@ module RuboCop
       # This cop makes sure that Ruby source files have snake_case
       # names. Ruby scripts (i.e. source files with a shebang in the
       # first line) are ignored.
-      # 
+      #
       # @example
       #   # bad
       #   exampleFile.rb
