eb9300e01a9a4e2a95e7fd1d2a2dceb9e9ddeb34
diegous <munoa.d@gmail.com>
2017-10-29 04:33:24 -0300

    [#4910] Add example to Naming/ConstantName cop (#4959)

--- lib/rubocop/cop/naming/constant_name.rb
+++ lib/rubocop/cop/naming/constant_name.rb
@@ -8,6 +8,15 @@ module RuboCop
       #
       # To avoid false positives, it ignores cases in which we cannot know
       # for certain the type of value that would be assigned to a constant.
+      #
+      # @example
+      #   # bad
+      #   InchInCm = 2.54
+      #   INCHinCM = 2.54
+      #   Inch_In_Cm = 2.54
+      #
+      #   # good
+      #   INCH_IN_CM = 2.54
       class ConstantName < Cop
         MSG = 'Use SCREAMING_SNAKE_CASE for constants.'.freeze
         SNAKE_CASE = /^[\dA-Z_]+$/
