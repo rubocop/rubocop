f3fec110ef2c68dade0e490705d1e4a1ee286237
Koichi ITO <koic.ito@gmail.com>
2017-11-11 16:09:32 +0900

    Update description of `Style/FrozenStringLiteralComment` cop
    The following is Matz's comment.
    https://bugs.ruby-lang.org/issues/8976#note-41
    
    AFAIK, Frozen string literal will be default in Ruby 3.0 or
    not yet determinable.
    Therefore, this commit makes the description somewhat vague.

--- lib/rubocop/cop/style/frozen_string_literal_comment.rb
+++ lib/rubocop/cop/style/frozen_string_literal_comment.rb
@@ -5,7 +5,7 @@ module RuboCop
     module Style
       # This cop is designed to help upgrade to Ruby 3.0. It will add the
       # comment `# frozen_string_literal: true` to the top of files to
-      # enable frozen string literals. Frozen string literals will be default
+      # enable frozen string literals. Frozen string literals may be default
       # in Ruby 3.0. The comment will be added below a shebang and encoding
       # comment. The frozen string literal comment is only valid in Ruby 2.3+.
       class FrozenStringLiteralComment < Cop
--- manual/cops_style.md
+++ manual/cops_style.md
@@ -1517,7 +1517,7 @@ Enabled | Yes
 
 This cop is designed to help upgrade to Ruby 3.0. It will add the
 comment `# frozen_string_literal: true` to the top of files to
-enable frozen string literals. Frozen string literals will be default
+enable frozen string literals. Frozen string literals may be default
 in Ruby 3.0. The comment will be added below a shebang and encoding
 comment. The frozen string literal comment is only valid in Ruby 2.3+.
 
