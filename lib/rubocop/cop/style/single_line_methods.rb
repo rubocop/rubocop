90f02a2f0f9860f55939ed7fa5dfca37449efaf1
Garett Arrowood <garettarrowood@gmail.com>
2017-11-23 07:32:06 -0500

    [#4910] Add examples to Style/SingleLineMethods

--- lib/rubocop/cop/style/single_line_methods.rb
+++ lib/rubocop/cop/style/single_line_methods.rb
@@ -3,8 +3,20 @@
 module RuboCop
   module Cop
     module Style
-      # This cop checks for single-line method definitions.
-      # It can optionally accept single-line methods with no body.
+      # This cop checks for single-line method definitions that contain a body.
+      # It will accept single-line methods with no body.
+      #
+      # @example
+      #   # bad
+      #   def some_method; body end
+      #   def link_to(url); {:name => url}; end
+      #   def @table.columns; super; end
+      #
+      #   # good
+      #   def no_op; end
+      #   def self.resource_class=(klass); end
+      #   def @table.columns; end
+      #
       class SingleLineMethods < Cop
         include AutocorrectAlignment
 
