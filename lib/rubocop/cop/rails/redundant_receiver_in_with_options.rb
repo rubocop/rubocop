0beb6d29d0577f7b69d4cc1c38aca333022a558f
Koichi ITO <koic.ito@gmail.com>
2017-12-05 18:12:48 +0900

    Add new `Rails/RedundantReceiverInWithOptions` cop
    ## Feature
    
    This cop checks for redundant receiver in `with_options`.
    Receiver is implicit from Rails 4.2 or higher.
    
    ### Bad case
    
    ```ruby
    class Account < ApplicationRecord
      with_options dependent: :destroy do |assoc|
        assoc.has_many :customers
        assoc.has_many :products
        assoc.has_many :invoices
        assoc.has_many :expenses
      end
    end
    ```
    
    ### Good case
    
    ```ruby
    class Account < ApplicationRecord
      with_options dependent: :destroy do
        has_many :customers
        has_many :products
        has_many :invoices
        has_many :expenses
      end
    end
    ```
    
    ### How to use
    
    ```console
    % cat app/models/account.rb
    # frozen_string_literal: true
    
    class Account < ApplicationRecord
      with_options dependent: :destroy do |assoc|
        assoc.has_many :customers
        assoc.has_many :products
        assoc.has_many :invoices
        assoc.has_many :expenses
      end
    end
    ```
    
    ```console
    % rubocop /tmp/app/models/account.rb --only Rails/RedundantReceiverInWithOptions
    Inspecting 1 file
    C
    
    Offenses:
    
    /tmp/app/models/account.rb:5:5: C: Rails/RedundantReceiverInWithOptions: Redundant receiver in with_options.
        assoc.has_many :customers
        ^^^^^
    /tmp/app/models/account.rb:6:5: C: Rails/RedundantReceiverInWithOptions: Redundant receiver in with_options.
        assoc.has_many :products
        ^^^^^
    /tmp/app/models/account.rb:7:5: C: Rails/RedundantReceiverInWithOptions: Redundant receiver in with_options.
        assoc.has_many :invoices
        ^^^^^
    /tmp/app/models/account.rb:8:5: C: Rails/RedundantReceiverInWithOptions: Redundant receiver in with_options.
        assoc.has_many :expenses
        ^^^^^
    
    1 file inspected, 4 offenses detected
    ```
    
    ## Other Information
    
    API documentation
    http://api.rubyonrails.org/?q=with_options

---
+++ lib/rubocop/cop/rails/redundant_receiver_in_with_options.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Rails
+      # This cop checks for redundant receiver in `with_options`.
+      # Receiver is implicit from Rails 4.2 or higher.
+      #
+      # @example
+      #   # bad
+      #   class Account < ApplicationRecord
+      #     with_options dependent: :destroy do |assoc|
+      #       assoc.has_many :customers
+      #       assoc.has_many :products
+      #       assoc.has_many :invoices
+      #       assoc.has_many :expenses
+      #     end
+      #   end
+      #
+      #   # good
+      #   class Account < ApplicationRecord
+      #     with_options dependent: :destroy do
+      #       has_many :customers
+      #       has_many :products
+      #       has_many :invoices
+      #       has_many :expenses
+      #     end
+      #   end
+      class RedundantReceiverInWithOptions < Cop
+        extend TargetRailsVersion
+
+        minimum_target_rails_version 4.2
+
+        MSG = 'Redundant receiver in `with_options`.'.freeze
+
+        def_node_matcher :with_options?, <<-PATTERN
+          (block
+            (send nil? :with_options
+              (...))
+            (args
+              (...))
+            ...)
+        PATTERN
+
+        def_node_search :assoc_has_redundant_receiver, <<-PATTERN
+          (send
+            (lvar _) ...)
+        PATTERN
+
+        def on_block(node)
+          with_options?(node) do
+            assoc_has_redundant_receiver(node).each do |assoc|
+              add_offense(assoc, location: assoc.receiver.loc.expression)
+            end
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.remove(node.receiver.loc.expression)
+            corrector.remove(node.loc.dot)
+            corrector.remove(block_argument_range(node))
+          end
+        end
+
+        private
+
+        def block_argument_range(node)
+          block_argument = node.parent.parent.children[1].loc.expression
+
+          range_between(
+            search_begin_pos_of_space_before_block_argument(
+              block_argument.begin_pos
+            ),
+            block_argument.end_pos
+          )
+        end
+
+        def search_begin_pos_of_space_before_block_argument(begin_pos)
+          position = begin_pos - 1
+
+          if processed_source.raw_source[position] == ' '
+            search_begin_pos_of_space_before_block_argument(position)
+          else
+            begin_pos
+          end
+        end
+      end
+    end
+  end
+end
