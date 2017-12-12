4969c81de20e72cfe153e879b138c96bbc421b36
Garett Arrowood <garettarrowood@gmail.com>
2017-12-12 17:25:39 -0500

    [Fix #5224] Solve false EmptyLinesAroundArgument offenses

--- spec/rubocop/cop/layout/empty_lines_around_arguments_spec.rb
+++ spec/rubocop/cop/layout/empty_lines_around_arguments_spec.rb
@@ -3,8 +3,8 @@
 describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
   subject(:cop) { described_class.new(config) }
 
-  context 'registers offense' do
-    it 'when empty line detected at top' do
+  context 'when extra lines' do
+    it 'registers offense for empty line before arg' do
       inspect_source(<<-RUBY.strip_indent)
         foo(
 
@@ -15,7 +15,7 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
         .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'when empty line detected at bottom' do
+    it 'registers offense for empty line after arg' do
       inspect_source(<<-RUBY.strip_indent)
         bar(
           [baz, qux]
@@ -26,9 +26,9 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
         .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'when empty line detected in the middle' do
+    it 'registers offense for empty line between args' do
       inspect_source(<<-RUBY.strip_indent)
-        do_something(
+        foo.do_something(
           baz,
 
           qux: 0
@@ -38,7 +38,19 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
         .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'when multiple empty lines are detected' do
+    it 'registers offense for empty line in-between one arg' do
+      inspect_source(<<-RUBY.strip_indent)
+        do_stuff([
+          "some text",
+
+          "some more text"
+        ])
+      RUBY
+      expect(cop.messages)
+        .to eq(['Empty line detected around arguments.'])
+    end
+
+    it 'registers offenses when multiple empty lines are detected' do
       inspect_source(<<-RUBY.strip_indent)
         foo(
           baz,
@@ -54,7 +66,7 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
         .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'when args start on defintion line' do
+    it 'registers offense when args start on definition line' do
       inspect_source(<<-RUBY.strip_indent)
         foo(biz,
 
@@ -64,7 +76,7 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
         .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'when empty line before a block argument' do
+    it 'registers offense when empty line between normal arg & block arg' do
       inspect_source(<<-RUBY.strip_indent)
         Foo.prepend(
           a,
@@ -80,51 +92,52 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
       expect(cop.messages)
         .to eq(['Empty line detected around arguments.'])
     end
-  end
 
-  context 'does not register offense' do
-    it 'for one line methods' do
+    it 'registers offense on correct line for single offense example' do
       inspect_source(<<-RUBY.strip_indent)
-        foo(bar)
+        class Foo
+
+          include Bar
+
+          def baz(qux)
+            fizz(
+              qux,
+
+              10
+            )
+          end
+        end
       RUBY
-      expect(cop.offenses.empty?).to be(true)
+      expect(cop.offenses.size).to eq 1
+      expect(cop.offenses.first.location.line).to eq 8
+      expect(cop.messages.uniq)
+        .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'for multiple correctly listed mixed args' do
+    it 'registers offense on correct lines for multi-offense example' do
       inspect_source(<<-RUBY.strip_indent)
-        foo(
-          bar,
-          [],
-          baz = nil,
-          qux: 2
+        something(1, 5)
+        something_else
+
+        foo(biz,
+
+            qux)
+
+        quux.map do
+
+        end.another.thing(
+
+          [baz]
         )
       RUBY
-      expect(cop.offenses.empty?).to be(true)
+      expect(cop.offenses.size).to eq 2
+      expect(cop.offenses[0].location.line).to eq 5
+      expect(cop.offenses[1].location.line).to eq 11
+      expect(cop.messages.uniq)
+        .to eq(['Empty line detected around arguments.'])
     end
 
-    it 'for correctly listed args starting on defintion line' do
-      inspect_source(<<-RUBY.strip_indent)
-        foo(bar,
-            [],
-            qux: 2)
-      RUBY
-      expect(cop.offenses.empty?).to be(true)
-    end
-
-    it 'when passed block argument with empty line' do
-      inspect_source(<<-RUBY.strip_indent)
-        Foo.prepend(Module.new do
-          def something; end
-
-          def anything; end
-        end)
-      RUBY
-      expect(cop.offenses.empty?).to be(true)
-    end
-  end
-
-  context 'autocorrects' do
-    it 'when empty line detected at top' do
+    it 'autocorrects empty line detected at top' do
       corrected = autocorrect_source(['foo(',
                                       '',
                                       '  bar',
@@ -134,7 +147,7 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
                                ')'].join("\n")
     end
 
-    it 'when empty line detected at bottom' do
+    it 'autocorrects empty line detected at bottom' do
       corrected = autocorrect_source(['foo(',
                                       '  baz: 1',
                                       '',
@@ -144,7 +157,7 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
                                ')'].join("\n")
     end
 
-    it 'when empty line detected in the middle' do
+    it 'autocorrects empty line detected in the middle' do
       corrected = autocorrect_source(['do_something(',
                                       '  [baz],',
                                       '',
@@ -156,7 +169,19 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
                                ')'].join("\n")
     end
 
-    it 'when multiple empty lines are detected' do
+    it 'autocorrects empty line in-between one arg' do
+      corrected = autocorrect_source(['do_stuff([',
+                                      "  'some text',",
+                                      '',
+                                      "  'some more text'",
+                                      '])'].join("\n"))
+      expect(corrected).to eq ['do_stuff([',
+                               "  'some text',",
+                               "  'some more text'",
+                               '])'].join("\n")
+    end
+
+    it 'autocorrects multiple empty lines' do
       corrected = autocorrect_source(['do_stuff(',
                                       '  baz,',
                                       '',
@@ -172,12 +197,76 @@ describe RuboCop::Cop::Layout::EmptyLinesAroundArguments, :config do
                                ')'].join("\n")
     end
 
-    it 'when args start on defintion line' do
+    it 'autocorrects args that start on definition line' do
       corrected = autocorrect_source(['bar(qux,',
                                       '',
                                       '    78)'].join("\n"))
       expect(corrected).to eq ['bar(qux,',
                                '    78)'].join("\n")
+    end
+  end
+
+  context 'when no extra lines' do
+    it 'accpets one line methods' do
+      inspect_source(<<-RUBY.strip_indent)
+        foo(bar)
+      RUBY
+      expect(cop.offenses.empty?).to be(true)
+    end
+
+    it 'accepts multiple listed mixed args' do
+      inspect_source(<<-RUBY.strip_indent)
+        foo(
+          bar,
+          [],
+          baz = nil,
+          qux: 2
+        )
+      RUBY
+      expect(cop.offenses.empty?).to be(true)
+    end
+
+    it 'accepts listed args starting on defintion line' do
+      inspect_source(<<-RUBY.strip_indent)
+        foo(bar,
+            [],
+            qux: 2)
+      RUBY
+      expect(cop.offenses.empty?).to be(true)
+    end
+
+    it 'accepts block argument with empty line' do
+      inspect_source(<<-RUBY.strip_indent)
+        Foo.prepend(Module.new do
+          def something; end
+
+          def anything; end
+        end)
+      RUBY
+      expect(cop.offenses.empty?).to be(true)
+    end
+
+    it 'accepts method with argument that trails off block' do
+      inspect_source(<<-RUBY.strip_indent)
+        fred.map do
+          <<-EOT
+            bar
+
+            foo
+          EOT
+        end.join("\n")
+      RUBY
+      expect(cop.offenses.empty?).to be(true)
+    end
+
+    it 'accepts method with no arguments that trails off block' do
+      inspect_source(<<-RUBY.strip_indent)
+        foo.baz do
+
+          bar
+        end.compact
+      RUBY
+      expect(cop.offenses.empty?).to be(true)
     end
   end
 end
