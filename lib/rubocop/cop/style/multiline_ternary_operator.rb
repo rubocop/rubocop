809fd2b2a72ed6017120bfe00c2425e6b0e4af93
Garett Arrowood <garettarrowood@gmail.com>
2017-12-15 17:31:11 -0500

    [#4910] Add examples to Stlye/MultilineTernaryOperator

--- lib/rubocop/cop/style/multiline_ternary_operator.rb
+++ lib/rubocop/cop/style/multiline_ternary_operator.rb
@@ -4,6 +4,25 @@ module RuboCop
   module Cop
     module Style
       # This cop checks for multi-line ternary op expressions.
+      #
+      # @example
+      #   # bad
+      #   a = cond ?
+      #     b : c
+      #   a = cond ? b :
+      #       c
+      #   a = cond ?
+      #       b :
+      #       c
+      #
+      #   # good
+      #   a = cond ? b : c
+      #   a =
+      #     if cond
+      #       b
+      #     else
+      #       c
+      #     end
       class MultilineTernaryOperator < Cop
         MSG = 'Avoid multi-line ternary operators, ' \
               'use `if` or `unless` instead.'.freeze
