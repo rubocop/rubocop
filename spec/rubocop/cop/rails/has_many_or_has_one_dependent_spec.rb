rubocop/rubocop

commit 5d86bd8158e81fe372c547bc272065ea00217217

[Fix #5234] Fix a false positive for `Rails/HasManyOrHasOneDependent`
Fixes #5234.

Since there was no test case using `class_name` option in `with_options`
block, this PR added it as a reproduction test and fixed it.

lib/rubocop/cop/rails/has_many_or_has_one_dependent.rb
@@ -24,7 +24,7 @@ module RuboCop
         MSG = 'Specify a `:dependent` option.'.freeze
 
         def_node_matcher :association_without_options?, <<-PATTERN
-          (send nil? {:has_many :has_one} _)
+          (send nil? {:has_many :has_one} _ ...)
         PATTERN
 
         def_node_matcher :association_with_options?, <<-PATTERN
@@ -49,18 +49,26 @@ module RuboCop
         def on_send(node)
           if !association_without_options?(node)
             return if valid_options?(association_with_options?(node))
-          else
-            n = node.parent.begin_type? ? node.parent.parent : node.parent
-
-            if with_options_block(n)
-              return if valid_options?(with_options_block(n))
-            end
+          elsif valid_options_in_with_options_block?(node)
+            return
           end
 
           add_offense(node, location: :selector)
         end
 
         private
+
+        def valid_options_in_with_options_block?(node)
+          return true unless node.parent
+
+          n = node.parent.begin_type? ? node.parent.parent : node.parent
+
+          if with_options_block(n)
+            return true if valid_options?(with_options_block(n))
+          end
+
+          false
+        end
 
         def valid_options?(options)
           return true unless options

spec/rubocop/cop/rails/has_many_or_has_one_dependent_spec.rb
@@ -51,6 +51,16 @@ describe RuboCop::Cop::Rails::HasManyOrHasOneDependent do
           end
         RUBY
       end
+
+      it 'does not register an offense for using `class_name` option' do
+        expect_no_offenses(<<-RUBY.strip_indent)
+          class Person
+            with_options dependent: :destroy do
+              has_one :foo, class_name: 'Foo'
+            end
+          end
+        RUBY
+      end
     end
   end
 

commit 5a70924c661263915b0d724d1a9714877cee3b01

[Fix #5293] Fix regression for `Rails/HasManyOrHasOneDependent` cop
Fixes #5293.
This commit changed the existing test codes and fixed the regression by #5235.

lib/rubocop/cop/rails/has_many_or_has_one_dependent.rb
@@ -24,7 +24,7 @@ module RuboCop
         MSG = 'Specify a `:dependent` option.'.freeze
 
         def_node_matcher :association_without_options?, <<-PATTERN
-          (send nil? {:has_many :has_one} _ ...)
+          (send nil? {:has_many :has_one} _)
         PATTERN
 
         def_node_matcher :association_with_options?, <<-PATTERN
@@ -47,11 +47,11 @@ module RuboCop
         PATTERN
 
         def on_send(node)
-          if !association_without_options?(node)
+          unless association_without_options?(node)
             return if valid_options?(association_with_options?(node))
-          elsif valid_options_in_with_options_block?(node)
-            return
           end
+
+          return if valid_options_in_with_options_block?(node)
 
           add_offense(node, location: :selector)
         end

spec/rubocop/cop/rails/has_many_or_has_one_dependent_spec.rb
@@ -23,12 +23,20 @@ describe RuboCop::Cop::Rails::HasManyOrHasOneDependent do
     end
 
     it 'does not register an offense when specifying `:dependent` strategy' do
-      expect_no_offenses('has_one :foo, dependent: :bar')
+      expect_no_offenses(<<-RUBY.strip_indent)
+        class Person
+          has_one :foo, dependent: :destroy
+        end
+      RUBY
     end
 
     context 'with :through option' do
       it 'does not register an offense for non-nil value' do
-        expect_no_offenses('has_one :foo, through: :bar')
+        expect_no_offenses(<<-RUBY.strip_indent)
+          class Person
+            has_one :foo, through: :bar
+          end
+        RUBY
       end
 
       it 'registers an offense for nil value' do