eea06c99aa2ab5ea4e9b9a7c817cab4267e16f81
Koichi ITO <koic.ito@gmail.com>
2017-12-30 02:08:37 +0900

    Remove unnecessary `require 'spec_helper'`
    Follow up of #4080.
    This commit is a cosmetic change to remove `require 'spec_helper'`
    added after #4080.

--- spec/rubocop/cop/rails/active_support_aliases_spec.rb
+++ spec/rubocop/cop/rails/active_support_aliases_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
 RSpec.describe RuboCop::Cop::Rails::ActiveSupportAliases do
   subject(:cop) { described_class.new }
 
--- spec/rubocop/cop/style/multiple_comparison_spec.rb
+++ spec/rubocop/cop/style/multiple_comparison_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
 RSpec.describe RuboCop::Cop::Style::MultipleComparison do
   subject(:cop) { described_class.new(config) }
 
