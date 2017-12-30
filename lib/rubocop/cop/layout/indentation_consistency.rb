04d720d9c6a08b5848c295b949f824b38dfe0d37
Koichi ITO <koic.ito@gmail.com>
2017-12-30 18:56:24 +0900

    Add an example of `EnforcedStyle` for `Layout/IndentationConsistency` cop
    Follow up of https://github.com/bbatsov/rubocop/pull/4880#issuecomment-338499947.
    
    This is only document change.
    
    This commit adds an example of `EnforcedStyle` to `Layout/IndentationConsistency`.
    
    Also this commit adds information on how to use by adding a description
    of `Layout/IndentationConsistency` cop written in config/default.yml.
    https://github.com/bbatsov/rubocop/blob/v0.52.1/config/default.yml#L312-L317

--- lib/rubocop/cop/layout/indentation_consistency.rb
+++ lib/rubocop/cop/layout/indentation_consistency.rb
@@ -5,13 +5,117 @@ module RuboCop
     module Layout
       # This cops checks for inconsistent indentation.
       #
-      # @example
+      # The difference between `rails` and `normal` is that the `rails` style
+      # prescribes that in classes and modules the `protected` and `private`
+      # modifier keywords shall be indented the same as public methods and that
+      # protected and private members shall be indented one step more than the
+      # modifiers. Other than that, both styles mean that entities on the same
+      # logical depth shall have the same indentation.
       #
+      # @example EnforcedStyle: normal (default)
+      #   # bad
       #   class A
       #     def test
       #       puts 'hello'
       #        puts 'world'
       #     end
+      #   end
+      #
+      #   # bad
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #       puts 'world'
+      #     end
+      #
+      #     protected
+      #
+      #       def foo
+      #       end
+      #
+      #     private
+      #
+      #       def bar
+      #       end
+      #   end
+      #
+      #   # good
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #       puts 'world'
+      #     end
+      #   end
+      #
+      #   # good
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #        puts 'world'
+      #     end
+      #
+      #     protected
+      #
+      #     def foo
+      #     end
+      #
+      #     private
+      #
+      #     def bar
+      #     end
+      #   end
+      #
+      # @example EnforcedStyle: rails
+      #   # bad
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #        puts 'world'
+      #     end
+      #   end
+      #
+      #   # bad
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #        puts 'world'
+      #     end
+      #
+      #     protected
+      #
+      #     def foo
+      #     end
+      #
+      #     private
+      #
+      #     def bar
+      #     end
+      #   end
+      #
+      #   # good
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #       puts 'world'
+      #     end
+      #   end
+      #
+      #   # good
+      #   class A
+      #     def test
+      #       puts 'hello'
+      #       puts 'world'
+      #     end
+      #
+      #     protected
+      #
+      #       def foo
+      #       end
+      #
+      #     private
+      #
+      #       def bar
+      #       end
       #   end
       class IndentationConsistency < Cop
         include Alignment
