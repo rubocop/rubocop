1003d19faa2179971e01de35270816b068532a6c
Garett Arrowood <garettarrowood@gmail.com>
2018-01-01 11:28:13 -0500

    Optimize tokens(node)

--- lib/rubocop/cop/util.rb
+++ lib/rubocop/cop/util.rb
@@ -121,7 +121,7 @@ module RuboCop
           .sub('Style', 'Styles')
       end
 
-      def tokens(node)
+      def tokens(node) # rubocop:disable Metrics/AbcSize
         @tokens ||= {}
         return @tokens[node.object_id] if @tokens[node.object_id]
 
@@ -129,9 +129,17 @@ module RuboCop
         begin_pos = source_range.begin_pos
         end_pos = source_range.end_pos
 
-        @tokens[node.object_id] = processed_source.tokens.select do |token|
-          token.end_pos <= end_pos && token.begin_pos >= begin_pos
+        tokens_to_node_end = processed_source.tokens.take_while do |token|
+          token.end_pos <= end_pos
         end
+
+        node_tokens = []
+        tokens_to_node_end.reverse_each do |token|
+          break unless token.begin_pos >= begin_pos
+          node_tokens.unshift(token)
+        end
+
+        @tokens[node.object_id] = node_tokens
       end
 
       private
