24d17c9c9ba89891f342415a8516400d1e873362
Garett Arrowood <garettarrowood@gmail.com>
2018-01-01 10:08:49 -0500

    Better examples with thorough documentation for Uncommunicative cops

--- lib/rubocop/cop/naming/uncommunicative_block_param_name.rb
+++ lib/rubocop/cop/naming/uncommunicative_block_param_name.rb
@@ -3,26 +3,36 @@
 module RuboCop
   module Cop
     module Naming
-      # This cop makes sure block parameter names meet a configurable
-      # level of description
+      # This cop checks block parameter names for how descriptive they
+      # are. It is highly configurable.
+      #
+      # The `MinNameLength` config option takes an integer. It represents
+      # the minimum amount of characters the name must be. Its default is 1.
+      # The `AllowNamesEndingInNumbers` config option takes a boolean. When
+      # set to false, this cop will register offenses for names ending with
+      # numbers. Its default is false. The `AllowedNames` config option
+      # takes an array of whitelisted names that will never register an
+      # offense. The `ForbiddenNames` config option takes an array of
+      # blacklisted names that will always register an offense.
       #
       # @example
       #   # bad
-      #   foo { |num1, num2| num1 + num2 }
-      #
       #   bar do |varOne, varTwo|
       #     varOne + varTwo
       #   end
       #
+      #   # With `AllowNamesEndingInNumbers` set to false
+      #   foo { |num1, num2| num1 * num2 }
+      #
       #   # With `MinParamNameLength` set to number greater than 1
-      #   baz { |x, y, z| do_stuff(x, y, z) }
+      #   baz { |a, b, c| do_stuff(a, b, c) }
       #
       #   # good
-      #   foo { |first_num, second_num| first_num + second_num }
-      #
-      #   bar do |var_one, var_two|
-      #     var_one + var_two
+      #   bar do |thud, fred|
+      #     thud + fred
       #   end
+      #
+      #   foo { |speed, distance| speed * distance }
       #
       #   baz { |age, height, gender| do_stuff(age, height, gender) }
       class UncommunicativeBlockParamName < Cop
