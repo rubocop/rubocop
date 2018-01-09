c32ce9e988ac3ec20bdef5a50b3eba240209b368
Keiji Yoshimi <walf443@gmail.com>
2018-01-09 23:03:27 +0900

    [#4910] Add examples for `Style/ClassVars` cop

--- lib/rubocop/cop/style/class_vars.rb
+++ lib/rubocop/cop/style/class_vars.rb
@@ -6,6 +6,29 @@ module RuboCop
       # This cop checks for uses of class variables. Offenses
       # are signaled only on assignment to class variables to
       # reduce the number of offenses that would be reported.
+      #
+      # Setting value for class variable need to take care.
+      # If some class has been inherited by other classes, setting value
+      # for class variable affected children classes.
+      # So using class instance variable is better in almost case.
+      #
+      # @example
+      #   # bad
+      #   class A
+      #     @@test = 10
+      #   end
+      #
+      #   # good
+      #   class A
+      #     @test = 10
+      #   end
+      #
+      #   class A
+      #     def test
+      #       @@test # you can access class variable without offence
+      #     end
+      #   end
+      #
       class ClassVars < Cop
         MSG = 'Replace class var %<class_var>s with a class ' \
               'instance var.'.freeze
