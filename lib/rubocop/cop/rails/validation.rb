5dcb881f92b4212cfd98f7d6e7e4cf5295356f30
gohdaniel15 <gohdaniel15@gmail.com>
2018-01-10 00:23:26 +0800

    [#4910] Add examples for `Rails/Validation` cop

--- lib/rubocop/cop/rails/validation.rb
+++ lib/rubocop/cop/rails/validation.rb
@@ -4,6 +4,32 @@ module RuboCop
   module Cop
     module Rails
       # This cop checks for the use of old-style attribute validation macros.
+      #
+      # @example
+      #   # bad
+      #   validates_acceptance_of :foo
+      #   validates_confirmation_of :foo
+      #   validates_exclusion_of :foo
+      #   validates_format_of :foo
+      #   validates_inclusion_of :foo
+      #   validates_length_of :foo
+      #   validates_numericality_of :foo
+      #   validates_presence_of :foo
+      #   validates_size_of :foo
+      #   validates_uniqueness_of :foo
+      #
+      #   # good
+      #   validates :foo, acceptance: true
+      #   validates :foo, confirmation: true
+      #   validates :foo, exclusion: true
+      #   validates :foo, format: true
+      #   validates :foo, inclusion: true
+      #   validates :foo, length: true
+      #   validates :foo, numericality: true
+      #   validates :foo, presence: true
+      #   validates :foo, size: true
+      #   validates :foo, uniqueness: true
+      #
       class Validation < Cop
         MSG = 'Prefer the new style validations `%<prefer>s` over ' \
               '`%<current>s`.'.freeze
--- manual/cops_rails.md
+++ manual/cops_rails.md
@@ -1687,6 +1687,34 @@ Enabled | Yes
 
 This cop checks for the use of old-style attribute validation macros.
 
+### Examples
+
+```ruby
+# bad
+validates_acceptance_of :foo
+validates_confirmation_of :foo
+validates_exclusion_of :foo
+validates_format_of :foo
+validates_inclusion_of :foo
+validates_length_of :foo
+validates_numericality_of :foo
+validates_presence_of :foo
+validates_size_of :foo
+validates_uniqueness_of :foo
+
+# good
+validates :foo, acceptance: true
+validates :foo, confirmation: true
+validates :foo, exclusion: true
+validates :foo, format: true
+validates :foo, inclusion: true
+validates :foo, length: true
+validates :foo, numericality: true
+validates :foo, presence: true
+validates :foo, size: true
+validates :foo, uniqueness: true
+```
+
 ### Configurable attributes
 
 Name | Default value | Configurable values
