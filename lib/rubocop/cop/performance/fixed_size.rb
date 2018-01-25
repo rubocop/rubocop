79c070a0950969b242dead5123b8c0a7022d5e68
Garett Arrowood <garettarrowood@gmail.com>
2018-01-25 07:47:52 -0500

    [#4910] Add examples to Performance/FixedSize

--- lib/rubocop/cop/performance/fixed_size.rb
+++ lib/rubocop/cop/performance/fixed_size.rb
@@ -4,6 +4,47 @@ module RuboCop
   module Cop
     module Performance
       # Do not compute the size of statically sized objects.
+      #
+      # @example
+      #   # String methods
+      #   # bad
+      #   'foo'.size
+      #   %q[bar].count
+      #   %(qux).length
+      #
+      #   # Symbol methods
+      #   # bad
+      #   :fred.size
+      #   :'baz'.length
+      #
+      #   # Array methods
+      #   # bad
+      #   [1, 2, thud].count
+      #   %W(1, 2, bar).size
+      #
+      #   # Hash methods
+      #   # bad
+      #   { a: corge, b: grault }.length
+      #
+      #   # good
+      #   foo.size
+      #   bar.count
+      #   qux.length
+      #
+      #   # good
+      #   :"#{fred}".size
+      #   CONST = :baz.length
+      #
+      #   # good
+      #   [1, 2, *thud].count
+      #   garply = [1, 2, 3]
+      #   garly.size
+      #
+      #   # good
+      #   { a: corge, **grault }.length
+      #   waldo = { a: corge, b: grault }
+      #   waldo.size
+      #
       class FixedSize < Cop
         MSG = 'Do not compute the size of statically sized objects.'.freeze
 
