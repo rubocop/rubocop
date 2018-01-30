11480882c4a9586fccf40844ee0fc215998678e7
wata_mac <watassbass@gmail.com>
2018-01-31 00:12:18 +0900

    Reverse key and value in NODE_MAP
    Reverse key and value so that we can get the class to generate from the
    AST type received in `RuboCop::AST::Node#updated`.

--- lib/rubocop/ast/builder.rb
+++ lib/rubocop/ast/builder.rb
@@ -2,41 +2,49 @@
 
 module RuboCop
   module AST
-    # `RuboCop::Builder` is an AST builder that is utilized to let `Parser`
+    # `RuboCop::AST::Builder` is an AST builder that is utilized to let `Parser`
     # generate ASTs with {RuboCop::AST::Node}.
     #
     # @example
     #   buffer = Parser::Source::Buffer.new('(string)')
     #   buffer.source = 'puts :foo'
     #
-    #   builder = RuboCop::Builder.new
-    #   parser = Parser::CurrentRuby.new(builder)
+    #   builder = RuboCop::AST::Builder.new
+    #   require 'parser/ruby25'
+    #   parser = Parser::Ruby25.new(builder)
     #   root_node = parser.parse(buffer)
     class Builder < Parser::Builders::Default
       NODE_MAP = {
-        AndNode          => [:and],
-        ArgsNode         => [:args],
-        ArrayNode        => [:array],
-        BlockNode        => [:block],
-        CaseNode         => [:case],
-        DefNode          => %i[def defs],
-        EnsureNode       => [:ensure],
-        ForNode          => [:for],
-        HashNode         => [:hash],
-        IfNode           => [:if],
-        KeywordSplatNode => [:kwsplat],
-        OrNode           => [:or],
-        PairNode         => [:pair],
-        RegexpNode       => [:regexp],
-        ResbodyNode      => [:resbody],
-        SendNode         => %i[csend send],
-        StrNode          => %i[str dstr xstr],
-        SuperNode        => %i[super zsuper],
-        SymbolNode       => [:sym],
-        UntilNode        => %i[until until_post],
-        WhenNode         => [:when],
-        WhileNode        => %i[while while_post],
-        YieldNode        => [:yield]
+        and:        AndNode,
+        args:       ArgsNode,
+        array:      ArrayNode,
+        block:      BlockNode,
+        case:       CaseNode,
+        def:        DefNode,
+        defs:       DefNode,
+        ensure:     EnsureNode,
+        for:        ForNode,
+        hash:       HashNode,
+        if:         IfNode,
+        kwsplat:    KeywordSplatNode,
+        or:         OrNode,
+        pair:       PairNode,
+        regexp:     RegexpNode,
+        resbody:    ResbodyNode,
+        csend:      SendNode,
+        send:       SendNode,
+        str:        StrNode,
+        dstr:       StrNode,
+        xstr:       StrNode,
+        super:      SuperNode,
+        zsuper:     SuperNode,
+        sym:        SymbolNode,
+        until:      UntilNode,
+        until_post: UntilNode,
+        when:       WhenNode,
+        while:      WhileNode,
+        while_post: WhileNode,
+        yield:      YieldNode
       }.freeze
 
       # Generates {Node} from the given information.
@@ -55,17 +63,7 @@ module RuboCop
       private
 
       def node_klass(type)
-        node_map[type] || Node
-      end
-
-      # Take the human readable constant and generate a hash map where each
-      # (mapped) node type is a key with its constant as the value.
-      def node_map
-        @node_map ||= begin
-          NODE_MAP.each_pair.each_with_object({}) do |(klass, types), map|
-            types.each { |type| map[type] = klass }
-          end
-        end
+        NODE_MAP[type] || Node
       end
     end
   end
