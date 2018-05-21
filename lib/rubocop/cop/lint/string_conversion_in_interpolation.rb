92b4f0c67d90dbc8fbc3db42816fc09b2f9f66bf
Maxim Krizhanovsky <darhazer@gmail.com>
2018-05-21 17:03:06 +0300

    Use node_matcher for better readability

--- lib/rubocop/cop/lint/string_conversion_in_interpolation.rb
+++ lib/rubocop/cop/lint/string_conversion_in_interpolation.rb
@@ -22,12 +22,13 @@ module RuboCop
         MSG_SELF = 'Use `self` instead of `Object#to_s` in ' \
                    'interpolation.'.freeze
 
+        def_node_matcher :to_s_without_args?, '(send _ :to_s)'
+
         def on_dstr(node)
           node.each_child_node(:begin) do |begin_node|
             final_node = begin_node.children.last
 
-            next unless final_node && final_node.send_type? &&
-                        final_node.method?(:to_s) && !final_node.arguments?
+            next unless to_s_without_args?(final_node)
 
             add_offense(final_node, location: :selector)
           end
@@ -35,7 +36,7 @@ module RuboCop
 
         def autocorrect(node)
           lambda do |corrector|
-            receiver, _method_name, *_args = *node
+            receiver = node.receiver
             corrector.replace(
               node.source_range,
               if receiver
