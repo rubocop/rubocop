ebd397654dc321a1f6187f83fff613036787dc24
Sven <sven.dittmer@gmx.net>
2018-06-03 05:29:43 +0200

    Add examples for Style/NumericLiteralPrefix (#5941)

--- lib/rubocop/cop/style/numeric_literal_prefix.rb
+++ lib/rubocop/cop/style/numeric_literal_prefix.rb
@@ -6,10 +6,33 @@ module RuboCop
       # This cop checks for octal, hex, binary and decimal literals using
       # uppercase prefixes and corrects them to lowercase prefix
       # or no prefix (in case of decimals).
-      # eg. for octal use `0o` instead of `0` or `0O`.
       #
-      # Can be configured to use `0` only for octal literals using
-      # `EnforcedOctalStyle` => `zero_only`
+      # @example EnforcedOctalStyle: zero_with_o (default)
+      #   # bad - missing octal prefix
+      #   num = 01234
+      #
+      #   # bad - uppercase prefix
+      #   num = 0O1234
+      #   num = 0X12AB
+      #   num = 0B10101
+      #
+      #   # bad - redundant decimal prefix
+      #   num = 0D1234
+      #   num = 0d1234
+      #
+      #   # good
+      #   num = 0o1234
+      #   num = 0x12AB
+      #   num = 0b10101
+      #   num = 1234
+      #
+      # @example EnforcedOctalStyle: zero_only
+      #   # bad
+      #   num = 0o1234
+      #   num = 0O1234
+      #
+      #   # good
+      #   num = 01234
       class NumericLiteralPrefix < Cop
         include IntegerNode
 
