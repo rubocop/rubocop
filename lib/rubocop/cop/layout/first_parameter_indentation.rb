bc3b904777bbe19898e5599c8467868878b2a9b3
Koichi ITO <koic.ito@gmail.com>
2018-06-09 08:29:43 +0900

    Restyle example formatting for `Layout/FirstParameterIndentation`
    Follow up of https://github.com/rubocop-hq/rubocop/pull/4880#issuecomment-338499947
    and #5699.
    
    This commit is a change of document format for `Layout/FirstParameterIndentation` cop.
    
    This PR uses `# rubocop:disable Metrics/LineLength` because line length of
    the following line exceeds 80.
    
    ```diff
    +      # @example EnforcedStyle: special_for_inner_method_call_in_parentheses (default)
    ```
    
    Descriptions of `EnforcedStyle` are quoted from default.yml.
    https://github.com/rubocop-hq/rubocop/blob/v0.57.1/config/default.yml#L365-L381

--- lib/rubocop/cop/layout/first_parameter_indentation.rb
+++ lib/rubocop/cop/layout/first_parameter_indentation.rb
@@ -2,6 +2,7 @@
 
 module RuboCop
   module Cop
+    # rubocop:disable Metrics/LineLength
     module Layout
       # This cop checks the indentation of the first parameter in a method call.
       # Parameters after the first one are checked by Layout/AlignParameters,
@@ -31,7 +32,9 @@ module RuboCop
       #   nested_first_param),
       #   second_param
       #
-      #   # Style: consistent
+      # @example EnforcedStyle: consistent
+      #   # The first parameter should always be indented one step more than the
+      #   # preceding line.
       #
       #   # good
       #   some_method(
@@ -55,7 +58,9 @@ module RuboCop
       #     nested_first_param),
       #   second_param
       #
-      #   # Style: consistent_relative_to_receiver
+      # @example EnforcedStyle: consistent_relative_to_receiver
+      #   # The first parameter should always be indented one level relative to
+      #   # the parent that is receiving the parameter
       #
       #   # good
       #   some_method(
@@ -79,7 +84,11 @@ module RuboCop
       #                 nested_first_param),
       #   second_params
       #
-      #   # Style: special_for_inner_method_call
+      # @example EnforcedStyle: special_for_inner_method_call
+      #   # The first parameter should normally be indented one step more than
+      #   # the preceding line, but if it's a parameter for a method call that
+      #   # is itself a parameter in a method call, then the inner parameter
+      #   # should be indented relative to the inner method.
       #
       #   # good
       #   some_method(
@@ -103,7 +112,10 @@ module RuboCop
       #                 nested_first_param),
       #   second_param
       #
-      #   # Style: special_for_inner_method_call_in_parentheses
+      # @example EnforcedStyle: special_for_inner_method_call_in_parentheses (default)
+      #   # Same as `special_for_inner_method_call` except that the special rule
+      #   # only applies if the outer method call encloses its arguments in
+      #   # parentheses.
       #
       #   # good
       #   some_method(
@@ -128,6 +140,7 @@ module RuboCop
       #   second_param
       #
       class FirstParameterIndentation < Cop
+        # rubocop:enable Metrics/LineLength
         include Alignment
         include ConfigurableEnforcedStyle
         include RangeHelp
