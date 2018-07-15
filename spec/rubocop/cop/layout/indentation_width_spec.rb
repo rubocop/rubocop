rubocop/rubocop

commit f3500b29ce231fbc18950e1a2a8dcd42e420f628

[Fix #4878] Fix false positive in `Layout/IndentationWidth` cop (#5996)
This cop would register an offense when multiple modifiers and
def were on the same line. This change fixes that.

lib/rubocop/cop/layout/indentation_width.rb
@@ -103,7 +103,11 @@ module RuboCop
 
           def_end_config = config.for_cop('Layout/DefEndAlignment')
           style = def_end_config['EnforcedStyleAlignWith'] || 'start_of_line'
-          base = style == 'def' ? node.first_argument : node
+          base = if style == 'def'
+                   node.first_argument
+                 else
+                   leftmost_modifier_of(node) || node
+                 end
 
           check_indentation(base.source_range, body)
           ignore_node(node.first_argument)
@@ -318,6 +322,10 @@ module RuboCop
 
         def configured_indentation_width
           cop_config['Width']
+        end
+
+        def leftmost_modifier_of(node)
+          node.each_ancestor(:send).to_a.last
         end
       end
     end

spec/rubocop/cop/layout/indentation_width_spec.rb
@@ -903,6 +903,34 @@ RSpec.describe RuboCop::Cop::Layout::IndentationWidth do
             RUBY
           end
         end
+
+        context 'when multiple modifiers and def are on the same line' do
+          it 'accepts a correctly aligned body' do
+            expect_no_offenses(<<-RUBY.strip_indent)
+              public foo def test
+                something
+              end
+            RUBY
+          end
+
+          it 'registers an offense for bad indentation of a def body' do
+            expect_offense(<<-RUBY.strip_indent)
+              public foo def test
+                    something
+              ^^^^^^ Use 2 (not 6) spaces for indentation.
+              end
+            RUBY
+          end
+
+          it 'registers an offense for bad indentation of a defs body' do
+            expect_offense(<<-RUBY.strip_indent)
+              public foo def self.test
+                    something
+              ^^^^^^ Use 2 (not 6) spaces for indentation.
+              end
+            RUBY
+          end
+        end
       end
 
       context 'when end is aligned with def' do

commit 61be5a58cdd0d9b6280254b7e4bad9c67eb1be5d

Fix a false positive for `Layout/IndentationWidth`
This PR fixes a false positive for `Layout/IndentationWidth` cop
when multiple modifiers are used in a block and a method call is
made at end of the block.

This false positive is a regression that was mixed in #5996.

With the change from RuboCop 0.57.1 to 0.57.2, the following code now
detects a false positive.

```ruby
obj = Class.new do
  private def private_property
    "That would be great."
  end

  protected def protected_property
    "I believe you have my stapler."
  end
end.new
```

https://github.com/rails/rails/blob/05bef14/actionview/test/template/form_helper/form_with_test.rb#L451-L459

The following is the result of running in rails/rails repo.

```console
% rubocop -V
0.58.1 (using Parser 2.5.1.2, running on ruby 2.5.1 x86_64-darwin17)
% rubocop actionview/test/template/form_helper/form_with_test.rb
Inspecting 1 file
C

Offenses:

actionview/test/template/form_helper/form_with_test.rb:453:9: C:
Layout/IndentationWidth: Use 2 (not -2) spaces for indentation.
        "That would be great."
        ^^
actionview/test/template/form_helper/form_with_test.rb:457:9: C:
Layout/IndentationWidth: Use 2 (not -2) spaces for indentation.
        "I believe you have my stapler."
        ^^

1 file inspected, 2 offenses detected
```

lib/rubocop/cop/layout/indentation_width.rb
@@ -325,7 +325,9 @@ module RuboCop
         end
 
         def leftmost_modifier_of(node)
-          node.each_ancestor(:send).to_a.last
+          return node unless node.parent && node.parent.send_type?
+
+          leftmost_modifier_of(node.parent)
         end
       end
     end

spec/rubocop/cop/layout/indentation_width_spec.rb
@@ -930,6 +930,40 @@ RSpec.describe RuboCop::Cop::Layout::IndentationWidth do
               end
             RUBY
           end
+
+          context 'when multiple modifiers are used in a block and' \
+                  'a method call is made at end of the block' do
+            it 'accepts a correctly aligned body' do
+              expect_no_offenses(<<-RUBY.strip_indent)
+                obj = Class.new do
+                  private def private_property
+                    "That would be great."
+                  end
+                end.new
+              RUBY
+            end
+
+            it 'registers an offense for bad indentation of a def' do
+              expect_offense(<<-RUBY.strip_indent)
+                obj = Class.new do
+                    private def private_property
+                ^^^^ Use 2 (not 4) spaces for indentation.
+                    end
+                end.new
+              RUBY
+            end
+
+            it 'registers an offense for bad indentation of a def body' do
+              expect_offense(<<-RUBY.strip_indent)
+                obj = Class.new do
+                  private def private_property
+                      "That would be great."
+                  ^^^^ Use 2 (not 4) spaces for indentation.
+                  end
+                end.new
+              RUBY
+            end
+          end
         end
       end
 