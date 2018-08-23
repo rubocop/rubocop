130ee5564d20d90c4181e1d7bb217b9740e78cc4
David Rodríguez <deivid.rodriguez@riseup.net>
2018-08-23 17:29:36 -0300

    Transform CI script to shell

---
+++ .travis.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -eo pipefail
+
+run() {
+  run_main_task
+  report_coverage
+  documentation
+  check_requiring_libraries
+}
+
+# Check requiring libraries successfully.
+# See https://github.com/rubocop-hq/rubocop/pull/4523#issuecomment-309136113
+check_requiring_libraries() {
+  logged ruby -I lib -r rubocop -e 'exit 0'
+}
+
+# Running YARD under jruby crashes so skip checking the manual.
+documentation() {
+  if ! is_jruby; then
+    logged bundle exec rake documentation_syntax_check generate_cops_documentation
+  fi
+}
+
+is_jruby() {
+  [ "$(ruby -e "puts RUBY_ENGINE == 'jruby'")" = 'true' ]
+}
+
+is_master() {
+  [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]
+}
+
+report_coverage() {
+  if is_test && is_master; then
+    logged bundle exec codeclimate-test-reporter
+  fi
+}
+
+run_main_task() {
+  if is_master || ! is_test || is_jruby; then
+    logged bundle exec rake "$TASK"
+  else
+    logged bundle exec rake "parallel:$TASK"
+  fi
+}
+
+logged() {
+  echo "$ $*"
+  time "$@"
+}
+
+is_test() {
+  [ "$TASK" != 'internal_investigation' ]
+}
+
+run
