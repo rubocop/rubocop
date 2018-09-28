b017f93134bc08a194cac950528244d7aeccaa5f
Koichi ITO <koic.ito@gmail.com>
2018-09-28 18:37:12 +0900

    Fix an error for `Rails/ReversibleMigration`
    This PR fixes an error for `Rails/ReversibleMigration` when
    block argument is empty.
    
    The following is a reproduction step.
    
    ```console
    % rubocop -V
    0.59.2 (using Parser 2.5.1.2, running on ruby 2.5.1 x86_64-darwin17)
    
    % cat db/migrate/20180926221900_change_invoices.rb
    class ChangeInvoices < ActiveRecord::Migration[5.2]
      def change
        change_table :invoices do |t|
        end
      end
    end
    
    % rubocop db/migrate/20180926221900_change_invoices.rb --only
      Rails/ReversibleMigration -d
    For /private/tmp: configuration from
    /Users/koic/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/rubocop-0.59.2/config/default.yml
    Inheriting configuration from
    /Users/koic/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/rubocop-0.59.2/config/enabled.yml
    Inheriting configuration from
    /Users/koic/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/rubocop-0.59.2/config/disabled.yml
    Inspecting 1 file
    Scanning /private/tmp/db/migrate/20180926221900_change_invoices.rb
    An error occurred while Rails/ReversibleMigration cop was inspecting
    /private/tmp/db/migrate/20180926221900_change_invoices.rb:3:4.
    undefined method `send_type?' for nil:NilClass
    /Users/koic/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/rubocop-0.59.2/lib/rubocop/cop/rails/reversible_migration.rb:238:in
    `block in check_change_table_node'
    /Users/koic/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/rubocop-0.59.2/lib/rubocop/cop/rails/reversible_migration.rb:156:in
    `change_table_call'
    
    (snip)
    
    Errors are usually caused by RuboCop bugs.
    Please, report your problems to RuboCop's issue tracker.
    https://github.com/rubocop-hq/rubocop/issues
    
    Mention the following information in the issue report:
    0.59.2 (using Parser 2.5.1.2, running on ruby 2.5.1 x86_64-darwin17)
    Finished in 0.3659559999941848 seconds
    ```

--- lib/rubocop/cop/rails/reversible_migration.rb
+++ lib/rubocop/cop/rails/reversible_migration.rb
@@ -169,6 +169,7 @@ module RuboCop
         def on_block(node)
           return unless within_change_method?(node)
           return if within_reversible_or_up_only_block?(node)
+          return if node.body.nil?
 
           check_change_table_node(node.send_node, node.body)
         end
