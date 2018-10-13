109cfa80c4f4eb704eb6e11dcfff7fa15f534b8a
Koichi ITO <koic.ito@gmail.com>
2018-10-13 18:55:58 +0900

    Add examples for `Style/CollectionMethods`
    This PR refers to the setting to default.yml as examples target.
    
    ```yaml
    PreferredMethods:
      collect: 'map'
      collect!: 'map!'
      inject: 'reduce'
      detect: 'find'
      find_all: 'select'
    ```
    
    https://github.com/rubocop-hq/rubocop/blob/v0.59.2/config/default.yml#L964-L975

--- lib/rubocop/cop/style/collection_methods.rb
+++ lib/rubocop/cop/style/collection_methods.rb
@@ -9,6 +9,32 @@ module RuboCop
       # Unfortunately we cannot actually know if a method is from
       # Enumerable or not (static analysis limitation), so this cop
       # can yield some false positives.
+      #
+      # You can customize the mapping from undesired method to desired method.
+      #
+      # e.g. to use `detect` over `find`:
+      #
+      #   Style/CollectionMethods:
+      #     PreferredMethods:
+      #       find: detect
+      #
+      # The default mapping for `PreferredMethods` behaves as follows.
+      #
+      # @example
+      #   # bad
+      #   items.collect
+      #   items.collect!
+      #   items.inject
+      #   items.detect
+      #   items.find_all
+      #
+      #   # good
+      #   items.map
+      #   items.map!
+      #   items.reduce
+      #   items.find
+      #   items.select
+      #
       class CollectionMethods < Cop
         include MethodPreference
 
--- manual/cops_style.md
+++ manual/cops_style.md
@@ -718,6 +718,34 @@ Unfortunately we cannot actually know if a method is from
 Enumerable or not (static analysis limitation), so this cop
 can yield some false positives.
 
+You can customize the mapping from undesired method to desired method.
+
+e.g. to use `detect` over `find`:
+
+  Style/CollectionMethods:
+    PreferredMethods:
+      find: detect
+
+The default mapping for `PreferredMethods` behaves as follows.
+
+### Examples
+
+```ruby
+# bad
+items.collect
+items.collect!
+items.inject
+items.detect
+items.find_all
+
+# good
+items.map
+items.map!
+items.reduce
+items.find
+items.select
+```
+
 ### Configurable attributes
 
 Name | Default value | Configurable values
