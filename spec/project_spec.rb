535e3c970a90c2b4826d41de87c9e7907a267ef3
Tom Lord <lord.thom@gmail.com>
2018-11-18 22:03:47 +0000

    Tweak expectation to avoid printing the whole file in failure message

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -84,7 +84,9 @@ RSpec.describe 'RuboCop Project', type: :feature do
     it 'has link definitions for all implicit links' do
       implicit_link_names = changelog.scan(/\[([^\]]+)\]\[\]/).flatten.uniq
       implicit_link_names.each do |name|
-        expect(changelog).to include("[#{name}]: http")
+        expect(changelog.include?("[#{name}]: http"))
+          .to be(true), "CHANGELOG.md is missing a link for #{name}. " \
+                        'Please add this link to the bottom of the file.'
       end
     end
 
