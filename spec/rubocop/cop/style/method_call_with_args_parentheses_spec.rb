3aa93511378d78e0a3712a8b5748e7e583ab0c64
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-11-20 19:37:49 +0200

    Parentheses omission check for method calls with logical operators
    This is an edge case for the newly introduced alternative style for the
    `Style/MethodCallWithArgsParentheses` cop that enforces the parentheses
    omission method calls style.
    
    When you call methods guarding them with logical operators, you could
    have wanted to be explicit, or you could caused a syntax error when you
    omit the parentheses. This change relaxes the `omit_parentheses` style
    check for those cases.

--- spec/rubocop/cop/style/method_call_with_args_parentheses_spec.rb
+++ spec/rubocop/cop/style/method_call_with_args_parentheses_spec.rb
@@ -271,7 +271,7 @@ RSpec.describe RuboCop::Cop::Style::MethodCallWithArgsParentheses, :config do
     end
   end
 
-  context 'when EnforcedStyle is omit' do
+  context 'when EnforcedStyle is omit_parentheses' do
     let(:cop_config) do
       { 'EnforcedStyle' => 'omit_parentheses' }
     end
@@ -363,6 +363,17 @@ RSpec.describe RuboCop::Cop::Style::MethodCallWithArgsParentheses, :config do
 
     it 'accepts parens in blocks with braces' do
       expect_no_offenses('foo(1) { 2 }')
+    end
+
+    it 'accepts parens in calls with logical operators' do
+      expect_no_offenses('foo(a) && bar(b)')
+      expect_no_offenses('foo(a) || bar(b)')
+    end
+
+    it 'accepts parens in calls with arguments with logical operators' do
+      expect_no_offenses('foo(a, b || c)')
+      expect_no_offenses('foo a, b || c')
+      expect_no_offenses('foo a, b(1) || c(2, d(3))')
     end
 
     it 'auto-corrects single-line calls' do
