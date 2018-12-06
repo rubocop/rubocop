rubocop/rubocop

commit 9477ef90b75da6d19c950be1d606d10ee7449380

[Fix #6433] Ruby 2.5 assigned block rescue alignment (#6437)
Fix Ruby 2.5 Layout/RescueEnsureAlignment error on assigned blocks.

lib/rubocop/cop/layout/rescue_ensure_alignment.rb
@@ -28,8 +28,9 @@ module RuboCop
               'aligned with `%<beginning>s` at ' \
               '%<begin_loc_line>d, %<begin_loc_column>d.'.freeze
         ANCESTOR_TYPES = %i[kwbegin def defs class module].freeze
-        RUBY_2_5_ANCESTOR_TYPES = (ANCESTOR_TYPES + [:block]).freeze
+        RUBY_2_5_ANCESTOR_TYPES = (ANCESTOR_TYPES + %i[block]).freeze
         ANCESTOR_TYPES_WITH_ACCESS_MODIFIERS = %i[def defs].freeze
+        ASSIGNMENT_TYPES = %i[lvasgn].freeze
 
         def on_resbody(node)
           check(node) unless modifier?(node)
@@ -115,10 +116,13 @@ module RuboCop
           ancestor_node = ancestor_node(node)
           return nil if ancestor_node.nil?
 
-          access_modifier_node = access_modifier_node(ancestor_node)
-          return ancestor_node if access_modifier_node.nil?
+          assignment_node = assignment_node(ancestor_node)
+          return assignment_node unless assignment_node.nil?
 
-          access_modifier_node
+          access_modifier_node = access_modifier_node(ancestor_node)
+          return access_modifier_node unless access_modifier_node.nil?
+
+          ancestor_node
         end
 
         def ancestor_node(node)
@@ -130,6 +134,15 @@ module RuboCop
             end
 
           node.each_ancestor(*ancestor_types).first
+        end
+
+        def assignment_node(node)
+          assignment_node = node.ancestors.first
+          return nil unless
+            assignment_node &&
+            ASSIGNMENT_TYPES.include?(assignment_node.type)
+
+          assignment_node
         end
 
         def access_modifier_node(node)

spec/rubocop/cop/layout/rescue_ensure_alignment_spec.rb
@@ -365,6 +365,16 @@ RSpec.describe RuboCop::Cop::Layout::RescueEnsureAlignment, :config do
       RUBY
     end
 
+    it 'accepts aligned rescue in assigned do-end block' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        result = [1, 2, 3].map do |el|
+          el.to_s
+        rescue StandardError => _exception
+          next
+        end
+      RUBY
+    end
+
     it 'accepts aligned rescue in do-end block in a method' do
       expect_no_offenses(<<-RUBY.strip_indent)
         def foo

commit f5acab026784634d5738f288f788fc67ab9ccad7

[Fix #6550] Prevent Layout/RescueEnsureAlignment from breaking on assigned begin-end
We were missing some test cases for this cop, and accidentally introduced a
regression in #6437.

This change adds a relevant test case and fixes the issue.

lib/rubocop/cop/layout/rescue_ensure_alignment.rb
@@ -114,7 +114,9 @@ module RuboCop
 
         def alignment_node(node)
           ancestor_node = ancestor_node(node)
-          return nil if ancestor_node.nil?
+
+          return ancestor_node if ancestor_node.nil? ||
+                                  ancestor_node.kwbegin_type?
 
           assignment_node = assignment_node(ancestor_node)
           return assignment_node unless assignment_node.nil?

spec/rubocop/cop/layout/rescue_ensure_alignment_spec.rb
@@ -354,6 +354,16 @@ RSpec.describe RuboCop::Cop::Layout::RescueEnsureAlignment, :config do
     RUBY
   end
 
+  it 'accepts correctly aligned rescue in assigned begin-end block' do
+    expect_no_offenses(<<-RUBY)
+      foo = begin
+              bar
+            rescue BazError
+              qux
+            end
+    RUBY
+  end
+
   context '>= Ruby 2.5', :ruby25 do
     it 'accepts aligned rescue in do-end block' do
       expect_no_offenses(<<-RUBY.strip_indent)