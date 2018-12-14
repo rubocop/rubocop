rubocop/rubocop

commit fce51ea25827d8967677de4cd039ca7a51736fc0

[Fix #6305] Fix infinite loop for `Layout/EmptyLinesAroundAccessModifier`
Fixes #6305.

This is a problem that occurs after #6236.

This PR fixes infinite loop for `Layout/EmptyLinesAroundAccessModifier`
and `Layout/EmptyLinesAroundClassBody` when specifying a superclass
that breaks the line.

```ruby
class AVerryLongClassNameDoYouLikeIt <
  ItEvenInheritsFromALongClassNameSOINeedToAddALineBrake

  protected

  def foobar
  end
end
```

With this PR, `Layout/EmptyLinesAroundAccessModifier` cop makes
aware of the above case that specifying a superclass that
breaks the line. This is the same behavior as writing a class
definition on a single line.

As a result, `Layout/EmptyLinesAroundAccessModifier` and
`Layout/EmptyLinesAroundClassBody` will not conflict with
the handling of empty line under the class definition.

This problem was caused by using the regular expression
in `EmptyLinesAroundAccessModifier#class_def?` method.
This PR will change it to use AST.

lib/rubocop/cop/layout/empty_lines_around_access_modifier.rb
@@ -29,6 +29,26 @@ module RuboCop
         MSG_BEFORE_AND_AFTER = 'Keep a blank line before and after ' \
                                '`%<modifier>s`.'.freeze
 
+        def on_class(node)
+          _name, superclass, _body = *node
+
+          @class_or_module_def_line = if superclass
+                                        superclass.first_line
+                                      else
+                                        node.source_range.first_line
+                                      end
+        end
+
+        def on_module(node)
+          @class_or_module_def_line = node.source_range.first_line
+        end
+
+        def on_sclass(node)
+          self_node, _body = *node
+
+          @class_or_module_def_line = self_node.source_range.first_line
+        end
+
         def on_send(node)
           return unless node.bare_access_modifier?
 
@@ -40,11 +60,10 @@ module RuboCop
         def autocorrect(node)
           lambda do |corrector|
             send_line = node.first_line
-            previous_line = processed_source[send_line - 2]
             next_line = processed_source[send_line]
             line = range_by_whole_lines(node.source_range)
 
-            unless previous_line_empty?(previous_line)
+            unless previous_line_empty?(send_line)
               corrector.insert_before(line, "\n")
             end
 
@@ -62,9 +81,12 @@ module RuboCop
           end
         end
 
-        def previous_line_empty?(previous_line)
+        def previous_line_empty?(send_line)
+          previous_line = previous_line_ignoring_comments(processed_source,
+                                                          send_line)
+
           block_start?(previous_line) ||
-            class_def?(previous_line) ||
+            class_def?(send_line) ||
             previous_line.blank?
         end
 
@@ -74,15 +96,16 @@ module RuboCop
 
         def empty_lines_around?(node)
           send_line = node.first_line
-          previous_line = previous_line_ignoring_comments(processed_source,
-                                                          send_line)
+
           next_line = processed_source[send_line]
 
-          previous_line_empty?(previous_line) && next_line_empty?(next_line)
+          previous_line_empty?(send_line) && next_line_empty?(next_line)
         end
 
         def class_def?(line)
-          line =~ /^\s*(class|module)/
+          return false unless @class_or_module_def_line
+
+          line == @class_or_module_def_line + 1
         end
 
         def block_start?(line)
@@ -94,10 +117,11 @@ module RuboCop
         end
 
         def message(node)
-          previous_line = processed_source[node.first_line - 2]
+          send_line = node.first_line
+          previous_line = processed_source[send_line - 2]
 
           if block_start?(previous_line) ||
-             class_def?(previous_line)
+             class_def?(send_line)
             format(MSG_AFTER, modifier: node.loc.selector.source)
           else
             format(MSG_BEFORE_AND_AFTER, modifier: node.loc.selector.source)

spec/rubocop/cop/layout/empty_lines_around_access_modifier_spec.rb
@@ -137,12 +137,68 @@ RSpec.describe RuboCop::Cop::Layout::EmptyLinesAroundAccessModifier do
       RUBY
     end
 
-    it 'accepts missing blank line when at the beginning of class/module' do
+    it 'accepts missing blank line when at the beginning of class' do
       expect_no_offenses(<<-RUBY.strip_indent)
         class Test
           #{access_modifier}
 
           def test; end
+        end
+      RUBY
+    end
+
+    it 'accepts missing blank line when at the beginning of module' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        module Test
+          #{access_modifier}
+
+          def test; end
+        end
+      RUBY
+    end
+
+    it 'accepts missing blank line when at the beginning of sclass' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        class << self
+          #{access_modifier}
+
+          def test; end
+        end
+      RUBY
+    end
+
+    it 'accepts missing blank line when specifying a superclass ' \
+       'that breaks the line' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        class Foo <
+              Bar
+          #{access_modifier}
+
+          def do_something
+          end
+        end
+      RUBY
+    end
+
+    it 'accepts missing blank line when specifying `self` ' \
+       'that breaks the line' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        class <<
+              self
+          #{access_modifier}
+
+          def do_something
+          end
+        end
+      RUBY
+    end
+
+    it 'accepts missing blank line when at the beginning of file' \
+       'when specifying a superclass that breaks the line' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        #{access_modifier}
+
+        def do_something
         end
       RUBY
     end

commit d6638419b4ec7b35dfb108c04fd7452dc9bc0179

[Fix #6566] Fix a false positive for `Layout/EmptyLinesAroundAccessModifier`
Fixes #6566.

This PR fixes a false positive for `Layout/EmptyLinesAroundAccessModifier`
when at the end of specifying a superclass is missing blank line.

The following is originally no offense code.
(It is probably a regression by #6307)

```console
% cat example.rb
class SomeController < SomeOtherController
  def index; end

  private
end

% rubocop example.rb --only Layout/EmptyLinesAroundAccessModifier -a
Inspecting 1 file
C

Offenses:

example.rb:4:3: C: [Corrected] Layout/EmptyLinesAroundAccessModifier:
Keep a blank line before and after private.
  private
  ^^^^^^^

1 file inspected, 1 offense detected, 1 offense corrected
```

Auto-correct adds a blank line after `private`. This is due to false positives.

```diff
 % cat example.rb
 class SomeController < SomeOtherController
   def index; end

   private
+
 end
```

This caused the infinite loop in `Layout/EmptyLinesAroundAccessModifier` and
`Layout/EmptyLinesAroundClassBody`.

```console
% rubocop example.rb --only Layout/EmptyLinesAroundClassBody -a
Inspecting 1 file
C

Offenses:

example.rb:5:1: C: [Corrected] Layout/EmptyLinesAroundClassBody: Extra
empty line detected at class body end.

1 file inspected, 1 offense detected, 1 offense corrected
```

Auto-correct removes the blank line from after of `private`.

```diff
 % cat example.rb
 class SomeController < SomeOtherController
   def index; end

   private
-
 end
```

It loops to the first code. This PR fixes the infinite loop.

lib/rubocop/cop/layout/empty_lines_around_access_modifier.rb
@@ -38,17 +38,24 @@ module RuboCop
         def on_class(node)
           _name, superclass, _body = *node
 
-          @class_or_module_def = superclass || node.source_range
+          @class_or_module_def_first_line = if superclass
+                                              superclass.first_line
+                                            else
+                                              node.source_range.first_line
+                                            end
+          @class_or_module_def_last_line = node.source_range.last_line
         end
 
         def on_module(node)
-          @class_or_module_def = node.source_range
+          @class_or_module_def_first_line = node.source_range.first_line
+          @class_or_module_def_last_line = node.source_range.last_line
         end
 
         def on_sclass(node)
           self_node, _body = *node
 
-          @class_or_module_def = self_node.source_range
+          @class_or_module_def_first_line = self_node.source_range.first_line
+          @class_or_module_def_last_line = self_node.source_range.last_line
         end
 
         def on_block(node)
@@ -106,9 +113,9 @@ module RuboCop
         end
 
         def class_def?(line)
-          return false unless @class_or_module_def
+          return false unless @class_or_module_def_first_line
 
-          line == @class_or_module_def.first_line + 1
+          line == @class_or_module_def_first_line + 1
         end
 
         def block_start?(line)
@@ -118,9 +125,9 @@ module RuboCop
         end
 
         def body_end?(line)
-          return false unless @class_or_module_def
+          return false unless @class_or_module_def_last_line
 
-          line == @class_or_module_def.last_line - 1
+          line == @class_or_module_def_last_line - 1
         end
 
         def message(node)

spec/rubocop/cop/layout/empty_lines_around_access_modifier_spec.rb
@@ -286,6 +286,17 @@ RSpec.describe RuboCop::Cop::Layout::EmptyLinesAroundAccessModifier do
       RUBY
     end
 
+    it 'accepts missing blank line when at the end of specifying ' \
+       'a superclass' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        class Test < Base
+          def test; end
+
+          #{access_modifier}
+        end
+      RUBY
+    end
+
     it 'requires blank line when next line started with end' do
       inspect_source(<<-RUBY.strip_indent)
         class Test