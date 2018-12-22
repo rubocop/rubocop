0befbb094b619fe7d92d2a1c586c0c0b90397c79
Adrien Siami <adrien.siami@gmail.com>
2018-12-22 18:08:35 +0100

    bbatsov review

--- lib/rubocop/cop/rails/link_to_blank.rb
+++ lib/rubocop/cop/rails/link_to_blank.rb
@@ -5,7 +5,7 @@ module RuboCop
     module Rails
       # This cop checks for calls to `link_to` that contain a
       # `target: '_blank'` but no `rel: 'noopener'`. This can be a security
-      #  risk as the loaded page will have control over the previous page
+      # risk as the loaded page will have control over the previous page
       # and could change its location for phishing purposes.
       #
       # @example
