cad7a4de828d3c0cba71e083a95c27b4ee049c29
Maxime Buffa <mbuffa@gmail.com>
2019-02-11 14:05:31 +0100

    Add new failing specs by @abrom

--- spec/rubocop/cop/lint/memorization_with_parameters_spec.rb
+++ spec/rubocop/cop/lint/memorization_with_parameters_spec.rb
@@ -26,6 +26,18 @@ RSpec.describe RuboCop::Cop::Lint::MemorizationWithParameters do
     it { expect(cop.offenses.size).to eq(0) }
   end
 
+  context 'when the assignment isn\'t used' do
+    subject(:source) do
+      <<-RUBY.strip_indent
+        def foo(bar)
+          @foo ||= baz
+        end
+      RUBY
+    end
+
+    it { expect(cop.offenses.size).to eq(0) }
+  end
+
   context 'with a simple assignment' do
     subject(:source) do
       <<-RUBY.strip_indent
@@ -39,11 +51,52 @@ RSpec.describe RuboCop::Cop::Lint::MemorizationWithParameters do
     it { expect(cop.offenses.first.message).to eq error_message }
   end
 
+  context 'with multiple parameters' do
+    subject(:source) do
+      <<-RUBY.strip_indent
+        def foo(bar, qux)
+          @foo ||= baz(bar, qux)
+        end
+      RUBY
+    end
+
+    it { expect(cop.offenses.size).to eq(1) }
+    it { expect(cop.offenses.first.message).to eq error_message }
+  end
+
+  context 'with a named parameter' do
+    subject(:source) do
+      <<-RUBY.strip_indent
+        def foo(bar:)
+          @foo ||= baz(bar)
+        end
+      RUBY
+    end
+
+    it { expect(cop.offenses.size).to eq(1) }
+    it { expect(cop.offenses.first.message).to eq error_message }
+  end
+
   context 'with a string interpolation' do
+    subject(:source) do
+      <<-'RUBY'.strip_indent
+        def foo(bar)
+          @foo ||= "Test #{bar}"
+        end
+      RUBY
+    end
+
+    it { expect(cop.offenses.size).to eq(1) }
+    it { expect(cop.offenses.first.message).to eq error_message }
+  end
+
+  context 'with a begin/end block' do
     subject(:source) do
       <<-RUBY.strip_indent
         def foo(bar)
-          @foo ||= "Test #{bar}"
+          @foo ||= begin
+            bar
+          end
         end
       RUBY
     end
