396321e3ff4608ce1fec825600daf7cf6ffe2ccd
Anuj Biyani <305254+anujbiyani@users.noreply.github.com>
2019-02-21 11:02:22 -0800

    Add InstanceVarsInClass cop
    This cop checks for instance variables used in classes outside of the initializer.
    It encourages using attr_reader or attr_accessor to set up safer methods to read from
    or write to your instance variable.
    
    inspired by the discussion in https://github.com/rubocop-hq/ruby-style-guide/issues/538

---
+++ lib/rubocop/cop/style/instance_vars_in_class.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Style
+      # This cop checks for instance variables used in classes outside of the
+      # initializer. It encourages using attr_reader or attr_accessor to set
+      # up safer methods to read from or write to your instance variable.
+      #
+      # In Rails projects, it's probably good to exclude controllers, jobs,
+      # and mailers straightaway.
+      #
+      # ```yaml
+      # Exclude:
+      #   - "app/controllers/**/*"
+      #   - "app/jobs/**/*"
+      #   - "app/mailers/**/*"
+      # ```
+      #
+      # @example
+      #
+      #   # bad
+      #   class Foo
+      #     def initialize
+      #       @foo = "bar"
+      #     end
+      #
+      #     def instance_method
+      #       @foo
+      #     end
+      #   end
+      #
+      #   # bad
+      #   class Foo
+      #     def initialize
+      #       @foo = "bar"
+      #     end
+      #
+      #     def instance_method
+      #       @foo = "baz"
+      #     end
+      #   end
+      #
+      #   # good
+      #   class Foo
+      #     attr_reader :foo
+      #
+      #     def initialize
+      #       @foo = "bar"
+      #     end
+      #
+      #     def instance_method
+      #       foo
+      #     end
+      #   end
+      #
+      #   # good
+      #   class Foo
+      #     attr_accessor :foo
+      #
+      #     def initialize
+      #       @foo = "bar"
+      #     end
+      #
+      #     def instance_method
+      #       foo = "baz"
+      #     end
+      #   end
+      #
+      class InstanceVarsInClass < Cop
+        MSG = 'Outside of the initializer, use an `attr_%<type>s ' \
+              ':%<var_name>s` instead of `%<usage>s`.'.freeze
+
+        def on_ivar(node)
+          return if !in_class?(node) || in_initializer?(node)
+
+          var_name, = *node
+          var_name = var_name.to_s[1..-1]
+
+          case node.type
+          when :ivasgn
+            usage = "@#{var_name}="
+            type = 'accessor'
+          when :ivar
+            usage = "@#{var_name}"
+            type = 'reader'
+          else
+            return
+          end
+
+          add_offense(node, message: format_message(type, usage, var_name))
+        end
+        alias on_ivasgn on_ivar
+
+        private
+
+        def format_message(type, usage, var_name)
+          format(MSG, type: type, var_name: var_name, usage: usage)
+        end
+
+        def in_class?(node)
+          node.each_ancestor(:class, :module).any?
+        end
+
+        def in_initializer?(node)
+          node.each_ancestor(:def).any? do |n|
+            method, = *n
+            method == :initialize
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/style/instance_vars_in_class_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::InstanceVarsInClass do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  context 'when using instance_variable by itself' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<-RUBY.strip_indent)
+        @instance_variable
+      RUBY
+    end
+  end
+
+  context 'in a class' do
+    context 'when using an instance variable instead of an attr_reader' do
+      it 'registers an offense' do
+        expect_offense(<<-RUBY.strip_indent)
+          class TestClass
+            def foo
+              @instance_variable
+              ^^^^^^^^^^^^^^^^^^ Outside of the initializer, use an `attr_reader :instance_variable` instead of `@instance_variable`.
+            end
+          end
+        RUBY
+      end
+    end
+
+    it 'informs the user when attr_accessor should be used instead' do
+      expect_offense(<<-RUBY.strip_indent)
+        class TestClass
+          def foo
+            @instance_variable = 1
+            ^^^^^^^^^^^^^^^^^^^^^^ Outside of the initializer, use an `attr_accessor :instance_variable` instead of `@instance_variable=`.
+          end
+        end
+      RUBY
+    end
+
+    context 'in initialize' do
+      it 'does not register an offense' do
+        expect_no_offenses(<<-RUBY.strip_indent)
+          class TestClass
+            def initialize
+              @instance_variable
+            end
+          end
+        RUBY
+      end
+    end
+  end
+end
