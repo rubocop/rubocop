a94aff94e23c6a978514f5cdb64ef7aea9578c8f
Ted Johansson <drenmi@gmail.com>
2019-04-22 16:30:35 +0800

    Fix InternalAffairs/NodeDestructuring offenses in RandomWithOffset

--- lib/rubocop/ast/node/range_node.rb
+++ lib/rubocop/ast/node/range_node.rb
@@ -6,6 +6,13 @@ module RuboCop
     # place of a plain node when the builder constructs the AST, making its
     # methods available to all `irange` and `erange` nodes within RuboCop.
     class RangeNode < Node
+      def begin
+        node_parts[0]
+      end
+
+      def end
+        node_parts[1]
+      end
     end
   end
 end
