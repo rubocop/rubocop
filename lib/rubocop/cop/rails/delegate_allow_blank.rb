619740f38bde3ee8d228ff4b6c6bb3ba201213c3
Maxim Krizhanovski <maxim.krizhanovski@toptal.com>
2019-04-30 00:35:23 +0300

    Refactor cops to use matching on children

--- lib/rubocop/cop/rails/delegate_allow_blank.rb
+++ lib/rubocop/cop/rails/delegate_allow_blank.rb
@@ -16,33 +16,19 @@ module RuboCop
       class DelegateAllowBlank < Cop
         MSG = '`allow_blank` is not a valid option, use `allow_nil`.'.freeze
 
-        def_node_matcher :delegate_options, <<-PATTERN
-          (send nil? :delegate _ $hash)
-        PATTERN
-
-        def_node_matcher :allow_blank_option?, <<-PATTERN
-          (pair (sym :allow_blank) true)
+        def_node_matcher :allow_blank_option, <<-PATTERN
+          (send nil? :delegate _ (hash <$(pair (sym :allow_blank) true) ...>))
         PATTERN
 
         def on_send(node)
-          offending_node = allow_blank_option(node)
-
-          return unless offending_node
-
-          add_offense(offending_node)
+          allow_blank_option(node) do |offending_node|
+            add_offense(offending_node)
+          end
         end
 
         def autocorrect(pair_node)
           lambda do |corrector|
             corrector.replace(pair_node.key.source_range, 'allow_nil')
-          end
-        end
-
-        private
-
-        def allow_blank_option(node)
-          delegate_options(node) do |hash|
-            hash.pairs.find { |opt| allow_blank_option?(opt) }
           end
         end
       end
