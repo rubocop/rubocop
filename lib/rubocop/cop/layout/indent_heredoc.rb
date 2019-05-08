633fdf387a6d9fac24cb8ce6aa453998b9a70b2d
Koichi ITO <koic.ito@gmail.com>
2019-05-08 13:17:59 +0900

    Set default `EnforcedStyle` to `always` for `Layout/IndentHeredoc`
    This commit sets default `EnforcedStyle` to `squiggly` option for
    `Layout/IndentHeredoc` and `auto_detection` option is removed.
    
    Because we no longer use Ruby 2.2, the original default `auto_detection`
    option is never used. Makes `squiggly` option enabled in Ruby 2.3 or
    higher a new default.

--- lib/rubocop/cop/layout/indent_heredoc.rb
+++ lib/rubocop/cop/layout/indent_heredoc.rb
@@ -12,31 +12,12 @@ module RuboCop
       #       this cop does not add any offenses for long here documents to
       #       avoid `Metrics/LineLength`'s offenses.
       #
-      # @example EnforcedStyle: auto_detection (default)
+      # @example EnforcedStyle: squiggly (default)
       #   # bad
       #   <<-RUBY
       #   something
       #   RUBY
       #
-      #   # good
-      #   # When using Ruby 2.3 or higher.
-      #   <<~RUBY
-      #     something
-      #   RUBY
-      #
-      #   # good
-      #   # When using Ruby 2.2 or lower and enabled Rails department.
-      #   # The following is possible to enable Rails department by
-      #   # adding for example:
-      #   #
-      #   # Rails:
-      #   #   Enabled: true
-      #   #
-      #   <<-RUBY.strip_heredoc
-      #     something
-      #   RUBY
-      #
-      # @example EnforcedStyle: squiggly
       #   # good
       #   # When EnforcedStyle is squiggly, bad code is auto-corrected to the
       #   # following code.
@@ -121,11 +102,7 @@ module RuboCop
           style = super
           return style unless style == :auto_detection
 
-          if target_ruby_version >= 2.3
-            :squiggly
-          elsif rails?
-            :active_support
-          end
+          :squiggly
         end
 
         def message(node)
@@ -229,16 +206,10 @@ module RuboCop
         end
 
         def check_style!
-          case style
-          when nil
-            raise Warning, "Auto-correction does not work for #{cop_name}. " \
-                           'Please configure EnforcedStyle.'
-          when :squiggly
-            if target_ruby_version < 2.3
-              raise Warning, '`squiggly` style is selectable only on Ruby ' \
-                             "2.3 or higher for #{cop_name}."
-            end
-          end
+          return if style
+
+          raise Warning, "Auto-correction does not work for #{cop_name}. " \
+                         'Please configure EnforcedStyle.'
         end
 
         def indented_body(node)
