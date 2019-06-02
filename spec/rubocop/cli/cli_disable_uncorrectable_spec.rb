779784c1e6a90fac9f58fdf86b707f24699009e0
Jonas Arvidsson <jonas054@gmail.com>
2019-06-02 11:51:01 +0200

    Improve cli_disable_uncorrectable_spec.rb
    Refactor to make more readable.

--- spec/rubocop/cli/cli_disable_uncorrectable_spec.rb
+++ spec/rubocop/cli/cli_disable_uncorrectable_spec.rb
@@ -7,8 +7,7 @@ RSpec.describe RuboCop::CLI, :isolated_environment do
 
   describe '--disable-uncorrectable' do
     let(:exit_code) do
-      cli
-        .run(%w[--auto-correct --format emacs --disable-uncorrectable --except Style/FrozenStringLiteralComment --no-display-cop-names])
+      cli.run(%w[--auto-correct --format simple --disable-uncorrectable])
     end
 
     it 'does not disable anything for cops that support autocorrect' do
@@ -16,47 +15,75 @@ RSpec.describe RuboCop::CLI, :isolated_environment do
       expect(exit_code).to eq(0)
       expect($stderr.string).to eq('')
       expect($stdout.string).to eq(<<-OUTPUT.strip_indent)
-        #{abs('example.rb')}:1:7: C: [Corrected] Surrounding space missing for operator `==`.
+        == example.rb ==
+        C:  1:  1: [Corrected] Style/FrozenStringLiteralComment: Missing magic comment # frozen_string_literal: true.
+        C:  1:  7: [Corrected] Layout/SpaceAroundOperators: Surrounding space missing for operator ==.
+        
+        1 file inspected, 2 offenses detected, 2 offenses corrected
       OUTPUT
-      expect(IO.readlines('example.rb').map(&:chomp)).to eq(['puts 1 == 2'])
+      expect(IO.read('example.rb')).to eq(<<-RUBY.strip_indent)
+        # frozen_string_literal: true
+
+        puts 1 == 2
+      RUBY
     end
 
     it 'adds one-line disable statement for one-line offenses' do
-      create_file('example.rb', ['def is_example',
-                                 '  true',
-                                 'end'])
+      create_file('example.rb', <<-RUBY.strip_indent)
+        def is_example
+          true
+        end
+      RUBY
       expect(exit_code).to eq(0)
       expect($stderr.string).to eq('')
-      expect($stdout.string)
-        .to eq("#{abs('example.rb')}:1:5: C: [Corrected] Rename `is_example` " \
-               "to `example?`.\n")
-      expect(IO.readlines('example.rb').map(&:chomp))
-        .to eq(['# rubocop:disable Naming/PredicateName',
-                'def is_example',
-                '  true',
-                'end',
-                '# rubocop:enable Naming/PredicateName'])
+      expect($stdout.string).to eq(<<-OUTPUT.strip_indent)
+        == example.rb ==
+        C:  1:  1: [Corrected] Style/FrozenStringLiteralComment: Missing magic comment # frozen_string_literal: true.
+        C:  1:  5: [Corrected] Naming/PredicateName: Rename is_example to example?.
+        
+        1 file inspected, 2 offenses detected, 2 offenses corrected
+      OUTPUT
+      expect(IO.read('example.rb')).to eq(<<-RUBY.strip_indent)
+        # rubocop:disable Naming/PredicateName
+        # frozen_string_literal: true
+
+        def is_example
+          true
+        end
+        # rubocop:enable Naming/PredicateName
+      RUBY
     end
 
     it 'adds before-and-after disable statement for multiline offenses' do
-      create_file('.rubocop.yml', ['Metrics/MethodLength:',
-                                   '  Max: 1'])
-      create_file('example.rb', ['def example',
-                                 "  puts 'line 1'",
-                                 "  puts 'line 2'",
-                                 'end'])
+      create_file('.rubocop.yml', <<-YAML.strip_indent)
+        Metrics/MethodLength:
+          Max: 1
+      YAML
+      create_file('example.rb', <<-RUBY.strip_indent)
+        def example
+          puts 'line 1'
+          puts 'line 2'
+        end
+      RUBY
       expect(exit_code).to eq(0)
       expect($stderr.string).to eq('')
-      expect($stdout.string)
-        .to eq("#{abs('example.rb')}:1:1: C: [Corrected] Method " \
-               "has too many lines. [2/1]\n")
-      expect(IO.readlines('example.rb').map(&:chomp))
-        .to eq(['# rubocop:disable Metrics/MethodLength',
-                'def example',
-                "  puts 'line 1'",
-                "  puts 'line 2'",
-                'end',
-                '# rubocop:enable Metrics/MethodLength'])
+      expect($stdout.string).to eq(<<-OUTPUT.strip_indent)
+        == example.rb ==
+        C:  1:  1: [Corrected] Metrics/MethodLength: Method has too many lines. [2/1]
+        C:  1:  1: [Corrected] Style/FrozenStringLiteralComment: Missing magic comment # frozen_string_literal: true.
+        
+        1 file inspected, 2 offenses detected, 2 offenses corrected
+      OUTPUT
+      expect(IO.read('example.rb')).to eq(<<-RUBY.strip_indent)
+        # rubocop:disable Metrics/MethodLength
+        # frozen_string_literal: true
+
+        def example
+          puts 'line 1'
+          puts 'line 2'
+        end
+        # rubocop:enable Metrics/MethodLength
+      RUBY
     end
   end
 end
