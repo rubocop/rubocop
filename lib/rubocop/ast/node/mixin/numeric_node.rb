7b33290221bc7252ede60cc72397dee1dddcdf22
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-06-06 10:09:00 +0300

    Extract common numeric utilities for int and float nodes

---
+++ lib/rubocop/ast/node/mixin/numeric_node.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module AST
+    # Common functionality for primitive numeric nodes: `int`, `float`, ...
+    module NumericNode
+      SIGN_REGEX = /\A[+-]/.freeze
+
+      # Checks whether this is literal has a sign.
+      #
+      # @example
+      #
+      #   +42
+      #
+      # @return [Boolean] whether this literal has a sign.
+      def sign?
+        source.match(SIGN_REGEX)
+      end
+    end
+  end
+end
