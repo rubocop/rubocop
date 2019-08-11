10b754d7a27ef723be64a93cfe4645ae55251294
Koichi ITO <koic.ito@gmail.com>
2019-08-12 03:02:32 +0900

    [Fix #7274] Add new `Lint/SendWithMixinArgument` cop
    Resolves #7274.
    
    This PR adds new `Lint/SendWithMixinArgument` cop.
    
    This cop checks for `send`, `public_send`, and `__send__` methods
    when using mix-in.
    
    ```ruby
    # bad
    Foo.send(:include, Bar)
    Foo.send(:prepend, Bar)
    Foo.send(:extend, Bar)
    
    # bad
    Foo.public_send(:include, Bar)
    Foo.public_send(:prepend, Bar)
    Foo.public_send(:extend, Bar)
    
    # bad
    Foo.__send__(:include, Bar)
    Foo.__send__(:prepend, Bar)
    Foo.__send__(:extend, Bar)
    
    # good
    Foo.include Bar
    Foo.prepend Bar
    Foo.extend Bar
    ```
    
    `include` and `prepend` methods were private methods until Ruby 2.0,
    they were mixed-in via `send` method. This cop uses Ruby 2.1 or higher
    style that can be called by public methods.
    
    ```console
    % ruby -v
    ruby 2.0.0p648 (2015-12-16 revision 53162) [x86_64-darwin13.0.2]
    % ruby -e 'module M end; Object.include(M)'
    -e:1:in `<main>': private method `include' called for Object:Class (NoMethodError)
    % ruby -e 'module M end; Object.prepend(M)'
    -e:1:in `<main>': private method `prepend' called for Object:Class (NoMethodError)
    
    % ruby -v
    ruby 2.1.10p492 (2016-04-01 revision 54464) [x86_64-darwin13.0]
    % ruby -e 'module M end; Object.include(M)' # no error
    % ruby -e 'module M end; Object.prepend(M)' # no error
    ```
    
    And `extend` method that was originally a public method is also targeted
    for style unification.
    
    ```console
    % ruby -v
    ruby 2.0.0p648 (2015-12-16 revision 53162) [x86_64-darwin13.0.2]
    % ruby -e 'module M end; Object.extend(M)' # no error
    
    % ruby -v
    ruby 2.1.10p492 (2016-04-01 revision 54464) [x86_64-darwin13.0]
    % ruby -e 'module M end; Object.extend(M)' # no error
    ```

--- manual/cops_lint.md
+++ manual/cops_lint.md
@@ -1939,6 +1939,45 @@ puts 'hello, world'
 puts 'hello, world'
 ```
 
+## Lint/SendWithMixinArgument
+
+Enabled by default | Safe | Supports autocorrection | VersionAdded | VersionChanged
+--- | --- | --- | --- | ---
+Enabled | Yes | Yes  | 0.75 | -
+
+This cop checks for `send`, `public_send`, and `__send__` methods
+when using mix-in.
+
+`include` and `prepend` methods were private methods until Ruby 2.0,
+they were mixed-in via `send` method. This cop uses Ruby 2.1 or
+higher style that can be called by public methods.
+And `extend` method that was originally a public method is also targeted
+for style unification.
+
+### Examples
+
+```ruby
+# bad
+Foo.send(:include, Bar)
+Foo.send(:prepend, Bar)
+Foo.send(:extend, Bar)
+
+# bad
+Foo.public_send(:include, Bar)
+Foo.public_send(:prepend, Bar)
+Foo.public_send(:extend, Bar)
+
+# bad
+Foo.__send__(:include, Bar)
+Foo.__send__(:prepend, Bar)
+Foo.__send__(:extend, Bar)
+
+# good
+Foo.include Bar
+Foo.prepend Bar
+Foo.extend Bar
+```
+
 ## Lint/ShadowedArgument
 
 Enabled by default | Safe | Supports autocorrection | VersionAdded | VersionChanged
---
+++ spec/rubocop/cop/lint/send_with_mixin_argument_spec.rb
@@ -0,0 +1,134 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Lint::SendWithMixinArgument do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  it 'registers an offense when using `send` with `include`' do
+    expect_offense(<<~RUBY)
+      Foo.send(:include, Bar)
+          ^^^^^^^^^^^^^^^^^^^ Use `include Bar` instead of `send(:include, Bar)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Foo.include Bar
+    RUBY
+  end
+
+  it 'registers an offense when using `send` with `prepend`' do
+    expect_offense(<<~RUBY)
+      Foo.send(:prepend, Bar)
+          ^^^^^^^^^^^^^^^^^^^ Use `prepend Bar` instead of `send(:prepend, Bar)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Foo.prepend Bar
+    RUBY
+  end
+
+  it 'registers an offense when using `send` with `extend`' do
+    expect_offense(<<~RUBY)
+      Foo.send(:extend, Bar)
+          ^^^^^^^^^^^^^^^^^^ Use `extend Bar` instead of `send(:extend, Bar)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Foo.extend Bar
+    RUBY
+  end
+
+  context 'when specifying a mixin method as a string' do
+    it 'registers an offense when using `send` with `include`' do
+      expect_offense(<<~RUBY)
+        Foo.send('include', Bar)
+            ^^^^^^^^^^^^^^^^^^^^ Use `include Bar` instead of `send('include', Bar)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Foo.include Bar
+      RUBY
+    end
+
+    it 'registers an offense when using `send` with `prepend`' do
+      expect_offense(<<~RUBY)
+        Foo.send('prepend', Bar)
+            ^^^^^^^^^^^^^^^^^^^^ Use `prepend Bar` instead of `send('prepend', Bar)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Foo.prepend Bar
+      RUBY
+    end
+
+    it 'registers an offense when using `send` with `extend`' do
+      expect_offense(<<~RUBY)
+        Foo.send('extend', Bar)
+            ^^^^^^^^^^^^^^^^^^^ Use `extend Bar` instead of `send('extend', Bar)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Foo.extend Bar
+      RUBY
+    end
+  end
+
+  it 'registers an offense when using `public_send` method' do
+    expect_offense(<<~RUBY)
+      Foo.public_send(:include, Bar)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `include Bar` instead of `public_send(:include, Bar)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Foo.include Bar
+    RUBY
+  end
+
+  it 'registers an offense when using `__send__` method' do
+    expect_offense(<<~RUBY)
+      Foo.__send__(:include, Bar)
+          ^^^^^^^^^^^^^^^^^^^^^^^ Use `include Bar` instead of `__send__(:include, Bar)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Foo.include Bar
+    RUBY
+  end
+
+  it 'does not register an offense when not using a mixin method' do
+    expect_no_offenses(<<~RUBY)
+      Foo.send(:do_something, Bar)
+    RUBY
+  end
+
+  it 'does not register an offense when using `include`' do
+    expect_no_offenses(<<~RUBY)
+      Foo.include Bar
+    RUBY
+  end
+
+  it 'does not register an offense when using `prepend`' do
+    expect_no_offenses(<<~RUBY)
+      Foo.prepend Bar
+    RUBY
+  end
+
+  it 'does not register an offense when using `extend`' do
+    expect_no_offenses(<<~RUBY)
+      Foo.extend Bar
+    RUBY
+  end
+
+  context 'when using namespace for module' do
+    it 'registers an offense when using `send` with `include`' do
+      expect_offense(<<~RUBY)
+        A::Foo.send(:include, B::Bar)
+               ^^^^^^^^^^^^^^^^^^^^^^ Use `include B::Bar` instead of `send(:include, B::Bar)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        A::Foo.include B::Bar
+      RUBY
+    end
+  end
+end
