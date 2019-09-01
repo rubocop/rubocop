4f311d7d1b189b1eae2e8fcf857bae85856958c7
Benjamin Quorning <benjamin@quorning.net>
2019-09-01 08:53:58 +0200

    Remove definition of String#strip_indent
    :tada:

--- lib/rubocop/core_ext/string.rb
+++ lib/rubocop/core_ext/string.rb
@@ -20,28 +20,4 @@ class String
       empty? || strip.empty?
     end
   end
-
-  unless method_defined? :strip_indent
-    # The method strips the whitespace preceding the base indentation.
-    # Useful for HEREDOCs and other multi-line strings.
-    #
-    # @example
-    #
-    #   code = <<~END
-    #     def test
-    #       some_method
-    #       other_method
-    #     end
-    #   END
-    #
-    #   #=> "def\n  some_method\n  \nother_method\nend"
-    #
-    # @todo Replace call sites with squiggly heredocs when required Ruby
-    #   version is >= 2.3.0
-    def strip_indent
-      leading_space = scan(/^[ \t]*(?=\S)/).min
-      indent = leading_space ? leading_space.size : 0
-      gsub(/^[ \t]{#{indent}}/, '')
-    end
-  end
 end
