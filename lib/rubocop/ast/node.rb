492ebf8504652c22880238b66176398bdf4ddef1
Marc-Andre Lafortune <github@marc-andre.ca>
2020-02-02 12:33:05 -0600

    Fix documentation [doc]
    None of these methods actually accept an array of symbols.
    
    Note: None of these methods appear to have specs

--- lib/rubocop/ast/node.rb
+++ lib/rubocop/ast/node.rb
@@ -141,9 +141,6 @@ module RuboCop
       #   Yield only nodes matching any of the types.
       #   @param [Symbol] type_a a node type
       #   @param [Symbol] type_b a node type
-      # @overload each_ancestor(types)
-      #   Yield only nodes matching any of types in the array.
-      #   @param [Array<Symbol>] types an array containing node types
       # @yieldparam [Node] node each ancestor node
       # @return [self] if a block is given
       # @return [Enumerator] if no block is given
@@ -178,9 +175,6 @@ module RuboCop
       #   Yield only nodes matching any of the types.
       #   @param [Symbol] type_a a node type
       #   @param [Symbol] type_b a node type
-      # @overload each_child_node(types)
-      #   Yield only nodes matching any of types in the array.
-      #   @param [Array<Symbol>] types an array containing node types
       # @yieldparam [Node] node each child node
       # @return [self] if a block is given
       # @return [Enumerator] if no block is given
@@ -216,9 +210,6 @@ module RuboCop
       #   Yield only nodes matching any of the types.
       #   @param [Symbol] type_a a node type
       #   @param [Symbol] type_b a node type
-      # @overload each_descendant(types)
-      #   Yield only nodes matching any of types in the array.
-      #   @param [Array<Symbol>] types an array containing node types
       # @yieldparam [Node] node each descendant node
       # @return [self] if a block is given
       # @return [Enumerator] if no block is given
@@ -254,9 +245,6 @@ module RuboCop
       #   Yield only nodes matching any of the types.
       #   @param [Symbol] type_a a node type
       #   @param [Symbol] type_b a node type
-      # @overload each_node(types)
-      #   Yield only nodes matching any of types in the array.
-      #   @param [Array<Symbol>] types an array containing node types
       # @yieldparam [Node] node each node
       # @return [self] if a block is given
       # @return [Enumerator] if no block is given
