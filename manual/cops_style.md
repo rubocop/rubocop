00e8170f011eafb157f3090a29cede6bcc016adf
Thierry Deo <thierry.deo@gmail.com>
2020-04-05 16:35:13 +0200

    Implement new Style/ExponentialNotation cop
    This cop implements the rule describe in the "exponential notation"
    section of the ruby style guide.
    It allows for an additional style not mentionned in the guide,
    `integral` which allows only integer mantissa, without any
    trailing zero.

--- manual/cops_style.md
+++ manual/cops_style.md
@@ -2101,6 +2101,86 @@ Pathname.new(__FILE__).parent.expand_path
 Pathname.new(__dir__).expand_path
 ```
 
+## Style/ExponentialNotation
+
+Enabled by default | Safe | Supports autocorrection | VersionAdded | VersionChanged
+--- | --- | --- | --- | ---
+Pending | Yes | No | 0.82 | -
+
+This cop enforces consistency when using exponential notation
+for numbers in the code (eg 1.2e4). Different styles are supported:
+- `scientific` which enforces a mantissa between 1 (inclusive)
+               and 10 (exclusive).
+- `engineering` which enforces the exponent to be a multiple of 3
+                and the mantissa to be between 0.1 (inclusive)
+                and 10 (exclusive).
+- `integral` which enforces the mantissa to always be a whole number
+             without trailing zeroes.
+
+### Examples
+
+#### EnforcedStyle: scientific (default)
+
+```ruby
+# Enforces a mantissa between 1 (inclusive) and 10 (exclusive).
+
+# bad
+10e6
+0.3e4
+11.7e5
+3.14e0
+
+# good
+1e7
+3e3
+1.17e6
+3.14
+```
+#### EnforcedStyle: engineering
+
+```ruby
+# Enforces using multiple of 3 exponents,
+# mantissa should be between 0.1 (inclusive) and 1000 (exclusive)
+
+# bad
+3.2e7
+0.1e5
+12e5
+1232e6
+
+# good
+32e6
+10e3
+1.2e6
+1.232e9
+```
+#### EnforcedStyle: integral
+
+```ruby
+# Enforces the mantissa to have no decimal part and no
+# trailing zeroes.
+
+# bad
+3.2e7
+0.1e5
+120e4
+
+# good
+32e6
+1e4
+12e5
+```
+
+### Configurable attributes
+
+Name | Default value | Configurable values
+--- | --- | ---
+EnforcedStyle | `scientific` | `scientific`, `engineering`, `integral`
+
+### References
+
+* [https://rubystyle.guide#exponential-notation](https://rubystyle.guide#exponential-notation)
+
 ## Style/FloatDivision
 
 Enabled by default | Safe | Supports autocorrection | VersionAdded | VersionChanged
---
+++ spec/rubocop/cop/style/exponential_notation_spec.rb
@@ -0,0 +1,155 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::ExponentialNotation, :config do
+  subject(:cop) { described_class.new(config) }
+
+  context 'EnforcedStyle is scientific' do
+    let(:cop_config) { { 'EnforcedStyle' => 'scientific' } }
+
+    it 'registers an offense for mantissa equal to 10' do
+      expect_offense(<<~RUBY)
+        10e6
+        ^^^^ Use a mantissa in [1, 10[.
+      RUBY
+    end
+
+    it 'registers an offense for mantissa greater than 10' do
+      expect_offense(<<~RUBY)
+        12.34e3
+        ^^^^^^^ Use a mantissa in [1, 10[.
+      RUBY
+    end
+
+    it 'registers an offense for mantissa smaller than 1' do
+      expect_offense(<<~RUBY)
+        0.314e1
+        ^^^^^^^ Use a mantissa in [1, 10[.
+      RUBY
+    end
+
+    it 'registers no offense for a regular float' do
+      expect_no_offenses('120.03')
+    end
+
+    it 'registers no offense for a float smaller than 1' do
+      expect_no_offenses('0.07390')
+    end
+
+    it 'registers no offense for a mantissa equal to 1' do
+      expect_no_offenses('1e6')
+    end
+
+    it 'registers no offense for a mantissa between 1 and 10' do
+      expect_no_offenses('3.1415e3')
+    end
+
+    it 'registers no offense for a negative mantissa' do
+      expect_no_offenses('-9.999e3')
+    end
+
+    it 'registers no offense for a negative exponent' do
+      expect_no_offenses('5.02e-3')
+    end
+  end
+
+  context 'EnforcedStyle is engineering' do
+    let(:cop_config) { { 'EnforcedStyle' => 'engineering' } }
+
+    it 'registers an offense for exponent equal to 4' do
+      expect_offense(<<~RUBY)
+        10e4
+        ^^^^ Use an exponent divisible by 3 and a mantissa in [0.1, 1000[.
+      RUBY
+    end
+
+    it 'registers an offense for exponent equal to -2' do
+      expect_offense(<<~RUBY)
+        12.3e-2
+        ^^^^^^^ Use an exponent divisible by 3 and a mantissa in [0.1, 1000[.
+      RUBY
+    end
+
+    it 'registers an offense for mantissa smaller than 0.1' do
+      expect_offense(<<~RUBY)
+        0.09e9
+        ^^^^^^ Use an exponent divisible by 3 and a mantissa in [0.1, 1000[.
+      RUBY
+    end
+
+    it 'registers an offense for a mantissa greater than -0.1' do
+      expect_offense(<<~RUBY)
+        -0.09e3
+        ^^^^^^^ Use an exponent divisible by 3 and a mantissa in [0.1, 1000[.
+      RUBY
+    end
+
+    it 'registers an offense for mantissa smaller than -1000' do
+      expect_offense(<<~RUBY)
+        -1012.34e6
+        ^^^^^^^^^^ Use an exponent divisible by 3 and a mantissa in [0.1, 1000[.
+      RUBY
+    end
+
+    it 'registers no offense for a mantissa equal to 1' do
+      expect_no_offenses('1e6')
+    end
+
+    it 'registers no offense for a regular float' do
+      expect_no_offenses('120.03')
+    end
+
+    it 'registers no offense for a float smaller than 1' do
+      expect_no_offenses('0.07390')
+    end
+
+    it 'registers no offense for a negative exponent' do
+      expect_no_offenses('3.1415e-12')
+    end
+
+    it 'registers no offense for a negative mantissa' do
+      expect_no_offenses('-999.9e3')
+    end
+
+    it 'registers no offense for a large mantissa' do
+      expect_no_offenses('968.64982e12')
+    end
+  end
+
+  context 'EnforcedStyle is integral' do
+    let(:cop_config) { { 'EnforcedStyle' => 'integral' } }
+
+    it 'registers an offense for decimal mantissa' do
+      expect_offense(<<~RUBY)
+        1.2e3
+        ^^^^^ Use an integer as mantissa, without trailing zero.
+      RUBY
+    end
+
+    it 'registers an offense for mantissa divisible by 10' do
+      expect_offense(<<~RUBY)
+        120e-4
+        ^^^^^^ Use an integer as mantissa, without trailing zero.
+      RUBY
+    end
+
+    it 'registers no offense for a regular float' do
+      expect_no_offenses('120.03')
+    end
+
+    it 'registers no offense for a float smaller than 1' do
+      expect_no_offenses('0.07390')
+    end
+
+    it 'registers no offense for an integral mantissa' do
+      expect_no_offenses('7652e7')
+    end
+
+    it 'registers no offense for negative mantissa' do
+      expect_no_offenses('-84e7')
+    end
+
+    it 'registers no offense for negative exponent' do
+      expect_no_offenses('84e-7')
+    end
+  end
+end
