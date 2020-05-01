ef31b89d0423dd20a9dfecebc31fdc80d7dbe92b
Koichi ITO <koic.ito@gmail.com>
2020-05-02 04:48:18 +0900

    Support autocorrection for `Lint/EnsureReturn`
    This PR supports autocorrection for `Lint/EnsureReturn`.

--- spec/rubocop/cop/lint/ensure_return_spec.rb
+++ spec/rubocop/cop/lint/ensure_return_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe RuboCop::Cop::Lint::EnsureReturn do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense for return in ensure' do
+  it 'registers an offense and corrects for return in ensure' do
     expect_offense(<<~RUBY)
       begin
         something
@@ -11,6 +11,35 @@ RSpec.describe RuboCop::Cop::Lint::EnsureReturn do
         file.close
         return
         ^^^^^^ Do not return from an `ensure` block.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      begin
+        something
+      ensure
+        file.close
+      end
+    RUBY
+  end
+
+  it 'registers an offense and corrects for return with argument in ensure' do
+    expect_offense(<<~RUBY)
+      begin
+        foo
+      ensure
+        bar
+        return baz
+        ^^^^^^ Do not return from an `ensure` block.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      begin
+        foo
+      ensure
+        bar
+        baz
       end
     RUBY
   end
@@ -22,6 +51,16 @@ RSpec.describe RuboCop::Cop::Lint::EnsureReturn do
         return
       ensure
         file.close
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when returning multiple values in `ensure`" do
+    expect_no_offenses(<<~RUBY)
+      begin
+        something
+      ensure
+        return foo, bar
       end
     RUBY
   end
