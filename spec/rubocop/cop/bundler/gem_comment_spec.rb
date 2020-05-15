c3bfc90cb3d69ce0f57be7691da9e83913c6d773
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-05-15 16:12:13 +0100

    Add tests for the new configuration.

--- spec/rubocop/cop/bundler/gem_comment_spec.rb
+++ spec/rubocop/cop/bundler/gem_comment_spec.rb
@@ -63,5 +63,61 @@ RSpec.describe RuboCop::Cop::Bundler::GemComment, :config do
         GEM
       end
     end
+
+    context 'and the OnlyIfVersionRestricted option is set to true' do
+      before { cop_config['OnlyIfVersionRestricted'] = true }
+
+      context 'and a gem is commented' do
+        it 'does not register an offense' do
+          expect_no_offenses(<<~RUBY, 'Gemfile')
+            # Style-guide enforcer.
+            gem 'rubocop'
+          RUBY
+        end
+      end
+
+      context 'and a gem is uncommented but not version restricted' do
+        it 'does not register an offense in a simple example' do
+          expect_no_offenses(<<-GEM, 'Gemfile')
+            gem 'rubocop'
+          GEM
+        end
+
+        it 'does not register an offense in a more complex example' do
+          expect_no_offenses(<<-GEM, 'Gemfile')
+            gem 'rubocop', group: development
+          GEM
+        end
+      end
+
+      context 'and a gem is uncommented and version restricted' do
+        context 'and has no other paramaters' do
+          it 'registers an offense' do
+            expect_offense(<<-GEM, 'Gemfile')
+              gem 'rubocop', '~> 12.0'
+              ^^^^^^^^^^^^^^^^^^^^^^^^ Missing gem description comment.
+            GEM
+          end
+        end
+
+        context 'and has multiple version restrictions' do
+          it 'registers an offense' do
+            expect_offense(<<-GEM, 'Gemfile')
+              gem 'rubocop', '~> 12.0', '>= 11.0'
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Missing gem description comment.
+            GEM
+          end
+        end
+
+        context 'and has extra unrelated keyword arguments' do
+          it 'registers an offense' do
+            expect_offense(<<-GEM, 'Gemfile')
+            gem 'rubocop', '~> 12.0', required: true
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Missing gem description comment.
+            GEM
+          end
+        end
+      end
+    end
   end
 end
