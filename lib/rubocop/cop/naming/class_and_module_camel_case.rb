d0a2d2a73710b9a9a17539ab5364b0a0d9214328
Tejas Bubane <tejasbubane@gmail.com>
2020-05-31 00:17:16 +0530

    [Fix #8063] Add new `AllowedNames` option for `Naming/ClassAndModuleCamelCase`
    Closes #8063

--- lib/rubocop/cop/naming/class_and_module_camel_case.rb
+++ lib/rubocop/cop/naming/class_and_module_camel_case.rb
@@ -6,6 +6,12 @@ module RuboCop
       # This cop checks for class and module names with
       # an underscore in them.
       #
+      # `AllowedNames` config takes an array of permitted names.
+      # Its default value is `['module_parent']`.
+      # These names can be full class/module names or part of the name.
+      # eg. Adding `my_class` to the `AllowedNames` config will allow names like
+      # `my_class`, `my_class::User`, `App::my_class`, `App::my_class::User`, etc.
+      #
       # @example
       #   # bad
       #   class My_Class
@@ -18,11 +24,15 @@ module RuboCop
       #   end
       #   module MyModule
       #   end
+      #   class module_parent::MyModule
+      #   end
       class ClassAndModuleCamelCase < Cop
         MSG = 'Use CamelCase for classes and modules.'
 
         def on_class(node)
-          return unless /_/.match?(node.loc.name.source)
+          allowed = /#{cop_config['AllowedNames'].join('|')}/
+          name = node.loc.name.source.gsub(allowed, '')
+          return unless /_/.match?(name)
 
           add_offense(node, location: :name)
         end
