d254042924a34f58d72ad2a3e103788f16f56f37
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-05-30 13:00:26 +0900

    fix small typo in project_spec.rb

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'RuboCop Project', type: :feature do
       end
     end
 
-    it 'have a period at EOL of description' do
+    it 'has a period at EOL of description' do
       cop_names.each do |name|
         description = config[name]['Description']
 
