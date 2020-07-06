b229cd129b4826ec21cd4eb98c2d9cfb3fd37f52
Bill Ruddock <bill.ruddock@gmail.com>
2020-07-06 03:06:32 +0100

    Allow historic DateTime with ::Date constant
    Allow historic DateTime using `::Date` constants in Style/DateTime cop.

--- lib/rubocop/cop/style/date_time.rb
+++ lib/rubocop/cop/style/date_time.rb
@@ -50,7 +50,7 @@ module RuboCop
         PATTERN
 
         def_node_matcher :historic_date?, <<~PATTERN
-          (send _ _ _ (const (const nil? :Date) _))
+          (send _ _ _ (const (const {nil? (cbase)} :Date) _))
         PATTERN
 
         def_node_matcher :to_datetime?, <<~PATTERN
