4796cf152dfc3f007fb900535e28dea24243aebc
fatkodima <fatkodima123@gmail.com>
2020-07-07 13:14:27 +0300

    Fix an error for `Style/BisectedAttrAccessor` when using `attr_reader` and `attr_writer` with splat arguments

--- spec/rubocop/cop/style/bisected_attr_accessor_spec.rb
+++ spec/rubocop/cop/style/bisected_attr_accessor_spec.rb
@@ -43,12 +43,34 @@ RSpec.describe RuboCop::Cop::Style::BisectedAttrAccessor do
     RUBY
   end
 
+  it 'registers an offense and corrects when both accessors of the splat exists' do
+    expect_offense(<<~RUBY)
+      class Foo
+        ATTRIBUTES = %i[foo bar]
+        attr_reader *ATTRIBUTES
+                    ^^^^^^^^^^^ Combine both accessors into `attr_accessor *ATTRIBUTES`.
+        attr_writer *ATTRIBUTES
+                    ^^^^^^^^^^^ Combine both accessors into `attr_accessor *ATTRIBUTES`.
+        other_macro :something
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      class Foo
+        ATTRIBUTES = %i[foo bar]
+        attr_accessor *ATTRIBUTES
+        
+        other_macro :something
+      end
+    RUBY
+  end
+
   it 'registers an offense and corrects when both accessors of the name exists and accessor contains multiple names' do
     expect_offense(<<~RUBY)
       class Foo
         attr_reader :baz, :bar, :quux
                           ^^^^ Combine both accessors into `attr_accessor :bar`.
-        attr_writer :bar
+        attr_writer :bar, :zoo
                     ^^^^ Combine both accessors into `attr_accessor :bar`.
         other_macro :something
       end
@@ -58,7 +80,7 @@ RSpec.describe RuboCop::Cop::Style::BisectedAttrAccessor do
       class Foo
         attr_accessor :bar
         attr_reader :baz, :quux
-        
+        attr_writer :zoo
         other_macro :something
       end
     RUBY
