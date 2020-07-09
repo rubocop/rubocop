a82feb0dbe8f3af07143a3de2e77aa5fb4154d57
Bill Ruddock <bill.ruddock@gmail.com>
2020-07-09 18:06:54 +0100

    Refactor empty when specs to remove shared examples
    Use expect_offense etc directly in each test case.

--- spec/rubocop/cop/lint/empty_when_spec.rb
+++ spec/rubocop/cop/lint/empty_when_spec.rb
@@ -1,164 +1,187 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Lint::EmptyWhen, :config do
-  shared_examples 'code with offense' do |code|
-    context "when checking #{code}" do
-      it 'registers an offense' do
-        expect_offense(code)
-        expect_no_corrections
-      end
-    end
-  end
-
-  shared_examples 'code without offense' do |code|
-    it 'does not register an offense' do
-      expect_no_offenses(code)
-    end
-  end
-
   let(:cop_config) { { 'AllowComments' => false } }
 
   context 'when a `when` body is missing' do
-    it_behaves_like 'code with offense', <<~RUBY
-      case foo
-      when :bar then 1
-      when :baz # nothing
-      ^^^^^^^^^ Avoid `when` branches without a body.
-      end
-    RUBY
+    it 'registers an offense for a missing when body' do
+      expect_offense(<<~RUBY)
+        case foo
+        when :bar then 1
+        when :baz # nothing
+        ^^^^^^^^^ Avoid `when` branches without a body.
+        end
+      RUBY
+      expect_no_corrections
+    end
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case foo
-      when :bar then 1
-      when :baz # nothing
-      ^^^^^^^^^ Avoid `when` branches without a body.
-      else 3
-      end
-    RUBY
+    it 'registers an offense for missing when body followed by else' do
+      expect_offense(<<~RUBY)
+        case foo
+        when :bar then 1
+        when :baz # nothing
+        ^^^^^^^^^ Avoid `when` branches without a body.
+        else 3
+        end
+      RUBY
+      expect_no_corrections
+    end
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case foo
-      when :bar then 1
-      when :baz then # nothing
-      ^^^^^^^^^ Avoid `when` branches without a body.
-      end
-    RUBY
+    it 'registers an offense for missing when ... then body' do
+      expect_offense(<<~RUBY)
+        case foo
+        when :bar then 1
+        when :baz then # nothing
+        ^^^^^^^^^ Avoid `when` branches without a body.
+        end
+      RUBY
+      expect_no_corrections
+    end
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case foo
-      when :bar then 1
-      when :baz then # nothing
-      ^^^^^^^^^ Avoid `when` branches without a body.
-      else 3
-      end
-    RUBY
+    it 'registers an offense for missing when ... then body followed by else' do
+      expect_offense(<<~RUBY)
+        case foo
+        when :bar then 1
+        when :baz then # nothing
+        ^^^^^^^^^ Avoid `when` branches without a body.
+        else 3
+        end
+      RUBY
+      expect_no_corrections
+    end
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case foo
-      when :bar
-        1
-      when :baz
-      ^^^^^^^^^ Avoid `when` branches without a body.
-        # nothing
-      end
-    RUBY
+    it 'registers an offense for missing when body with a comment' do
+      expect_offense(<<~RUBY)
+        case foo
+        when :bar
+          1
+        when :baz
+        ^^^^^^^^^ Avoid `when` branches without a body.
+          # nothing
+        end
+      RUBY
+      expect_no_corrections
+    end
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case foo
-      when :bar
-        1
-      when :baz
-      ^^^^^^^^^ Avoid `when` branches without a body.
-        # nothing
-      else
-        3
-      end
-    RUBY
+    it 'registers an offense for missing when body with a comment ' \
+       'followed by else' do
+      expect_offense(<<~RUBY)
+        case foo
+        when :bar
+          1
+        when :baz
+        ^^^^^^^^^ Avoid `when` branches without a body.
+          # nothing
+        else
+          3
+        end
+      RUBY
+      expect_no_corrections
+    end
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case
-      when :bar
-        1
-      when :baz
-      ^^^^^^^^^ Avoid `when` branches without a body.
-        # nothing
-      else
-        3
-      end
-    RUBY
+    it 'registers an offense when case line has no expression' do
+      expect_offense(<<~RUBY)
+        case
+        when :bar
+          1
+        when :baz
+        ^^^^^^^^^ Avoid `when` branches without a body.
+          # nothing
+        else
+          3
+        end
+      RUBY
+      expect_no_corrections
+    end
   end
 
   context 'when a `when` body is present' do
-    it_behaves_like 'code without offense', <<~RUBY
-      case foo
-      when :bar then 1
-      when :baz then 2
-      end
-    RUBY
+    it 'accepts case with when ... then statements' do
+      expect_no_offenses(<<~RUBY)
+        case foo
+        when :bar then 1
+        when :baz then 2
+        end
+      RUBY
+    end
 
-    it_behaves_like 'code without offense', <<~RUBY
-      case foo
-      when :bar then 1
-      when :baz then 2
-      else 3
-      end
-    RUBY
+    it 'accepts case with when ... then statements and else clause' do
+      expect_no_offenses(<<~RUBY)
+        case foo
+        when :bar then 1
+        when :baz then 2
+        else 3
+        end
+      RUBY
+    end
 
-    it_behaves_like 'code without offense', <<~RUBY
-      case foo
-      when :bar
-        1
-      when :baz
-        2
-      end
-    RUBY
+    it 'accepts case with when bodies' do
+      expect_no_offenses(<<~RUBY)
+        case foo
+        when :bar
+          1
+        when :baz
+          2
+        end
+      RUBY
+    end
 
-    it_behaves_like 'code without offense', <<~RUBY
-      case foo
-      when :bar
-        1
-      when :baz
-        2
-      else
-        3
-      end
-    RUBY
-    it_behaves_like 'code without offense', <<~RUBY
-      case
-      when :bar
-        1
-      when :baz
-        2
-      else
-        3
-      end
-    RUBY
+    it 'accepts case with when bodies and else clause' do
+      expect_no_offenses(<<~RUBY)
+        case foo
+        when :bar
+          1
+        when :baz
+          2
+        else
+          3
+        end
+      RUBY
+    end
+
+    it 'accepts with no case line expression' do
+      expect_no_offenses(<<~RUBY)
+        case
+        when :bar
+          1
+        when :baz
+          2
+        else
+          3
+        end
+      RUBY
+    end
   end
 
   context 'when `AllowComments: true`' do
     let(:cop_config) { { 'AllowComments' => true } }
 
-    it_behaves_like 'code without offense', <<~RUBY
-      case condition
-      when foo
-        do_something
-      when bar
-        # do nothing
-      end
-    RUBY
+    it 'accepts an empty when body with a comment' do
+      expect_no_offenses(<<~RUBY)
+        case condition
+        when foo
+          do_something
+        when bar
+          # do nothing
+        end
+      RUBY
+    end
   end
 
   context 'when `AllowComments: false`' do
     let(:cop_config) { { 'AllowComments' => false } }
 
-    it_behaves_like 'code with offense', <<~RUBY
-      case condition
-      when foo
-        do_something
-      when bar
-      ^^^^^^^^ Avoid `when` branches without a body.
-        # do nothing
-      end
-    RUBY
+    it 'registers an offense for empty when body with a comment' do
+      expect_offense(<<~RUBY)
+        case condition
+        when foo
+          do_something
+        when bar
+        ^^^^^^^^ Avoid `when` branches without a body.
+          # do nothing
+        end
+      RUBY
+      expect_no_corrections
+    end
   end
 end
