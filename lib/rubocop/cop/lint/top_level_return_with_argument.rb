7d7beefbc6100ff2d5ad5634c680bd8b2f433d36
Ravi Teja Gannavarapu <grt4004@gmail.com>
2020-07-21 02:00:47 +0530

    Updated cop functionality

--- lib/rubocop/cop/lint/top_level_return_with_argument.rb
+++ lib/rubocop/cop/lint/top_level_return_with_argument.rb
@@ -5,15 +5,37 @@ module RuboCop
     module Lint
       # This cop checks for top level return with arguments. If there is a
       # top-level return statement with an argument, then the argument is
-      # always ignored. This is detected automatically in Ruby 2.7 and up,
-      # but RuboCop doesn't detect it yet.
+      # always ignored. This is detected automatically since Ruby 2.7.
       #
-      # @example
+      # This cop works by validating the ancestors of the return node. A
+      # top-level return node's ancestors satisfy either of the conditions:
+      #
+      # 1. The parent should be `nil`.
+      # 2. The parent should be an instance of `AST::Node` & the parent.parent
+      #    is `nil`.
+      #
+      # Case 1 occurs in cases where the top-level return statement is the
+      # only piece of code in the whole file. It being the only statement,
+      # the statement does not have a parent, like so:
+      #
+      #   return 1, 2, 3
+      #
+      # Case 2 occurs in cases where the file contains statements other than
+      # just the top-level return statement. In such cases, the parent is of
+      # the type `AST::Node` and the parent.parent is `nil`, like so:
+      #
       #   foo
       #
-      #   return # Allowed since Ruby 2.4.
+      #   return 1, 2, 3 # Allowed since Ruby 2.4.
       #
       #   bar
+      #
+      # All other return statements are usually defined inside a method, or in
+      # a block. Thus, unlike the top-level return, these statements have
+      # parents belonging to the `AST::DefNode`, `AST::BlockNode` or 
+      # `AST::IfNode` class.
+      #
+      # @example
       #
       #   # Detected since Ruby 2.7
       #   return 1 # 1 is always ignored.
@@ -21,9 +43,17 @@ module RuboCop
         MSG = 'Top level return with argument detected.'
 
         def on_return(return_node)
-          if return_node.parent.instance_of?(AST::Node) && !return_node.arguments.empty?
+          if ancestors_valid?(return_node) && !return_node.arguments.empty?
             add_offense(return_node)
           end
+        end
+
+        private
+
+        def ancestors_valid?(return_node)
+          return true if return_node.parent.nil?
+          return true if return_node.parent.instance_of?(AST::Node) && return_node.parent.parent.nil?
+          return false
         end
       end
     end
--- spec/rubocop/cop/lint/top_level_return_with_argument.rb
+++ spec/rubocop/cop/lint/top_level_return_with_argument.rb
@@ -1,51 +1,79 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Lint::TopLevelReturnWithArgument, :config do
-  let (:cop_config) do
-    { 'AllowComments' => true }
+  context "Files with only top-level return statement" do
+    it 'Expect no offense from the top level return node' do
+      expect_no_offenses(<<~RUBY)
+        return
+      RUBY
+    end
+
+    it 'Expect offense from the top level return node' do
+      expect_offense(<<~RUBY)
+        return 1, 2, 3 # Should raise a `top level return with argument detected` offense
+        ^^^^^^^^^^^^^^ Top level return with argument detected.
+      RUBY
+    end
+
+    it 'Expect multiple offenses from the top level return node' do
+      expect_offense(<<~RUBY)
+        return 1, 2, 3
+        ^^^^^^^^^^^^^^ Top level return with argument detected.
+
+        return 1, 2, 3
+        ^^^^^^^^^^^^^^ Top level return with argument detected.
+
+        return 1, 2, 3
+        ^^^^^^^^^^^^^^ Top level return with argument detected.
+      RUBY
+    end
   end
 
-  contextit 'Expect no offense from the top level return node' do
-    expect_no_offenses(<<~RUBY)
-      foo
+  context "File contains multiple statements other than the top-level return statement" do
+    it 'Expect no offense from the top level return node with block level return' do
+      expect_no_offenses(<<~RUBY)
+        foo
 
-      return
+        [1, 2, 3, 4, 5].each { |n| return n }
 
-      bar
-    RUBY
+        return # Should raise a `top level return with argument detected` offense
+
+        bar
+      RUBY
+    end
+
+    it 'Expect offense from the top level return node' do
+      expect_offense(<<~RUBY)
+        foo
+
+        [1, 2, 3, 4, 5].each { |n| return n }
+
+        return 1, 2, 3 # Should raise a `top level return with argument detected` offense
+        ^^^^^^^^^^^^^^ Top level return with argument detected.
+
+        bar
+      RUBY
+    end
   end
 
-  it 'Expect offense from the top level return node' do
-    expect_offense(<<~RUBY)
-      foo
+  context "Code segment with method-level return statements" do
+    it 'Expect no offense from the method-level return statement' do
+      expect_no_offenses(<<~RUBY)
+        def method
+          return "Hello World"
+        end
+      RUBY
+    end
 
-      return 1, 2, 3 # Should raise a `top level return with argument detected` offense
-      ^^^^^^^^^^^^^^ Top level return with argument detected.
-      bar
-    RUBY
-  end
+    it "Expect offense when method-level & top-level return co-exist" do
+      expect_offense(<<~RUBY)
+        def method
+          return "Hello World"
+        end
 
-  it 'Expect no offense from the top level return node with block level return' do
-    expect_no_offenses(<<~RUBY)
-      foo
-
-      [1, 2, 3, 4, 5].each { |n| return n }
-
-      return # Should raise a `top level return with argument detected` offense
-
-      bar
-    RUBY
-  end
-
-  it 'Expect offense from the top level return node' do
-    expect_offense(<<~RUBY)
-      foo
-
-      [1, 2, 3, 4, 5].each { |n| return n }
-
-      return 1, 2, 3 # Should raise a `top level return with argument detected` offense
-      ^^^^^^^^^^^^^^ Top level return with argument detected.
-      bar
-    RUBY
+        return 1, 2, 3
+        ^^^^^^^^^^^^^^ Top level return with argument detected.
+      RUBY
+    end
   end
 end
