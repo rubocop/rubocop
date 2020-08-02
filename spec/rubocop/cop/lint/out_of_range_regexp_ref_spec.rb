29d162fe70f08821de39517c5130c53261e6ac1d
Sonali Bhavsar <sonalibhavsar977@gmail.com>
2020-08-02 19:10:06 +0530

    Update examples in specs

--- spec/rubocop/cop/lint/out_of_range_regexp_ref_spec.rb
+++ spec/rubocop/cop/lint/out_of_range_regexp_ref_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe RuboCop::Cop::Lint::OutOfRangeRegexpRef do
 
   let(:config) { RuboCop::Config.new }
 
+  it 'registers an offense when references are used before any Regexp' do
+    expect_offense(<<~RUBY)
+      puts $3
+           ^^ Do not use out of range reference for the Regexp.
+    RUBY
+  end
+
   it 'registers an offense when out of range references are used for named captures' do
     expect_offense(<<~RUBY)
       /(?<foo>FOO)(?<bar>BAR)/ =~ "FOOBAR"
@@ -64,67 +71,19 @@ RSpec.describe RuboCop::Cop::Lint::OutOfRangeRegexpRef do
   it 'does not register offense when using a Regexp cannot be processed by regexp_parser gem' do
     expect_no_offenses(<<~'RUBY')
       /data = ({"words":.+}}}[^}]*})/m
+      puts $1
     RUBY
   end
 
   # RuboCop does not know a value of variables that it will contain in the regexp literal.
   # For example, `/(?<foo>#{var}*)` is interpreted as `/(?<foo>*)`.
   # So it does not offense when variables are used in regexp literals.
-  context 'when containing a non-regexp literal' do
-    it 'does not register an offence when containing a lvar' do
-      expect_no_offenses(<<~'RUBY')
-        var = '(\d+)'
-        /(?<foo>#{var}*)/ =~ "12"
-        puts $1
-        puts $2
-      RUBY
-    end
-
-    it 'does not register an offence when containing a ivar' do
-      expect_no_offenses(<<~'RUBY')
-        @var = '(\d+)'
-        /(?<foo>#{@var}*)/ =~ "12"
-        puts $1
-        puts $3
-      RUBY
-    end
-
-    it 'does not register an offence when containing a cvar' do
-      expect_no_offenses(<<~'RUBY')
-        @@var = '(\d+)'
-        /(?<foo>#{@@var}*)/ =~ "12"
-        puts $1
-        puts $4
-      RUBY
-    end
-
-    it 'does not register an offence when containing a gvar' do
-      expect_no_offenses(<<~'RUBY')
-        $var = '(\d+)'
-        /(?<foo>#{$var}*)/ =~ "12"
-        puts $1
-        puts $2
-      RUBY
-    end
-
-    it 'does not register an offence when containing a method' do
-      expect_no_offenses(<<~'RUBY')
-        def do_something
-          '(\d+)'
-        end
-        /(?<foo>#{do_something}*)/ =~ "12"
-        puts $1
-        puts $4
-      RUBY
-    end
-
-    it 'does not register an offence when containing a constant' do
-      expect_no_offenses(<<~'RUBY')
-        CONST = "12"
-        /(?<foo>#{CONST}*)/ =~ "12"
-        puts $1
-        puts $3
-      RUBY
-    end
+  it 'does not register an offence Regexp containing non literal' do
+    expect_no_offenses(<<~'RUBY')
+      var = '(\d+)'
+      /(?<foo>#{var}*)/ =~ "12"
+      puts $1
+      puts $2
+    RUBY
   end
 end
