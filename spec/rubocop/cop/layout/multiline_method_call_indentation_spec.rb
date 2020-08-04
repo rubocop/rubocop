22be89b42677ba66b6824c6a0170b919b2d3fa8e
Bill Ruddock <bill.ruddock@gmail.com>
2020-08-04 01:45:42 +0100

    Fix line continuations in spec examples
    Fix backslash before newline.

--- spec/rubocop/cop/layout/multiline_method_call_indentation_spec.rb
+++ spec/rubocop/cop/layout/multiline_method_call_indentation_spec.rb
@@ -459,7 +459,7 @@ RSpec.describe RuboCop::Cop::Layout::MultilineMethodCallIndentation do
     end
 
     it 'does not check binary operations when string wrapped with backslash' do
-      expect_no_offenses(<<~RUBY)
+      expect_no_offenses(<<~'RUBY')
         flash[:error] = 'Here is a string ' \
                         'That spans' <<
           'multiple lines'
