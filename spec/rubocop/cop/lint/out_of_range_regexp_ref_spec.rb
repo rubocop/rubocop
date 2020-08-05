7552fed46be62af283d072b05e0ee3cda7c10fa1
Eugene Kenny <elkenny@gmail.com>
2020-08-06 00:49:30 +0100

    Handle more matching methods in Lint/OutOfRangeRegexpRef
    `when`, `grep`, `gsub`, `gsub!` `sub`, `sub!`, `[]`, `slice`, `slice!`,
    `scan`, `index`, `rindex`, `partition`, `rpartition`, `start_with?`, and
    `end_with?` can all be used to match regexps, so we should update the
    range of valid references when we encounter them.

--- spec/rubocop/cop/lint/out_of_range_regexp_ref_spec.rb
+++ spec/rubocop/cop/lint/out_of_range_regexp_ref_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe RuboCop::Cop::Lint::OutOfRangeRegexpRef do
     RUBY
   end
 
-  it 'registers an offense when the regexp comes after `=~`' do
+  it 'registers an offense when the regexp appears on the right hand side of `=~`' do
     expect_offense(<<~RUBY)
       "foobar" =~ /(foo)(bar)/
       puts $3
@@ -132,5 +132,154 @@ RSpec.describe RuboCop::Cop::Lint::OutOfRangeRegexpRef do
       foo_bar_regexp =~ 'foobar'
       puts $2
     RUBY
+  end
+
+  it 'does not register an offense when in range references are used inside a when clause' do
+    expect_no_offenses(<<~RUBY)
+      case "foobar"
+      when /(foo)(bar)/
+        $2
+      end
+    RUBY
+  end
+
+  it 'registers an offense when out of range references are used inside a when clause' do
+    expect_offense(<<~RUBY)
+      case "foobar"
+      when /(foo)(bar)/
+        $3
+        ^^ Do not use out of range reference for the Regexp.
+      end
+    RUBY
+  end
+
+  it 'uses the maximum number of captures for when clauses with multiple conditions' do
+    expect_no_offenses(<<~RUBY)
+      case "foobarbaz"
+      when /(foo)(bar)/, /(bar)baz/
+        $2
+      end
+    RUBY
+
+    expect_offense(<<~RUBY)
+      case "foobarbaz"
+      when /(foo)(bar)/, /(bar)baz/
+        $3
+        ^^ Do not use out of range reference for the Regexp.
+      end
+    RUBY
+  end
+
+  it 'only registers an offense for when clauses when the regexp is matched as a literal' do
+    expect_no_offenses(<<~RUBY)
+      case some_string
+      when some_regexp
+        $2
+      end
+    RUBY
+  end
+
+  it 'ignores regexp when clause conditions that contain interpolations' do
+    expect_offense(<<~'RUBY')
+      case "foobarbaz"
+      when /(foo)(bar)/, /#{var}/
+        $3
+        ^^ Do not use out of range reference for the Regexp.
+      end
+    RUBY
+  end
+
+  context 'matching with `grep`' do
+    it 'does not register an offense when in range references are used' do
+      expect_no_offenses(<<~RUBY)
+        %w[foo foobar].grep(/(foo)/) { $1 }
+      RUBY
+    end
+
+    it 'registers an offense when out of range references are used' do
+      expect_offense(<<~RUBY)
+        %w[foo foobar].grep(/(foo)/) { $2 }
+                                       ^^ Do not use out of range reference for the Regexp.
+      RUBY
+    end
+
+    it 'only registers an offense when the regexp is matched as a literal' do
+      expect_no_offenses(<<~RUBY)
+        %w[foo foobar].grep(some_regexp) { $2 }
+      RUBY
+    end
+  end
+
+  context 'matching with `[]`' do
+    it 'does not register an offense when in range references are used' do
+      expect_no_offenses(<<~RUBY)
+        "foobar"[/(foo)(bar)/]
+        puts $2
+      RUBY
+    end
+
+    it 'registers an offense when out of range references are used' do
+      expect_offense(<<~RUBY)
+        "foobar"[/(foo)(bar)/]
+        puts $3
+             ^^ Do not use out of range reference for the Regexp.
+      RUBY
+    end
+
+    it 'only registers an offense when the regexp is matched as a literal' do
+      expect_no_offenses(<<~RUBY)
+        "foobar"[some_regexp]
+        puts $3
+      RUBY
+    end
+  end
+
+  %i[gsub gsub! sub sub! scan].each do |method|
+    context "matching with #{method}" do
+      it 'does not register an offense when in range references are used' do
+        expect_no_offenses(<<~RUBY)
+          "foobar".#{method}(/(foo)(bar)/) { $2 }
+        RUBY
+      end
+
+      it 'registers an offense when out of range references are used' do
+        expect_offense(<<~RUBY, method: method)
+          "foobar".%{method}(/(foo)(bar)/) { $3 }
+                   _{method}                 ^^ Do not use out of range reference for the Regexp.
+        RUBY
+      end
+
+      it 'only registers an offense when the regexp is matched as a literal' do
+        expect_no_offenses(<<~RUBY)
+          some_string.#{method}(some_regexp) { $3 }
+        RUBY
+      end
+    end
+  end
+
+  %i[match slice slice! index rindex partition rpartition start_with? end_with?].each do |method|
+    context "matching with #{method}" do
+      it 'does not register an offense when in range references are used' do
+        expect_no_offenses(<<~RUBY)
+          "foobar".#{method}(/(foo)(bar)/)
+          puts $2
+        RUBY
+      end
+
+      it 'registers an offense when out of range references are used' do
+        expect_offense(<<~RUBY)
+          "foobar".#{method}(/(foo)(bar)/)
+          puts $3
+               ^^ Do not use out of range reference for the Regexp.
+        RUBY
+      end
+
+      it 'only registers an offense when the regexp is matched as a literal' do
+        expect_no_offenses(<<~RUBY)
+          "foobar".#{method}(some_regexp)
+          puts $3
+        RUBY
+      end
+    end
   end
 end
