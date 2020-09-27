3b777aca02715214cbcb9e6a18376bf985dfc170
Owen Stephens <owen@owenstephens.co.uk>
2020-09-27 11:23:45 +0100

    Test parsed_tree

--- spec/rubocop/ext/regexp_node_spec.rb
+++ spec/rubocop/ext/regexp_node_spec.rb
@@ -32,4 +32,51 @@ RSpec.describe RuboCop::Ext::RegexpNode do
       it { is_expected.to match [named] }
     end
   end
+
+  describe '#parsed_tree' do
+    let(:source) { '/foo#{bar}baz/' }
+
+    context 'with interpolation: :ignore' do
+      context 'with a regexp containing interpolation' do
+        it { expect(node.parsed_tree(interpolation: :ignore)).to eq(nil) }
+      end
+
+      context 'with a regexp not containing interpolation' do
+        let(:source) { '/foobaz/' }
+
+        it 'returns the expected tree' do
+          tree = node.parsed_tree(interpolation: :ignore)
+
+          expect(tree).to be_an(Regexp::Expression::Root)
+          expect(tree.to_s).to eq('foobaz')
+        end
+      end
+    end
+
+    context 'with interpolation: :blank' do
+      context 'with a regexp containing interpolation' do
+        it 'returns the expected blanked tree' do
+          tree = node.parsed_tree(interpolation: :blank)
+
+          expect(tree).to be_an(Regexp::Expression::Root)
+          expect(tree.to_s).to eq('foo      baz')
+        end
+      end
+
+      context 'with a regexp not containing interpolation' do
+        let(:source) { '/foobaz/' }
+
+        it 'returns the expected tree' do
+          tree = node.parsed_tree(interpolation: :blank)
+
+          expect(tree).to be_an(Regexp::Expression::Root)
+          expect(tree.to_s).to eq('foobaz')
+        end
+      end
+    end
+
+    context 'with interpolation: :other' do
+      it { expect { node.parsed_tree(interpolation: :other) }.to raise_error(ArgumentError) }
+    end
+  end
 end
