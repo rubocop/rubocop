7d5415bec882ee05125a819683575f14b3c1c79b
Julio Cesar Fausto <jcf@jcfausto.com>
2020-10-07 21:53:33 +0200

    Remove unnecessary and duplicated tests

--- spec/rubocop/cop/lint/no_return_in_begin_end_blocks_spec.rb
+++ spec/rubocop/cop/lint/no_return_in_begin_end_blocks_spec.rb
@@ -5,34 +5,6 @@ RSpec.describe RuboCop::Cop::Lint::NoReturnInBeginEndBlocks do
 
   let(:config) { RuboCop::Config.new }
 
-  def return_inside_or_assignment_block
-    <<-RUBY
-      @bad_method ||= begin
-        return "odd number" if rand(1..2).odd?
-
-        "even number"
-      end
-    RUBY
-  end
-
-  def no_return_inside_or_assignment_block
-    <<-RUBY
-      @good_method ||= begin
-        if rand(1..2).odd?
-          "odd number"
-        else
-          "even number"
-        end
-      end
-    RUBY
-  end
-
-  context 'when no return is used inside an or assignment block' do
-    it 'does not register an offense' do
-      expect_no_offenses(no_return_inside_or_assignment_block)
-    end
-  end
-
   shared_examples 'rejects return inside a block' do |operator|
     it "rejects a return statement inside a block when using #{operator}" do
       expect_offense(<<-RUBY)
