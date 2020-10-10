67178235a461634507486ecfe9fa1fa1a88bd948
fatkodima <fatkodima123@gmail.com>
2020-10-10 18:49:32 +0300

    Rework `Lint/RedundantSafeNavigation` to be more safe

--- spec/rubocop/cop/lint/redundant_safe_navigation_spec.rb
+++ spec/rubocop/cop/lint/redundant_safe_navigation_spec.rb
@@ -2,53 +2,97 @@
 
 RSpec.describe RuboCop::Cop::Lint::RedundantSafeNavigation, :config do
   let(:cop_config) do
-    { 'IgnoredMethods' => [] }
+    { 'AllowedMethods' => %w[respond_to?] }
   end
 
-  it 'registers an offense and corrects when `.&` is used for `nil` method' do
+  it 'registers an offense and corrects when `&.` is used inside `if` condition' do
     expect_offense(<<~RUBY)
-      foo&.respond_to?(:bar)
-         ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+      if foo&.respond_to?(:bar)
+            ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+        do_something
+      elsif foo&.respond_to?(:baz)
+               ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+        do_something_else
+      end
     RUBY
 
     expect_correction(<<~RUBY)
-      foo.respond_to?(:bar)
+      if foo.respond_to?(:bar)
+        do_something
+      elsif foo.respond_to?(:baz)
+        do_something_else
+      end
     RUBY
   end
 
-  it 'registers an offense and corrects when multiple `.&` are used for `nil` methods' do
+  it 'registers an offense and corrects when `&.` is used inside `unless` condition' do
     expect_offense(<<~RUBY)
-      foo.do_something&.dup&.inspect
-                      ^^^^^ Redundant safe navigation detected.
-                           ^^^^^^^^^ Redundant safe navigation detected.
+      do_something unless foo&.respond_to?(:bar)
+                             ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
     RUBY
 
     expect_correction(<<~RUBY)
-      foo.do_something.dup.inspect
+      do_something unless foo.respond_to?(:bar)
     RUBY
   end
 
-  it 'does not register an offense when using non-nil method with `.&`' do
-    expect_no_offenses(<<~RUBY)
-      foo.&do_something
-    RUBY
-  end
+  %i[while until].each do |loop_type|
+    it 'registers an offense and corrects when `&.` is used inside `#{loop_type}` condition' do
+      expect_offense(<<~RUBY, loop_type: loop_type)
+        %{loop_type} foo&.respond_to?(:bar)
+        _{loop_type}    ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+          do_something
+        end
 
-  it 'does not register an offense when using `nil` method without `.&`' do
-    expect_no_offenses(<<~RUBY)
-      foo.dup
-    RUBY
-  end
+        begin
+          do_something
+        end %{loop_type} foo&.respond_to?(:bar)
+            _{loop_type}    ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+      RUBY
 
-  context 'when AllowedMethods is set' do
-    let(:cop_config) do
-      { 'IgnoredMethods' => ['to_f'] }
-    end
+      expect_correction(<<~RUBY)
+        #{loop_type} foo.respond_to?(:bar)
+          do_something
+        end
 
-    it 'does not register an offense when using ignored `nil` method with `.&`' do
-      expect_no_offenses(<<~RUBY)
-        foo&.to_f
+        begin
+          do_something
+        end #{loop_type} foo.respond_to?(:bar)
       RUBY
     end
+  end
+
+  it 'registers an offense and corrects when `&.` is used inside complex condition' do
+    expect_offense(<<~RUBY)
+      do_something if foo&.respond_to?(:bar) && !foo&.respond_to?(:baz)
+                         ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+                                                    ^^^^^^^^^^^^^^^^^^^ Redundant safe navigation detected.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      do_something if foo.respond_to?(:bar) && !foo.respond_to?(:baz)
+    RUBY
+  end
+
+  it 'does not register an offense when using `&.` outside of conditions' do
+    expect_no_offenses(<<~RUBY)
+      foo&.respond_to?(:bar)
+
+      if condition
+        foo&.respond_to?(:bar)
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `&.` with non-allowed method in condition' do
+    expect_no_offenses(<<~RUBY)
+      do_something if foo&.bar?
+    RUBY
+  end
+
+  it 'does not register an offense when using `&.respond_to?` with `nil` specific method as argument in condition' do
+    expect_no_offenses(<<~RUBY)
+      do_something if foo&.respond_to?(:to_a)
+    RUBY
   end
 end
