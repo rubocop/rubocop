985cf2632d991c7cd46c09e587e425f10feda81c
Joshua Pinter <joshuapinter@gmail.com>
2020-10-29 13:33:13 -0600

    [Fix #8966] Enforce no spaces in empty parens.
    Even with `style` set to `spaces`.
    
    Regardless of if we like spaces in our parens or if we don't, empty parens should not have spaces.
    
    This commit fixes `SpaceInsideParens` Cop when the `style` is `spaces` so that it doesn't require a space inside the parens. Moreover, it registers an offence when there _is_ a space inside empty parens.
    
    Additionally, a test was added for the default `style` to ensure there are no spaces inside empty parens.
    
    And finally, the docs were updated to show that spaces are not permitted with _either_ `style`.
    
    Note, had to extra out `process_with_space_style` to avoid method length Cop offence.
    
    Fixes https://github.com/rubocop-hq/rubocop/issues/8966.

--- lib/rubocop/cop/layout/space_inside_parens.rb
+++ lib/rubocop/cop/layout/space_inside_parens.rb
@@ -11,10 +11,12 @@ module RuboCop
       #   # bad
       #   f( 3)
       #   g = (a + 3 )
+      #   f( )
       #
       #   # good
       #   f(3)
       #   g = (a + 3)
+      #   f()
       #
       # @example EnforcedStyle: space
       #   # The `space` style enforces that parentheses have a space at the
@@ -44,11 +46,7 @@ module RuboCop
           @processed_source = processed_source
 
           if style == :space
-            each_missing_space(processed_source.tokens) do |range|
-              add_offense(range, message: MSG_SPACE) do |corrector|
-                corrector.insert_before(range, ' ')
-              end
-            end
+            process_with_space_style(processed_source)
           else
             each_extraneous_space(processed_source.tokens) do |range|
               add_offense(range) do |corrector|
@@ -59,6 +57,21 @@ module RuboCop
         end
 
         private
+
+        def process_with_space_style(processed_source)
+          processed_source.tokens.each_cons(2) do |token1, token2|
+            each_extraneous_space_in_empty_parens(token1, token2) do |range|
+              add_offense(range) do |corrector|
+                corrector.remove(range)
+              end
+            end
+            each_missing_space(token1, token2) do |range|
+              add_offense(range, message: MSG_SPACE) do |corrector|
+                corrector.insert_before(range, ' ')
+              end
+            end
+          end
+        end
 
         def each_extraneous_space(tokens)
           tokens.each_cons(2) do |token1, token2|
@@ -73,15 +86,21 @@ module RuboCop
           end
         end
 
-        def each_missing_space(tokens)
-          tokens.each_cons(2) do |token1, token2|
-            next if can_be_ignored?(token1, token2)
+        def each_extraneous_space_in_empty_parens(token1, token2)
+          return unless token1.left_parens? && token2.right_parens?
 
-            if token1.left_parens?
-              yield range_between(token2.begin_pos, token2.begin_pos + 1)
-            elsif token2.right_parens?
-              yield range_between(token2.begin_pos, token2.end_pos)
-            end
+          return if range_between(token1.begin_pos, token2.end_pos).source == '()'
+
+          yield range_between(token1.end_pos, token2.begin_pos)
+        end
+
+        def each_missing_space(token1, token2)
+          return if can_be_ignored?(token1, token2)
+
+          if token1.left_parens?
+            yield range_between(token2.begin_pos, token2.begin_pos + 1)
+          elsif token2.right_parens?
+            yield range_between(token2.begin_pos, token2.end_pos)
           end
         end
 
@@ -95,6 +114,9 @@ module RuboCop
 
         def can_be_ignored?(token1, token2)
           return true unless parens?(token1, token2)
+
+          # Ignore empty parentheses.
+          return true if range_between(token1.begin_pos, token2.end_pos).source == '()'
 
           # If the second token is a comment, that means that a line break
           # follows, and that the rules for space inside don't apply.
