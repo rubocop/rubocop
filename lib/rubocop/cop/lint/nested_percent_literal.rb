e2cd58c70696aad96ee7d89616ad5c9a346d342c
Koichi ITO <koic.ito@gmail.com>
2020-11-02 23:49:39 +0900

    Add good examples for `Lint/NestedPercentLiteral`
    This PR adds good examples for `Lint/NestedPercentLiteral` because
    complement examples that has only bad example.

--- lib/rubocop/cop/lint/nested_percent_literal.rb
+++ lib/rubocop/cop/lint/nested_percent_literal.rb
@@ -15,6 +15,20 @@ module RuboCop
       #     valid_attributes: %i[name content],
       #     nested_attributes: %i[name content %i[incorrectly nested]]
       #   }
+      #
+      #   # good
+      #
+      #   # Neither is incompatible with the bad case, but probably the intended code.
+      #   attributes = {
+      #     valid_attributes: %i[name content],
+      #     nested_attributes: [:name, :content, %i[incorrectly nested]]
+      #   }
+      #
+      #   attributes = {
+      #     valid_attributes: %i[name content],
+      #     nested_attributes: [:name, :content, [:incorrectly, :nested]]
+      #   }
+      #
       class NestedPercentLiteral < Base
         include PercentLiteral
 
