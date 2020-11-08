0df32b28d525cb8e2e04fad977235c9dc8bd8734
Marc-Andre Lafortune <github@marc-andre.ca>
2020-11-08 01:09:21 -0500

    Mark `Style/InfiniteLoop` as unsafe

---
+++ changelog/fix_mark_styleinfiniteloop_as_unsafe.md
@@ -0,0 +1,1 @@
+* [#9008](https://github.com/rubocop-hq/rubocop/pull/9008): Mark `Style/InfiniteLoop` as unsafe. ([@marcandre][])
--- lib/rubocop/cop/style/infinite_loop.rb
+++ lib/rubocop/cop/style/infinite_loop.rb
@@ -5,6 +5,10 @@ module RuboCop
     module Style
       # Use `Kernel#loop` for infinite loops.
       #
+      # This cop is marked as unsafe as the rule does not necessarily
+      # apply if the body might raise a `StopIteration` exception; contrary to
+      # other infinite loops, `Kernel#loop` silently rescues that and returns `nil`.
+      #
       # @example
       #   # bad
       #   while true
