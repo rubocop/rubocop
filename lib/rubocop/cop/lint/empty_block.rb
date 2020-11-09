a197d11d10660a31586f001c08184d056115dd4d
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2020-11-09 16:42:13 -0500

    [Fix #9015] Update `Lint/EmptyBlock` to allow for empty lambdas.

--- lib/rubocop/cop/lint/empty_block.rb
+++ lib/rubocop/cop/lint/empty_block.rb
@@ -7,6 +7,8 @@ module RuboCop
       # Such empty blocks are typically an oversight or we should provide a comment
       # be clearer what we're aiming for.
       #
+      # Empty lambdas are ignored by default.
+      #
       # @example
       #   # bad
       #   items.each { |item| }
@@ -30,11 +32,28 @@ module RuboCop
       #
       #   items.each { |item| } # TODO: implement later (inline comment)
       #
+      # @example AllowEmptyLambdas: true (default)
+      #   # good
+      #   allow(subject).to receive(:callable).and_return(-> {})
+      #
+      #   placeholder = lambda do
+      #   end
+      #   (callable || placeholder).call
+      #
+      # @example AllowEmptyLambdas: false
+      #   # bad
+      #   allow(subject).to receive(:callable).and_return(-> {})
+      #
+      #   placeholder = lambda do
+      #   end
+      #   (callable || placeholder).call
+      #
       class EmptyBlock < Base
         MSG = 'Empty block detected.'
 
         def on_block(node)
           return if node.body
+          return if allow_empty_lambdas? && node.lambda?
           return if cop_config['AllowComments'] && allow_comment?(node)
 
           add_offense(node)
@@ -47,6 +66,10 @@ module RuboCop
 
           line_comment = processed_source.comment_at_line(node.source_range.line)
           !line_comment || !comment_disables_cop?(line_comment.loc.expression.source)
+        end
+
+        def allow_empty_lambdas?
+          cop_config['AllowEmptyLambdas']
         end
 
         def comment_disables_cop?(comment)
