fa07cd1140d4c5049fee302257a3d3749b3cec76
Koichi ITO <koic.ito@gmail.com>
2020-11-11 11:14:48 +0900

    [Fix #9023] Mark unsafe for `Style/CollectionCompact`.
    Fixes #9023.
    
    This PR marks unsafe for `Style/CollectionCompact` by default because
    false positives may occur in the nil check of block arguments to the
    receiver object.
    For example, `[[1, 2], [3, nil]].reject { |first, second| second.nil? }`
    and `[[1, 2], [3, nil]].compact` are not compatible. This will work fine
    when the receiver is a hash object.

---
+++ changelog/change_mark_unsafe_for_collection_compact.md
@@ -0,0 +1,1 @@
+* [#9023](https://github.com/rubocop-hq/rubocop/issues/9023): Mark unsafe for `Style/CollectionCompact`. ([@koic][])
--- lib/rubocop/cop/style/collection_compact.rb
+++ lib/rubocop/cop/style/collection_compact.rb
@@ -6,6 +6,12 @@ module RuboCop
       # This cop checks for places where custom logic on rejection nils from arrays
       # and hashes can be replaced with `{Array,Hash}#{compact,compact!}`.
       #
+      # It is marked as unsafe by default because false positives may occur in the
+      # nil check of block arguments to the receiver object.
+      # For example, `[[1, 2], [3, nil]].reject { |first, second| second.nil? }`
+      # and `[[1, 2], [3, nil]].compact` are not compatible. This will work fine
+      # when the receiver is a hash object.
+      #
       # @example
       #   # bad
       #   array.reject { |e| e.nil? }
