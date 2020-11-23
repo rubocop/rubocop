e702f91ac47142e35844748e44001386bf33defa
Tejas Bubane <tejasbubane@gmail.com>
2020-11-24 00:57:14 +0530

    [Fix #9083] Ignore method calls that don't have exactly one argument in `Style/RedundantArgument` (#9085)
    Also:
    
    * Add a check that an explicit receiver must be present.
    * Refactor tests for the cop.

--- spec/rubocop/cop/style/redundant_argument_spec.rb
+++ spec/rubocop/cop/style/redundant_argument_spec.rb
@@ -3,100 +3,63 @@
 RSpec.describe RuboCop::Cop::Style::RedundantArgument, :config do
   subject(:cop) { described_class.new(config) }
 
-  context 'join' do
-    let(:cop_config) do
-      { 'Methods' => { 'join' => '' } }
-    end
-
-    it 'registers an offense when using `#join` with empty string argument' do
-      expect_offense(<<~RUBY)
-        foo.join('')
-        ^^^^^^^^^^^^ Argument '' is redundant because it is implied by default.
-      RUBY
-    end
-
-    it 'registers an offense when using `#join` with double quoted string' do
-      expect_offense(<<~RUBY)
-        foo.join("")
-        ^^^^^^^^^^^^ Argument "" is redundant because it is implied by default.
-      RUBY
-    end
-
-    it 'registers an offense when using `#join` on literal arrays' do
-      expect_offense(<<~RUBY)
-        [1, 2, 3].join("")
-        ^^^^^^^^^^^^^^^^^^ Argument "" is redundant because it is implied by default.
-      RUBY
-    end
-
-    it 'does not register an offense when using `#join` with no arguments' do
-      expect_no_offenses(<<~RUBY)
-        foo.join
-      RUBY
-    end
-
-    it 'does not register an offense when using `#join` with array literals' do
-      expect_no_offenses(<<~RUBY)
-        [1, 2, 3].join
-      RUBY
-    end
-
-    it 'does not register an offense when using `#join` with separator' do
-      expect_no_offenses(<<~RUBY)
-        foo.join(',')
-      RUBY
-    end
+  let(:cop_config) do
+    { 'Methods' => { 'join' => '', 'split' => ' ' } }
   end
 
-  context 'split' do
-    let(:cop_config) do
-      { 'Methods' => { 'split' => ' ' } }
-    end
+  it 'registers an offense when method called on variable' do
+    expect_offense(<<~RUBY)
+      foo.join('')
+      ^^^^^^^^^^^^ Argument '' is redundant because it is implied by default.
+      foo.split(' ')
+      ^^^^^^^^^^^^^^ Argument ' ' is redundant because it is implied by default.
+    RUBY
+  end
 
-    it 'registers an offense when using `#split` with space' do
-      expect_offense(<<~RUBY)
-        foo.split(' ')
-        ^^^^^^^^^^^^^^ Argument ' ' is redundant because it is implied by default.
-      RUBY
-    end
+  it 'registers an offense when method called on literals' do
+    expect_offense(<<~RUBY)
+      [1, 2, 3].join('')
+      ^^^^^^^^^^^^^^^^^^ Argument '' is redundant because it is implied by default.
+      "first second".split(' ')
+      ^^^^^^^^^^^^^^^^^^^^^^^^^ Argument ' ' is redundant because it is implied by default.
+    RUBY
+  end
 
-    it 'registers an offense when using `#split` on string literal' do
-      expect_offense(<<~RUBY)
-        "first second".split(' ')
-        ^^^^^^^^^^^^^^^^^^^^^^^^^ Argument ' ' is redundant because it is implied by default.
-      RUBY
-    end
+  it 'works with double-quoted strings when configuration is single-quotes' do
+    expect_offense(<<~RUBY)
+      foo.join("")
+      ^^^^^^^^^^^^ Argument "" is redundant because it is implied by default.
+      "first second".split(" ")
+      ^^^^^^^^^^^^^^^^^^^^^^^^^ Argument " " is redundant because it is implied by default.
+    RUBY
+  end
 
-    it 'registers an offense when using `#join` with double quoted string' do
-      expect_offense(<<~RUBY)
-        foo.split(" ")
-        ^^^^^^^^^^^^^^ Argument " " is redundant because it is implied by default.
-      RUBY
-    end
+  it 'does not register an offense when method called with no arguments' do
+    expect_no_offenses(<<~RUBY)
+      foo.join
+      "first second".split
+    RUBY
+  end
 
-    it 'does not register an offense when using `#split` with no arguments' do
-      expect_no_offenses(<<~RUBY)
-        foo.split
-      RUBY
-    end
+  it 'does not register an offense when method called with more than one arguments' do
+    expect_no_offenses(<<~RUBY)
+      foo.join('', 2)
+      [1, 2, 3].split(" ", true, {})
+    RUBY
+  end
 
-    it 'does not register an offense when using `#split` with string literal' do
-      expect_no_offenses(<<~RUBY)
-        "first second".split
-      RUBY
-    end
+  it 'does not register an offense when method called with different argument' do
+    expect_no_offenses(<<~RUBY)
+      foo.join(',')
+      foo.split(',')
+    RUBY
+  end
 
-    it 'does not register an offense when using `#split` with separator' do
-      expect_no_offenses(<<~RUBY)
-        foo.split(',')
-      RUBY
-    end
-
-    it 'does not register an offense when using `#split` with empty string' do
-      expect_no_offenses(<<~RUBY)
-        foo.split('')
-      RUBY
-    end
+  it 'does not register an offense when method called with no receiver' do
+    expect_no_offenses(<<~RUBY)
+      join('')
+      split(' ')
+    RUBY
   end
 
   context 'non-builtin method' do
