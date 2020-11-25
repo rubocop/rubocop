356baf893363a87354a2d4f3874ec7753adb9a05
fatkodima <fatkodima123@gmail.com>
2020-11-25 20:29:31 +0200

    `Metrics/ParameterLists` supports `MaxOptionalParameters` config parameter

---
+++ changelog/new_add_max_optional_parameters_to_parameter_lists_cop.md
@@ -0,0 +1,1 @@
+* [#9010](https://github.com/rubocop-hq/rubocop/pull/9010): `Metrics/ParameterLists` supports `MaxOptionalParameters` config parameter. ([@fatkodima][])
--- lib/rubocop/cop/metrics/parameter_lists.rb
+++ lib/rubocop/cop/metrics/parameter_lists.rb
@@ -36,14 +36,43 @@ module RuboCop
       #   # good (assuming Max is 3)
       #   def foo(a, b, c, d: 1)
       #   end
+      #
+      # This cop also checks for the maximum number of optional parameters.
+      # This can be configured using the `MaxOptionalParameters` config option.
+      #
+      # @example MaxOptionalParameters: 3 (default)
+      #   # good
+      #   def foo(a = 1, b = 2, c = 3)
+      #   end
+      #
+      # @example MaxOptionalParameters: 2
+      #   # bad
+      #   def foo(a = 1, b = 2, c = 3)
+      #   end
+      #
       class ParameterLists < Base
         include ConfigurableMax
 
         MSG = 'Avoid parameter lists longer than %<max>d parameters. ' \
               '[%<count>d/%<max>d]'
+        OPTIONAL_PARAMETERS_MSG = 'Method has too many optional parameters. [%<count>d/%<max>d]'
 
         NAMED_KEYWORD_TYPES = %i[kwoptarg kwarg].freeze
         private_constant :NAMED_KEYWORD_TYPES
+
+        def on_def(node)
+          optargs = node.arguments.select(&:optarg_type?)
+          return if optargs.count <= max_optional_parameters
+
+          message = format(
+            OPTIONAL_PARAMETERS_MSG,
+            max: max_optional_parameters,
+            count: optargs.count
+          )
+
+          add_offense(node, message: message)
+        end
+        alias on_defs on_def
 
         def on_args(node)
           count = args_count(node)
@@ -72,6 +101,10 @@ module RuboCop
 
         def max_params
           cop_config['Max']
+        end
+
+        def max_optional_parameters
+          cop_config['MaxOptionalParameters']
         end
 
         def count_keyword_args?
