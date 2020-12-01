rubocop/rubocop

commit 9a8777f9034ecd174395c9a772688c484ac58af8

AllowedIdentifiers now also checks variable assignments (#9136)

lib/rubocop/cop/naming/variable_number.rb
@@ -104,6 +104,8 @@ module RuboCop
         def on_arg(node)
           @node = node
           name, = *node
+          return if allowed_identifier?(name)
+
           check_name(node, name, node.loc.name)
         end
         alias on_lvasgn on_arg
@@ -139,7 +141,7 @@ module RuboCop
         end
 
         def allowed_identifier?(name)
-          allowed_identifiers.include?(name.to_s)
+          allowed_identifiers.include?(name.to_s.gsub('@', ''))
         end
 
         def allowed_identifiers

spec/rubocop/cop/naming/variable_number_spec.rb
@@ -293,6 +293,24 @@ RSpec.describe RuboCop::Cop::Naming::VariableNumber, :config do
       }
     end
 
+    it 'does not register an offense for a local variable name that is allowed' do
+      expect_no_offenses(<<~RUBY)
+        capture3 = :foo
+      RUBY
+    end
+
+    it 'does not register an offense for a instance variable name that is allowed' do
+      expect_no_offenses(<<~RUBY)
+        @capture3 = :foo
+      RUBY
+    end
+
+    it 'does not register an offense for a class variable name that is allowed' do
+      expect_no_offenses(<<~RUBY)
+        @@capture3 = :foo
+      RUBY
+    end
+
     it 'does not register an offense for a method name that is allowed' do
       expect_no_offenses(<<~RUBY)
         def capture3

commit ce3d1a198dbd25fac83fc869671404ef08ebafc8

Fix rubocop failure introduced in 9a8777f

lib/rubocop/cop/naming/variable_number.rb
@@ -141,7 +141,7 @@ module RuboCop
         end
 
         def allowed_identifier?(name)
-          allowed_identifiers.include?(name.to_s.gsub('@', ''))
+          allowed_identifiers.include?(name.to_s.delete('@'))
         end
 
         def allowed_identifiers