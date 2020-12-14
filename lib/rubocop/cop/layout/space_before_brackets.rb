edbfb1235d779342588a046b3078f483131646d6
Koichi ITO <koic.ito@gmail.com>
2020-12-14 22:27:54 +0900

    Add new `Layout/SpaceBeforeBrackets` cop
    Follow https://github.com/rubocop-hq/ruby-style-guide/pull/856.
    
    This PR adds new `Layout/SpaceBeforeBrackets` cop.
    
    ```ruby
    # bad
    collection [index_or_key]
    
    # good
    collection[index_or_key]
    ```
    
    This cop is marked as unsafe because it can occur false positives
    for `do_something [this_is_an_array_literal_argument]` that take
    an array without parentheses as an argument.
    
    I noticed this issue below.
    https://github.com/rsim/oracle-enhanced/pull/2076/files#
    diff-0d21136da5aee9b25213dcf874f62418fa24a06a90237e069642a5af54e7b2efL494-R494

---
+++ lib/rubocop/cop/layout/space_before_brackets.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Layout
+      # Checks for space between the name of a receiver and a left
+      # brackets.
+      #
+      # This cop is marked as unsafe because it can occur false positives
+      # for `do_something [this_is_an_array_literal_argument]` that take
+      # an array without parentheses as an argument.
+      #
+      # @example
+      #
+      #   # bad
+      #   collection [index_or_key]
+      #
+      #   # good
+      #   collection[index_or_key]
+      #
+      class SpaceBeforeBrackets < Base
+        include RangeHelp
+        extend AutoCorrector
+
+        MSG = 'Remove the space before the opening brackets.'
+
+        def_node_matcher :bad_method?, <<~PATTERN
+          (send nil? :bad_method ...)
+        PATTERN
+
+        def on_send(node)
+          return if node.parenthesized? || node.parent&.send_type?
+          return unless (first_argument = node.first_argument)
+
+          begin_pos = first_argument.source_range.begin_pos
+
+          return unless (range = offense_range(node, first_argument, begin_pos))
+
+          register_offense(range)
+        end
+
+        private
+
+        def offense_range(node, first_argument, begin_pos)
+          if space_before_brackets?(node, first_argument)
+            range_between(node.loc.selector.end_pos, begin_pos)
+          elsif node.method?(:[]=)
+            end_pos = node.receiver.source_range.end_pos
+
+            return if begin_pos - end_pos == 1
+
+            range_between(end_pos, begin_pos - 1)
+          end
+        end
+
+        def register_offense(range)
+          add_offense(range) do |corrector|
+            corrector.remove(range)
+          end
+        end
+
+        def space_before_brackets?(node, first_argument)
+          node.receiver.nil? && first_argument.array_type? && node.arguments.size == 1
+        end
+      end
+    end
+  end
+end
