adfea8c1ec5d24c6e729fed7d9bfa819780e2b13
Koichi ITO <koic.ito@gmail.com>
2020-12-29 00:30:31 +0900

    Add new `Lint/RedundantDirGlobSort` cop
    Sort globbed results by default in Ruby 3.0.
    https://bugs.ruby-lang.org/issues/8709
    
    This PR adds new `Lint/RedundantDirGlobSort` cop.
    It checks for redundant `sort` method to `Dir.glob` and `Dir[]`.
    
    ```ruby
    # bad
    Dir.glob('./lib/**/*.rb').sort.each do |file|
    end
    
    Dir['./lib/**/*.rb'].sort.each do |file|
    end
    
    # good
    Dir.glob('./lib/**/*.rb').each do |file|
    end
    
    Dir['./lib/**/*.rb'].each do |file|
    end
    
    # good - Respect intent if `sort` keyword option is specified.
    Dir.glob('./lib/**/*.rb', sort: false).each do |file|
    end
    ```
    
    Related PR #9300.

---
+++ changelog/new_add_new_lint_redundant_dir_glob_sort_cop.md
@@ -0,0 +1,1 @@
+* [#9301](https://github.com/rubocop-hq/rubocop/pull/9301): Add new `Lint/RedundantDirGlobSort` cop. ([@koic][])
---
+++ spec/rubocop/cop/lint/redundant_dir_glob_sort_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Lint::RedundantDirGlobSort, :config do
+  context 'when Ruby 3.0 or higher', :ruby30 do
+    it 'registers an offense and correction when using `Dir.glob.sort`' do
+      expect_offense(<<~RUBY)
+        Dir.glob(Rails.root.join('test', '*.rb')).sort.each(&method(:require))
+                                                  ^^^^ Remove redundant `sort`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Dir.glob(Rails.root.join('test', '*.rb')).each(&method(:require))
+      RUBY
+    end
+
+    it 'registers an offense and correction when using `::Dir.glob.sort`' do
+      expect_offense(<<~RUBY)
+        ::Dir.glob(Rails.root.join('test', '*.rb')).sort.each(&method(:require))
+                                                    ^^^^ Remove redundant `sort`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ::Dir.glob(Rails.root.join('test', '*.rb')).each(&method(:require))
+      RUBY
+    end
+
+    it 'registers an offense and correction when using `Dir[].sort.each do`' do
+      expect_offense(<<~RUBY)
+        Dir['./lib/**/*.rb'].sort.each do |file|
+                             ^^^^ Remove redundant `sort`.
+        end
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Dir['./lib/**/*.rb'].each do |file|
+        end
+      RUBY
+    end
+
+    it 'registers an offense and correction when using `Dir[].sort.each(&do_something)`' do
+      expect_offense(<<~RUBY)
+        Dir['./lib/**/*.rb'].sort.each(&method(:require))
+                             ^^^^ Remove redundant `sort`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Dir['./lib/**/*.rb'].each(&method(:require))
+      RUBY
+    end
+
+    it 'does not register an offense when not using `sort` with `sort: false` option for `Dir`' do
+      expect_no_offenses(<<~RUBY)
+        Dir.glob(Rails.root.join('test', '*.rb'), sort: false).each do
+        end
+      RUBY
+    end
+  end
+
+  context 'when Ruby 2.7 or lower', :ruby27 do
+    it 'does not register an offense and correction when using `Dir.glob.sort`' do
+      expect_no_offenses(<<~RUBY)
+        Dir.glob(Rails.root.join('test', '*.rb')).sort.each(&method(:require))
+      RUBY
+    end
+
+    it 'does not register an offense and correction when using `::Dir.glob.sort`' do
+      expect_no_offenses(<<~RUBY)
+        ::Dir.glob(Rails.root.join('test', '*.rb')).sort.each(&method(:require))
+      RUBY
+    end
+
+    it 'does not register an offense and correction when using `Dir[].sort.each do`' do
+      expect_no_offenses(<<~RUBY)
+        Dir['./lib/**/*.rb'].sort.each do |file|
+        end
+      RUBY
+    end
+
+    it 'does not register an offense and correction when using `Dir[].sort.each(&do_something)`' do
+      expect_no_offenses(<<~RUBY)
+        Dir['./lib/**/*.rb'].sort.each(&method(:require))
+      RUBY
+    end
+  end
+
+  it 'does not register an offense when not using `sort` for `Dir`' do
+    expect_no_offenses(<<~RUBY)
+      Dir['./lib/**/*.rb'].each do |file|
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `sort` without a receiver' do
+    expect_no_offenses(<<~RUBY)
+      sort.do_something
+    RUBY
+  end
+end
