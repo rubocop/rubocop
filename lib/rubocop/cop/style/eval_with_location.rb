fcb3a94c90472c7f5562a089db6565c70c1f4950
Taichi Ishitani <taichi730@gmail.com>
2021-01-22 23:23:58 +0900

    Improve documentation for Style/EvalWithLocation cop
    Add description and example code for the case that a string variable
    is given as a code string.

--- lib/rubocop/cop/style/eval_with_location.rb
+++ lib/rubocop/cop/style/eval_with_location.rb
@@ -41,6 +41,18 @@ module RuboCop
       #     def do_something
       #     end
       #   RUBY
+      #
+      # This cop works only when a string literal is given as a code string.
+      # No offence is reported if a string variable is given as below:
+      #
+      # @example
+      #   # not checked
+      #   code = <<-RUBY
+      #     def do_something
+      #     end
+      #   RUBY
+      #   eval code
+      #
       class EvalWithLocation < Base
         extend AutoCorrector
 
