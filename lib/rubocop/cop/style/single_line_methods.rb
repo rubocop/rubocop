91616f6a8abd764e552fb73f036ea687476b3363
Koichi ITO <koic.ito@gmail.com>
2021-01-26 17:04:31 +0900

    Fix a typo for `Style/SingleLineMethods`
    Follow https://github.com/rubocop-hq/rubocop/pull/9281#issuecomment-751654689.
    
    This PR fixes a typo for `Style/SingleLineMethods`.
    
    `EnforcedStyle: allow` -> `EnforcedStyle: allow_single_line`

--- lib/rubocop/cop/style/single_line_methods.rb
+++ lib/rubocop/cop/style/single_line_methods.rb
@@ -8,7 +8,7 @@ module RuboCop
       #
       # Endless methods added in Ruby 3.0 are also accepted by this cop.
       #
-      # If `Style/EndlessMethod` is enabled with `EnforcedStyle: allow` or
+      # If `Style/EndlessMethod` is enabled with `EnforcedStyle: allow_single_line` or
       # `allow_always`, single-line methods will be auto-corrected to endless
       # methods if there is only one statement in the body.
       #
