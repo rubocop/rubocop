fc91406856e10494da3683d6104ebfef6b545a32
Koichi ITO <koic.ito@gmail.com>
2021-01-30 14:27:03 +0900

    Mark `Style/IfWithBooleanLiteralBranche` as unsafe auto-correction
    Follow https://github.com/rubocop-hq/rubocop/pull/9461#issuecomment-770046288.
    
    This PR marks `Style/IfWithBooleanLiteralBranche` as unsafe auto-correction.

---
+++ changelog/change_mark_unsafe_autocorrect_for_if_with_boolean_literal_branches.md
@@ -0,0 +1,1 @@
+* [#9476](https://github.com/rubocop-hq/rubocop/pull/9476): Mark `Style/IfWithBooleanLiteralBranche` as unsafe auto-correction. ([@koic][])
--- lib/rubocop/cop/style/if_with_boolean_literal_branches.rb
+++ lib/rubocop/cop/style/if_with_boolean_literal_branches.rb
@@ -6,6 +6,8 @@ module RuboCop
       # This cop checks for redundant `if` with boolean literal branches.
       # It checks only conditions to return boolean value (`true` or `false`) for safe detection.
       # The conditions to be checked are comparison methods, predicate methods, and double negative.
+      # However, auto-correction is unsafe because there is no guarantee that all predicate methods
+      # will return boolean value. Those methods can be allowed with `AllowedMethods` config.
       #
       # @example
       #   # bad
