b82c20f7a6dd762a27b319175464c38cd16eab43
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2021-02-10 14:31:45 -0500

    [Fix #9500] Update `Lint/Debugger` so that only specific receivers for debug methods lead to offenses.
    Prior to #8929, `Lint/Debugger` looked for a specific receiver/method pair, but that PR changed it so that any of the `DebuggerReceivers` would apply, *or also* no receiver. This caused regressions (#7636 and #9500, for instance) because method names specified as debugger by this cop would now register offenses when called with no receiver.
    
    In order to fix it, I changed the configuration for `Lint/Debugger` a bit, but in a way that should not cause any major incompatibility. The allow list is now specified including its receiver so that the receiver can be checked. Existing configurations that just pass in a method name will be treated as expecting no receiver (there's a slight incompatibility there because previously `foo` would be matched by `Kernel.foo` but I think this is ok (and is resolvable by adding `Kernel.foo` to the configuration).
    
    I've also grouped the debugger methods by source (it's often confusing to me where each method comes from), which also allows an end-user to disable a pre-defined group of methods by specifying `Byebug: ~` for example.
    
    Finally I've deprecated the `DebuggerReceivers` configuration as it is no longer useful.

--- spec/rubocop/cop/lint/debugger_spec.rb
+++ spec/rubocop/cop/lint/debugger_spec.rb
@@ -1,69 +1,104 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Lint::Debugger, :config do
-  context 'with configured debugger methods' do
+  context 'with the DebuggerMethods configuration' do
     let(:cop_config) do
       { 'DebuggerMethods' => %w[custom_debugger] }
     end
 
-    it 'does not report an offense for a byebug call' do
+    it 'does not register an offense for a byebug call' do
       expect_no_offenses(<<~RUBY)
         byebug
       RUBY
     end
 
-    it 'reports an offense for a debugger method call' do
+    it 'registers an offense for a `custom_debugger` call' do
       expect_offense(<<~RUBY)
         custom_debugger
         ^^^^^^^^^^^^^^^ Remove debugger entry point `custom_debugger`.
       RUBY
     end
+
+    context 'nested custom configurations' do
+      let(:cop_config) do
+        {
+          'DebuggerMethods' => {
+            'Custom' => %w[custom_debugger]
+          }
+        }
+      end
+
+      it 'registers an offense for a `custom_debugger call' do
+        expect_offense(<<~RUBY)
+          custom_debugger
+          ^^^^^^^^^^^^^^^ Remove debugger entry point `custom_debugger`.
+        RUBY
+      end
+    end
   end
 
-  it 'reports an offense for a debugger call' do
-    expect_offense(<<~RUBY)
-      debugger
-      ^^^^^^^^ Remove debugger entry point `debugger`.
-    RUBY
+  context 'built-in methods' do
+    it 'registers an offense for a irb binding call' do
+      expect_offense(<<~RUBY)
+        binding.irb
+        ^^^^^^^^^^^ Remove debugger entry point `binding.irb`.
+      RUBY
+    end
+
+    it 'registers an offense for a binding.irb with Kernel call' do
+      expect_offense(<<~RUBY)
+        Kernel.binding.irb
+        ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.binding.irb`.
+      RUBY
+    end
   end
 
-  it 'reports an offense for a byebug call' do
-    expect_offense(<<~RUBY)
-      byebug
-      ^^^^^^ Remove debugger entry point `byebug`.
-    RUBY
+  context 'byebug' do
+    it 'registers an offense for a byebug call' do
+      expect_offense(<<~RUBY)
+        byebug
+        ^^^^^^ Remove debugger entry point `byebug`.
+      RUBY
+    end
+
+    it 'registers an offense for a byebug with an argument call' do
+      expect_offense(<<~RUBY)
+        byebug foo
+        ^^^^^^^^^^ Remove debugger entry point `byebug foo`.
+      RUBY
+    end
+
+    it 'registers an offense for a Kernel.byebug call' do
+      expect_offense(<<~RUBY)
+        Kernel.byebug
+        ^^^^^^^^^^^^^ Remove debugger entry point `Kernel.byebug`.
+      RUBY
+    end
+
+    it 'registers an offense for a remote_byebug call' do
+      expect_offense(<<~RUBY)
+        remote_byebug
+        ^^^^^^^^^^^^^ Remove debugger entry point `remote_byebug`.
+      RUBY
+    end
+
+    it 'registers an offense for a Kernel.remote_byebug call' do
+      expect_offense(<<~RUBY)
+        Kernel.remote_byebug
+        ^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.remote_byebug`.
+      RUBY
+    end
   end
 
-  it 'reports an offense for a pry binding call' do
-    expect_offense(<<~RUBY)
-      binding.pry
-      ^^^^^^^^^^^ Remove debugger entry point `binding.pry`.
-    RUBY
-  end
-
-  it 'reports an offense for a remote_pry binding call' do
-    expect_offense(<<~RUBY)
-      binding.remote_pry
-      ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.remote_pry`.
-    RUBY
-  end
-
-  it 'reports an offense for a pry_remote binding call' do
-    expect_offense(<<~RUBY)
-      binding.pry_remote
-      ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.pry_remote`.
-    RUBY
-  end
-
-  context 'with capybara debug method call' do
-    it 'reports an offense for save_and_open_page' do
+  context 'capybara' do
+    it 'registers an offense for save_and_open_page' do
       expect_offense(<<~RUBY)
         save_and_open_page
         ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `save_and_open_page`.
       RUBY
     end
 
-    it 'reports an offense for save_and_open_screenshot' do
+    it 'registers an offense for save_and_open_screenshot' do
       expect_offense(<<~RUBY)
         save_and_open_screenshot
         ^^^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `save_and_open_screenshot`.
@@ -71,14 +106,14 @@ RSpec.describe RuboCop::Cop::Lint::Debugger, :config do
     end
 
     context 'with an argument' do
-      it 'reports an offense for save_and_open_page' do
+      it 'registers an offense for save_and_open_page' do
         expect_offense(<<~RUBY)
           save_and_open_page foo
           ^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `save_and_open_page foo`.
         RUBY
       end
 
-      it 'reports an offense for save_and_open_screenshot' do
+      it 'registers an offense for save_and_open_screenshot' do
         expect_offense(<<~RUBY)
           save_and_open_screenshot foo
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `save_and_open_screenshot foo`.
@@ -87,120 +122,176 @@ RSpec.describe RuboCop::Cop::Lint::Debugger, :config do
     end
   end
 
-  it 'reports an offense for a debugger with an argument call' do
-    expect_offense(<<~RUBY)
-      debugger foo
-      ^^^^^^^^^^^^ Remove debugger entry point `debugger foo`.
-    RUBY
+  context 'pry' do
+    it 'registers an offense for a pry binding call' do
+      expect_offense(<<~RUBY)
+        binding.pry
+        ^^^^^^^^^^^ Remove debugger entry point `binding.pry`.
+      RUBY
+    end
+
+    it 'registers an offense for a remote_pry binding call' do
+      expect_offense(<<~RUBY)
+        binding.remote_pry
+        ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.remote_pry`.
+      RUBY
+    end
+
+    it 'registers an offense for a pry_remote binding call' do
+      expect_offense(<<~RUBY)
+        binding.pry_remote
+        ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.pry_remote`.
+      RUBY
+    end
+
+    it 'registers an offense for a pry binding with an argument call' do
+      expect_offense(<<~RUBY)
+        binding.pry foo
+        ^^^^^^^^^^^^^^^ Remove debugger entry point `binding.pry foo`.
+      RUBY
+    end
+
+    it 'registers an offense for a remote_pry binding with an argument call' do
+      expect_offense(<<~RUBY)
+        binding.remote_pry foo
+        ^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.remote_pry foo`.
+      RUBY
+    end
+
+    it 'registers an offense for a pry_remote binding with an argument call' do
+      expect_offense(<<~RUBY)
+        binding.pry_remote foo
+        ^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.pry_remote foo`.
+      RUBY
+    end
+
+    it 'registers an offense for a binding.pry with Kernel call' do
+      expect_offense(<<~RUBY)
+        Kernel.binding.pry
+        ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.binding.pry`.
+      RUBY
+    end
+
+    it 'registers an offense for a Pry.rescue call' do
+      expect_offense(<<~RUBY)
+        def method
+          Pry.rescue { puts 1 }
+          ^^^^^^^^^^ Remove debugger entry point `Pry.rescue`.
+          ::Pry.rescue { puts 1 }
+          ^^^^^^^^^^^^ Remove debugger entry point `::Pry.rescue`.
+        end
+      RUBY
+    end
+
+    it 'does not register an offense for a `pry` call without binding' do
+      expect_no_offenses('pry')
+    end
+
+    it 'does not register an offense for a `rescue` call without Pry' do
+      expect_no_offenses(<<~RUBY)
+        begin
+        rescue StandardError
+        end
+      RUBY
+    end
   end
 
-  it 'reports an offense for a byebug with an argument call' do
-    expect_offense(<<~RUBY)
-      byebug foo
-      ^^^^^^^^^^ Remove debugger entry point `byebug foo`.
-    RUBY
+  context 'rails' do
+    it 'registers an offense for a debugger call' do
+      expect_offense(<<~RUBY)
+        debugger
+        ^^^^^^^^ Remove debugger entry point `debugger`.
+      RUBY
+    end
+
+    it 'registers an offense for a debugger with an argument call' do
+      expect_offense(<<~RUBY)
+        debugger foo
+        ^^^^^^^^^^^^ Remove debugger entry point `debugger foo`.
+      RUBY
+    end
+
+    it 'registers an offense for a debugger with Kernel call' do
+      expect_offense(<<~RUBY)
+        Kernel.debugger
+        ^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.debugger`.
+      RUBY
+    end
+
+    it 'registers an offense for a debugger with ::Kernel call' do
+      expect_offense(<<~RUBY)
+        ::Kernel.debugger
+        ^^^^^^^^^^^^^^^^^ Remove debugger entry point `::Kernel.debugger`.
+      RUBY
+    end
   end
 
-  it 'reports an offense for a pry binding with an argument call' do
-    expect_offense(<<~RUBY)
-      binding.pry foo
-      ^^^^^^^^^^^^^^^ Remove debugger entry point `binding.pry foo`.
-    RUBY
+  context 'web console' do
+    it 'registers an offense for a `binding.console` call' do
+      expect_offense(<<~RUBY)
+        binding.console
+        ^^^^^^^^^^^^^^^ Remove debugger entry point `binding.console`.
+      RUBY
+    end
+
+    it 'does not register an offense for `console` without a receiver' do
+      expect_no_offenses('console')
+    end
   end
 
-  it 'reports an offense for a remote_pry binding with an argument call' do
-    expect_offense(<<~RUBY)
-      binding.remote_pry foo
-      ^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.remote_pry foo`.
-    RUBY
-  end
-
-  it 'reports an offense for a pry_remote binding with an argument call' do
-    expect_offense(<<~RUBY)
-      binding.pry_remote foo
-      ^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `binding.pry_remote foo`.
-    RUBY
-  end
-
-  it 'reports an offense for a remote_byebug call' do
-    expect_offense(<<~RUBY)
-      remote_byebug
-      ^^^^^^^^^^^^^ Remove debugger entry point `remote_byebug`.
-    RUBY
-  end
-
-  it 'reports an offense for a web console binding call' do
-    expect_offense(<<~RUBY)
-      binding.console
-      ^^^^^^^^^^^^^^^ Remove debugger entry point `binding.console`.
-    RUBY
-  end
-
-  it 'does not report an offense for a non-pry binding' do
+  it 'does not register an offense for a binding method that is not disallowed' do
     expect_no_offenses('binding.pirate')
-  end
-
-  it 'reports an offense for a debugger with Kernel call' do
-    expect_offense(<<~RUBY)
-      Kernel.debugger
-      ^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.debugger`.
-    RUBY
-  end
-
-  it 'reports an offense for a debugger with ::Kernel call' do
-    expect_offense(<<~RUBY)
-      ::Kernel.debugger
-      ^^^^^^^^^^^^^^^^^ Remove debugger entry point `::Kernel.debugger`.
-    RUBY
-  end
-
-  it 'reports an offense for a binding.pry with Kernel call' do
-    expect_offense(<<~RUBY)
-      Kernel.binding.pry
-      ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.binding.pry`.
-    RUBY
-  end
-
-  it 'reports an offense for save_and_open_page with Kernel' do
-    expect_offense(<<~RUBY)
-      Kernel.save_and_open_page
-      ^^^^^^^^^^^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.save_and_open_page`.
-    RUBY
   end
 
   %w[debugger byebug console pry remote_pry pry_remote irb save_and_open_page
      save_and_open_screenshot remote_byebug].each do |src|
-    it "does not report an offense for a #{src} in comments" do
-      expect_no_offenses("# #{src}")
+    it "does not register an offense for a #{src} in comments" do
+      expect_no_offenses(<<~RUBY)
+        # #{src}
+        # Kernel.#{src}
+      RUBY
     end
 
-    it "does not report an offense for a #{src} method" do
+    it "does not register an offense for a #{src} method" do
       expect_no_offenses("code.#{src}")
     end
   end
 
-  it 'reports an offense for a Pry.rescue call' do
-    expect_offense(<<~RUBY)
-      def method
-        Pry.rescue { puts 1 }
-        ^^^^^^^^^^ Remove debugger entry point `Pry.rescue`.
-        ::Pry.rescue { puts 1 }
-        ^^^^^^^^^^^^ Remove debugger entry point `::Pry.rescue`.
-      end
-    RUBY
+  context 'when a method group is disabled with nil' do
+    let!(:old_pry_config) { cur_cop_config['DebuggerMethods']['Pry'] }
+
+    before { cur_cop_config['DebuggerMethods']['Pry'] = nil }
+
+    after { cur_cop_config['DebuggerMethods']['Pry'] = old_pry_config }
+
+    it 'does not register an offense for a Pry debugger call' do
+      expect_no_offenses('binding.pry')
+    end
+
+    it 'does register an offense for another group' do
+      expect_offense(<<~RUBY)
+        binding.irb
+        ^^^^^^^^^^^ Remove debugger entry point `binding.irb`.
+      RUBY
+    end
   end
 
-  it 'reports an offense for a irb binding call' do
-    expect_offense(<<~RUBY)
-      binding.irb
-      ^^^^^^^^^^^ Remove debugger entry point `binding.irb`.
-    RUBY
-  end
+  context 'when a method group is disabled with false' do
+    let!(:old_pry_config) { cur_cop_config['DebuggerMethods']['Pry'] }
 
-  it 'reports an offense for a binding.irb with Kernel call' do
-    expect_offense(<<~RUBY)
-      Kernel.binding.irb
-      ^^^^^^^^^^^^^^^^^^ Remove debugger entry point `Kernel.binding.irb`.
-    RUBY
+    before { cur_cop_config['DebuggerMethods']['Pry'] = false }
+
+    after { cur_cop_config['DebuggerMethods']['Pry'] = old_pry_config }
+
+    it 'does not register an offense for a Pry debugger call' do
+      expect_no_offenses('binding.pry')
+    end
+
+    it 'does register an offense for another group' do
+      expect_offense(<<~RUBY)
+        binding.irb
+        ^^^^^^^^^^^ Remove debugger entry point `binding.irb`.
+      RUBY
+    end
   end
 end
