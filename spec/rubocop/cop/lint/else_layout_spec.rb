rubocop/rubocop

commit 219dfe1935232d6bc8e0c8f74f6ec566414e1297

Replace `IfNode` mixin with node extension
This change replaces the use of a cop mixin for access to logic related
to `if` nodes, and instead decorates said nodes with the appropriate
module upon initialization.

lib/rubocop/cop/lint/else_layout.rb
@@ -28,12 +28,10 @@ module RuboCop
       #     do_that
       #   end
       class ElseLayout < Cop
-        include IfNode
+        MSG = 'Odd `else` layout detected. Did you mean to use `elsif`?'.freeze
 
         def on_if(node)
-          return if ternary?(node)
-          # ignore modifier ops & elsif nodes
-          return unless node.loc.end
+          return if node.ternary? || node.elsif?
 
           check(node)
         end
@@ -41,39 +39,25 @@ module RuboCop
         private
 
         def check(node)
-          return unless node
-          return check_else(node) if else?(node)
+          return unless node.else_branch
 
-          check_if(node) if if?(node)
+          if node.else? && node.loc.else.is?('else')
+            check_else(node)
+          elsif node.if?
+            check(node.else_branch)
+          end
         end
 
         def check_else(node)
-          _cond, _if_branch, else_branch = *node
-          return unless else_branch && else_branch.begin_type?
+          else_branch = node.else_branch
 
-          first_else_expr = else_branch.children.first
-          return unless first_else_expr.source_range.line == node.loc.else.line
+          return unless else_branch.begin_type?
 
-          add_offense(first_else_expr, :expression, message)
-        end
+          first_else = else_branch.children.first
 
-        def check_if(node)
-          _cond, _if_branch, else_branch = *node
-          check(else_branch)
-        end
+          return unless first_else.source_range.line == node.loc.else.line
 
-        def if?(node)
-          node.loc.respond_to?(:keyword) &&
-            %w(if elsif).include?(node.loc.keyword.source)
-        end
-
-        def else?(node)
-          node.loc.respond_to?(:else) && node.loc.else &&
-            node.loc.else.is?('else')
-        end
-
-        def message
-          'Odd `else` layout detected. Did you mean to use `elsif`?'
+          add_offense(first_else, :expression)
         end
       end
     end

.rubocop_todo.yml
@@ -13,7 +13,7 @@ Metrics/AbcSize:
 # Offense count: 32
 # Configuration parameters: CountComments.
 Metrics/ClassLength:
-  Max: 182
+  Max: 183
 
 # Offense count: 29
 Metrics/CyclomaticComplexity:

lib/rubocop.rb
@@ -20,6 +20,7 @@ require 'rubocop/name_similarity'
 require 'rubocop/node_pattern'
 require 'rubocop/string_interpreter'
 require 'rubocop/ast_node/sexp'
+require 'rubocop/ast_node/node_extensions/if_node'
 require 'rubocop/ast_node'
 require 'rubocop/ast_node/builder'
 require 'rubocop/ast_node/traversal'
@@ -67,7 +68,6 @@ require 'rubocop/cop/mixin/first_element_line_break'
 require 'rubocop/cop/mixin/frozen_string_literal'
 require 'rubocop/cop/mixin/hash_node'
 require 'rubocop/cop/mixin/hash_alignment'
-require 'rubocop/cop/mixin/if_node'
 require 'rubocop/cop/mixin/integer_node'
 require 'rubocop/cop/mixin/on_method_def'
 require 'rubocop/cop/mixin/match_range'

lib/rubocop/ast_node.rb
@@ -58,7 +58,7 @@ module RuboCop
     # @see http://rubydoc.info/gems/ast/AST/Node:initialize
     def initialize(type, children = [], properties = {})
       @mutable_attributes = {}
-
+      
       # ::AST::Node#initialize freezes itself.
       super
 

lib/rubocop/ast_node/node_extensions/if_node.rb
@@ -1,0 +1,63 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module NodeExtension
+    # A node extension for `if` nodes.
+    module IfNode
+      def if?
+        keyword == 'if'
+      end
+
+      def unless?
+        keyword == 'unless'
+      end
+
+      def elsif?
+        keyword == 'elsif'
+      end
+
+      def keyword
+        ternary? ? '' : loc.keyword.source
+      end
+
+      def ternary?
+        loc.respond_to?(:question)
+      end
+
+      def else?
+        loc.respond_to?(:else) && loc.else
+      end
+
+      def modifier_form?
+        (if? || unless?) && super
+      end
+
+      def condition
+        if_node_parts[0]
+      end
+
+      def if_branch
+        if_node_parts[1]
+      end
+
+      def nested_conditional?
+        node_parts[1..2].compact.any?(&:if_type?)
+      end
+
+      def false_branch
+        node_parts[2]
+      end
+      alias else_branch false_branch
+
+      def if_node_parts
+        if unless?
+          condition, else_clause, if_clause = *self
+        else
+          condition, if_clause, else_clause = *self
+        end
+
+        [condition, if_clause, else_clause]
+      end
+    end
+  end
+end

lib/rubocop/cop/lint/condition_position.rb
@@ -23,10 +23,8 @@ module RuboCop
       #     do_something
       #   end
       class ConditionPosition < Cop
-        include IfNode
-
         def on_if(node)
-          return if ternary?(node)
+          return if node.ternary?
 
           check(node)
         end
@@ -42,21 +40,16 @@ module RuboCop
         private
 
         def check(node)
-          return if !node.loc.keyword.is?('elsif') && node.loc.end.nil?
+          return if !(node.if_type? && node.elsif?) && node.loc.end.nil?
 
           condition, = *node
-          return unless on_different_line?(node.loc.keyword.line,
-                                           condition.source_range.line)
+          return if node.loc.keyword.line == condition.source_range.line
 
           add_offense(condition, :expression, message(node.loc.keyword.source))
         end
 
         def message(keyword)
           "Place the condition on the same line as `#{keyword}`."
-        end
-
-        def on_different_line?(keyword_line, cond_line)
-          keyword_line != cond_line
         end
       end
     end

lib/rubocop/cop/lint/end_alignment.rb
@@ -53,7 +53,6 @@ module RuboCop
       class EndAlignment < Cop
         include CheckAssignment
         include EndKeywordAlignment
-        include IfNode
 
         def on_class(node)
           check_other_alignment(node)
@@ -64,7 +63,7 @@ module RuboCop
         end
 
         def on_if(node)
-          check_other_alignment(node) unless ternary?(node)
+          check_other_alignment(node) unless node.ternary?
         end
 
         def on_while(node)
@@ -88,7 +87,7 @@ module RuboCop
           # we check if it's an if/unless/while/until.
           return unless (rhs = first_part_of_call_chain(rhs))
           return unless [:if, :while, :until, :case].include?(rhs.type)
-          return if ternary?(rhs)
+          return if rhs.if_type? && rhs.ternary?
 
           check_asgn_alignment(node, rhs)
         end

commit 2e67d34802f202cdc359381377a15de9c94170c9

Fix a false negative for `Lint/ElseLayout`
Regression by https://github.com/rubocop-hq/rubocop/commit/219dfe1.

This PR fixes a false negative for `Lint/ElseLayout`
when using multiple `elsif`s.

lib/rubocop/cop/lint/else_layout.rb
@@ -47,7 +47,7 @@ module RuboCop
         MSG = 'Odd `else` layout detected. Did you mean to use `elsif`?'
 
         def on_if(node)
-          return if node.ternary? || node.elsif?
+          return if node.ternary?
 
           check(node)
         end

spec/rubocop/cop/lint/else_layout_spec.rb
@@ -69,6 +69,36 @@ RSpec.describe RuboCop::Cop::Lint::ElseLayout, :config do
     RUBY
   end
 
+  it 'registers and corrects an offense when using multiple `elsif`s' do
+    expect_offense(<<~RUBY)
+      if condition_foo
+        foo
+      elsif condition_bar
+        bar
+      elsif condition_baz
+        baz
+      else qux
+           ^^^ Odd `else` layout detected. Did you mean to use `elsif`?
+        quux
+        corge
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      if condition_foo
+        foo
+      elsif condition_bar
+        bar
+      elsif condition_baz
+        baz
+      else
+        qux
+        quux
+        corge
+      end
+    RUBY
+  end
+
   it 'handles ternary ops' do
     expect_no_offenses('x ? a : b')
   end