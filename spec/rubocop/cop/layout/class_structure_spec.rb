20d8ed46e47d0fcd1e9d579bbf82c378dc06669c
Marc-Andre Lafortune <github@marc-andre.ca>
2021-03-09 00:59:42 -0500

    [Fixes #9573] Consider inline visibility for macros

--- spec/rubocop/cop/layout/class_structure_spec.rb
+++ spec/rubocop/cop/layout/class_structure_spec.rb
@@ -470,4 +470,15 @@ RSpec.describe RuboCop::Cop::Layout::ClassStructure, :config do
       end
     RUBY
   end
+
+  pending 'handles categories visibility with inline style too' do
+    expect_offense(<<~RUBY)
+      class A
+        private attr_accessor :foo
+
+        attr_accessor :bar
+        ^^^^^^^^^^^^^^^^^^ `attribute_macros` is supposed to appear before `private_attribute_macros`.
+      end
+    RUBY
+  end
 end
