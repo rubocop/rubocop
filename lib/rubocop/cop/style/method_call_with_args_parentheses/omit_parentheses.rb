a6a14293a22b74ba9bea5b2385607deafff513a7
Genadi Samokovarov <gsamokovarov@gmail.com>
2021-03-17 16:30:50 +0200

    Allow parentheses in operator method calls for `Style/MethodCallWithArgsParentheses`
    While calls like `data&.[] value`, `value.=== other` do compile, I won't
    blame people if they want to leave the parentheses so they know what's
    going on.
    
    I'm proposing we legitimize the parens when calling operator methods.  I
    don't think this deserves extra configuration.

--- lib/rubocop/cop/style/method_call_with_args_parentheses/omit_parentheses.rb
+++ lib/rubocop/cop/style/method_call_with_args_parentheses/omit_parentheses.rb
@@ -5,6 +5,7 @@ module RuboCop
     module Style
       class MethodCallWithArgsParentheses
         # Style omit_parentheses
+        # rubocop:disable Metrics/ModuleLength
         module OmitParentheses
           TRAILING_WHITESPACE_REGEX = /\s+\Z/.freeze
           OMIT_MSG = 'Omit parentheses for method calls with arguments.'
@@ -15,7 +16,7 @@ module RuboCop
           def omit_parentheses(node)
             return unless node.parenthesized?
             return if inside_endless_method_def?(node)
-            return if node.implicit_call?
+            return if syntax_like_method_call?(node)
             return if super_call_without_arguments?(node)
             return if allowed_camel_case_method_call?(node)
             return if legitimate_call_with_parentheses?(node)
@@ -41,6 +42,10 @@ module RuboCop
           def inside_endless_method_def?(node)
             # parens are required around arguments inside an endless method
             node.each_ancestor(:def).any?(&:endless?) && node.arguments.any?
+          end
+
+          def syntax_like_method_call?(node)
+            node.implicit_call? || node.operator_method?
           end
 
           def super_call_without_arguments?(node)
@@ -173,6 +178,7 @@ module RuboCop
               node.loc.operator.begin < target.loc.begin
           end
         end
+        # rubocop:enable Metrics/ModuleLength
       end
     end
   end
