c5913b3c00294fbb758bc1589acc4af8a6a944dc
Koichi ITO <koic.ito@gmail.com>
2021-03-23 22:31:18 +0900

    [Fix #5953] Fix a false positive for `Style/AccessModifierDeclarations`
    Fixes #5953.
    
    This PR fixes a false positive for `Style/AccessModifierDeclarations`
    when using `module_function` with symbol.

--- lib/rubocop/cop/style/access_modifier_declarations.rb
+++ lib/rubocop/cop/style/access_modifier_declarations.rb
@@ -79,7 +79,7 @@ module RuboCop
 
         # @!method access_modifier_with_symbol?(node)
         def_node_matcher :access_modifier_with_symbol?, <<~PATTERN
-          (send nil? {:private :protected :public} (sym _))
+          (send nil? {:private :protected :public :module_function} (sym _))
         PATTERN
 
         def on_send(node)
