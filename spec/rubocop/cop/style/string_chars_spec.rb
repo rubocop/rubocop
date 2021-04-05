fbc77da536942ac1f4c48305924661812a69219b
Koichi ITO <koic.ito@gmail.com>
2021-04-05 13:51:56 +0900

    [Fix #9676] Fix an error for `Style/StringChars`
    Fixes #9676.
    
    This PR fixes an error for `Style/StringChars` when using `split` without parentheses.

--- spec/rubocop/cop/style/string_chars_spec.rb
+++ spec/rubocop/cop/style/string_chars_spec.rb
@@ -34,6 +34,17 @@ RSpec.describe RuboCop::Cop::Style::StringChars, :config do
     RUBY
   end
 
+  it 'registers and corrects an offense when using `split` without parentheses' do
+    expect_offense(<<~RUBY)
+      do_something { |foo| foo.split '' }
+                               ^^^^^^^^ Use `chars` instead of `split ''`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      do_something { |foo| foo.chars }
+    RUBY
+  end
+
   it 'does not register an offense when using `chars`' do
     expect_no_offenses(<<~RUBY)
       string.chars
