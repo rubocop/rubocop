3bd037c6c78c436c44336b19e616095d1cd1c6e5
Koichi ITO <koic.ito@gmail.com>
2021-05-10 08:49:37 +0900

    Add new `Style/MultilineInPatternThen` cop
    Follow https://github.com/rubocop/ruby-style-guide/pull/873.
    
    This PR adds new `Style/MultilineInPatternThen` cop.
    It checks uses of the `then` keyword in multi-line `in` statement.
    
    ```ruby
    # bad
    case expression
    in pattern then
    end
    
    # good
    case expression
    in pattern
    end
    ```
    
    This cop is similar to `Style/MultilineWhenThen` (and `Style/MultilineIfThen`)
    but with different supported syntax and Ruby version (requires 2.7 or higher).

---
+++ changelog/new_add_new_style_multiline_in_pattern_then_cop.md
@@ -0,0 +1,1 @@
+* [#9834](https://github.com/rubocop/rubocop/pull/9834): Add new `Style/MultilineInPatternThen` cop. ([@koic][])
---
+++ lib/rubocop/cop/style/multiline_in_pattern_then.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Style
+      # This cop checks uses of the `then` keyword in multi-line `in` statement.
+      #
+      # @example
+      #   # bad
+      #   case expression
+      #   in pattern then
+      #   end
+      #
+      #   # good
+      #   case expression
+      #   in pattern
+      #   end
+      #
+      #   # good
+      #   case expression
+      #   in pattern then do_something
+      #   end
+      #
+      #   # good
+      #   case expression
+      #   in pattern then do_something(arg1,
+      #                                arg2)
+      #   end
+      #
+      class MultilineInPatternThen < Base
+        include RangeHelp
+        extend AutoCorrector
+        extend TargetRubyVersion
+
+        minimum_target_ruby_version 2.7
+
+        MSG = 'Do not use `then` for multiline `in` statement.'
+
+        def on_in_pattern(node)
+          return if !node.then? || require_then?(node)
+
+          range = node.loc.begin
+          add_offense(range) do |corrector|
+            corrector.remove(
+              range_with_surrounding_space(range: range, side: :left, newlines: false)
+            )
+          end
+        end
+
+        private
+
+        # Requires `then` for write `in` and its body on the same line.
+        def require_then?(in_pattern_node)
+          return true if in_pattern_node.pattern.first_line != in_pattern_node.pattern.last_line
+          return false unless in_pattern_node.body
+
+          in_pattern_node.loc.line == in_pattern_node.body.loc.line
+        end
+      end
+    end
+  end
+end
