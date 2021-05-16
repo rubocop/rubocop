0cc37f6a603b70f65f904766a21741c89f55289f
Koichi ITO <koic.ito@gmail.com>
2021-05-16 16:36:10 +0900

    Add new `Lint/EmptyInPattern` cop
    This PR adds new `Lint/EmptyInPattern` cop for Ruby 2.7's pattern matching.
    It checks for the presence of `in` branches without a body.
    
    ```ruby
    # bad
    case condition
    in [a]
      do_something
    in [a, b]
    end
    
    # good
    case condition
    in [a]
      do_something
    in [a, b]
      nil
    end
    
    # good - AllowComments: true (default)
    case condition
    in [a]
      do_something
    in [a, b]
      # noop
    end
    
    # bad - AllowComments: false
    case condition
    in [a]
      do_something
    in [a, b]
      # noop
    end
    ```
    
    This cop is similar to `Lint/EmptyWhen`, but with different supported
    syntax and Ruby version (requires 2.7 or higher).
    
    And this PR use https://github.com/rubocop/rubocop-ast/pull/183 feature,
    so it requires RuboCop AST 1.6.0 or higher.

---
+++ lib/rubocop/cop/lint/empty_in_pattern.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Lint
+      # This cop checks for the presence of `in` pattern branches without a body.
+      #
+      # @example
+      #
+      #   # bad
+      #   case condition
+      #   in [a]
+      #     do_something
+      #   in [a, b]
+      #   end
+      #
+      #   # good
+      #   case condition
+      #   in [a]
+      #     do_something
+      #   in [a, b]
+      #     nil
+      #   end
+      #
+      # @example AllowComments: true (default)
+      #
+      #   # good
+      #   case condition
+      #   in [a]
+      #     do_something
+      #   in [a, b]
+      #     # noop
+      #   end
+      #
+      # @example AllowComments: false
+      #
+      #   # bad
+      #   case condition
+      #   in [a]
+      #     do_something
+      #   in [a, b]
+      #     # noop
+      #   end
+      #
+      class EmptyInPattern < Base
+        extend TargetRubyVersion
+
+        MSG = 'Avoid `in` branches without a body.'
+
+        minimum_target_ruby_version 2.7
+
+        def on_case_match(node)
+          node.in_pattern_branches.each do |branch|
+            next if branch.body || cop_config['AllowComments'] && comment_lines?(node)
+
+            add_offense(branch)
+          end
+        end
+      end
+    end
+  end
+end
