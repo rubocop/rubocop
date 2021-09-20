72daf0c005fec36e0b11e8e373bcd692a5ecce65
Jean Boussier <jean.boussier@gmail.com>
2021-09-20 15:28:53 +0200

    `Style/RedundantFreeze` stop considering `ENV` values as immutable
    Ref: https://github.com/rubocop/rubocop/pull/6784
    
    `ENV["foo"]` returns a mutable string.
    
    ```ruby
    >> ENV["PATH"].frozen?
    => true
    >> ENV["PATH"].object_id
    => 260
    >> ENV["PATH"].object_id
    => 280
    ```
    
    As such it's perfectly legitimate to freeze them in a pattern such as:
    
    ```ruby
    PATH = ENV["PATH"].freeze
    ```

---
+++ changelog/fix_redundant_freeze_env_values.md
@@ -0,0 +1,1 @@
+* [#10099](https://github.com/rubocop/rubocop/pull/10099): Update`Style/RedundantFreeze` to stop considering `ENV` values as immutable. ([@byroot][])
--- spec/rubocop/cop/style/redundant_freeze_spec.rb
+++ spec/rubocop/cop/style/redundant_freeze_spec.rb
@@ -20,8 +20,6 @@ RSpec.describe RuboCop::Cop::Style::RedundantFreeze, :config do
   it_behaves_like 'immutable objects', '1.5'
   it_behaves_like 'immutable objects', ':sym'
   it_behaves_like 'immutable objects', ':""'
-  it_behaves_like 'immutable objects', "ENV['foo']"
-  it_behaves_like 'immutable objects', "::ENV['foo']"
   it_behaves_like 'immutable objects', "'foo'.count"
   it_behaves_like 'immutable objects', '(1 + 2)'
   it_behaves_like 'immutable objects', '(2 > 1)'
@@ -44,6 +42,8 @@ RSpec.describe RuboCop::Cop::Style::RedundantFreeze, :config do
   it_behaves_like 'mutable objects', "('a' * 20)"
   it_behaves_like 'mutable objects', '(a + b)'
   it_behaves_like 'mutable objects', '([42] * 42)'
+  it_behaves_like 'mutable objects', "ENV['foo']"
+  it_behaves_like 'mutable objects', "::ENV['foo']"
 
   it 'allows .freeze on  method call' do
     expect_no_offenses('TOP_TEST = Something.new.freeze')
