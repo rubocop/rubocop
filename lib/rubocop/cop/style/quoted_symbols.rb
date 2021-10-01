e0c8de802f6d19c8267c554472c19626267f02a0
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2021-10-01 15:38:16 -0400

    Clarify documentation for `Style/QuotedSymbols`.

--- lib/rubocop/cop/style/quoted_symbols.rb
+++ lib/rubocop/cop/style/quoted_symbols.rb
@@ -4,7 +4,8 @@ module RuboCop
   module Cop
     module Style
       # Checks if the quotes used for quoted symbols match the configured defaults.
-      # By default uses the same configuration as `Style/StringLiterals`.
+      # By default uses the same configuration as `Style/StringLiterals`; if that
+      # cop is not enabled, the default `EnforcedStyle` is `single_quotes`.
       #
       # String interpolation is always kept in double quotes.
       #
