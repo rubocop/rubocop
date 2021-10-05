7e21d7feb1701c9da0ea91115ca038898e1a46af
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2021-10-05 15:11:18 -0400

    Update `InternalAffairs/LocationLineEqualityComparison` to handle `node.loc.first_line`, `node.first_line` and `node.source_range.line`.
    All of these are aliases for `node.loc.line`.

--- spec/rubocop/cop/internal_affairs/location_line_equality_comparison_spec.rb
+++ spec/rubocop/cop/internal_affairs/location_line_equality_comparison_spec.rb
@@ -12,6 +12,50 @@ RSpec.describe RuboCop::Cop::InternalAffairs::LocationLineEqualityComparison, :c
     RUBY
   end
 
+  it 'registers and corrects an offense when comparing `#loc.source_range` with LHS and RHS' do
+    expect_offense(<<~RUBY)
+      node.source_range.line == node.parent.source_range.line
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `same_line?(node, node.parent)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      same_line?(node, node.parent)
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using `loc.first_line`' do
+    expect_offense(<<~RUBY)
+      node.loc.line == node.parent.loc.first_line
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `same_line?(node, node.parent)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      same_line?(node, node.parent)
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using `source_range.first_line`' do
+    expect_offense(<<~RUBY)
+      node.source_range.line == node.parent.source_range.first_line
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `same_line?(node, node.parent)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      same_line?(node, node.parent)
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using `first_line`' do
+    expect_offense(<<~RUBY)
+      node.loc.line == node.parent.first_line
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `same_line?(node, node.parent)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      same_line?(node, node.parent)
+    RUBY
+  end
+
   it 'does not register an offense when using `same_line?`' do
     expect_no_offenses(<<~RUBY)
       same_line?(node, node.parent)
