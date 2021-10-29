4e49d2782be2f45f712ba1075282e2f0abad24a4
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2021-10-29 16:25:09 -0400

    Added additional documentation to `Naming/FileName` for currently undocumented configurations.

--- lib/rubocop/cop/naming/file_name.rb
+++ lib/rubocop/cop/naming/file_name.rb
@@ -14,6 +14,18 @@ module RuboCop
       # (i.e. `bundler-console` becomes `Bundler::Console`). As such, the
       # gemspec is supposed to be named `bundler-console.gemspec`.
       #
+      # When `ExpectMatchingDefinition` (default: `false`) is `true`, the cop requires
+      # each file to have a class, module or `Struct` defined in it that matches
+      # the filename. This can be further configured using
+      # `CheckDefinitionPathHierarchy` (default: `true`) to determine whether the
+      # path should match the namespace of the above definition.
+      #
+      # When `IgnoreExecutableScripts` (default: `true`) is `true`, files that start
+      # with a shebang line are not considered by the cop.
+      #
+      # When `Regex` is set, the cop will flag any filename that does not match
+      # the regular expression.
+      #
       # @example
       #   # bad
       #   lib/layoutManager.rb
