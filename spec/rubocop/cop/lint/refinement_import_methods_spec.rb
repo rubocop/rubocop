c2185849da91a92a6c68596bd3ee1eaab8a9b0f8
Koichi ITO <koic.ito@gmail.com>
2022-01-08 07:47:46 +0900

    Add new `Lint/RefinementImportMethods` cop
    Follow up to https://github.com/ruby/ruby/commit/6606597.
    
    This cop supports Ruby 3.1's `Refinement#import_methods` and
    checks if `include` or `prepend` is called in `refine` block.
    These methods are deprecated and should be replaced with `import_methods`.
    
    ## safety
    
    This cop's autocorrection is unsafe because `include M` will affect the included class
    if any changes are made to module `M`.
    On the other hand, `import_methods M` uses a snapshot of method definitions,
    thus it will not be affected if module `M` changes.
    
    ## example
    
    ```ruby
    # bad
    refine Foo do
      include Bar
    end
    
    # bad
    refine Foo do
      prepend Bar
    end
    
    # good
    refine Foo do
     import_methods Bar
    end
    ```

---
+++ spec/rubocop/cop/lint/refinement_import_methods_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Lint::RefinementImportMethods, :config do
+  context 'Ruby >= 3.1', :ruby31 do
+    it 'registers an offense when using `include` in `refine` block' do
+      expect_offense(<<~RUBY)
+        refine Foo do
+          include Bar
+          ^^^^^^^ Use `import_methods` instead of `include` because it is deprecated in Ruby 3.1.
+        end
+      RUBY
+    end
+
+    it 'registers an offense when using `prepend` in `refine` block' do
+      expect_offense(<<~RUBY)
+        refine Foo do
+          prepend Bar
+          ^^^^^^^ Use `import_methods` instead of `prepend` because it is deprecated in Ruby 3.1.
+        end
+      RUBY
+    end
+
+    it 'does not register an offense when using `import_methods` in `refine` block' do
+      expect_no_offenses(<<~RUBY)
+        refine Foo do
+          import_methods Bar
+        end
+      RUBY
+    end
+
+    it 'does not register an offense when using `include` with a receiver in `refine` block' do
+      expect_no_offenses(<<~RUBY)
+        refine Foo do
+          Bar.include Baz
+        end
+      RUBY
+    end
+  end
+
+  context 'Ruby <= 3.0', :ruby30 do
+    it 'does not register an offense when using `include` in `refine` block' do
+      expect_no_offenses(<<~RUBY)
+        refine Foo do
+          include Bar
+        end
+      RUBY
+    end
+
+    it 'does not register an offense when using `prepend` in `refine` block' do
+      expect_no_offenses(<<~RUBY)
+        refine Foo do
+          prepend Bar
+        end
+      RUBY
+    end
+  end
+end
