fed5c291071d857b59a5dc794b61d169ae075475
Koichi ITO <koic.ito@gmail.com>
2022-03-07 21:11:08 +0900

    Tweak the doc for `Layout/CaseIndentation`
    Follow up https://github.com/rubocop/rubocop/pull/10444#issuecomment-1060404850.

--- lib/rubocop/cop/layout/case_indentation.rb
+++ lib/rubocop/cop/layout/case_indentation.rb
@@ -3,7 +3,7 @@
 module RuboCop
   module Cop
     module Layout
-      # This cop checks how the `when` and `in`s of a `case` expression
+      # This cop checks how the `when` and ``in``s of a `case` expression
       # are indented in relation to its `case` or `end` keyword.
       #
       # It will register a separate offense for each misaligned `when` and `in`.
