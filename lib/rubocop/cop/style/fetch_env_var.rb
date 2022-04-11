17a52efd5773c078698ed0d4bfa6dd4aad255f34
Miyake J Takuma <i.takuma.miyake@globis.co.jp>
2022-04-11 21:21:26 +0900

    Simplify the logic, and add some comments

--- lib/rubocop/cop/style/fetch_env_var.rb
+++ lib/rubocop/cop/style/fetch_env_var.rb
@@ -37,7 +37,7 @@ module RuboCop
         def on_send(node)
           env_with_bracket?(node) do |expression|
             break if allowed_var?(expression)
-            break unless offensive?(node)
+            break if allowable_use?(node)
 
             add_offense(node, message: format(MSG, key: expression.source)) do |corrector|
               corrector.replace(node, "ENV.fetch(#{expression.source}, nil)")
@@ -51,61 +51,23 @@ module RuboCop
           expression.str_type? && cop_config['AllowedVars'].include?(expression.value)
         end
 
-        # rubocop:disable Metrics/CyclomaticComplexity
-        def offensive?(node)
-          only_node_of_expression?(node) ||
-            method_argument?(node) ||
-            array_element?(node) ||
-            hash_key?(node) ||
-            compared?(node) ||
-            case?(node) ||
-            operand_of_or?(node) ||
-            last_child_of_parent_node?(node)
-        end
-        # rubocop:enable Metrics/CyclomaticComplexity
+        def used_as_flag?(node)
+          return false if node.root?
 
-        def case?(node)
-          node.parent&.case_type? || node.parent&.when_type?
+          node.parent.if_type? || (node.parent.send_type? && node.parent.prefix_bang?)
         end
 
-        def only_node_of_expression?(node)
-          node.parent.nil?
+        def message_chained_with_dot?(node)
+          return false if node.root?
+
+          node.parent.send_type? && node.parent.dot?
         end
 
-        def method_argument?(node)
-          return false unless node.parent&.send_type?
-
-          node.parent.arguments.include?(node)
-        end
-
-        def array_element?(node)
-          return false unless node.parent&.array_type?
-
-          node.parent.children.include?(node)
-        end
-
-        def hash_key?(node)
-          return false unless node.parent&.pair_type?
-
-          node.parent.children.first == node
-        end
-
-        def compared?(node)
-          return false unless node.parent&.send_type?
-
-          node.parent.comparison_method?
-        end
-
-        def operand_of_or?(node)
-          return false unless node.parent&.or_type?
-
-          node.parent.children.include?(node)
-        end
-
-        def last_child_of_parent_node?(node)
-          return false unless node.parent
-
-          node.parent.children.last == node
+        # Allow if used as a flag (e.g., `if ENV['X']` or `!ENV['X']`) because
+        # it simply checks whether the variable is set.
+        # Also allow if receiving a message with a dot syntax, e.g. `ENV['X'].nil?`.
+        def allowable_use?(node)
+          used_as_flag?(node) || message_chained_with_dot?(node)
         end
       end
     end
