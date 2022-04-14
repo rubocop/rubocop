69ed7d5fcb93b36d3e9cbf0204b27a2a95684d8e
Koichi ITO <koic.ito@gmail.com>
2022-04-15 01:18:42 +0900

    Fix an incorrect auto-correct for `Style/MultilineTernaryOperator`
    Follow up https://github.com/rubocop/rubocop/pull/8661.
    
    This PR fixes an incorrect auto-correct for `Style/MultilineTernaryOperator`
    when returning a multiline ternary operator expression.
    Using `break`, `next`, or method call also causes the same syntax error as `return`.
    
    And this PR will change these auto-correct to single-line ternary operator as valid syntax.

--- lib/rubocop/cop/style/multiline_ternary_operator.rb
+++ lib/rubocop/cop/style/multiline_ternary_operator.rb
@@ -6,7 +6,8 @@ module RuboCop
       # This cop checks for multi-line ternary op expressions.
       #
       # NOTE: `return if ... else ... end` is syntax error. If `return` is used before
-      # multiline ternary operator expression, it cannot be auto-corrected.
+      # multiline ternary operator expression, it will be auto-corrected to single-line
+      # ternary operator. The same is true for `break`, `next`, and method call.
       #
       # @example
       #   # bad
@@ -18,6 +19,10 @@ module RuboCop
       #       b :
       #       c
       #
+      #   return cond ?
+      #          b :
+      #          c
+      #
       #   # good
       #   a = cond ? b : c
       #   a = if cond
@@ -25,20 +30,39 @@ module RuboCop
       #   else
       #     c
       #   end
+      #
+      #   return cond ? b : c
+      #
       class MultilineTernaryOperator < Base
         extend AutoCorrector
 
-        MSG = 'Avoid multi-line ternary operators, use `if` or `unless` instead.'
+        MSG_IF = 'Avoid multi-line ternary operators, use `if` or `unless` instead.'
+        MSG_SINGLE_LINE = 'Avoid multi-line ternary operators, use single-line instead.'
+        SINGLE_LINE_TYPES = %i[return break next send].freeze
 
         def on_if(node)
           return unless offense?(node)
 
-          add_offense(node) do |corrector|
-            # `return if ... else ... end` is syntax error. If `return` is used before
-            # multiline ternary operator expression, it cannot be auto-corrected.
-            next unless offense?(node) && !node.parent.return_type?
+          message = enforce_single_line_ternary_operator?(node) ? MSG_SINGLE_LINE : MSG_IF
 
-            corrector.replace(node, <<~RUBY.chop)
+          add_offense(node, message: message) do |corrector|
+            next unless offense?(node)
+
+            corrector.replace(node, replacement(node))
+          end
+        end
+
+        private
+
+        def offense?(node)
+          node.ternary? && node.multiline?
+        end
+
+        def replacement(node)
+          if enforce_single_line_ternary_operator?(node)
+            "#{node.condition.source} ? #{node.if_branch.source} : #{node.else_branch.source}"
+          else
+            <<~RUBY.chop
               if #{node.condition.source}
                 #{node.if_branch.source}
               else
@@ -48,10 +72,8 @@ module RuboCop
           end
         end
 
-        private
-
-        def offense?(node)
-          node.ternary? && node.multiline?
+        def enforce_single_line_ternary_operator?(node)
+          SINGLE_LINE_TYPES.include?(node.parent.type)
         end
       end
     end
