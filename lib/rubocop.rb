3848ec26004a1244a7d0651a0290a4b58fb7842a
Koichi ITO <koic.ito@gmail.com>
2022-05-16 08:06:02 +0900

    Rename `Gemspec/TestFilesAssignment` to `Gemspec/DeprecatedAttributeAssignment`
    Follow up https://github.com/rubocop/rubocop/pull/10105#issuecomment-924588815.
    
    This PR renames `Gemspec/TestFilesAssignment` to `Gemspec/DeprecatedAttributeAssignment`.
    
    The already released `Gemspec/DateAssignment` can be integrated in the future.
    However that integration is pending as breaking changes cannot be made in RuboCop 1.x.
    
    Anyway, I think #10634 can be able to integrate into the cop of this name.
    
    There is no changelog as this is an unreleased cop naming change.

--- lib/rubocop.rb
+++ lib/rubocop.rb
@@ -162,12 +162,12 @@ require_relative 'rubocop/cop/bundler/ordered_gems'
 
 require_relative 'rubocop/cop/gemspec/date_assignment'
 require_relative 'rubocop/cop/gemspec/dependency_version'
+require_relative 'rubocop/cop/gemspec/deprecated_attribute_assignment'
 require_relative 'rubocop/cop/gemspec/duplicated_assignment'
 require_relative 'rubocop/cop/gemspec/ordered_dependencies'
 require_relative 'rubocop/cop/gemspec/require_mfa'
 require_relative 'rubocop/cop/gemspec/required_ruby_version'
 require_relative 'rubocop/cop/gemspec/ruby_version_globals_usage'
-require_relative 'rubocop/cop/gemspec/test_files_assignment'
 
 require_relative 'rubocop/cop/layout/access_modifier_indentation'
 require_relative 'rubocop/cop/layout/argument_alignment'
