02405b6d64690698ec4693ace04ff51cf0883fa5
Koichi ITO <koic.ito@gmail.com>
2022-05-23 13:17:02 +0900

    [Fix #10656] Mark `Style/RedundantInterpolation` as unsafe autocorrection
    Fixes #10656.
    
    This PR marks `Style/RedundantInterpolation` as unsafe autocorrection
    because when calling a destructive method to string, the resulting
    string may have different behavior or raise `FrozenError`.
    
    ```ruby
    x = 'a'
    y = "#{x}"
    y << 'b'   # return 'ab'
    x          # return 'a'
    y = x.to_s
    y << 'b'   # return 'ab'
    x          # return 'ab'
    
    x = 'a'.freeze
    y = "#{x}"
    y << 'b'   # return 'ab'.
    y = x.to_s
    y << 'b'   # raise `FrozenError`.
    ```
    
    Updating the auto-corrected code from `'a'.to_s` to `'a'.to_s.dup` will get safe, but it
    will make `dup` redundant when unneeded. And This would go against the cop's name "redundant".
    That means replacing it with a different redundancy that cannot be removed by static analysis.
    So I decided to mark it as unsafe.

---
+++ changelog/change_mark_unsafe_autocorrection_for_style_redundant_interpolation.md
@@ -0,0 +1,1 @@
+* [#10656](https://github.com/rubocop/rubocop/issues/10656): Mark `Style/RedundantInterpolation` as unsafe autocorrection. ([@koic][])
--- lib/rubocop/cop/style/redundant_interpolation.rb
+++ lib/rubocop/cop/style/redundant_interpolation.rb
@@ -5,6 +5,27 @@ module RuboCop
     module Style
       # Checks for strings that are just an interpolated expression.
       #
+      # @safety
+      #   Autocorrection is unsafe because when calling a destructive method to string,
+      #   the resulting string may have different behavior or raise `FrozenError`.
+      #
+      #   [source,ruby]
+      #   ----
+      #   x = 'a'
+      #   y = "#{x}"
+      #   y << 'b'   # return 'ab'
+      #   x          # return 'a'
+      #   y = x.to_s
+      #   y << 'b'   # return 'ab'
+      #   x          # return 'ab'
+      #
+      #   x = 'a'.freeze
+      #   y = "#{x}"
+      #   y << 'b'   # return 'ab'.
+      #   y = x.to_s
+      #   y << 'b'   # raise `FrozenError`.
+      #   ----
+      #
       # @example
       #
       #   # bad
