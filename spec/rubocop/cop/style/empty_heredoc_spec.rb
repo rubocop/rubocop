2c4215fa5fc42f4e92184daa52eaa22b56de60b1
Koichi ITO <koic.ito@gmail.com>
2022-07-16 13:12:54 +0900

    Add new `Style/EmptyHeredoc` cop
    ## Summary
    
    Checks for using empty heredoc to reduce redundancy.
    
    ```ruby
    # bad
    <<~EOS
    EOS
    
    <<-EOS
    EOS
    
    <<EOS
    EOS
    
    # good
    ''
    
    # bad
    do_something(<<~EOS)
    EOS
    
    do_something(<<-EOS)
    EOS
    
    do_something(<<EOS)
    EOS
    
    # good
    do_something('')
    ```
    
    ## Other Information
    
    I noticed the following redundancy in the RuboCop test code:
    
    ```ruby
    expect_correction(<<~RUBY)
    RUBY
    ```
    
    This cop solves it generically.

---
+++ spec/rubocop/cop/style/empty_heredoc_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::EmptyHeredoc, :config do
+  it 'registers an offense when using empty `<<~EOS` heredoc' do
+    expect_offense(<<~RUBY)
+      <<~EOS
+      ^^^^^^ Use an empty string literal instead of heredoc.
+      EOS
+    RUBY
+
+    expect_correction(<<~RUBY)
+      ''
+    RUBY
+  end
+
+  it 'registers an offense when using empty `<<-EOS` heredoc' do
+    expect_offense(<<~RUBY)
+      <<-EOS
+      ^^^^^^ Use an empty string literal instead of heredoc.
+      EOS
+    RUBY
+
+    expect_correction(<<~RUBY)
+      ''
+    RUBY
+  end
+
+  it 'registers an offense when using empty `<<EOS` heredoc' do
+    expect_offense(<<~RUBY)
+      <<EOS
+      ^^^^^ Use an empty string literal instead of heredoc.
+      EOS
+    RUBY
+
+    expect_correction(<<~RUBY)
+      ''
+    RUBY
+  end
+
+  it 'registers an offense when using empty heredoc single argument' do
+    expect_offense(<<~RUBY)
+      do_something(<<~EOS)
+                   ^^^^^^ Use an empty string literal instead of heredoc.
+      EOS
+    RUBY
+
+    expect_correction(<<~RUBY)
+      do_something('')
+    RUBY
+  end
+
+  it 'registers an offense when using empty heredoc argument with other argument' do
+    expect_offense(<<~RUBY)
+      do_something(<<~EOS, arg)
+                   ^^^^^^ Use an empty string literal instead of heredoc.
+      EOS
+    RUBY
+
+    expect_correction(<<~RUBY)
+      do_something('', arg)
+    RUBY
+  end
+
+  it 'does not register an offense when using not empty heredoc' do
+    expect_no_offenses(<<~RUBY)
+      <<~EOS
+        Hello.
+      EOS
+    RUBY
+  end
+end
