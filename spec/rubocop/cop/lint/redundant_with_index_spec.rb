58a6577096acc86b5e1eda97c44178c9fb8dda03
Genadi Samokovarov <gsamokovarov@gmail.com>
2022-08-12 14:26:43 +0300

    Fix Lint/RedundantWithIndex with numblocks

--- spec/rubocop/cop/lint/redundant_with_index_spec.rb
+++ spec/rubocop/cop/lint/redundant_with_index_spec.rb
@@ -51,4 +51,32 @@ RSpec.describe RuboCop::Cop::Lint::RedundantWithIndex, :config do
   it 'accepts an index is used as a block argument' do
     expect_no_offenses('ary.each_with_index { |v, i| v; i }')
   end
+
+  context 'Ruby 2.7', :ruby27 do
+    it 'registers an offense for `ary.each_with_index { _1 }` and corrects to `ary.each`' do
+      expect_offense(<<~RUBY)
+        ary.each_with_index { _1 }
+            ^^^^^^^^^^^^^^^ Use `each` instead of `each_with_index`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary.each { _1 }
+      RUBY
+    end
+
+    it 'registers an offense when using `ary.each.with_index { _1 }` and corrects to `ary.each`' do
+      expect_offense(<<~RUBY)
+        ary.each.with_index { _1 }
+                 ^^^^^^^^^^ Remove redundant `with_index`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary.each { _1 }
+      RUBY
+    end
+
+    it 'accepts an index is used as a numblock argument' do
+      expect_no_offenses('ary.each_with_index { _1; _2 }')
+    end
+  end
 end
