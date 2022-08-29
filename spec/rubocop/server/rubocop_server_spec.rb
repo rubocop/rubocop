78eb21e9d9f58ac20fd2d54e1c00c1df28a5eeee
Koichi ITO <koic.ito@gmail.com>
2022-08-30 07:15:00 +0900

    Add a spec for server mode
    ## Summary
    
    This PR adds a spec for server mode that applies .rubocop.yml configuration
    changes even during server startup.
    
    This is already achieved because the server loads .rubocop.yml configuration
    for each analysis request from the client.
    
    ## Other Information
    
    The feature mentioned in the following article has been implemented for
    configuration change of .rubocop.yml.
    
    > You should keep in mind that you'll need to restart the server manually
    > if you change RuboCop's configuration.
    
    https://metaredux.com/posts/2022/07/26/rubocop-serves-much-faster.html
    
    The test this PR adds ensure that behavior.

--- spec/rubocop/server/rubocop_server_spec.rb
+++ spec/rubocop/server/rubocop_server_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe 'rubocop --server', :isolated_environment do # rubocop:disable RS
 
   include_context 'cli spec behavior'
 
+  after do
+    `ruby -I . "#{rubocop}" --stop-server`
+  end
+
   if RuboCop::Server.support_server?
     context 'when using `--server` option after updating RuboCop' do
       before do
@@ -14,10 +18,6 @@ RSpec.describe 'rubocop --server', :isolated_environment do # rubocop:disable RS
         # Emulating RuboCop updates. `0.99.9` is a version value for testing that
         # will never be used in the real world RuboCop version.
         RuboCop::Server::Cache.write_version_file('0.99.9')
-      end
-
-      after do
-        `ruby -I . "#{rubocop}" --stop-server`
       end
 
       it 'displays a restart information message' do
@@ -35,6 +35,47 @@ RSpec.describe 'rubocop --server', :isolated_environment do # rubocop:disable RS
         expect(`ruby -I . "#{rubocop}" #{options}`).to start_with(
           'RuboCop version incompatibility found, RuboCop server restarting...'
         )
+      end
+    end
+
+    context 'when using `--server` option after running server and updating configuration' do
+      it 'applies .rubocop.yml configuration changes even during server startup' do
+        # FIXME: Avoid flaky test for RSpec 4. It may be related that test-queue is not available.
+        # https://github.com/rubocop/rubocop/pull/10806#discussion_r918415067
+        skip if ENV['GITHUB_JOB'] == 'rspec4'
+
+        create_file('example.rb', <<~RUBY)
+          x = 0
+          puts x
+        RUBY
+
+        # Disable `Style/FrozenStringLiteralComment` cop.
+        create_file('.rubocop.yml', <<~RUBY)
+          AllCops:
+            NewCops: enable
+            SuggestExtensions: false
+          Style/FrozenStringLiteralComment:
+            Enabled: false
+        RUBY
+
+        message = expect(`ruby -I . "#{rubocop}" --server`)
+        message.to start_with('RuboCop server starting on ')
+        message.to include('no offenses')
+
+        # Enable `Style/FrozenStringLiteralComment` cop.
+        create_file('.rubocop.yml', <<~RUBY)
+          AllCops:
+            NewCops: enable
+            SuggestExtensions: false
+          Style/FrozenStringLiteralComment:
+            Enabled: true
+        RUBY
+
+        message = expect(`ruby -I . "#{rubocop}" --server`)
+        message.not_to start_with('RuboCop server starting on ')
+        message.to include(<<~OFFENSE_DETECTED)
+          Style/FrozenStringLiteralComment: Missing frozen string literal comment.
+        OFFENSE_DETECTED
       end
     end
   end
