f90d7cf98d1c2080e359334a50e60940ad2399ed
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-10-12 16:32:12 -0400

    Fix "led" → "lead" typo

--- lib/rubocop/cop/style/rescue_modifier.rb
+++ lib/rubocop/cop/style/rescue_modifier.rb
@@ -9,7 +9,7 @@ module RuboCop
       # reasons:
       #
       # * The syntax of modifier form `rescue` can be misleading because it
-      #   might led us to believe that `rescue` handles the given exception
+      #   might lead us to believe that `rescue` handles the given exception
       #   but it actually rescue all exceptions to return the given rescue
       #   block. In this case, value returned by handle_error or
       #   SomeException.
