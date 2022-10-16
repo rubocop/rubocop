73320cf1dcd798af6117e1329952449338d93a08
Koichi ITO <koic.ito@gmail.com>
2022-10-16 16:20:12 +0900

    Add new `Style/RedundantEach` cop
    Follow up https://github.com/rubocop/rubocop-rails/pull/812/files#r994033301.
    
    This PR adds new `Style/RedundantEach` cop that checks for redundant `each`.
    
    This cop is unsafe, as it can produce false positives if the receiver
    is not an `Enumerable`.
    
    ```ruby
    # bad
    array.each.each { |v| do_something(v) }
    
    # good
    array.each { |v| do_something(v) }
    
    # bad
    array.each.each_with_index { |v, i| do_something(v, i) }
    
    # good
    array.each.with_index { |v, i| do_something(v, i) }
    array.each_with_index { |v, i| do_something(v, i) }
    
    # bad
    array.each.each_with_object { |v, o| do_something(v, o) }
    
    # good
    array.each.with_object { |v, o| do_something(v, o) }
    array.each_with_object { |v, o| do_something(v, o) }
    ```

---
+++ changelog/new_add_new_style_redundant_each_cop.md
@@ -0,0 +1,1 @@
+* [#11110](https://github.com/rubocop/rubocop/pull/11110): Add new `Style/RedundantEach` cop. ([@koic][])
---
+++ spec/rubocop/cop/style/redundant_each_spec.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::RedundantEach, :config do
+  it 'registers an offense when using `each.each`' do
+    expect_offense(<<~RUBY)
+      array.each.each { |v| do_something(v) }
+           ^^^^^ Remove redundant `each`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      array.each { |v| do_something(v) }
+    RUBY
+  end
+
+  it 'registers an offense when using `each.each_with_index`' do
+    expect_offense(<<~RUBY)
+      array.each.each_with_index { |v| do_something(v) }
+           ^^^^^ Remove redundant `each`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      array.each.with_index { |v| do_something(v) }
+    RUBY
+  end
+
+  it 'registers an offense when using `each.each_with_object`' do
+    expect_offense(<<~RUBY)
+      array.each.each_with_object([]) { |v, o| do_something(v, o) }
+           ^^^^^ Remove redundant `each`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      array.each.with_object([]) { |v, o| do_something(v, o) }
+    RUBY
+  end
+
+  it 'registers an offense when using a method starting with `each_` with `each_with_index`' do
+    expect_offense(<<~RUBY)
+      context.each_child_node.each_with_index.any? do |node, index|
+                              ^^^^^^^^^^^^^^^ Use `with_index` to remove redundant `each`.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      context.each_child_node.with_index.any? do |node, index|
+      end
+    RUBY
+  end
+
+  it 'registers an offense when using a method starting with `each_` with `each_with_object`' do
+    expect_offense(<<~RUBY)
+      context.each_child_node.each_with_object([]) do |node, object|
+                              ^^^^^^^^^^^^^^^^ Use `with_object` to remove redundant `each`.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      context.each_child_node.with_object([]) do |node, object|
+      end
+    RUBY
+  end
+
+  it 'registers an offense when using `reverse_each.each`' do
+    expect_offense(<<~RUBY)
+      context.reverse_each.each { |i| do_something(i) }
+                          ^^^^^ Remove redundant `each`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      context.reverse_each { |i| do_something(i) }
+    RUBY
+  end
+
+  it 'registers an offense when using `each.reverse_each`' do
+    expect_offense(<<~RUBY)
+      context.each.reverse_each { |i| do_something(i) }
+             ^^^^^ Remove redundant `each`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      context.reverse_each { |i| do_something(i) }
+    RUBY
+  end
+
+  it 'registers an offense when using `reverse_each.each_with_index`' do
+    expect_offense(<<~RUBY)
+      context.reverse_each.each_with_index.any? do |node, index|
+                           ^^^^^^^^^^^^^^^ Use `with_index` to remove redundant `each`.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      context.reverse_each.with_index.any? do |node, index|
+      end
+    RUBY
+  end
+
+  it 'registers an offense when using `reverse_each.each_with_object`' do
+    expect_offense(<<~RUBY)
+      scope_stack.reverse_each.each_with_object([]) do |scope, variables|
+                               ^^^^^^^^^^^^^^^^ Use `with_object` to remove redundant `each`.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      scope_stack.reverse_each.with_object([]) do |scope, variables|
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using only single `each`' do
+    expect_no_offenses(<<~RUBY)
+      array.each { |v| do_something(v) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each.with_index`' do
+    expect_no_offenses(<<~RUBY)
+      array.each.with_index { |v, i| do_something(v, i) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each_with_index`' do
+    expect_no_offenses(<<~RUBY)
+      array.each_with_index { |v, i| do_something(v, i) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each.with_object`' do
+    expect_no_offenses(<<~RUBY)
+      array.each.with_object { |v, o| do_something(v, o) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each_with_object`' do
+    expect_no_offenses(<<~RUBY)
+      array.each_with_object { |v, o| do_something(v, o) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each_with_index.reverse_each`' do
+    expect_no_offenses(<<~RUBY)
+      array.each_with_index.reverse_each do |value, index|
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `each_ancestor.each`' do
+    expect_no_offenses(<<~RUBY)
+      node.each_ancestor(:def, :defs, :block).each do |ancestor|
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `reverse_each {}.each {}`' do
+    expect_no_offenses(<<~RUBY)
+      array.reverse_each { |i| foo(i) }.each { |i| bar(i) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each {}.reverse_each {}`' do
+    expect_no_offenses(<<~RUBY)
+      array.each { |i| foo(i) }.reverse_each { |i| bar(i) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each {}.each_with_index {}`' do
+    expect_no_offenses(<<~RUBY)
+      array.each { |i| foo(i) }.each_with_index { |i| bar(i) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each {}.each_with_object([]) {}`' do
+    expect_no_offenses(<<~RUBY)
+      array.each { |i| foo(i) }.each_with_object([]) { |i| bar(i) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `each_foo {}.each_with_object([]) {}`' do
+    expect_no_offenses(<<~RUBY)
+      array.each_foo { |i| foo(i) }.each_with_object([]) { |i| bar(i) }
+    RUBY
+  end
+end
