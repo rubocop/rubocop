5e267b6c2b9a1ee40a4cc5bc1d589902b98a5578
Ryo Nakamura <r7kamura@gmail.com>
2022-11-05 08:24:41 +0900

    Add `RuboCop::Cop::RangeHelp#range_with_comments_and_lines`

--- lib/rubocop/cop/mixin/range_help.rb
+++ lib/rubocop/cop/mixin/range_help.rb
@@ -126,6 +126,29 @@ module RuboCop
         pos += size * step while condition && src[pos + offset, size] == needle
         pos.negative? ? 0 : pos
       end
+
+      def range_with_comments_and_lines(node)
+        range_by_whole_lines(range_with_comments(node), include_final_newline: true)
+      end
+
+      def range_with_comments(node)
+        ranges = [
+          node,
+          *@processed_source.ast_with_comments[node]
+        ].map do |element|
+          element.location.expression
+        end
+        ranges.reduce do |result, range|
+          add_range(result, range)
+        end
+      end
+
+      def add_range(range1, range2)
+        range1.with(
+          begin_pos: [range1.begin_pos, range2.begin_pos].min,
+          end_pos: [range1.end_pos, range2.end_pos].max
+        )
+      end
     end
   end
 end
--- lib/rubocop/cop/style/access_modifier_declarations.rb
+++ lib/rubocop/cop/style/access_modifier_declarations.rb
@@ -202,31 +202,7 @@ module RuboCop
         end
 
         def remove_node(corrector, node)
-          corrector.remove(
-            range_by_whole_lines(
-              range_with_comments(node),
-              include_final_newline: true
-            )
-          )
-        end
-
-        def range_with_comments(node)
-          ranges = [
-            node,
-            *processed_source.ast_with_comments[node]
-          ].map do |element|
-            element.location.expression
-          end
-          ranges.reduce do |result, range|
-            add_range(result, range)
-          end
-        end
-
-        def add_range(range1, range2)
-          range1.with(
-            begin_pos: [range1.begin_pos, range2.begin_pos].min,
-            end_pos: [range1.end_pos, range2.end_pos].max
-          )
+          corrector.remove(range_with_comments_and_lines(node))
         end
       end
     end
