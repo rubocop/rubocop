8f334c086e893fe96e7143c9acb879c093b45357
Koichi ITO <koic.ito@gmail.com>
2022-12-27 19:56:46 +0900

    Add new `Style/MinMaxComparison` cop
    This PR adds new `Style/MinMaxComparison` cop.
    
    This cop Enforces the use of `max` or `min` instead of comparison for greater or less.
    It is unsafe because even if a value has `<` or `>` method, it is not necessarily `Comparable`.
    
    ```ruby
    # bad
    a > b ? a : b
    a >= b ? a : b
    
    # good
    [a, b].max
    
    # bad
    a < b ? a : b
    a <= b ? a : b
    
    # good
    [a, b].min
    ```

---
+++ spec/rubocop/cop/style/min_max_comparison_spec.rb
@@ -0,0 +1,154 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::MinMaxComparison, :config do
+  it 'registers and corrects an offense when using `a > b ? a : b`' do
+    expect_offense(<<~RUBY)
+      a > b ? a : b
+      ^^^^^^^^^^^^^ Use `[a, b].max` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].max
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a >= b ? a : b`' do
+    expect_offense(<<~RUBY)
+      a >= b ? a : b
+      ^^^^^^^^^^^^^^ Use `[a, b].max` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].max
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a < b ? b : a`' do
+    expect_offense(<<~RUBY)
+      a < b ? b : a
+      ^^^^^^^^^^^^^ Use `[a, b].max` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].max
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a <= b ? b : a`' do
+    expect_offense(<<~RUBY)
+      a <= b ? b : a
+      ^^^^^^^^^^^^^^ Use `[a, b].max` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].max
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a < b ? a : b`' do
+    expect_offense(<<~RUBY)
+      a < b ? a : b
+      ^^^^^^^^^^^^^ Use `[a, b].min` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].min
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a <= b ? a : b`' do
+    expect_offense(<<~RUBY)
+      a <= b ? a : b
+      ^^^^^^^^^^^^^^ Use `[a, b].min` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].min
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a > b ? b : a`' do
+    expect_offense(<<~RUBY)
+      a > b ? b : a
+      ^^^^^^^^^^^^^ Use `[a, b].min` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].min
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a >= b ? b : a`' do
+    expect_offense(<<~RUBY)
+      a >= b ? b : a
+      ^^^^^^^^^^^^^^ Use `[a, b].min` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].min
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a > b a : b` with `if/else`' do
+    expect_offense(<<~RUBY)
+      if a > b
+      ^^^^^^^^ Use `[a, b].max` instead.
+        a
+      else
+        b
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].max
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `a < b a : b` with `if/else`' do
+    expect_offense(<<~RUBY)
+      if a < b
+      ^^^^^^^^ Use `[a, b].min` instead.
+        a
+      else
+        b
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      [a, b].min
+    RUBY
+  end
+
+  it 'does not register an offense when using `a > b ? c : d`' do
+    expect_no_offenses(<<~RUBY)
+      a > b ? c : d
+    RUBY
+  end
+
+  it 'does not register an offense when using `condition ? a : b`' do
+    expect_no_offenses(<<~RUBY)
+      condition ? a : b
+    RUBY
+  end
+
+  it 'does not register an offense when using `elsif`' do
+    expect_no_offenses(<<~RUBY)
+      if a
+      elsif foo(b)
+        b
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `[a, b].max`' do
+    expect_no_offenses(<<~RUBY)
+      [a, b].max
+    RUBY
+  end
+
+  it 'does not register an offense when using `[a, b].min`' do
+    expect_no_offenses(<<~RUBY)
+      [a, b].min
+    RUBY
+  end
+end
