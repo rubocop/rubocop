aa03bb9883ba5ad034eb8930ab643afd0df05584
Koichi ITO <koic.ito@gmail.com>
2022-12-31 14:25:32 +0900

    [Fix 11357] Tweak the doc for `Security/CompoundHash`
    `Array#hash` is not always fast:
    
    ```ruby
    puts `ruby -v`
    
    require 'benchmark/ips'
    
    Benchmark.ips do |x|
      x.report('hash') { [15, 0].hash }
      x.report('^')    { 15 ^ 0 }
    
      x.compare!
    end
    ```
    
    ```console
    % ruby compound_hash
    "ruby 3.2.0 (2022-12-25 revision a528908271) [x86_64-darwin19]\n"
    Warming up --------------------------------------
                    hash   964.061k i/100ms
                       ^     1.748M i/100ms
    Calculating -------------------------------------
                    hash      9.192M (± 5.1%) i/s -     46.275M in   5.049622s
                        ^     17.161M (± 4.1%) i/s -     85.676M in   5.001648s
    
    Comparison:
                       ^: 17160606.2 i/s
                    hash:  9192284.5 i/s - 1.87x  (± 0.00) slower
    ```

--- lib/rubocop/cop/security/compound_hash.rb
+++ lib/rubocop/cop/security/compound_hash.rb
@@ -9,7 +9,8 @@ module RuboCop
       # Manually combining hashes is error prone and hard to follow, especially
       # when there are many values. Poor implementations may also introduce
       # performance or security concerns if they are prone to collisions.
-      # Delegating to `Array#hash` is clearer, faster, and safer.
+      # Delegating to `Array#hash` is clearer and safer, although it might be slower
+      # depending on the use case.
       #
       # @safety
       #   This cop may be unsafe if the application logic depends on the hash
