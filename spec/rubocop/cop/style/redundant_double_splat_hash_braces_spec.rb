2e4991295a116485a46e6596f88dbf28e4b5268b
Koichi ITO <koic.ito@gmail.com>
2023-01-14 03:48:11 +0900

    Fix an incorrect autocorrect for `Style/RedundantDoubleSplatHashBraces`
    This PR fixes the following incorrect autocorrect for `Style/RedundantDoubleSplatHashBraces`
    when using nested double splat hash braces:
    
    ```ruby
    do_something(**{foo: bar, **{baz: qux}})
    ```
    
    ```console
    % rubocop --only Style/RedundantDoubleSplatHashBraces -a
    (snip)
    
    example.rb:1:14: C: [Corrected] Style/RedundantDoubleSplatHashBraces:
    Remove the redundant double splat and braces, use keyword arguments directly.
    do_something(**{foo: bar, **{baz: qux}})
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    1 file inspected, 1 offense detected, 1 offense corrected
    ```
    
    Before:
    
    ```diff
    -do_something(**{foo: bar, **{baz: qux}})
    +do_something(foo: bar)
    ```
    
    After:
    
    ```diff
    -do_something(**{foo: bar, **{baz: qux}})
    +do_something(foo: bar, baz: qux)
    ```

--- spec/rubocop/cop/style/redundant_double_splat_hash_braces_spec.rb
+++ spec/rubocop/cop/style/redundant_double_splat_hash_braces_spec.rb
@@ -12,6 +12,18 @@ RSpec.describe RuboCop::Cop::Style::RedundantDoubleSplatHashBraces, :config do
     RUBY
   end
 
+  it 'registers an offense when using nested double splat hash braces' do
+    expect_offense(<<~RUBY)
+      do_something(**{foo: bar, **{baz: qux}})
+                                ^^^^^^^^^^^^ Remove the redundant double splat and braces, use keyword arguments directly.
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^ Remove the redundant double splat and braces, use keyword arguments directly.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      do_something(foo: bar, baz: qux)
+    RUBY
+  end
+
   it 'registers an offense when using double splat in double splat hash braces' do
     expect_offense(<<~RUBY)
       do_something(**{foo: bar, **options})
@@ -32,6 +44,12 @@ RSpec.describe RuboCop::Cop::Style::RedundantDoubleSplatHashBraces, :config do
   it 'does not register an offense when using empty double splat hash braces arguments' do
     expect_no_offenses(<<~RUBY)
       do_something(**{})
+    RUBY
+  end
+
+  it 'does not register an offense when using empty hash braces arguments' do
+    expect_no_offenses(<<~RUBY)
+      do_something({})
     RUBY
   end
 
