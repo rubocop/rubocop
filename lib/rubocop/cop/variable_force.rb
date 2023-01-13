b48ebef423f74c3083bf2cd7ab79e8546f28a95e
Koichi ITO <koic.ito@gmail.com>
2023-01-14 06:13:52 +0900

    [Fix #11444] Fix a false positive for `Lint/ShadowingOuterLocalVariable`
    Fixes #11444.
    
    This PR fixes a false positive for `Lint/ShadowingOuterLocalVariable`
    when using numbered block parameter.
    
    It also fixes the following error as a side effect:
    
    ```ruby
    array.each {
      bar = _1
    }.each { |bar|
    }
    ```
    
    ```console
    % bundle exec rubocop example.rb --only Lint/ShadowingOuterLocalVariable -d
    (snip)
    
    Scanning /Users/koic/src/github.com/koic/rubocop-issues/11448/example.rb
    An error occurred while VariableForce cop was inspecting /Users/koic/src/github.com/koic/rubocop-issues/11448/example.rb.
    undefined method `when_type?' for nil:NilClass
    /Users/koic/src/github.com/rubocop/rubocop/lib/rubocop/cop/lint/shadowing_outer_local_variable.rb:86:in `variable_node'
    ```

--- lib/rubocop/cop/variable_force.rb
+++ lib/rubocop/cop/variable_force.rb
@@ -50,7 +50,7 @@ module RuboCop
 
       ZERO_ARITY_SUPER_TYPE = :zsuper
 
-      TWISTED_SCOPE_TYPES = %i[block class sclass defs module].freeze
+      TWISTED_SCOPE_TYPES = %i[block numblock class sclass defs module].freeze
       SCOPE_TYPES = (TWISTED_SCOPE_TYPES + [:def]).freeze
 
       SEND_TYPE = :send
