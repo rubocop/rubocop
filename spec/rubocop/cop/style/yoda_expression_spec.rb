9b1a9ae025112ecfb9336c8b1b908efc688f67a7
Koichi ITO <koic.ito@gmail.com>
2023-01-28 01:37:58 +0900

    Fix a false negative for `Style/YodaExpression`
    Follow up https://github.com/rubocop/rubocop/pull/11512#issuecomment-1408044549.
    
    This PR fixes a false negative for `Style/YodaExpression` when using constant.

--- spec/rubocop/cop/style/yoda_expression_spec.rb
+++ spec/rubocop/cop/style/yoda_expression_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe RuboCop::Cop::Style::YodaExpression, :config do
   let(:cop_config) { { 'SupportedOperators' => ['*', '+'] } }
 
-  it 'registers an offense when using simple offended example' do
+  it 'registers an offense when numeric literal on left' do
     expect_offense(<<~RUBY)
       1 + x
       ^^^^^ Non-literal operand (`x`) should be first.
@@ -14,7 +14,18 @@ RSpec.describe RuboCop::Cop::Style::YodaExpression, :config do
     RUBY
   end
 
-  it 'registers an offense and corrects when using complex offended example' do
+  it 'registers an offense when constant on left' do
+    expect_offense(<<~RUBY)
+      CONST + x
+      ^^^^^^^^^ Non-literal operand (`x`) should be first.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      x + CONST
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using complex use of numeric literals' do
     expect_offense(<<~RUBY)
       2 + (1 + x)
       ^^^^^^^^^^^ Non-literal operand (`(1 + x)`) should be first.
@@ -25,16 +36,32 @@ RSpec.describe RuboCop::Cop::Style::YodaExpression, :config do
     RUBY
   end
 
-  it 'accepts numeric on the right' do
-    expect_no_offenses(<<~RUBY)
-      1 + 2
-      1 + 2.2
+  it 'registers an offense and corrects when using complex use of constants' do
+    expect_offense(<<~RUBY)
+      TWO + (ONE + x)
+      ^^^^^^^^^^^^^^^ Non-literal operand (`(ONE + x)`) should be first.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      (x + ONE) + TWO
     RUBY
   end
 
-  it 'accepts non integer' do
+  it 'accepts numeric literal on the right' do
     expect_no_offenses(<<~RUBY)
-      x + 1
+      x + 42
+    RUBY
+  end
+
+  it 'accepts constant on the right' do
+    expect_no_offenses(<<~RUBY)
+      x + FORTY_TWO
+    RUBY
+  end
+
+  it 'accepts neither numeric literal nor constant' do
+    expect_no_offenses(<<~RUBY)
+      x + y
     RUBY
   end
 
