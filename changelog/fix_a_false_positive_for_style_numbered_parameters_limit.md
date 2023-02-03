c1f188644fba1aaa7efa7703a4a73c46f8a59604
Koichi ITO <koic.ito@gmail.com>
2023-02-03 18:17:18 +0900

    [Fix #11535] Fix a false positive for `Style/NumberedParametersLimit`
    Fixes #11535.
    
    This PR fixes a false positive for `Style/NumberedParametersLimit`
    when only `_2` or higher numbered parameter is used.

---
+++ changelog/fix_a_false_positive_for_style_numbered_parameters_limit.md
@@ -0,0 +1,1 @@
+* [#11535](https://github.com/rubocop/rubocop/issues/11535): Fix a false positive for `Style/NumberedParametersLimit` when only `_2` or higher numbered parameter is used. ([@koic][])
--- spec/rubocop/cop/style/numbered_parameters_limit_spec.rb
+++ spec/rubocop/cop/style/numbered_parameters_limit_spec.rb
@@ -54,6 +54,31 @@ RSpec.describe RuboCop::Cop::Style::NumberedParametersLimit, :config do
     context 'when Max is 1' do
       let(:max) { 1 }
 
+      it 'does not register an offense when only numbered parameter `_1` is used once' do
+        expect_no_offenses(<<~RUBY)
+          foo { do_something(_1) }
+        RUBY
+      end
+
+      it 'does not register an offense when only numbered parameter `_1` is used twice' do
+        expect_no_offenses(<<~RUBY)
+          foo { do_something(_1, _1) }
+        RUBY
+      end
+
+      it 'does not register an offense when only numbered parameter `_9` is used once' do
+        expect_no_offenses(<<~RUBY)
+          foo { do_something(_9) }
+        RUBY
+      end
+
+      it 'does not register an offense when using numbered parameter with underscored local variable' do
+        expect_no_offenses(<<~RUBY)
+          _lvar = 42
+          foo { do_something(_2, _lvar) }
+        RUBY
+      end
+
       it 'uses the right offense message' do
         expect_offense(<<~RUBY)
           foo { do_something(_1, _2, _3, _4, _5) }
