rubocop/rubocop

commit 767b1f6b73bf4c1f2c636332e03224bb9205dcec

Fix missing parentheses in shorthand hash syntax as argument calls
Previously, `Style/HashSyntax` would autocorrect the code:

```ruby
raise Errors.new 'No!', foo: foo
```
to 👇:

```ruby
raise Errors.new 'No!', foo:
```

While it may lead to valid code _in this case_, parentheses around
`Error.new` are required in edge cases like condition that we can inline
to a modifier form after the applying the shorthand hash syntax.

In this example:

```ruby
if condition?
  raise Error.new 'A long error message...', foo: foo
end
```

we would autocorrect to invalid Ruby code:

```ruby
raise Error.new 'A long error message...', foo: if condition?
```

Putting parentheses in method calls that return value as an argument in
another call is the safest autocorrection in this case. I'm starting to
wonder whether we always put the parentheses in the method calls.

lib/rubocop/cop/mixin/hash_shorthand_syntax.rb
@@ -95,16 +95,19 @@ module RuboCop
           use_modifier_form_without_parenthesized_method_call?(method_dispatch_node)
       end
 
+      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
       def def_node_that_require_parentheses(node)
         last_pair = node.parent.pairs.last
         return unless last_pair.key.source == last_pair.value.source
-        return unless (method_dispatch_node = find_ancestor_method_dispatch_node(node))
-        return unless without_parentheses_call_expr_follows?(method_dispatch_node)
+        return unless (dispatch_node = find_ancestor_method_dispatch_node(node))
+        return if node.respond_to?(:parenthesized?) && !node.parenthesized?
+        return unless last_expression?(dispatch_node) || method_dispatch_as_argument?(dispatch_node)
 
         def_node = node.each_ancestor(:send, :csend, :super, :yield).first
 
         DefNode.new(def_node) unless def_node && def_node.arguments.empty?
       end
+      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
       def find_ancestor_method_dispatch_node(node)
         return unless (ancestor = node.parent.parent)
@@ -129,15 +132,16 @@ module RuboCop
         ancestor.ancestors.any? { |node| node.respond_to?(:modifier_form?) && node.modifier_form? }
       end
 
-      def without_parentheses_call_expr_follows?(ancestor)
-        return false unless ancestor.respond_to?(:parenthesized?) && !ancestor.parenthesized?
+      def last_expression?(ancestor)
+        ancestor.right_sibling ||
+          ancestor.each_ancestor.find { |node| node.assignment? || node.send_type? }&.right_sibling
+      end
 
-        right_sibling = ancestor.right_sibling
-        right_sibling ||= ancestor.each_ancestor.find do |node|
-          node.assignment? || node.send_type?
-        end&.right_sibling
+      def method_dispatch_as_argument?(method_dispatch_node)
+        parent = method_dispatch_node.parent
+        return false unless parent
 
-        !!right_sibling
+        parent.call_type? || parent.super_type? || parent.yield_type?
       end
 
       def breakdown_value_types_of_hash(hash_node)

spec/rubocop/cop/style/hash_syntax_spec.rb
@@ -1385,6 +1385,21 @@ RSpec.describe RuboCop::Cop::Style::HashSyntax, :config do
         RUBY
       end
 
+      it 'registers an offense in arguments as method calls with hash omissions' do
+        expect_offense(<<~RUBY)
+          if condition?
+            raise LongLongLongLongError.new 'A long, long, long, long, really long message', foo: foo
+                                                                                                  ^^^ Omit the hash value.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          if condition?
+            raise LongLongLongLongError.new('A long, long, long, long, really long message', foo:)
+          end
+        RUBY
+      end
+
       context 'when hash roket syntax' do
         let(:enforced_style) { 'hash_rockets' }
 

commit f5f2cb1833f9abdbad30d95bc7d6ee45cea6705f

Fix a broken shorthand syntax autocorrection
Calls like `foo(bar: bar).baz` were autocorrected to `foo(bar:)).baz`.
Notice the extra parentheses. I have broken this in #11516.

lib/rubocop/cop/mixin/hash_shorthand_syntax.rb
@@ -95,19 +95,19 @@ module RuboCop
           use_modifier_form_without_parenthesized_method_call?(method_dispatch_node)
       end
 
-      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
       def def_node_that_require_parentheses(node)
         last_pair = node.parent.pairs.last
         return unless last_pair.key.source == last_pair.value.source
         return unless (dispatch_node = find_ancestor_method_dispatch_node(node))
-        return if node.respond_to?(:parenthesized?) && !node.parenthesized?
+        return if dispatch_node.parenthesized?
         return unless last_expression?(dispatch_node) || method_dispatch_as_argument?(dispatch_node)
 
         def_node = node.each_ancestor(:send, :csend, :super, :yield).first
 
         DefNode.new(def_node) unless def_node && def_node.arguments.empty?
       end
-      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity
 
       def find_ancestor_method_dispatch_node(node)
         return unless (ancestor = node.parent.parent)

spec/rubocop/cop/style/hash_syntax_spec.rb
@@ -1090,6 +1090,28 @@ RSpec.describe RuboCop::Cop::Style::HashSyntax, :config do
         RUBY
       end
 
+      it 'registers an offense in chained calls' do
+        expect_offense(<<~RUBY)
+          create(:foo, bar: bar).baz
+                            ^^^ Omit the hash value.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          create(:foo, bar:).baz
+        RUBY
+      end
+
+      it 'registers an offense in chained calls with dispatch keywords' do
+        expect_offense(<<~RUBY)
+          yield(:foo, bar: bar).baz
+                           ^^^ Omit the hash value.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          yield(:foo, bar:).baz
+        RUBY
+      end
+
       it 'registers an offense when without parentheses call expr follows after nested method call' do
         # Add parentheses to prevent syntax errors shown in the URL: https://bugs.ruby-lang.org/issues/18396
         expect_offense(<<~RUBY)