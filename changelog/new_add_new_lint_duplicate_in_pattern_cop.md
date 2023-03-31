f8417db007bb28db8c8a7dc8e4561b88f7bce759
Koichi ITO <koic.ito@gmail.com>
2023-04-01 03:45:30 +0900

    Add new `Lint/DuplicateMatchPattern` cop
    This PR adds new `Lint/DuplicateMatchPattern` cop.
    It checks that there are no repeated patterns used in `in` keywords.
    
    ```ruby
    # bad
    case x
    in 'first'
      do_something
    in 'first'
      do_something_else
    end
    
    # good
    case x
    in 'first'
      do_something
    in 'second'
      do_something_else
    end
    ```
    
    I was wondering whether to extend existing `Lint/DuplicateCaseCondition`,
    but it is a different inspection target from the concept named "`case` condition".
    So, I implemented it as a new `Lint/DuplicateMatchPattern` for "match pattern".

---
+++ changelog/new_add_new_lint_duplicate_in_pattern_cop.md
@@ -0,0 +1,1 @@
+* [#11749](https://github.com/rubocop/rubocop/pull/11749): Add new `Lint/DuplicateMatchPattern` cop. ([@koic][])
---
+++ spec/rubocop/cop/lint/duplicate_match_pattern_spec.rb
@@ -0,0 +1,171 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Lint::DuplicateMatchPattern, :config, :ruby27 do
+  it 'registers an offense for repeated `in` patterns' do
+    expect_offense(<<~RUBY)
+      case x
+      in foo
+        first_method
+      in bar
+        second_method
+      in foo
+         ^^^ Duplicate `in` pattern detected.
+        third_method
+      end
+    RUBY
+  end
+
+  it 'registers an offense for subsequent repeated `in` patterns' do
+    expect_offense(<<~RUBY)
+      case x
+      in foo
+        first_method
+      in foo
+         ^^^ Duplicate `in` pattern detected.
+        second_method
+      end
+    RUBY
+  end
+
+  it 'registers multiple offenses for multiple repeated `in` patterns' do
+    expect_offense(<<~RUBY)
+      case x
+      in foo
+        first_method
+      in bar
+        second_method
+      in foo
+         ^^^ Duplicate `in` pattern detected.
+        third_method
+      in bar
+         ^^^ Duplicate `in` pattern detected.
+        fourth_method
+      end
+    RUBY
+  end
+
+  it 'registers multiple offenses for repeated alternative patterns' do
+    expect_offense(<<~RUBY)
+      case x
+      in foo | bar
+        first_method
+      in bar | foo
+         ^^^^^^^^^ Duplicate `in` pattern detected.
+        second_method
+      end
+    RUBY
+  end
+
+  it 'does not register for not equivalent alternative patterns' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in foo | bar | baz
+        first_method
+      in foo | bar
+        second_method
+      end
+    RUBY
+  end
+
+  it 'registers an offense for repeated array patterns with elements in the same order' do
+    expect_offense(<<~RUBY)
+      case x
+      in [foo, bar]
+        first_method
+      in [foo, bar]
+         ^^^^^^^^^^ Duplicate `in` pattern detected.
+        second_method
+      end
+    RUBY
+  end
+
+  it 'does not register an offense for repeated array patterns with elements in different order' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in [foo, bar]
+        first_method
+      in [bar, foo]
+        second_method
+      end
+    RUBY
+  end
+
+  it 'does not register similar but not equivalent && array patterns' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in [foo, bar]
+        first_method
+      in [foo, bar, baz]
+        second_method
+      end
+    RUBY
+  end
+
+  it 'registers an offense for repeated hash patterns with elements in the same order' do
+    expect_offense(<<~RUBY)
+      case x
+      in foo: a, bar: b
+        first_method
+      in foo: a, bar: b
+         ^^^^^^^^^^^^^^ Duplicate `in` pattern detected.
+        second_method
+      end
+    RUBY
+  end
+
+  it 'registers an offense for repeated hash patterns with elements in different order' do
+    expect_offense(<<~RUBY)
+      case x
+      in foo: a, bar: b
+        first_method
+      in bar: b, foo: a
+         ^^^^^^^^^^^^^^ Duplicate `in` pattern detected.
+        second_method
+      end
+    RUBY
+  end
+
+  it 'does not register similar but not equivalent && hash patterns' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in foo: a, bar: b
+        first_method
+      in foo: a, bar: b, baz: c
+        second_method
+      end
+    RUBY
+  end
+
+  it 'does not register trivial `in` patterns' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in false
+        first_method
+      end
+    RUBY
+  end
+
+  it 'does not register non-redundant `in` patterns' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in foo
+        first_method
+      in bar
+        second_method
+      end
+    RUBY
+  end
+
+  it 'does not register non-redundant `in` patterns with an else clause' do
+    expect_no_offenses(<<~RUBY)
+      case x
+      in foo
+        method_name
+      in bar
+        second_method
+      else
+        third_method
+      end
+    RUBY
+  end
+end
