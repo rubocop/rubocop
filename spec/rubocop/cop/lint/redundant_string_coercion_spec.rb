d3d70616a4747bf33ff023a2c7485383693fcaf1
Koichi ITO <koic.ito@gmail.com>
2023-04-10 02:43:49 +0900

    Make `Lint/RedundantStringCoercion` aware of print method arguments
    This PR makes `Lint/RedundantStringCoercion` aware of print method arguments.
    The name of this cop is "RedundantStringCoercion", so it makes more sense to
    extend the feature than to provide a new cop I think.

--- spec/rubocop/cop/lint/redundant_string_coercion_spec.rb
+++ spec/rubocop/cop/lint/redundant_string_coercion_spec.rb
@@ -54,4 +54,88 @@ RSpec.describe RuboCop::Cop::Lint::RedundantStringCoercion, :config do
   it 'does not explode on empty interpolation' do
     expect_no_offenses('"this is #{} silly"')
   end
+
+  it 'registers an offense and corrects `to_s` in `print` arguments' do
+    expect_offense(<<~RUBY)
+      print first.to_s, second.to_s
+                  ^^^^ Redundant use of `Object#to_s` in `print`.
+                               ^^^^ Redundant use of `Object#to_s` in `print`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      print first, second
+    RUBY
+  end
+
+  it 'registers an offense and corrects `to_s` in `print` arguments without receiver' do
+    expect_offense(<<~RUBY)
+      print to_s, to_s
+            ^^^^ Use `self` instead of `Object#to_s` in `print`.
+                  ^^^^ Use `self` instead of `Object#to_s` in `print`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      print self, self
+    RUBY
+  end
+
+  it 'registers an offense and corrects `to_s` in `puts` arguments' do
+    expect_offense(<<~RUBY)
+      puts first.to_s, second.to_s
+                 ^^^^ Redundant use of `Object#to_s` in `puts`.
+                              ^^^^ Redundant use of `Object#to_s` in `puts`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      puts first, second
+    RUBY
+  end
+
+  it 'registers an offense and corrects `to_s` in `warn` arguments' do
+    expect_offense(<<~RUBY)
+      warn first.to_s, second.to_s
+                 ^^^^ Redundant use of `Object#to_s` in `warn`.
+                              ^^^^ Redundant use of `Object#to_s` in `warn`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      warn first, second
+    RUBY
+  end
+
+  it 'registers an offense and corrects `to_s` in `puts` arguments without receiver' do
+    expect_offense(<<~RUBY)
+      puts to_s, to_s
+           ^^^^ Use `self` instead of `Object#to_s` in `puts`.
+                 ^^^^ Use `self` instead of `Object#to_s` in `puts`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      puts self, self
+    RUBY
+  end
+
+  it 'does not register an offense when not using `to_s` in `print` arguments' do
+    expect_no_offenses(<<~RUBY)
+      print obj.do_something
+    RUBY
+  end
+
+  it 'does not register an offense when not using `to_s` in `puts` arguments' do
+    expect_no_offenses(<<~RUBY)
+      puts obj.do_something
+    RUBY
+  end
+
+  it 'does not register an offense when using `to_s` in `p` arguments' do
+    expect_no_offenses(<<~RUBY)
+      p first.to_s, second.to_s
+    RUBY
+  end
+
+  it 'does not register an offense when using `to_s` in `print` arguments with receiver' do
+    expect_no_offenses(<<~RUBY)
+      obj.print first.to_s, second.to_s
+    RUBY
+  end
 end
