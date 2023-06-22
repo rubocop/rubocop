rubocop/rubocop

commit c9ccb2f35c752b42c3e87f926ee7ed74fd26dca8

Change include paths to be relative to working directory rather than derived dir

lib/rubocop/config_loader_resolver.rb
@@ -33,7 +33,7 @@ module RuboCop
                       inherit_mode: determine_inherit_mode(hash, k))
           end
           hash[k] = v
-          fix_include_paths(base_config.loaded_path, hash, path, k, v) if v.key?('Include')
+          fix_include_paths(base_config.loaded_path, hash, k, v) if v.key?('Include')
         end
       end
     end
@@ -42,13 +42,12 @@ module RuboCop
     # base configuration are relative to the directory where the base configuration file is. For the
     # derived configuration, we need to make those paths relative to where the derived configuration
     # file is.
-    def fix_include_paths(base_config_path, hash, path, key, value)
+    def fix_include_paths(base_config_path, hash, key, value)
       return unless File.basename(base_config_path).start_with?('.rubocop')
 
       base_dir = File.dirname(base_config_path)
-      derived_dir = File.dirname(path)
       hash[key]['Include'] = value['Include'].map do |include_path|
-        PathUtil.relative_path(File.join(base_dir, include_path), derived_dir)
+        PathUtil.relative_path(File.join(base_dir, include_path), Dir.pwd)
       end
     end
 

spec/rubocop/config_loader_spec.rb
@@ -283,7 +283,7 @@ RSpec.describe RuboCop::ConfigLoader do
       end
 
       it 'gets an Include that is relative to the subdirectory' do
-        expect(configuration_from_file['Style/StringLiterals']['Include']).to eq(['**/*.rb'])
+        expect(configuration_from_file['Style/StringLiterals']['Include']).to eq(['dir/**/*.rb'])
       end
 
       it 'ignores parent AllCops/Exclude if ignore_parent_exclusion is true' do
@@ -341,7 +341,7 @@ RSpec.describe RuboCop::ConfigLoader do
       end
 
       it 'gets an Include that is relative to the subdirectory' do
-        expect(configuration_from_file['Style/StringLiterals']['Include']).to eq(['../src/**/*.rb'])
+        expect(configuration_from_file['Style/StringLiterals']['Include']).to eq(['src/**/*.rb'])
       end
     end
 

spec/rubocop/cop/bundler/duplicated_gem_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe RuboCop::Cop::Bundler::DuplicatedGem, :config do
 
   context 'when investigating Ruby files' do
     it 'does not register any offenses' do
-      expect_no_offenses(<<~RUBY, 'foo.rb')
+      expect_no_offenses(<<~RUBY, '/foo.rb')
         # cop will not read these contents
         gem('rubocop')
         gem('rubocop')

spec/rubocop/cop/bundler/gem_comment_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe RuboCop::Cop::Bundler::GemComment, :config do
 
   context 'when investigating Ruby files' do
     it 'does not register any offenses' do
-      expect_no_offenses(<<~RUBY, 'foo.rb')
+      expect_no_offenses(<<~RUBY, '/foo.rb')
         gem('rubocop')
       RUBY
     end

spec/rubocop/cop/cop_spec.rb
@@ -318,7 +318,7 @@ RSpec.describe RuboCop::Cop::Cop, :config do
     end
 
     context 'when the file doesn\'t match the Include configuration' do
-      let(:file) { 'bar.rb' }
+      let(:file) { '/bar.rb' }
 
       it { is_expected.to be(false) }
     end

commit f23519f6d908018b1849dc52a487b4ae3b7189b6

Don't adjust Includes if base and derived configs are in the same folder
This fixes an issue caused by #11416, where files no longer match when
rubocop is run from a child directory.

lib/rubocop/config_loader_resolver.rb
@@ -33,7 +33,7 @@ module RuboCop
                       inherit_mode: determine_inherit_mode(hash, k))
           end
           hash[k] = v
-          fix_include_paths(base_config.loaded_path, hash, k, v) if v.key?('Include')
+          fix_include_paths(base_config.loaded_path, path, hash, k, v) if v.key?('Include')
         end
       end
     end
@@ -42,10 +42,16 @@ module RuboCop
     # base configuration are relative to the directory where the base configuration file is. For the
     # derived configuration, we need to make those paths relative to where the derived configuration
     # file is.
-    def fix_include_paths(base_config_path, hash, key, value)
+    def fix_include_paths(base_config_path, derived_config_path, hash, key, value)
       return unless File.basename(base_config_path).start_with?('.rubocop')
 
       base_dir = File.dirname(base_config_path)
+      derived_dir = File.dirname(derived_config_path)
+
+      # If the two files are in the same directory, don't adjust anything. This is equivalent to the
+      # configuration being specified in a single file
+      return if base_dir == derived_dir
+
       hash[key]['Include'] = value['Include'].map do |include_path|
         PathUtil.relative_path(File.join(base_dir, include_path), Dir.pwd)
       end

spec/rubocop/config_loader_spec.rb
@@ -345,6 +345,33 @@ RSpec.describe RuboCop::ConfigLoader do
       end
     end
 
+    context 'when a file inherits from another file in the same directory' do
+      let(:file_path) { '.rubocop.yml' }
+
+      before do
+        allow(Dir).to receive(:pwd).and_return("#{File.dirname(file_path)}/src")
+        create_file('.rubocop_base.yml', <<~YAML)
+          AllCops:
+            Exclude:
+              - vendor/**
+          Style/StringLiterals:
+            Include:
+              - '**/*.rb'
+        YAML
+
+        create_file(file_path, ['inherit_from: .rubocop_base.yml'])
+      end
+
+      it 'the Include and Exclude lists are not adjusted' do
+        excludes = configuration_from_file['AllCops']['Exclude']
+        expect(excludes).to eq([File.expand_path('vendor/**')])
+      end
+
+      it 'gets an Include that is relative to the subdirectory' do
+        expect(configuration_from_file['Style/StringLiterals']['Include']).to eq(['**/*.rb'])
+      end
+    end
+
     context 'when a file inherits and overrides an Exclude' do
       let(:file_path) { '.rubocop.yml' }
       let(:message) do