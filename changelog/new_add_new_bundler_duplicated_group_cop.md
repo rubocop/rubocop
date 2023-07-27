5a1b9109eaeedb0e9d3bf6f9d38fd38f5e985566
Marek Wysinski <owlking@gmx.com>
2023-07-27 19:18:49 +0100

    Add new `Bundler/DuplicatedGroup` cop

---
+++ changelog/new_add_new_bundler_duplicated_group_cop.md
@@ -0,0 +1,1 @@
+* Add new `Bundler/DuplicatedGroup` cop. ([@OwlKing][])
---
+++ lib/rubocop/cop/bundler/duplicated_group.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Bundler
+      # A Gem group should be listed only once in a Gemfile.
+      # @example
+      #   # bad
+      #   group :development do
+      #     gem 'rubocop'
+      #   end
+      #
+      #   group :development do
+      #     gem 'rubocop-rails'
+      #   end
+      #
+      #   # bad
+      #   group :development, :test do
+      #     gem 'rubocop'
+      #   end
+      #   group :test, :development do
+      #     gem 'rspec'
+      #   end
+      #
+      #   # good
+      #   group :development do
+      #     gem 'rubocop'
+      #   end
+      #
+      #   group :development, :test do
+      #     gem 'rspec'
+      #   end
+      #
+      #   # good
+      #   gem 'rubocop', groups: [:development, :test]
+      #   gem 'rspec', groups: [:development, :test]
+      #
+      class DuplicatedGroup < Base
+        include RangeHelp
+
+        MSG = 'Gem group `%<group_name>s` already defined on line ' \
+              '%<line_of_first_occurrence>d of the Gemfile.'
+
+        def on_new_investigation
+          return if processed_source.blank?
+
+          duplicated_group_nodes.each do |nodes|
+            nodes[1..].each do |node|
+              register_offense(node, node.arguments.map(&:value).join(', '), nodes.first.first_line)
+            end
+          end
+        end
+
+        private
+
+        # @!method group_declarations(node)
+        def_node_search :group_declarations, '(send nil? :group ...)'
+
+        def duplicated_group_nodes
+          group_declarations(processed_source.ast)
+            .group_by { |node| node.arguments.map(&:value).map(&:to_s).sort }
+            .values
+            .select { |nodes| nodes.size > 1 }
+        end
+
+        def register_offense(node, group_name, line_of_first_occurrence)
+          line_range = node.loc.column...node.loc.last_column
+          offense_location = source_range(processed_source.buffer, node.first_line, line_range)
+          message = format(
+            MSG,
+            group_name: group_name,
+            line_of_first_occurrence: line_of_first_occurrence
+          )
+          add_offense(offense_location, message: message)
+        end
+      end
+    end
+  end
+end
