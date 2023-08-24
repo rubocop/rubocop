8abcf7ea8a919e213fae44129b75326c61f40617
Martin Emde <martin.emde@gmail.com>
2023-08-24 13:21:58 -0700

    Fix shadowing of FileFinder stop_dir in isolated tests.
    Previously, stop_dir was ignored when root_level was present.
    
    Tests relied on the root_level matching the stop_dir which
    allowed some tests to pass even when stop_dir was being ignored.
    
    Use FileFinder.root_level only for protecting test env by setting it at
    the actual root of the tmpdir in isolated environment tests.

--- spec/isolated_environment_spec.rb
+++ spec/isolated_environment_spec.rb
@@ -10,8 +10,15 @@ RSpec.describe 'isolated environment', :isolated_environment, type: :feature do
   # directory is under the user's home directory. On any platform we don't want
   # a .rubocop.yml file in the temporary directory to affect the outcome of
   # rspec.
+  #
+  # For this test, we shift the root_level down to the work directory so we
+  # can place a file above the root_level and ensure it is not loaded.
   it 'is not affected by a config file above the work directory' do
-    create_file('../.rubocop.yml', ['inherit_from: missing_file.yml'])
+    ignored_path = File.expand_path(File.join(RuboCop::FileFinder.root_level, '.rubocop.yml'))
+    create_file(ignored_path, ['inherit_from: missing_file.yml'])
+
+    RuboCop::FileFinder.root_level = File.join(RuboCop::FileFinder.root_level, 'work')
+
     create_file('ex.rb', ['# frozen_string_literal: true'])
     # A return value of 0 means that the erroneous config file was not read.
     expect(cli.run([])).to eq(0)
