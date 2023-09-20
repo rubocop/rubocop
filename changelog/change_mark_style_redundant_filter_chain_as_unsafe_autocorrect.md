66e4a2fc4c427b997c867a47a1eaddecdd1ccf7c
Koichi ITO <koic.ito@gmail.com>
2023-09-20 19:17:41 +0900

    [Fix #12210] Mark `Style/RedundantFilterChain` as unsafe autocorrect
    Fixes #12210.
    
    This PR marks `Style/RedundantFilterChain` as unsafe autocorrect.

---
+++ changelog/change_mark_style_redundant_filter_chain_as_unsafe_autocorrect.md
@@ -0,0 +1,1 @@
+* [#12210](https://github.com/rubocop/rubocop/issues/12210): Mark `Style/RedundantFilterChain` as unsafe autocorrect. ([@koic][])
--- lib/rubocop/cop/style/redundant_filter_chain.rb
+++ lib/rubocop/cop/style/redundant_filter_chain.rb
@@ -6,6 +6,12 @@ module RuboCop
       # Identifies usages of `any?`, `empty?` or `none?` predicate methods
       # chained to `select`/`filter`/`find_all` and change them to use predicate method instead.
       #
+      # @safety
+      #   This cop's autocorrection is unsafe because `array.select.any?` evaluates all elements
+      #   through the `select` method, while `array.any?` uses short-circuit evaluation.
+      #   In other words, `array.select.any?` guarantees the evaluation of every element,
+      #   but `array.any?` does not necessarily evaluate all of them.
+      #
       # @example
       #   # bad
       #   arr.select { |x| x > 1 }.any?
