e389a33266c0555d2544d95c58a52190ced2bf37
Koichi ITO <koic.ito@gmail.com>
2023-11-06 13:50:46 +0900

    [Fix #12363] Fix incorrect rendering in `HTMLFormatter` formatter
    Fixes #12363.
    
    This PR fixes incorrect rendering of HTML character entities in `HTMLFormatter` formatter.

---
+++ changelog/fix_incorrect_rendering_in_formatter_html_formatter.md
@@ -0,0 +1,1 @@
+* [#12363](https://github.com/rubocop/rubocop/issues/12363): Fix incorrect rendering of HTML character entities in `HTMLFormatter` formatter. ([@koic][])
--- spec/fixtures/html_formatter/project/app/models/book.rb
+++ spec/fixtures/html_formatter/project/app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   def someMethod
     foo = bar = baz
+    qux(quux.scan(/&amp;&lt;/))
     Regexp.new(/\A<p>(.*)<\/p>\Z/m).match(full_document)[1] rescue full_document
   end
 end
