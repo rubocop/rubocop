de86a68a5260ce80ebac5691ecc6a8e929bd38a7
Koichi ITO <koic.ito@gmail.com>
2023-11-19 01:57:50 +0900

    [Fix #12370] Make `Style/HashEachMethods` aware of unused block value
    Fixes #12370.
    
    This PR makes `Style/HashEachMethods` aware of unused block value.
    And this PR suppresses the following new offense in this repository:
    
    ```console
    $ bundle exec rubocop --only Style/HashEachMethods -A
    (snip)
    
    Offenses:
    
    lib/rubocop/cop/style/bisected_attr_accessor.rb:36:11: C: [Corrected] Style/HashEachMethods:
    Use each_value instead of each, and remove the unused _visibility block argument.
              find_macros(class_node.body).each do |_visibility, macros| ...
              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    1519 files inspected, 1 offense detected, 1 offense corrected
    ```

--- lib/rubocop/cop/style/hash_each_methods.rb
+++ lib/rubocop/cop/style/hash_each_methods.rb
@@ -17,10 +17,16 @@ module RuboCop
       # @example
       #   # bad
       #   hash.keys.each { |k| p k }
-      #   hash.values.each { |v| p v }
+      #   hash.each { |k, unused_value| p k }
       #
       #   # good
       #   hash.each_key { |k| p k }
+      #
+      #   # bad
+      #   hash.values.each { |v| p v }
+      #   hash.each { |unused_key, v| p v }
+      #
+      #   # good
       #   hash.each_value { |v| p v }
       #
       # @example AllowedReceivers: ['execute']
@@ -33,10 +39,16 @@ module RuboCop
         extend AutoCorrector
 
         MSG = 'Use `%<prefer>s` instead of `%<current>s`.'
+        UNUSED_BLOCK_ARG_MSG = "#{MSG.chop} and remove the unused `%<unused_code>s` block argument."
 
         # @!method kv_each(node)
         def_node_matcher :kv_each, <<~PATTERN
           ({block numblock} $(send (send _ ${:keys :values}) :each) ...)
+        PATTERN
+
+        # @!method each_arguments(node)
+        def_node_matcher :each_arguments, <<~PATTERN
+          (block (send _ :each)(args $_key $_value) ...)
         PATTERN
 
         # @!method kv_each_with_block_pass(node)
@@ -44,11 +56,27 @@ module RuboCop
           (send $(send _ ${:keys :values}) :each (block_pass (sym _)))
         PATTERN
 
+        # rubocop:disable Metrics/AbcSize
         def on_block(node)
           kv_each(node) do |target, method|
-            register_kv_offense(target, method)
+            register_kv_offense(target, method) and return
+          end
+
+          return unless (key, value = each_arguments(node))
+
+          if unused_block_arg_exist?(node, value.source)
+            message = message('each_key', node.method_name, value.source)
+            unused_range = key.source_range.end.join(value.source_range.end)
+
+            register_each_args_offense(node, message, 'each_key', unused_range)
+          elsif unused_block_arg_exist?(node, key.source)
+            message = message('each_value', node.method_name, key.source)
+            unused_range = key.source_range.begin.join(value.source_range.begin)
+
+            register_each_args_offense(node, message, 'each_value', unused_range)
           end
         end
+        # rubocop:enable Metrics/AbcSize
 
         alias on_numblock on_block
 
@@ -66,6 +94,23 @@ module RuboCop
 
           add_offense(kv_range(target), message: format_message(method)) do |corrector|
             correct_key_value_each(target, corrector)
+          end
+        end
+
+        def unused_block_arg_exist?(node, block_arg_source)
+          node.body.each_descendant(:lvar).map(&:source).none?(block_arg_source)
+        end
+
+        def message(prefer, method_name, unused_code)
+          format(
+            UNUSED_BLOCK_ARG_MSG, prefer: prefer, current: method_name, unused_code: unused_code
+          )
+        end
+
+        def register_each_args_offense(node, message, prefer, unused_range)
+          add_offense(node, message: message) do |corrector|
+            corrector.replace(node.send_node.loc.selector, prefer)
+            corrector.remove(unused_range)
           end
         end
 
