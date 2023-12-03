ef155149e11667965d1be3e46c1969753f977fd2
Koichi ITO <koic.ito@gmail.com>
2023-12-04 01:59:10 +0900

    [Fix #12473] Make `Style/ClassCheck` aware of safe navigation operator
    Fixes #12473.
    
    This PR makes `Style/ClassCheck` aware of safe navigation operator.

---
+++ changelog/fix_make_style_class_check_aware_of_safe_navigation_operator.md
@@ -0,0 +1,1 @@
+* [#12473](https://github.com/rubocop/rubocop/issues/12473): Make `Style/ClassCheck` aware of safe navigation operator. ([@koic][])
--- spec/rubocop/cop/style/class_check_spec.rb
+++ spec/rubocop/cop/style/class_check_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Style::ClassCheck, :config do
-  context 'when enforced style is is_a?' do
+  context 'when enforced style is `is_a?`' do
     let(:cop_config) { { 'EnforcedStyle' => 'is_a?' } }
 
-    it 'registers an offense for kind_of? and corrects to is_a?' do
+    it 'registers an offense for `kind_of?` and corrects to `is_a?`' do
       expect_offense(<<~RUBY)
         x.kind_of? y
           ^^^^^^^^ Prefer `Object#is_a?` over `Object#kind_of?`.
@@ -14,12 +14,23 @@ RSpec.describe RuboCop::Cop::Style::ClassCheck, :config do
         x.is_a? y
       RUBY
     end
+
+    it 'registers an offense for `kind_of?` is safe navigation called and corrects to `is_a?`' do
+      expect_offense(<<~RUBY)
+        x&.kind_of? y
+           ^^^^^^^^ Prefer `Object#is_a?` over `Object#kind_of?`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x&.is_a? y
+      RUBY
+    end
   end
 
   context 'when enforced style is kind_of?' do
     let(:cop_config) { { 'EnforcedStyle' => 'kind_of?' } }
 
-    it 'registers an offense for is_a? and corrects to kind_of?' do
+    it 'registers an offense for `is_a?` and corrects to `kind_of?`' do
       expect_offense(<<~RUBY)
         x.is_a? y
           ^^^^^ Prefer `Object#kind_of?` over `Object#is_a?`.
@@ -27,6 +38,17 @@ RSpec.describe RuboCop::Cop::Style::ClassCheck, :config do
 
       expect_correction(<<~RUBY)
         x.kind_of? y
+      RUBY
+    end
+
+    it 'registers an offense for `is_a?` is safe navigation called and corrects to `kind_of?`' do
+      expect_offense(<<~RUBY)
+        x&.is_a? y
+           ^^^^^ Prefer `Object#kind_of?` over `Object#is_a?`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x&.kind_of? y
       RUBY
     end
   end
