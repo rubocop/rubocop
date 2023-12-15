203b0cd10a5145d4d7201c6f10a9d04428e3f690
Sam Bostock <sam.bostock@shopify.com>
2023-12-15 12:03:27 -0600

    Teach `Lint/AssignmentInCondition` about `case`

---
+++ changelog/change_teach_lint_assignment_in_condition_about_case.md
@@ -0,0 +1,1 @@
+* [#12546](https://github.com/rubocop/rubocop/pull/12546): Teach `Lint/AssignmentInCondition` about `case`. ([@sambostock][])
--- lib/rubocop/cop/lint/assignment_in_condition.rb
+++ lib/rubocop/cop/lint/assignment_in_condition.rb
@@ -68,6 +68,7 @@ module RuboCop
         end
         alias on_while on_if
         alias on_until on_if
+        alias on_case on_if
 
         private
 
