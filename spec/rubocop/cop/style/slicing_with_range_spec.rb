4278f84a6b79c35326486b97d9a3f9dc3818c3e4
Koichi ITO <koic.ito@gmail.com>
2024-01-06 01:48:32 +0900

    [Fix #12233] Make `Style/SlicingWithRange` aware of beginless range
    Fixes #12233.
    
    This PR makes `Style/SlicingWithRange` aware of redundant and beginless range.
    
    Ruby 2.7 introduced beginless ranges. But, unlike the somewhat obscure `-1` in
    `ary[1..-1]`, the `0` in `ary[0..42]` is clear as a starting point.
    In fact, changing it to `ary[..42]` could potentially make it less readable.
    Therefore, `ary[0..42]` should respect the original programmer's intent.
    On the other hand, `ary[nil..42]` could be replaced with `ary[..42]`.
    Similarly, `ary[1..nil]` could be replaced with `ary[1..]`.
    
    Moreover, `[0..-1]` in `ary[0..-1]` is redundant and simply synonymous with `ary`.
    This PR has also made adjustments to detect this case.
    While this is a byproduct of supporting beginless and endless ranges,
    I think including this change in `Style/SlicingWithRange` cop may be suitable
    and not feel out of place.

--- spec/rubocop/cop/style/slicing_with_range_spec.rb
+++ spec/rubocop/cop/style/slicing_with_range_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe RuboCop::Cop::Style::SlicingWithRange, :config do
   context '<= Ruby 2.5', :ruby25 do
-    it 'reports no offense for array slicing with -1' do
+    it 'reports no offense for array slicing end with `-1`' do
       expect_no_offenses(<<~RUBY)
         ary[1..-1]
       RUBY
@@ -10,7 +10,46 @@ RSpec.describe RuboCop::Cop::Style::SlicingWithRange, :config do
   end
 
   context '>= Ruby 2.6', :ruby26 do
-    it 'reports an offense for slicing to ..-1' do
+    it 'reports an offense for slicing with `[0..-1]`' do
+      expect_offense(<<~RUBY)
+        ary[0..-1]
+           ^^^^^^^ Remove the useless `[0..-1]`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary
+      RUBY
+    end
+
+    it 'does not register an offense for slicing with `[0...-1]`' do
+      expect_no_offenses(<<~RUBY)
+        ary[0...-1]
+      RUBY
+    end
+
+    it 'reports an offense for slicing with `[0..nil]`' do
+      expect_offense(<<~RUBY)
+        ary[0..nil]
+           ^^^^^^^^ Remove the useless `[0..nil]`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary
+      RUBY
+    end
+
+    it 'reports an offense for slicing with `[0...nil]`' do
+      expect_offense(<<~RUBY)
+        ary[0...nil]
+           ^^^^^^^^^ Remove the useless `[0...nil]`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary
+      RUBY
+    end
+
+    it 'reports an offense for slicing to `..-1`' do
       expect_offense(<<~RUBY)
         ary[1..-1]
            ^^^^^^^ Prefer `[1..]` over `[1..-1]`.
@@ -21,7 +60,29 @@ RSpec.describe RuboCop::Cop::Style::SlicingWithRange, :config do
       RUBY
     end
 
-    it 'reports an offense for slicing from expression to ..-1' do
+    it 'reports an offense for slicing to `..nil`' do
+      expect_offense(<<~RUBY)
+        ary[1..nil]
+           ^^^^^^^^ Prefer `[1..]` over `[1..nil]`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary[1..]
+      RUBY
+    end
+
+    it 'reports an offense for slicing to `...nil`' do
+      expect_offense(<<~RUBY)
+        ary[1...nil]
+           ^^^^^^^^^ Prefer `[1...]` over `[1...nil]`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary[1...]
+      RUBY
+    end
+
+    it 'reports an offense for slicing from expression to `..-1`' do
       expect_offense(<<~RUBY)
         ary[fetch_start(true).first..-1]
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `[fetch_start(true).first..]` over `[fetch_start(true).first..-1]`.
@@ -49,9 +110,32 @@ RSpec.describe RuboCop::Cop::Style::SlicingWithRange, :config do
         ranges = [1..-1]
       RUBY
     end
+
+    it 'reports no offense for array slicing start with 0' do
+      expect_no_offenses(<<~RUBY)
+        ary[0..42]
+      RUBY
+    end
   end
 
   context '>= Ruby 2.7', :ruby27 do
+    it 'reports an offense for slicing with `[nil..42]`' do
+      expect_offense(<<~RUBY)
+        ary[nil..42]
+           ^^^^^^^^^ Prefer `[..42]` over `[nil..42]`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ary[..42]
+      RUBY
+    end
+
+    it 'does not register an offense for slicing with `[0..42]`' do
+      expect_no_offenses(<<~RUBY)
+        ary[0..42]
+      RUBY
+    end
+
     it 'reports no offense for startless' do
       expect_no_offenses(<<~RUBY)
         ary[..-1]
