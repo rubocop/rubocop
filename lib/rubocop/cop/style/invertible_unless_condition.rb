f15152468c627794a839c7c12ae37766199d664f
Sam Bostock <sam.bostock@shopify.com>
2024-02-28 11:19:04 -0500

    Add InvertibleUnlessCondition implicit receiver example
    This change was meant to be in #12711.

--- lib/rubocop/cop/style/invertible_unless_condition.rb
+++ lib/rubocop/cop/style/invertible_unless_condition.rb
@@ -32,12 +32,14 @@ module RuboCop
       #   foo unless x != y
       #   foo unless x >= 10
       #   foo unless x.even?
+      #   foo unless odd?
       #
       #   # good
       #   foo if bar
       #   foo if x == y
       #   foo if x < 10
       #   foo if x.odd?
+      #   foo if even?
       #
       #   # bad (complex condition)
       #   foo unless x != y || x.even?
