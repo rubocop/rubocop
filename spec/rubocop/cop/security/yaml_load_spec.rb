8acce2851bbd59d44a18becf2bc919cd26b49db2
Koichi ITO <koic.ito@gmail.com>
2024-02-29 18:04:34 +0900

    Tweak specs for `Security/YAMLLoad`
    Follow https://github.com/rubocop/rubocop/pull/10424.
    
    Ruby 3.1+ (Psych 4) uses `Psych.load` as `Psych.safe_load` by default.
    So, only Ruby 3.0 and earlier will be warned about using `YAML.load`.

--- spec/rubocop/cop/security/yaml_load_spec.rb
+++ spec/rubocop/cop/security/yaml_load_spec.rb
@@ -13,26 +13,28 @@ RSpec.describe RuboCop::Cop::Security::YAMLLoad, :config do
     expect_no_offenses('Module::YAML.load("foo")')
   end
 
-  it 'registers an offense and corrects load with a literal string' do
-    expect_offense(<<~RUBY)
-      YAML.load("--- !ruby/object:Foo {}")
-           ^^^^ Prefer using `YAML.safe_load` over `YAML.load`.
-    RUBY
-
-    expect_correction(<<~RUBY)
-      YAML.safe_load("--- !ruby/object:Foo {}")
-    RUBY
-  end
-
-  it 'registers an offense and corrects a fully qualified ::YAML.load' do
-    expect_offense(<<~RUBY)
-      ::YAML.load("--- foo")
+  context 'Ruby <= 3.0', :ruby30 do
+    it 'registers an offense and corrects load with a literal string' do
+      expect_offense(<<~RUBY)
+        YAML.load("--- !ruby/object:Foo {}")
              ^^^^ Prefer using `YAML.safe_load` over `YAML.load`.
-    RUBY
+      RUBY
 
-    expect_correction(<<~RUBY)
-      ::YAML.safe_load("--- foo")
-    RUBY
+      expect_correction(<<~RUBY)
+        YAML.safe_load("--- !ruby/object:Foo {}")
+      RUBY
+    end
+
+    it 'registers an offense and corrects a fully qualified ::YAML.load' do
+      expect_offense(<<~RUBY)
+        ::YAML.load("--- foo")
+               ^^^^ Prefer using `YAML.safe_load` over `YAML.load`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ::YAML.safe_load("--- foo")
+      RUBY
+    end
   end
 
   # Ruby 3.1+ (Psych 4) uses `Psych.load` as `Psych.safe_load` by default.
