6165c97aadb6d8ca4b5f145df32e25b202b87225
Koichi ITO <koic.ito@gmail.com>
2024-03-31 15:14:46 +0900

    Use `RESTRICT_ON_SEND` for `Style/RedundantCurrentDirectoryInPath`

--- lib/rubocop/cop/style/redundant_current_directory_in_path.rb
+++ lib/rubocop/cop/style/redundant_current_directory_in_path.rb
@@ -18,10 +18,10 @@ module RuboCop
         extend AutoCorrector
 
         MSG = 'Remove the redundant current directory path.'
+        RESTRICT_ON_SEND = %i[require_relative].freeze
         CURRENT_DIRECTORY_PATH = './'
 
         def on_send(node)
-          return unless node.method?(:require_relative)
           return unless (first_argument = node.first_argument)
           return unless first_argument.str_content&.start_with?(CURRENT_DIRECTORY_PATH)
           return unless (index = first_argument.source.index(CURRENT_DIRECTORY_PATH))
