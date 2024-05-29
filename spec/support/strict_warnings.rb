54421d6df7fb245f836fe8074690171c02b3d7b6
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-05-29 11:50:41 +0200

    [Fix #12910] Ensure that RuboCop runs warning-free
    This is inspired by how Rails approaches this

---
+++ spec/support/strict_warnings.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# Ensure that RuboCop runs warning-free. This hooks into Ruby's `warn`
+# method and raises when an unexpected warning is encountered.
+module StrictWarnings
+  class WarningError < StandardError; end
+
+  # Warnings from 3rd-party gems, or other things that are unactionable
+  SUPPRESSED_WARNINGS = Regexp.union(
+    %r{lib/parser/builders/default.*Unknown escape},
+    %r{lib/parser/builders/default.*character class has duplicated range},
+    /Float.*out of range/, # also from the parser gem
+    /`Process` does not respond to `fork` method/, # JRuby
+    /File#readline accesses caller method's state and should not be aliased/, # JRuby, test stub
+    /instance variable @.* not initialized/ # Ruby 2.7
+  )
+
+  def warn(message, ...)
+    return if SUPPRESSED_WARNINGS.match?(message)
+
+    super
+    # RuboCop uses `warn` to display some of its output and tests assert against
+    # that. Assume that warnings are intentional when stderr is redirected.
+    return if $stderr.is_a?(StringIO)
+    # Ignore warnings from dev/rc ruby versions. Things are subject to change and
+    # contributors should not be bothered by them with red CI.
+    return if RUBY_PATCHLEVEL == -1
+
+    raise WarningError, message
+  end
+
+  def self.enable!
+    $VERBOSE = true
+    Warning[:deprecated] = true
+    Warning.singleton_class.prepend(self)
+  end
+end
