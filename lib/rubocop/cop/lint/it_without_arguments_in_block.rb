22ba92f687514a1485f3c1478428dbc5e67318d3
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-09-14 11:32:04 +0200

    Improve performance of `Lint/ItWithoutArgumentsInBlock`
    Using `on_block` is expensive, since this checks each send node in a block.
    Instead, use `on_send`. This is much cheaper since barely any send nodes will be named `it`.
    
    From `bundle exec prof:slow_cops`
    ```
         551  (    2.9%)  RuboCop::Cop::Layout::IndentationConsistency#on_begin
         402  (    2.2%)  RuboCop::Cop::Layout::RedundantLineBreak#on_send
         358  (    1.9%)  RuboCop::Cop::Lint::ItWithoutArgumentsInBlock#on_block
    (snip)
    ```
    
    After:
    ```
    (103 previous entries)
          42  (    0.2%)  RuboCop::Cop::Lint::ItWithoutArgumentsInBlock#on_send
    (snip)
    ```
    
    Overall, this saves about 200ms for the RuboCop repo.

--- lib/rubocop/cop/lint/it_without_arguments_in_block.rb
+++ lib/rubocop/cop/lint/it_without_arguments_in_block.rb
@@ -29,25 +29,16 @@ module RuboCop
 
         MSG = '`it` calls without arguments will refer to the first block param in Ruby 3.4; ' \
               'use `it()` or `self.it`.'
+        RESTRICT_ON_SEND = %i[it].freeze
 
-        def on_block(node) # rubocop:disable InternalAffairs/NumblockHandler
-          return unless (body = node.body)
-          return unless node.arguments.empty_and_without_delimiters?
+        def on_send(node)
+          return unless (block_node = node.each_ancestor(:block).first)
+          return unless block_node.arguments.empty_and_without_delimiters?
 
-          if body.send_type? && deprecated_it_method?(body)
-            add_offense(body)
-          else
-            body.each_descendant(:send).each do |send_node|
-              next unless deprecated_it_method?(send_node)
-
-              add_offense(send_node)
-            end
-          end
+          add_offense(node) if deprecated_it_method?(node)
         end
 
         def deprecated_it_method?(node)
-          return false unless node.method?(:it)
-
           !node.receiver && node.arguments.empty? && !node.parenthesized? && !node.block_literal?
         end
       end
