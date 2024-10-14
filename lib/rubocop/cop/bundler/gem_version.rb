60432f5e440af96ea3637f530a9c218de7611938
Koichi ITO <koic.ito@gmail.com>
2024-10-14 23:08:36 +0900

    Apply `RESTRICT_ON_SEND` to `Bundler/GemVersion`

--- lib/rubocop/cop/bundler/gem_version.rb
+++ lib/rubocop/cop/bundler/gem_version.rb
@@ -56,6 +56,7 @@ module RuboCop
 
         REQUIRED_MSG = 'Gem version specification is required.'
         FORBIDDEN_MSG = 'Gem version specification is forbidden.'
+        RESTRICT_ON_SEND = %i[gem].freeze
         VERSION_SPECIFICATION_REGEX = /^\s*[~<>=]*\s*[0-9.]+/.freeze
 
         # @!method includes_version_specification?(node)
