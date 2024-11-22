375a76d2de0d1f8d205489ca5b361b7cca2b1eb9
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2024-11-22 11:32:31 -0500

    Add `maximum_target_ruby_version` to `TargetRubyVersion`

--- lib/rubocop/cop/mixin/target_ruby_version.rb
+++ lib/rubocop/cop/mixin/target_ruby_version.rb
@@ -8,12 +8,28 @@ module RuboCop
         @minimum_target_ruby_version
       end
 
+      def required_maximum_ruby_version
+        @maximum_target_ruby_version
+      end
+
       def minimum_target_ruby_version(version)
         @minimum_target_ruby_version = version
       end
 
+      def maximum_target_ruby_version(version)
+        @maximum_target_ruby_version = version
+      end
+
       def support_target_ruby_version?(version)
-        required_minimum_ruby_version <= version
+        # By default, no minimum or maximum versions of ruby are required
+        # to run any cop. In order to do a simple numerical comparison of
+        # the requested version against any requirements, we use 0 and
+        # Infinity as the default values to indicate no minimum (0) and no
+        # maximum (Infinity).
+        min = required_minimum_ruby_version || 0
+        max = required_maximum_ruby_version || Float::INFINITY
+
+        min <= version && max >= version
       end
     end
   end
