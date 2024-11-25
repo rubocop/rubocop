c8cdb3d432c0a31fedcc7b91497613b05fd43316
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2024-11-25 15:43:16 -0500

    [Fix #13486] Add new `Style/DigChain` cop
    Adds a cop to check for subsequent `dig` calls within a method chain that could be combined into a single `dig` call.

---
+++ lib/rubocop/cop/style/dig_chain.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Style
+      # Check for chained `dig` calls that can be collapsed into a single `dig`.
+      #
+      # @safety
+      #   This cop is unsafe because it cannot be guaranteed that the receiver
+      #   is an `Enumerable` or does not have a nonstandard implementation
+      #   of `dig`.
+      #
+      # @example
+      #   # bad
+      #   x.dig(:foo).dig(:bar).dig(:baz)
+      #   x.dig(:foo, :bar).dig(:baz)
+      #   x.dig(:foo, :bar)&.dig(:baz)
+      #
+      #   # good
+      #   x.dig(:foo, :bar, :baz)
+      #
+      #   # good - `dig`s cannot be combined
+      #   x.dig(:foo).bar.dig(:baz)
+      #
+      class DigChain < Base
+        extend AutoCorrector
+        include RangeHelp
+        include CommentsHelp
+
+        MSG = 'Use `%<replacement>s` instead of chaining.'
+        RESTRICT_ON_SEND = %i[dig].freeze
+
+        # @!method dig?(node)
+        def_node_matcher :dig?, <<~PATTERN
+          (call _ :dig $!{hash block_pass}+)
+        PATTERN
+
+        def on_send(node)
+          return if ignored_node?(node)
+          return unless node.loc.dot
+          return unless dig?(node)
+
+          range, arguments = inspect_chain(node)
+          return if invalid_arguments?(arguments)
+          return unless range
+
+          register_offense(node, range, arguments)
+        end
+        alias on_csend on_send
+
+        private
+
+        # Walk up the method chain while the receiver is `dig` with arguments.
+        def inspect_chain(node)
+          arguments = node.arguments.dup
+          end_pos = node.source_range.end_pos
+
+          while dig?((node = node.receiver))
+            begin_pos = node.loc.dot ? node.loc.dot.begin_pos + 1 : 0
+            arguments.unshift(*node.arguments)
+            ignore_node(node)
+          end
+
+          return unless begin_pos
+
+          [range_between(begin_pos, end_pos), arguments]
+        end
+
+        def invalid_arguments?(arguments)
+          # If any of the arguments are arguments forwarding (`...`), it can only be the
+          # first argument, or else the resulting code will have a syntax error.
+
+          return false unless arguments&.any?
+
+          forwarded_args_index = arguments.index(&:forwarded_args_type?)
+          forwarded_args_index && forwarded_args_index < (arguments.size - 1)
+        end
+
+        def register_offense(node, range, arguments)
+          arguments = arguments.map(&:source).join(', ')
+          replacement = "dig(#{arguments})"
+
+          add_offense(range, message: format(MSG, replacement: replacement)) do |corrector|
+            corrector.replace(range, replacement)
+
+            comments_in_range(node).reverse_each do |comment|
+              corrector.insert_before(node, "#{comment.source}\n")
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/style/dig_chain_spec.rb
@@ -0,0 +1,221 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::DigChain, :config do
+  it 'does not register an offense for unchained `dig` without a receiver' do
+    expect_no_offenses(<<~RUBY)
+      dig
+    RUBY
+  end
+
+  it 'does not register an offense for `X::dig`' do
+    expect_no_offenses(<<~RUBY)
+      X::dig(:foo, :bar)
+    RUBY
+  end
+
+  it 'does not register an offense for dig chained to something else' do
+    expect_no_offenses(<<~RUBY)
+      x.dig(:foo).bar
+      x.bar.dig(:foo)
+    RUBY
+  end
+
+  it 'does not register an offense when the chain is broken up' do
+    expect_no_offenses(<<~RUBY)
+      x.dig(:foo).first.dig(:bar)
+    RUBY
+  end
+
+  it 'does not register an offense for dig without arguments' do
+    expect_no_offenses(<<~RUBY)
+      x.dig
+    RUBY
+  end
+
+  it 'does not register an offense for chained dig without arguments' do
+    expect_no_offenses(<<~RUBY)
+      x.dig(:foo, :bar).dig
+      x.dig.dig(:foo, :bar)
+    RUBY
+  end
+
+  it 'does not register an offense for a hash' do
+    expect_no_offenses(<<~RUBY)
+      x.dig({ foo: 1, bar: 2 }).dig({ baz: 3, quux: 4 })
+    RUBY
+  end
+
+  it 'does not register an offense for kwargs' do
+    expect_no_offenses(<<~RUBY)
+      x.dig(foo: 1, bar: 2).dig(baz: 3, quux: 4)
+    RUBY
+  end
+
+  context 'with chained `dig` calls' do
+    it 'registers an offense and corrects with single values' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo).dig(:bar).dig(:baz)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.dig(:foo, :bar, :baz)
+      RUBY
+    end
+
+    it 'registers an offense and corrects when attached to a chain of non-dig receivers' do
+      expect_offense(<<~RUBY)
+        x.y.z.dig(:foo).dig(:bar).dig(:baz)
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.y.z.dig(:foo, :bar, :baz)
+      RUBY
+    end
+
+    it 'registers an offense and corrects with multiple values' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo, :bar).dig(:baz, :quux)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz, :quux)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.dig(:foo, :bar, :baz, :quux)
+      RUBY
+    end
+
+    it 'registers an offense and corrects with safe navigation' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo, :bar)&.dig(:baz, :quux)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz, :quux)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.dig(:foo, :bar, :baz, :quux)
+      RUBY
+    end
+
+    it 'registers an offense and corrects without a receiver' do
+      expect_offense(<<~RUBY)
+        dig(:foo, :bar).dig(:baz, :quux)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz, :quux)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        dig(:foo, :bar, :baz, :quux)
+      RUBY
+    end
+
+    it 'registers an offense and corrects with `::` instead of dot' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo, :bar)::dig(:baz, :quux)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz, :quux)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.dig(:foo, :bar, :baz, :quux)
+      RUBY
+    end
+
+    it 'registers an offense and corrects with splat' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo).dig(*names).dig(:bar)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, *names, :bar)` instead of chaining.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.dig(:foo, *names, :bar)
+      RUBY
+    end
+
+    it 'registers an offense and corrects when split over multiple lines' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo)
+          ^^^^^^^^^ Use `dig(:foo, :bar, :baz)` instead of chaining.
+         .dig(:bar)
+         .dig(:baz)
+      RUBY
+
+      expect_correction(<<~RUBY)
+        x.dig(:foo, :bar, :baz)
+      RUBY
+    end
+
+    it 'registers an offense and corrects when split over multiple lines with comments' do
+      expect_offense(<<~RUBY)
+        x.dig(:foo) # comment 1
+          ^^^^^^^^^^^^^^^^^^^^^ Use `dig(:foo, :bar, :baz)` instead of chaining.
+         .dig(:bar) # comment 2
+         .dig(:baz) # comment 3
+      RUBY
+
+      expect_correction(<<~RUBY)
+        # comment 1
+        # comment 2
+        x.dig(:foo, :bar, :baz) # comment 3
+      RUBY
+    end
+
+    context '`...` argument forwarding' do
+      it 'registers an offense and corrects with forwarded args' do
+        expect_offense(<<~RUBY)
+          def foo(...)
+            x.dig(:foo).dig(...)
+              ^^^^^^^^^^^^^^^^^^ Use `dig(:foo, ...)` instead of chaining.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          def foo(...)
+            x.dig(:foo, ...)
+          end
+        RUBY
+      end
+
+      it 'does not register an offense when combining would add a syntax error' do
+        expect_no_offenses(<<~RUBY)
+          def foo(...)
+            x.dig(...).dig(:foo)
+            x.dig(...).dig(...)
+          end
+        RUBY
+      end
+
+      context 'Ruby >= 3.1', :ruby31 do
+        it 'does not register an offense when `dig` is given a forwarded anonymous block' do
+          expect_no_offenses(<<~RUBY)
+            def foo(&)
+              x.dig(&).dig(:foo)
+            end
+          RUBY
+        end
+      end
+
+      context 'Ruby >= 3.2', :ruby32 do
+        it 'registers an offense and corrects with forwarded anonymous splat' do
+          expect_offense(<<~RUBY)
+            def foo(*)
+              x.dig(*).dig(*)
+                ^^^^^^^^^^^^^ Use `dig(*, *)` instead of chaining.
+            end
+          RUBY
+
+          expect_correction(<<~RUBY)
+            def foo(*)
+              x.dig(*, *)
+            end
+          RUBY
+        end
+
+        it 'does not register an offense with forwarded anonymous kwsplat' do
+          expect_no_offenses(<<~RUBY)
+            def foo(**)
+              x.dig(**).dig(:foo)
+            end
+          RUBY
+        end
+      end
+    end
+  end
+end
