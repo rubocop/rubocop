64c6a5f891c3e6f7603e9c26502c165bf175285f
Koichi ITO <koic.ito@gmail.com>
2024-12-09 12:50:49 +0900

    [Fix #13556] Fix false positives for `Style/FileNull`
    Fixes #13556.
    
    This PR fixes false positives for `Style/FileNull` when using `'nul'` string.
    
    This PR ensures only files that use the string `'/dev/null'` are targeted for detection.
    This is because the string `'nul'` is not limited to the null device.
    This behavior results in false negatives when the `'/dev/null'` string is not used,
    but it is a trade-off to avoid false positives.

--- lib/rubocop/cop/style/file_null.rb
+++ lib/rubocop/cop/style/file_null.rb
@@ -8,6 +8,12 @@ module RuboCop
       # Only looks for full string matches, substrings within a longer string are not
       # considered.
       #
+      # However, only files that use the string `'/dev/null'` are targeted for detection.
+      # This is because the string `'NUL'` is not limited to the null device.
+      # This behavior results in false negatives when the `'/dev/null'` string is not used,
+      # but it is a trade-off to avoid false positives. `NULL:`
+      # Unlike `'NUL'`, `'NUL:'` is regarded as something like `C:` and is always detected.
+      #
       # NOTE: Uses inside arrays and hashes are ignored.
       #
       # @safety
@@ -42,11 +48,21 @@ module RuboCop
         REGEXP = %r{\A(/dev/null|NUL:?)\z}i.freeze
         MSG = 'Use `File::NULL` instead of `%<source>s`.'
 
+        def on_new_investigation
+          return unless (ast = processed_source.ast)
+
+          @contain_dev_null_string_in_file = ast.each_descendant(:str).any? do |str|
+            content = str.str_content
+
+            valid_string?(content) && content.downcase == '/dev/null' # rubocop:disable Style/FileNull
+          end
+        end
+
         def on_str(node)
           value = node.value
-
-          return if invalid_string?(value)
+          return unless valid_string?(value)
           return if acceptable?(node)
+          return if value.downcase == 'nul' && !@contain_dev_null_string_in_file # rubocop:disable Style/FileNull
           return unless REGEXP.match?(value)
 
           add_offense(node, message: format(MSG, source: value)) do |corrector|
@@ -56,8 +72,8 @@ module RuboCop
 
         private
 
-        def invalid_string?(value)
-          value.empty? || !value.valid_encoding?
+        def valid_string?(value)
+          !value.empty? && value.valid_encoding?
         end
 
         def acceptable?(node)
