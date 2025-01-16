16f949d2e1ee3d8743729d062d66851281a1c092
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2025-01-16 12:23:12 -0500

    Add support for safe navigation to `Layout/MultilineMethodCallBraceLayout`

---
+++ changelog/change_add_support_for_safe_navigation_to_20250116122319.md
@@ -0,0 +1,1 @@
+* [#13711](https://github.com/rubocop/rubocop/pull/13711): Add support for safe navigation to `Layout/MultilineMethodCallBraceLayout`. ([@dvandersluis][])
--- spec/rubocop/cop/layout/multiline_method_call_brace_layout_spec.rb
+++ spec/rubocop/cop/layout/multiline_method_call_brace_layout_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Layout::MultilineMethodCallBraceLayout, :config do
-  let(:cop_config) { { 'EnforcedStyle' => 'symmetrical' } }
+  let(:enforced_style) { 'symmetrical' }
+  let(:cop_config) { { 'EnforcedStyle' => enforced_style } }
 
   it 'ignores implicit calls' do
     expect_no_offenses(<<~RUBY)
@@ -80,7 +81,7 @@ RSpec.describe RuboCop::Cop::Layout::MultilineMethodCallBraceLayout, :config do
   end
 
   context 'when EnforcedStyle is new_line' do
-    let(:cop_config) { { 'EnforcedStyle' => 'new_line' } }
+    let(:enforced_style) { 'new_line' }
 
     it 'still ignores single-line calls' do
       expect_no_offenses('puts("Hello world!")')
@@ -115,6 +116,66 @@ RSpec.describe RuboCop::Cop::Layout::MultilineMethodCallBraceLayout, :config do
         super(bar(baz,
           ham)) # comment
       RUBY
+    end
+  end
+
+  context 'with safe navigation' do
+    it 'ignores single-line calls' do
+      expect_no_offenses('foo&.bar(1,2)')
+    end
+
+    context 'with EnforcedStyle: symmetrical' do
+      let(:enforced_style) { 'symmetrical' }
+
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          foo&.bar(
+            baz)
+               ^ Closing method call brace must be on the line after the last argument when opening brace is on a separate line from the first argument.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo&.bar(
+            baz
+          )
+        RUBY
+      end
+    end
+
+    context 'with EnforcedStyle: new_line' do
+      let(:enforced_style) { 'new_line' }
+
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          foo&.bar(
+            baz)
+               ^ Closing method call brace must be on the line after the last argument.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo&.bar(
+            baz
+          )
+        RUBY
+      end
+    end
+
+    context 'with EnforcedStyle: same_line' do
+      let(:enforced_style) { 'same_line' }
+
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          foo&.bar(
+            baz
+          )
+          ^ Closing method call brace must be on the same line as the last argument.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo&.bar(
+            baz)
+        RUBY
+      end
     end
   end
 end
