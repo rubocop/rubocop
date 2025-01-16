57207104fc9d33d6525cc102ceeaa3a864ba806e
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2025-01-16 14:13:18 -0500

    Add support for safe navigation to `InternalAffairs/LocationExpression`

--- spec/rubocop/cop/internal_affairs/location_expression_spec.rb
+++ spec/rubocop/cop/internal_affairs/location_expression_spec.rb
@@ -12,6 +12,17 @@ RSpec.describe RuboCop::Cop::InternalAffairs::LocationExpression, :config do
     RUBY
   end
 
+  it 'registers and corrects an offense when using `node&.location&.expression`' do
+    expect_offense(<<~RUBY)
+      node&.location&.expression
+            ^^^^^^^^^^^^^^^^^^^^ Use `source_range` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node&.source_range
+    RUBY
+  end
+
   it 'registers and corrects an offense when using `loc.expression`' do
     expect_offense(<<~RUBY)
       node.loc.expression
@@ -20,6 +31,17 @@ RSpec.describe RuboCop::Cop::InternalAffairs::LocationExpression, :config do
 
     expect_correction(<<~RUBY)
       node.source_range
+    RUBY
+  end
+
+  it 'registers and corrects an offense when using `node&.loc&.expression`' do
+    expect_offense(<<~RUBY)
+      node&.loc&.expression
+            ^^^^^^^^^^^^^^^ Use `source_range` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node&.source_range
     RUBY
   end
 
@@ -34,9 +56,26 @@ RSpec.describe RuboCop::Cop::InternalAffairs::LocationExpression, :config do
     RUBY
   end
 
+  it 'registers and corrects an offense when using `node&.loc&.expression&.end_pos`' do
+    expect_offense(<<~RUBY)
+      node&.loc&.expression&.end_pos
+            ^^^^^^^^^^^^^^^ Use `source_range` instead.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node&.source_range&.end_pos
+    RUBY
+  end
+
   it 'does not register an offense when using `location.expression` without a receiver' do
     expect_no_offenses(<<~RUBY)
       location.expression
+    RUBY
+  end
+
+  it 'does not register an offense when using `location&.expression` without a receiver' do
+    expect_no_offenses(<<~RUBY)
+      location&.expression
     RUBY
   end
 
@@ -46,9 +85,21 @@ RSpec.describe RuboCop::Cop::InternalAffairs::LocationExpression, :config do
     RUBY
   end
 
+  it 'does not register an offense when using `loc&.expression` without a receiver' do
+    expect_no_offenses(<<~RUBY)
+      loc&.expression
+    RUBY
+  end
+
   it 'does not register an offense when assigning `node.location`' do
     expect_no_offenses(<<~RUBY)
       loc = node.location
+    RUBY
+  end
+
+  it 'does not register an offense when assigning `node&.location`' do
+    expect_no_offenses(<<~RUBY)
+      loc = node&.location
     RUBY
   end
 end
