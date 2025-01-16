ce643899e4ef7c25326159ff5678e8e89c0ca602
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2025-01-16 13:31:53 -0500

    Add support for safe navigation to `InternalAffairs/RedundantSourceRange`
    I only updated the cop for `node&.source_range&.source`, because the other variations (eg. `add_offense(node.source_range)`, `corrector.replace(node.source_range, ...)` don't make sense to use when a potentially nil node.

--- spec/rubocop/cop/internal_affairs/redundant_source_range_spec.rb
+++ spec/rubocop/cop/internal_affairs/redundant_source_range_spec.rb
@@ -12,9 +12,26 @@ RSpec.describe RuboCop::Cop::InternalAffairs::RedundantSourceRange, :config do
     RUBY
   end
 
+  it 'registers an offense when using `node.source_range&.source`' do
+    expect_offense(<<~RUBY)
+      node&.source_range&.source
+            ^^^^^^^^^^^^ Remove the redundant `source_range`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node&.source
+    RUBY
+  end
+
   it 'does not register an offense when using `node.source`' do
     expect_no_offenses(<<~RUBY)
       node.source
+    RUBY
+  end
+
+  it 'does not register an offense when using `node&.source`' do
+    expect_no_offenses(<<~RUBY)
+      node&.source
     RUBY
   end
 
