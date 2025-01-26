794437ec0f7b2121d419cbf085ac47b590889cb1
Koichi ITO <koic.ito@gmail.com>
2025-01-26 15:23:59 +0900

    Revert "Update `EnforcedStyle` of `RSpec/ExampleWithoutDescription`"
    This reverts commit 2dde71f8814656d9a1f0f19d3cfbb79bbd455d0a.
    
    CI is failing.
    https://github.com/rubocop/rubocop/actions/runs/12972031433/job/36179057539

--- spec/rubocop/lsp/severity_spec.rb
+++ spec/rubocop/lsp/severity_spec.rb
@@ -7,38 +7,52 @@ RSpec.describe RuboCop::LSP::Severity do
     context 'when RuboCop severity is fatal' do
       let(:rubocop_severity) { 'fatal' }
 
-      it { is_expected.to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::ERROR) }
+      it {
+        expect(lsp_severity).to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::ERROR)
+      }
     end
 
     context 'when RuboCop severity is error' do
       let(:rubocop_severity) { 'error' }
 
-      it { is_expected.to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::ERROR) }
+      it {
+        expect(lsp_severity).to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::ERROR)
+      }
     end
 
     context 'when RuboCop severity is warning' do
       let(:rubocop_severity) { 'warning' }
 
-      it { is_expected.to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::WARNING) }
+      it {
+        expect(lsp_severity).to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::WARNING)
+      }
     end
 
     context 'when RuboCop severity is convention' do
       let(:rubocop_severity) { 'convention' }
 
-      it { is_expected.to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::INFORMATION) }
+      it {
+        expect(lsp_severity).to eq(
+          LanguageServer::Protocol::Constant::DiagnosticSeverity::INFORMATION
+        )
+      }
     end
 
     context 'when RuboCop severity is refactor' do
       let(:rubocop_severity) { 'refactor' }
 
-      it { is_expected.to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::HINT) }
+      it {
+        expect(lsp_severity).to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::HINT)
+      }
     end
 
     context 'when RuboCop severity is unknown severity' do
       let(:rubocop_severity) { 'UNKNOWN' }
 
-      it { is_expected.to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::HINT) }
-      it { expect { lsp_severity }.to output("[server] Unknown severity: UNKNOWN\n").to_stderr }
+      it {
+        expect { lsp_severity }.to output("[server] Unknown severity: UNKNOWN\n").to_stderr
+        expect(lsp_severity).to eq(LanguageServer::Protocol::Constant::DiagnosticSeverity::HINT)
+      }
     end
   end
 end
