8b2f448d17aff19b414123654475d7d44a4230e4
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-01-28 21:37:09 +0100

    Fix a false negative for `Layout/SingleLineBlockChain` with numblocks

---
+++ changelog/fix_single_line_block_chain_numblock.md
@@ -0,0 +1,1 @@
+* [#13764](https://github.com/rubocop/rubocop/pull/13764): Fix a false negative for `Layout/SingleLineBlockChain` with numblocks. ([@earlopain][])
--- spec/rubocop/cop/layout/single_line_block_chain_spec.rb
+++ spec/rubocop/cop/layout/single_line_block_chain_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe RuboCop::Cop::Layout::SingleLineBlockChain, :config do
     RUBY
   end
 
+  it 'registers an offense for method call chained on the same line as a numblock' do
+    expect_offense(<<~RUBY)
+      example.select { _1.cond? }.join('-')
+                                 ^^^^^ Put method call on a separate line if chained to a single line block.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      example.select { _1.cond? }
+      .join('-')
+    RUBY
+  end
+
   it 'registers an offense for safe navigation method call chained on the same line as a block' do
     expect_offense(<<~RUBY)
       example.select { |item| item.cond? }&.join('-')
