rubocop/rubocop

commit 8acafc7b0ecb09153dab1afe71db6c5983dc825f

Extension plugin is loaded automatically with `require 'rubocop/rspec/support'`
With this change, extension plugins that use `require 'rubocop/rspec/support'` for testing no longer need to include
the following code in `spec/spec_helper.rb`.

```ruby
project_root = Pathname.new(__dir__).join('..')
RuboCop::ConfigLoader.inject_defaults!(project_root.join('config', 'default.yml'))
RuboCop::ConfigObsoletion.files << project_root.join('config', 'obsoletion.yml')
```

https://github.com/rubocop/rubocop-rails/blob/0e23cab4232cde7b1a74bafc758d19784576c527/spec/spec_helper.rb#L8-L10

In other words, extension plugins using RSpec can remove code related to `Inject`.

Accordingly, the purpose of `RuboCop::ConfigLoader.inject_defaults!` will be clarified and documented.
When testing a plugin using `rubocop/rspec/support`, the plugin is loaded automatically,
so this API is usually not needed. It is intended to be used only when implementing tests
that do not use `rubocop/rspec/support`.
It may become unnecessary as a public API in the future, but it is being retained for now as a precaution.

lib/rubocop/rspec/cop_helper.rb
@@ -13,6 +13,13 @@ module CopHelper
   let(:parser_engine) { ENV.fetch('PARSER_ENGINE', :parser_whitequark).to_sym }
   let(:rails_version) { false }
 
+  before(:all) do
+    plugins = Gem.loaded_specs.filter_map do |feature_name, feature_specification|
+      feature_name if feature_specification.metadata['default_lint_roller_plugin']
+    end
+    RuboCop::Plugin.integrate_plugins(RuboCop::Config.new, plugins)
+  end
+
   def inspect_source(source, file = nil)
     RuboCop::Formatter::DisabledConfigFormatter.config_to_allow_offenses = {}
     RuboCop::Formatter::DisabledConfigFormatter.detected_styles = {}

lib/rubocop/config_loader.rb
@@ -163,6 +163,10 @@ module RuboCop
         end
       end
 
+      # This API is primarily intended for testing and documenting plugins.
+      # When testing a plugin using `rubocop/rspec/support`, the plugin is loaded automatically,
+      # so this API is usually not needed. It is intended to be used only when implementing tests
+      # that do not use `rubocop/rspec/support`.
       # rubocop:disable Metrics/MethodLength
       def inject_defaults!(config_yml_path)
         if Pathname(config_yml_path).directory?

commit 68698570b93eebb059f20f627b145927c9aef7b2

Use plugified RuboCop Performance (1.24) for development
This commit uses plugified RuboCop Performance (1.24) for development and suppresses the following warning:

```console
$ bundle exec rubocop
rubocop-performance extension supports plugin, specify `plugins: rubocop-performance`
instead of `require: rubocop-performance` in /Users/koic/src/github.com/rubocop/rubocop/.rubocop.yml.
For more information, see https://docs.rubocop.org/rubocop/plugin_migration_guide.html.
```

Furthermore, by skipping unnecessary processing in RuboCop's core tests, the following error is prevented.

```console
$ bundle exec rspec spec/rubocop/config_store_spec.rb:23
Run options: include {locations: {"./spec/rubocop/config_store_spec.rb" => [23]}}

Randomized with seed 950
F

Failures:

  1) RuboCop::ConfigStore.for always uses config specified in command line
     Failure/Error: @enrollment_queue = []

     FrozenError:
       can't modify frozen RuboCop::Cop::Registry: #<RuboCop::Cop::Registry:0x0000000106a92958
@registry={#<RuboCop::Cop::Badge:0x0000000124b08cc0 @department=:Migration, @department_name="Migration", @cop_name="DepartmentName",
```

This is a bug in the RuboCop core development environment introduced in https://github.com/rubocop/rubocop/pull/13840.

To determine this, `ENV['RUBOCOP_CORE_DEVELOPMENT'] = true` is set in `spec/spec_helper.rb` of RuboCop core development environment,
allowing tests to check whether they are being executed from RuboCop core development environment.

But this change does not affect extension gems. So, this change does not affect third-party extensions, so there is no changelog entry.

.rubocop.yml
@@ -4,9 +4,9 @@ inherit_from: .rubocop_todo.yml
 
 plugins:
   - rubocop-internal_affairs
+  - rubocop-performance
 
 require:
-  - rubocop-performance
   - rubocop-rspec
   - rubocop-rake
 

Gemfile
@@ -13,7 +13,7 @@ gem 'memory_profiler', '!= 1.0.2', platform: :mri
 gem 'prism', '~> 1.2'
 gem 'rake', '~> 13.0'
 gem 'rspec', '~> 3.7'
-gem 'rubocop-performance', '~> 1.23.0'
+gem 'rubocop-performance', '~> 1.24.0'
 gem 'rubocop-rake', '~> 0.6.0'
 gem 'rubocop-rspec', '~> 3.4.0'
 # Ruby LSP supports Ruby 3.0+.

lib/rubocop/rspec/cop_helper.rb
@@ -14,6 +14,8 @@ module CopHelper
   let(:rails_version) { false }
 
   before(:all) do
+    next if ENV['RUBOCOP_CORE_DEVELOPMENT']
+
     plugins = Gem.loaded_specs.filter_map do |feature_name, feature_specification|
       feature_name if feature_specification.metadata['default_lint_roller_plugin']
     end

spec/spec_helper.rb
@@ -21,6 +21,10 @@ rescue LoadError
   # Pry is not activated.
 end
 
+# NOTE: To avoid executing processes needed only for extension plugins in `rubocop/rspec/support`,
+# an environment variable is used to indicate that it is running in the RuboCop core development.
+ENV['RUBOCOP_CORE_DEVELOPMENT'] = 'true'
+
 # Require supporting files exposed for testing.
 require 'rubocop/rspec/support'
 