b6678159b618a9274d56fd4a95310fa48f36666c
Koichi ITO <koic.ito@gmail.com>
2025-03-12 11:26:34 +0900

    [Fix #13975] Fix false positives for `Style/RedundantCurrentDirectoryInPath`
    This PR fixes false positives for `Style/RedundantCurrentDirectoryInPath`
    when using a complex current directory path in `require_relative`.
    
    Fixes #13975.

--- lib/rubocop/cop/style/redundant_current_directory_in_path.rb
+++ lib/rubocop/cop/style/redundant_current_directory_in_path.rb
@@ -20,19 +20,29 @@ module RuboCop
 
         MSG = 'Remove the redundant current directory path.'
         RESTRICT_ON_SEND = %i[require_relative].freeze
-        CURRENT_DIRECTORY_PATH = './'
+        CURRENT_DIRECTORY_PREFIX = %r{./+}.freeze
+        REDUNDANT_CURRENT_DIRECTORY_PREFIX = /\A#{CURRENT_DIRECTORY_PREFIX}/.freeze
 
         def on_send(node)
           return unless (first_argument = node.first_argument)
-          return unless first_argument.str_content&.start_with?(CURRENT_DIRECTORY_PATH)
-          return unless (index = first_argument.source.index(CURRENT_DIRECTORY_PATH))
+          return unless (index = first_argument.source.index(CURRENT_DIRECTORY_PREFIX))
+          return unless (redundant_length = redundant_path_length(first_argument.str_content))
 
           begin_pos = first_argument.source_range.begin.begin_pos + index
-          range = range_between(begin_pos, begin_pos + 2)
+          end_pos = begin_pos + redundant_length
+          range = range_between(begin_pos, end_pos)
 
           add_offense(range) do |corrector|
             corrector.remove(range)
           end
+        end
+
+        private
+
+        def redundant_path_length(path)
+          return unless (match = path&.match(REDUNDANT_CURRENT_DIRECTORY_PREFIX))
+
+          match[0].length
         end
       end
     end
