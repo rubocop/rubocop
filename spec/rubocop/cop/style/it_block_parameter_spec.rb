5342a6e5002f3c3bd8642c75eb06091d245555ea
Koichi ITO <koic.ito@gmail.com>
2025-03-25 00:00:52 +0900

    [Fix #13597] Add new `Style/ItBlockParameter` cop
    This PR adds new `Style/ItBlockParameter` cop that
    checks for replacement with or disallowing of the `it` block parameter.
    
    It provides three `EnforcedStyle` options:
    
    1. `allow_named_parameter` (default) ... Detects only numbered block parameters.
    2. `always` ... Always uses the `it` block parameter.
    3. `disallow` ... Disallows the `it` block parameter.
    
    A single numbered parameter is detected when `allow_named_parameter` or `always`.
    
    ## `EnforcedStyle: allow_named_parameter` (default)
    
    ```ruby
    # bad
    block { do_something(_1) }
    
    # good
    block { do_something(it) }
    block { |param| do_something(named_param) }
    ```
    
    ## `EnforcedStyle: always`
    
    ```ruby
    # bad
    block { do_something(_1) }
    block { |param| do_something(named_param) }
    
    # good
    block { do_something(it) }
    ```
    
    ## `EnforcedStyle: disallow`
    
    ```ruby
    # bad
    block { do_something(it) }
    
    # good
    block { do_something(_1) }
    block { |param| do_something(named_param) }
    ```
    
    If a parameter is appropriately named, it should be respected. Also, there seems to be no reason to prefer `_1` when `it` can be used instead.
    The default style `EnforcedStyle: allow_named_parameter` reflects these considerations.
    
    Closes #13597.

---
+++ spec/rubocop/cop/style/it_block_parameter_spec.rb
@@ -0,0 +1,236 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::ItBlockParameter, :config do
+  context '>= Ruby 3.4', :ruby34 do
+    context 'EnforcedStyle: allow_named_parameter' do
+      let(:cop_config) { { 'EnforcedStyle' => 'allow_named_parameter' } }
+
+      it 'registers an offense when using a single numbered parameters' do
+        expect_offense(<<~RUBY)
+          block { do_something(_1) }
+                               ^^ Use `it` block parameter.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+
+      it 'registers an offense when using twice a single numbered parameters' do
+        expect_offense(<<~RUBY)
+          block do
+            foo(_1)
+                ^^ Use `it` block parameter.
+            bar(_1)
+                ^^ Use `it` block parameter.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block do
+            foo(it)
+            bar(it)
+          end
+        RUBY
+      end
+
+      it 'does not register an offense when using `it` block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+
+      it 'does not register an offense when using named block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { |arg| do_something(arg) }
+        RUBY
+      end
+
+      it 'does not register an offense when using multiple numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1, _2) }
+        RUBY
+      end
+
+      it 'does not register an offense when using a single numbered parameters `_2`' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_2) }
+        RUBY
+      end
+    end
+
+    context 'EnforcedStyle: always' do
+      let(:cop_config) { { 'EnforcedStyle' => 'always' } }
+
+      it 'registers an offense when using a single numbered parameters' do
+        expect_offense(<<~RUBY)
+          block { do_something(_1) }
+                               ^^ Use `it` block parameter.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+
+      it 'registers an offense when using twice a single numbered parameters' do
+        expect_offense(<<~RUBY)
+          block do
+            foo(_1)
+                ^^ Use `it` block parameter.
+            bar(_1)
+                ^^ Use `it` block parameter.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block do
+            foo(it)
+            bar(it)
+          end
+        RUBY
+      end
+
+      it 'registers an offense when using a single named block parameters' do
+        expect_offense(<<~RUBY)
+          block { |arg| do_something(arg) }
+                                     ^^^ Use `it` block parameter.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block {  do_something(it) }
+        RUBY
+      end
+
+      it 'registers an offense when using twice a single named parameters' do
+        expect_offense(<<~RUBY)
+          block do |arg|
+            foo(arg)
+                ^^^ Use `it` block parameter.
+            bar(arg)
+                ^^^ Use `it` block parameter.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block do#{' '}
+            foo(it)
+            bar(it)
+          end
+        RUBY
+      end
+
+      it 'does not register an offense when using `it` block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+
+      it 'does not register an offense when using multiple numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1, _2) }
+        RUBY
+      end
+
+      it 'does not register an offense when using a single numbered parameters `_2`' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_2) }
+        RUBY
+      end
+
+      it 'does not register an offense when using multiple named block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { |foo, bar| do_something(foo, bar) }
+        RUBY
+      end
+    end
+
+    context 'EnforcedStyle: disallow' do
+      let(:cop_config) { { 'EnforcedStyle' => 'disallow' } }
+
+      it 'registers an offense when using `it` block parameters', unsupported_on: :parser do
+        expect_offense(<<~RUBY)
+          block { do_something(it) }
+                               ^^ Avoid using `it` block parameter.
+        RUBY
+
+        expect_no_corrections
+      end
+
+      it 'registers an offense when using twice `it` block parameters', unsupported_on: :parser do
+        expect_offense(<<~RUBY)
+          block do
+            foo(it)
+                ^^ Avoid using `it` block parameter.
+            bar(it)
+                ^^ Avoid using `it` block parameter.
+          end
+        RUBY
+
+        expect_no_corrections
+      end
+
+      it 'does not register an offense when using a single numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1) }
+        RUBY
+      end
+
+      it 'does not register an offense when using named block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { |arg| do_something(arg) }
+        RUBY
+      end
+
+      it 'does not register an offense when using multiple numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1, _2) }
+        RUBY
+      end
+
+      it 'does not register an offense when using a single numbered parameters `_2`' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_2) }
+        RUBY
+      end
+    end
+  end
+
+  context '<= Ruby 3.3', :ruby33 do
+    context 'EnforcedStyle: allow_named_parameter' do
+      let(:cop_config) { { 'EnforcedStyle' => 'allow_named_parameter' } }
+
+      it 'does not register an offense when using a single numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1) }
+        RUBY
+      end
+    end
+
+    context 'EnforcedStyle: always' do
+      let(:cop_config) { { 'EnforcedStyle' => 'always' } }
+
+      it 'does not register an offense when using a single numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1) }
+        RUBY
+      end
+
+      it 'does not register an offense when using a single named block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { |arg| do_something(arg) }
+        RUBY
+      end
+    end
+
+    context 'EnforcedStyle: disallow' do
+      let(:cop_config) { { 'EnforcedStyle' => 'disallow' } }
+
+      it 'does not register an offense when using `it` block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+    end
+  end
+end
