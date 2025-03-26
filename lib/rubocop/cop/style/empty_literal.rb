2fd515cd6b6288780ba6d116367ea56ca0b3664c
Lovro Bikic <lovro.bikic@infinum.hr>
2025-03-26 22:13:42 +0100

    Add note about frozen string literals for Style/EmptyLiteral

--- lib/rubocop/cop/style/empty_literal.rb
+++ lib/rubocop/cop/style/empty_literal.rb
@@ -6,6 +6,10 @@ module RuboCop
       # Checks for the use of a method, the result of which
       # would be a literal, like an empty array, hash, or string.
       #
+      # NOTE: When frozen string literals are enabled, `String.new`
+      # isn't corrected to an empty string since the former is
+      # mutable and the latter would be frozen.
+      #
       # @example
       #   # bad
       #   a = Array.new
