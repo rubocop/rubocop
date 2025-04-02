5f7ea1de99e01d3ac2d4e4274a58cc088384042d
Koichi ITO <koic.ito@gmail.com>
2025-04-02 14:47:25 +0900

    Add `EnforcedStyle: allow_single_line` as the default to `Style/ItBlockParameter`
    This PR adds `EnforcedStyle: allow_single_line` as the default to `Style/ItBlockParameter`.
    
    This adds the default configuration that follows `EnforcedStyle: allow_single_line (default)` of
    `Style/NumberedParameters` cop:
    https://docs.rubocop.org/rubocop/1.75/cops_style.html#stylenumberedparameters
    
    In RuboCop, numbered block parameters are allowed only in single-line blocks.
    It makes sense to apply the same default style to `it` block parameters as well.

--- spec/rubocop/cop/style/it_block_parameter_spec.rb
+++ spec/rubocop/cop/style/it_block_parameter_spec.rb
@@ -2,6 +2,74 @@
 
 RSpec.describe RuboCop::Cop::Style::ItBlockParameter, :config do
   context '>= Ruby 3.4', :ruby34 do
+    context 'EnforcedStyle: allow_single_line' do
+      let(:cop_config) { { 'EnforcedStyle' => 'allow_single_line' } }
+
+      it 'registers an offense when using multiline `it` parameters', unsupported_on: :parser do
+        expect_offense(<<~RUBY)
+          block do
+          ^^^^^^^^ Avoid using numbered parameters for multi-line blocks.
+            do_something(it)
+          end
+        RUBY
+
+        expect_no_corrections
+      end
+
+      it 'registers an offense when using a single numbered parameters' do
+        expect_offense(<<~RUBY)
+          block { do_something(_1) }
+                               ^^ Use `it` block parameter.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+
+      it 'registers an offense when using twice a single numbered parameters' do
+        expect_offense(<<~RUBY)
+          block do
+            foo(_1)
+                ^^ Use `it` block parameter.
+            bar(_1)
+                ^^ Use `it` block parameter.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          block do
+            foo(it)
+            bar(it)
+          end
+        RUBY
+      end
+
+      it 'does not register an offense when using `it` block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(it) }
+        RUBY
+      end
+
+      it 'does not register an offense when using named block parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { |arg| do_something(arg) }
+        RUBY
+      end
+
+      it 'does not register an offense when using multiple numbered parameters' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_1, _2) }
+        RUBY
+      end
+
+      it 'does not register an offense when using a single numbered parameters `_2`' do
+        expect_no_offenses(<<~RUBY)
+          block { do_something(_2) }
+        RUBY
+      end
+    end
+
     context 'EnforcedStyle: only_numbered_parameters' do
       let(:cop_config) { { 'EnforcedStyle' => 'only_numbered_parameters' } }
 
