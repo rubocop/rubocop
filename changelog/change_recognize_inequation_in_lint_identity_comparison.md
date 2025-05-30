1734d3ae59971f2c585115e59182568de3f122b4
Lovro Bikic <lovro.bikic@infinum.hr>
2025-05-30 10:55:56 +0200

    Recognize inequation in Lint/IdentityComparison

---
+++ changelog/change_recognize_inequation_in_lint_identity_comparison.md
@@ -0,0 +1,1 @@
+* [#14215](https://github.com/rubocop/rubocop/pull/14215): Recognize inequation (`!=`) in `Lint/IdentityComparison`. ([@lovro-bikic][])
--- spec/rubocop/cop/lint/identity_comparison_spec.rb
+++ spec/rubocop/cop/lint/identity_comparison_spec.rb
@@ -4,11 +4,22 @@ RSpec.describe RuboCop::Cop::Lint::IdentityComparison, :config do
   it 'registers an offense and corrects when using `==` for comparison between `object_id`s' do
     expect_offense(<<~RUBY)
       foo.object_id == bar.object_id
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `equal?` instead `==` when comparing `object_id`.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `equal?` instead of `==` when comparing `object_id`.
     RUBY
 
     expect_correction(<<~RUBY)
       foo.equal?(bar)
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using `!=` for comparison between `object_id`s' do
+    expect_offense(<<~RUBY)
+      foo.object_id != bar.object_id
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `!equal?` instead of `!=` when comparing `object_id`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      !foo.equal?(bar)
     RUBY
   end
 
@@ -18,27 +29,57 @@ RSpec.describe RuboCop::Cop::Lint::IdentityComparison, :config do
     RUBY
   end
 
+  it 'does not register an offense when using `!=` for comparison between `object_id` and other' do
+    expect_no_offenses(<<~RUBY)
+      foo.object_id != bar.do_something
+    RUBY
+  end
+
   it 'does not register an offense when a receiver that is not `object_id` uses `==`' do
     expect_no_offenses(<<~RUBY)
       foo.do_something == bar.object_id
     RUBY
   end
 
-  it 'does not register an offense when using `==`' do
+  it 'does not register an offense when a receiver that is not `object_id` uses `!=`' do
     expect_no_offenses(<<~RUBY)
-      foo.equal(bar)
+      foo.do_something != bar.object_id
     RUBY
   end
 
-  it 'does not register an offense when lhs is `object_id` without receiver' do
+  it 'does not register an offense when using `equal?`' do
+    expect_no_offenses(<<~RUBY)
+      foo.equal?(bar)
+    RUBY
+  end
+
+  it 'does not register an offense when using `!equal?`' do
+    expect_no_offenses(<<~RUBY)
+      !foo.equal?(bar)
+    RUBY
+  end
+
+  it 'does not register an offense when lhs is `object_id` without receiver when using `==`' do
     expect_no_offenses(<<~RUBY)
       object_id == bar.object_id
     RUBY
   end
 
-  it 'does not register an offense when rhs is `object_id` without receiver' do
+  it 'does not register an offense when lhs is `object_id` without receiver when using `!=`' do
+    expect_no_offenses(<<~RUBY)
+      object_id != bar.object_id
+    RUBY
+  end
+
+  it 'does not register an offense when rhs is `object_id` without receiver when using `==`' do
     expect_no_offenses(<<~RUBY)
       foo.object_id == object_id
+    RUBY
+  end
+
+  it 'does not register an offense when rhs is `object_id` without receiver when using `!=`' do
+    expect_no_offenses(<<~RUBY)
+      foo.object_id != object_id
     RUBY
   end
 end
