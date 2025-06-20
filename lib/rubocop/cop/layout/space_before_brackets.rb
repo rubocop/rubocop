600d0a22a1538d39156e85806e646dad95984c68
Koichi ITO <koic.ito@gmail.com>
2025-06-20 10:53:58 +0900

    [Fix #14313] Fix a false positive for `Layout/SpaceBeforeBrackets`
    This PR fixes a false positive for `Layout/SpaceBeforeBrackets` when
    call desugared `Hash#[]` to lvar receiver with a space around the dot.
    
    Fixes #14313.

--- lib/rubocop/cop/layout/space_before_brackets.rb
+++ lib/rubocop/cop/layout/space_before_brackets.rb
@@ -22,24 +22,17 @@ module RuboCop
         RESTRICT_ON_SEND = %i[[] []=].freeze
 
         def on_send(node)
+          return if node.loc.dot
+
           receiver_end_pos = node.receiver.source_range.end_pos
           selector_begin_pos = node.loc.selector.begin_pos
           return if receiver_end_pos >= selector_begin_pos
-          return if dot_before_brackets?(node, receiver_end_pos, selector_begin_pos)
 
           range = range_between(receiver_end_pos, selector_begin_pos)
 
           add_offense(range) do |corrector|
             corrector.remove(range)
           end
-        end
-
-        private
-
-        def dot_before_brackets?(node, receiver_end_pos, selector_begin_pos)
-          return false unless node.loc.respond_to?(:dot) && (dot = node.loc.dot)
-
-          dot.begin_pos == receiver_end_pos && dot.end_pos == selector_begin_pos
         end
       end
     end
