4ed2a3d278e97dce13fa40d60439c20a956da86f
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2025-07-15 09:45:29 -0400

    [Fix #14322] Expand the scope of `Style/ItAssignment` to consider all local variable and method parameter names

---
+++ changelog/change_expand_the_scope_of_it_assignment_to_consider_all_20250715094540.md
@@ -0,0 +1,1 @@
+* [#14322](https://github.com/rubocop/rubocop/issues/14322): Expand the scope of `Style/ItAssignment` to consider all local variable and method parameter names. ([@dvandersluis][])
--- lib/rubocop/cop/style/it_assignment.rb
+++ lib/rubocop/cop/style/it_assignment.rb
@@ -3,33 +3,90 @@
 module RuboCop
   module Cop
     module Style
-      # Checks for assignments to a local `it` variable inside a block
+      # Checks for local variables and method parameters named `it`,
       # where `it` can refer to the first anonymous parameter as of Ruby 3.4.
+      # Use a meaningful variable name instead.
       #
-      # Although Ruby allows reassigning `it` in these cases, it could
+      # NOTE: Although Ruby allows reassigning `it` in these cases, it could
       # cause confusion if `it` is used as a block parameter elsewhere.
-      # For consistency, this also applies to numblocks and blocks with
-      # parameters, even though `it` cannot be used in those cases.
       #
       # @example
       #   # bad
-      #   foo { it = 5 }
-      #   foo { |bar| it = bar }
-      #   foo { it = _2 }
+      #   it = 5
       #
-      #   # good - use a different variable name
-      #   foo { var = 5 }
-      #   foo { |bar| var = bar }
-      #   foo { bar = _2 }
+      #   # good
+      #   var = 5
+      #
+      #   # bad
+      #   def foo(it)
+      #   end
+      #
+      #   # good
+      #   def foo(arg)
+      #   end
+      #
+      #   # bad
+      #   def foo(it = 5)
+      #   end
+      #
+      #   # good
+      #   def foo(arg = 5)
+      #   end
+      #
+      #   # bad
+      #   def foo(*it)
+      #   end
+      #
+      #   # good
+      #   def foo(*args)
+      #   end
+      #
+      #   # bad
+      #   def foo(it:)
+      #   end
+      #
+      #   # good
+      #   def foo(arg:)
+      #   end
+      #
+      #   # bad
+      #   def foo(it: 5)
+      #   end
+      #
+      #   # good
+      #   def foo(arg: 5)
+      #   end
+      #
+      #   # bad
+      #   def foo(**it)
+      #   end
+      #
+      #   # good
+      #   def foo(**kwargs)
+      #   end
+      #
+      #   # bad
+      #   def foo(&it)
+      #   end
+      #
+      #   # good
+      #   def foo(&block)
+      #   end
       class ItAssignment < Base
         MSG = '`it` is the default block parameter; consider another name.'
 
         def on_lvasgn(node)
           return unless node.name == :it
-          return unless node.each_ancestor(:any_block).any?
 
           add_offense(node.loc.name)
         end
+        alias on_arg on_lvasgn
+        alias on_optarg on_lvasgn
+        alias on_restarg on_lvasgn
+        alias on_blockarg on_lvasgn
+        alias on_kwarg on_lvasgn
+        alias on_kwoptarg on_lvasgn
+        alias on_kwrestarg on_lvasgn
       end
     end
   end
