b776d32b4fa253a9b212489c3c4dea9a8845acba
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-08-12 10:45:22 +0200

    [Fix #14435] Fix false negatives for regexp cops when `Lint/DuplicateRegexpCharacterClassElement` is enabled
    It does `expressions = expressions.to_a`, which does not create a copy. So other cops
    that run after would not see all
    the expressions.
    
    `.dup` would solve this, but all this does is work around a bug in `regexp_parser` < 2.7.0.
    RuboCop now requires >= 2.9.3 so all that code can simply be removed.
    
    I didn't add a new test because the code that would be tested is gone now.
    
    Also see https://github.com/rubocop/rubocop/pull/13875 for something quite similar.

---
+++ changelog/fix_false_negatives_regexp_cops.md
@@ -0,0 +1,1 @@
+* [#14435](https://github.com/rubocop/rubocop/issues/14435): Fix false negatives for regexp cops when `Lint/DuplicateRegexpCharacterClassElement` is enabled. ([@earlopain][])
--- lib/rubocop/cop/lint/duplicate_regexp_character_class_element.rb
+++ lib/rubocop/cop/lint/duplicate_regexp_character_class_element.rb
@@ -24,8 +24,6 @@ module RuboCop
 
         MSG_REPEATED_ELEMENT = 'Duplicate element inside regexp character class'
 
-        OCTAL_DIGITS_AFTER_ESCAPE = 2
-
         def on_regexp(node)
           each_repeated_character_class_element_loc(node) do |loc|
             add_offense(loc, message: MSG_REPEATED_ELEMENT) do |corrector|
@@ -40,9 +38,9 @@ module RuboCop
 
             seen = Set.new
             group_expressions(node, expr.expressions) do |group|
-              group_source = group.map(&:to_s).join
+              group_source = group.to_s
 
-              yield source_range(group) if seen.include?(group_source)
+              yield group.expression if seen.include?(group_source)
 
               seen << group_source
             end
@@ -52,38 +50,11 @@ module RuboCop
         private
 
         def group_expressions(node, expressions)
-          # Create a mutable list to simplify state tracking while we iterate.
-          expressions = expressions.to_a
+          expressions.each do |expression|
+            next if within_interpolation?(node, expression)
 
-          until expressions.empty?
-            # With we may need to compose a group of multiple expressions.
-            group = [expressions.shift]
-            next if within_interpolation?(node, group.first)
-
-            # With regexp_parser < 2.7 escaped octal sequences may be up to 3
-            # separate expressions ("\\0", "0", "1").
-            pop_octal_digits(group, expressions) if escaped_octal?(group.first.to_s)
-
-            yield(group)
+            yield(expression)
           end
-        end
-
-        def pop_octal_digits(current_child, expressions)
-          OCTAL_DIGITS_AFTER_ESCAPE.times do
-            next_child = expressions.first
-            break unless octal?(next_child.to_s)
-
-            current_child << expressions.shift
-          end
-        end
-
-        def source_range(children)
-          return children.first.expression if children.size == 1
-
-          range_between(
-            children.first.expression.begin_pos,
-            children.last.expression.begin_pos + children.last.to_s.length
-          )
         end
 
         def skip_expression?(expr)
@@ -97,14 +68,6 @@ module RuboCop
           parse_tree_child_loc = child.expression
 
           interpolation_locs(node).any? { |il| il.overlaps?(parse_tree_child_loc) }
-        end
-
-        def escaped_octal?(string)
-          string.length == 2 && string[0] == '\\' && octal?(string[1])
-        end
-
-        def octal?(char)
-          ('0'..'7').cover?(char)
         end
 
         def interpolation_locs(node)
