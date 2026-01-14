4ae5606a79731adce6070f77cdc9479943316c6c
Koichi ITO <koic.ito@gmail.com>
2026-01-15 01:22:42 +0900

    Fix an error for `Style/GuardClause`
    This PR fixes the following error for `Style/GuardClause`
    when using heredoc as an argument of raise in `else` branch and `if` branch is empty.
    
    ```console
    $ cat example.rb
    def func
      if condition
      else
        raise <<~MESSAGE
          text
        MESSAGE
      end
    end
    
    $ bundle exec rubocop -a --only Style/GuardClause -d
    (snip)
    
    /Users/koic/src/github.com/rubocop/rubocop/lib/rubocop/cop/style/guard_clause.rb:230:
    in 'RuboCop::Cop::Style::GuardClause#autocorrect_heredoc_argument': undefined method 'source_range' for nil (NoMethodError)
    
              remove_whole_lines(corrector, leave_branch.source_range)
                                                        ^^^^^^^^^^^^^
            from /Users/koic/src/github.com/rubocop/rubocop/lib/rubocop/cop/style/guard_clause.rb:211:
            in 'RuboCop::Cop::Style::GuardClause#autocorrect'
    ```

--- spec/rubocop/cop/style/guard_clause_spec.rb
+++ spec/rubocop/cop/style/guard_clause_spec.rb
@@ -514,6 +514,30 @@ RSpec.describe RuboCop::Cop::Style::GuardClause, :config do
     RUBY
   end
 
+  it 'registers an offense when using heredoc as an argument of raise in `else` branch and `if` branch is empty' do
+    expect_offense(<<~RUBY)
+      def func
+        if condition
+        ^^ Use a guard clause (`raise <<~MESSAGE unless condition`) instead of wrapping the code inside a conditional expression.
+        else
+          raise <<~MESSAGE
+            oops
+          MESSAGE
+        end
+      end
+    RUBY
+
+    # NOTE: Let `Layout/HeredocIndentation`, `Layout/ClosingHeredocIndentation`, and
+    #       `Layout/IndentationConsistency` cops autocorrect inconsistent indentations.
+    expect_correction(<<~RUBY)
+      def func
+        raise <<~MESSAGE unless condition
+            oops
+          MESSAGE
+      end
+    RUBY
+  end
+
   it 'registers an offense when using heredoc as an argument of raise in `then` branch and it does not have `else` branch' do
     expect_offense(<<~RUBY)
       def func
