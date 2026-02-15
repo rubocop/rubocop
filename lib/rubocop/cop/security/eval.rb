fa648c740f251d18f7dbb8c71da3ebdb91c2a6e2
Bozhidar Batsov <bozhidar@batsov.dev>
2026-02-15 22:00:23 +0200

    Improve documentation for Security/Eval
    Add rationale explaining why dynamic eval is dangerous (code injection),
    document the nuance that literal strings are not flagged, and add good
    examples showing safer alternatives.

--- lib/rubocop/cop/security/eval.rb
+++ lib/rubocop/cop/security/eval.rb
@@ -3,15 +3,28 @@
 module RuboCop
   module Cop
     module Security
-      # Checks for the use of `Kernel#eval` and `Binding#eval`.
+      # Checks for the use of `Kernel#eval` and `Binding#eval` with
+      # dynamic strings as arguments. Evaluating non-literal strings
+      # can enable code injection attacks and makes it difficult to
+      # reason about what code will actually be executed.
+      #
+      # Calls to `eval` with literal strings are not flagged by this cop,
+      # as they do not pose the same injection risk.
       #
       # @example
       #
       #   # bad
-      #
       #   eval(something)
       #   binding.eval(something)
       #   Kernel.eval(something)
+      #
+      #   # good - use safer alternatives
+      #   obj.public_send(method_name)
+      #   obj.send(method_name, *args)
+      #
+      #   # good - literal strings are allowed
+      #   eval("1 + 1")
+      #   binding.eval("foo")
       class Eval < Base
         MSG = 'The use of `eval` is a serious security risk.'
         RESTRICT_ON_SEND = %i[eval].freeze
