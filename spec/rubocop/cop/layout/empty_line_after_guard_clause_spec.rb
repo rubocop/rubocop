rubocop/rubocop

commit f4247730509e7a69efcc99296094391e1a9fa06d

[Fix #15021] Fix false positives in `Layout/EmptyLineAfterGuardClause`
This PR fixes false positives in `Layout/EmptyLineAfterGuardClause`
when using a guard clause followed by a multi-line guard clause
with `raise`, `fail`, `return`, `break`, or `next`.

Fixes #15021.

lib/rubocop/cop/layout/empty_line_after_guard_clause.rb
@@ -60,6 +60,11 @@ module RuboCop
         END_OF_HEREDOC_LINE = 1
         SIMPLE_DIRECTIVE_COMMENT_PATTERN = /\A# *:nocov:\z/.freeze
 
+        # @!method guard_clause_branch?(node)
+        def_node_matcher :guard_clause_branch?, <<~PATTERN
+          {(send nil? {:raise :fail} ...) return break next}
+        PATTERN
+
         def on_if(node)
           return if correct_style?(node)
           return if multiple_statements_on_line?(node)
@@ -97,14 +102,16 @@ module RuboCop
         end
 
         def correct_style?(node)
-          !contains_guard_clause?(node) ||
+          !node.if_branch&.guard_clause? ||
             next_line_rescue_or_ensure?(node) ||
             next_sibling_parent_empty_or_else?(node) ||
             next_sibling_empty_or_guard_clause?(node)
         end
 
         def contains_guard_clause?(node)
-          node.if_branch&.guard_clause?
+          return false unless (branch = node.if_branch)
+
+          guard_clause_branch?(branch)
         end
 
         def next_line_empty_or_allowed_directive_comment?(line)

spec/rubocop/cop/layout/empty_line_after_guard_clause_spec.rb
@@ -399,6 +399,97 @@ RSpec.describe RuboCop::Cop::Layout::EmptyLineAfterGuardClause, :config do
     RUBY
   end
 
+  it 'accepts a guard clause followed by a multi-line guard clause with `raise`' do
+    expect_no_offenses(<<~RUBY)
+      def foo
+        return if something?
+        if something_else?
+          raise bar(
+            baz
+          )
+        end
+      end
+    RUBY
+  end
+
+  it 'accepts a guard clause followed by a multi-line guard clause with `fail`' do
+    expect_no_offenses(<<~RUBY)
+      def foo
+        return if something?
+        if something_else?
+          fail bar(
+            baz
+          )
+        end
+      end
+    RUBY
+  end
+
+  it 'accepts a guard clause followed by a multi-line guard clause with `return`' do
+    expect_no_offenses(<<~RUBY)
+      def foo
+        return if something?
+        if something_else?
+          return bar(
+            baz
+          )
+        end
+      end
+    RUBY
+  end
+
+  it 'accepts a guard clause followed by a multi-line guard clause with `break`' do
+    expect_no_offenses(<<~RUBY)
+      collection.each do |item|
+        break if something?
+        if something_else?
+          break bar(
+            baz
+          )
+        end
+      end
+    RUBY
+  end
+
+  it 'accepts a guard clause followed by a multi-line guard clause with `next`' do
+    expect_no_offenses(<<~RUBY)
+      collection.each do |item|
+        next if something?
+        if something_else?
+          next bar(
+            baz
+          )
+        end
+      end
+    RUBY
+  end
+
+  it 'registers an offense for a guard clause followed by a multi-line if with non-guard body' do
+    expect_offense(<<~RUBY)
+      def foo
+        return if something?
+        ^^^^^^^^^^^^^^^^^^^^ Add empty line after guard clause.
+        if something_else?
+          bar(
+            baz
+          )
+        end
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      def foo
+        return if something?
+
+        if something_else?
+          bar(
+            baz
+          )
+        end
+      end
+    RUBY
+  end
+
   it 'accepts a modifier if when the next line is `end`' do
     expect_no_offenses(<<~RUBY)
       def foo

commit 49ad1439bbec95bf17c4bf3ddc1cff6b6ae3ab9d

Fix `EmptyLineAfterGuardClause` for consecutive `and return` guards
f4247730509e7a69efcc99296094391e1a9fa06d introduced a regression where
consecutive guard clauses using `and return` were no longer treated as
a group, requiring blank lines between them. Fix by also checking
`Node#guard_clause?` which already handles compound guard clauses.

lib/rubocop/cop/layout/empty_line_after_guard_clause.rb
@@ -111,7 +111,7 @@ module RuboCop
         def contains_guard_clause?(node)
           return false unless (branch = node.if_branch)
 
-          guard_clause_branch?(branch)
+          branch.guard_clause? || guard_clause_branch?(branch)
         end
 
         def next_line_empty_or_allowed_directive_comment?(line)

spec/rubocop/cop/layout/empty_line_after_guard_clause_spec.rb
@@ -399,6 +399,17 @@ RSpec.describe RuboCop::Cop::Layout::EmptyLineAfterGuardClause, :config do
     RUBY
   end
 
+  it 'accepts multiple guard clauses using `and return`' do
+    expect_no_offenses(<<~RUBY)
+      def foo
+        render :bar and return if condition1?
+        render :baz and return if condition2?
+
+        foobar
+      end
+    RUBY
+  end
+
   it 'accepts a guard clause followed by a multi-line guard clause with `raise`' do
     expect_no_offenses(<<~RUBY)
       def foo