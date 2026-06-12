061235d64fe337324f59234fc8ac558b82a526d4
hammadxcm <hammadkhanxcm@gmail.com>
2026-06-13 04:24:27 +0500

    [Fix #15042] Add `DisallowedCops` option to `Style/DisableCopsWithinSourceCodeDirective`

--- spec/rubocop/cop/style/disable_cops_within_source_code_directive_spec.rb
+++ spec/rubocop/cop/style/disable_cops_within_source_code_directive_spec.rb
@@ -93,4 +93,114 @@ RSpec.describe RuboCop::Cop::Style::DisableCopsWithinSourceCodeDirective, :confi
       end
     end
   end
+
+  context 'with DisallowedCops' do
+    let(:cop_config) { { 'DisallowedCops' => ['Lint/Void', 'Security/Eval'] } }
+
+    context 'when a disallowed cop is disabled' do
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          foo # rubocop:disable Lint/Void
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^ RuboCop disable/enable directives for `Lint/Void` are not permitted.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo#{trailing_whitespace}
+        RUBY
+      end
+    end
+
+    context 'when a non-disallowed cop is disabled' do
+      it 'does not register an offense' do
+        expect_no_offenses(<<~RUBY)
+          def foo # rubocop:disable Metrics/AbcSize
+          end
+        RUBY
+      end
+    end
+
+    context 'when enabling all cops' do
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          foo # rubocop:enable all
+              ^^^^^^^^^^^^^^^^^^^^ RuboCop disable/enable directives for `all` are not permitted.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo#{trailing_whitespace}
+        RUBY
+      end
+    end
+
+    context 'when disabling all cops' do
+      # `rubocop:disable all` disables this cop as well, so its offense is suppressed.
+      it 'does not register an offense' do
+        expect_no_offenses(<<~RUBY)
+          foo # rubocop:disable all
+        RUBY
+      end
+    end
+
+    context 'when a mix of disallowed and non-disallowed cops are disabled' do
+      it 'registers an offense only for the disallowed cops and corrects' do
+        expect_offense(<<~RUBY)
+          foo # rubocop:disable Metrics/AbcSize, Lint/Void, Style/AndOr
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ RuboCop disable/enable directives for `Lint/Void` are not permitted.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo # rubocop:disable Metrics/AbcSize, Style/AndOr
+        RUBY
+      end
+    end
+
+    context 'when multiple disallowed cops are disabled along with allowed ones' do
+      it 'registers an offense for all disallowed cops and corrects' do
+        expect_offense(<<~RUBY)
+          foo # rubocop:disable Lint/Void, Metrics/AbcSize, Security/Eval
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ RuboCop disable/enable directives for `Lint/Void`, `Security/Eval` are not permitted.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          foo # rubocop:disable Metrics/AbcSize
+        RUBY
+      end
+    end
+
+    context 'when a non-disallowed cop is enabled' do
+      it 'does not register an offense' do
+        expect_no_offenses(<<~RUBY)
+          # rubocop:enable Metrics/AbcSize
+        RUBY
+      end
+    end
+
+    context 'when using leading source comment' do
+      it 'does not register an offense' do
+        expect_no_offenses(<<~RUBY)
+          # comment
+
+          class Foo
+          end
+        RUBY
+      end
+    end
+  end
+
+  context 'when both AllowedCops and DisallowedCops are set' do
+    let(:cop_config) do
+      { 'AllowedCops' => ['Lint/Void'], 'DisallowedCops' => ['Security/Eval'] }
+    end
+
+    it 'gives DisallowedCops precedence and ignores AllowedCops' do
+      expect_offense(<<~RUBY)
+        foo # rubocop:disable Lint/Void, Security/Eval
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ RuboCop disable/enable directives for `Security/Eval` are not permitted.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo # rubocop:disable Lint/Void
+      RUBY
+    end
+  end
 end
