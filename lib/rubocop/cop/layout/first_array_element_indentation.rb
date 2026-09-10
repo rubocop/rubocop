0ea82864ede226d63a272f1e318d49a43215e13c
Mridankan Mandal <xerontitan90@gmail.com>
2026-09-10 15:02:28 +0530

    [Fix #15638] Fix first array indentation loop
    Signed-off-by: Mridankan Mandal <xerontitan90@gmail.com>

--- lib/rubocop/cop/layout/first_array_element_indentation.rb
+++ lib/rubocop/cop/layout/first_array_element_indentation.rb
@@ -89,21 +89,34 @@ module RuboCop
               'in an array, relative to %<base_description>s.'
 
         def on_array(node)
-          return if style != :consistent && enforce_first_argument_with_fixed_indentation?
+          return unless node.loc.begin
+          return if autocorrect_incompatible_with_other_cops?(node, nil)
 
-          check(node, nil) if node.loc.begin
+          check(node, nil)
         end
 
         def on_send(node)
-          return if style != :consistent && enforce_first_argument_with_fixed_indentation?
-
           each_argument_node(node, :array) do |array_node, left_parenthesis|
+            next if autocorrect_incompatible_with_other_cops?(array_node, left_parenthesis)
+
             check(array_node, left_parenthesis)
           end
         end
         alias on_csend on_send
 
         private
+
+        def autocorrect_incompatible_with_other_cops?(array_node, left_parenthesis)
+          return false unless enforce_first_argument_with_fixed_indentation?
+          return true if style != :consistent
+
+          # `Layout/ArrayAlignment` does not align single-element arrays.
+          return false if array_node.children.size < 2
+
+          _base_column, indent_base_type =
+            indent_base(array_node.loc.begin, array_node.values.first, left_parenthesis)
+          indent_base_type != :start_of_line
+        end
 
         def autocorrect(corrector, node)
           AlignmentCorrector.correct(corrector, processed_source, node, @column_delta)
