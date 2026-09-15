39ec33ec620c02dc830709cc3f7f1920c73d6e68
viralpraxis <iaroslav2k@gmail.com>
2026-09-15 14:52:41 +0300

    Fix an incorrect autocorrect for `Style/Lambda` cop
    An MRE:
    
    ```bash
    $ bundle exec rubocop --debug --stdin /dev/stdin -a --config <(cat <<'YAML'
    AllCops:
      DisabledByDefault: true
      SuggestExtensions: false
      TargetRubyVersion: 3.4
    Style/Lambda:
      Enabled: true
      EnforcedStyle: lambda
    YAML
    ) <<'RUBY'
    ->(aa = ..bb) { cc }
    RUBY
    configuration from /proc/self/fd/11
    Inspecting 1 file
    Scanning /dev/stdin
    F
    
    Offenses:
    
    /dev/stdin:1:1: C: [Corrected] Style/Lambda: Use the lambda method for all lambdas.
    ->(aa = ..bb) { cc }
    ^^
    /dev/stdin:1:24: F: Lint/Syntax: expected the block parameters to end with |
    lambda { |aa = ..bb| cc }
                           ^
    ```
    
    Found by `rubocop-nightly`.

--- lib/rubocop/cop/correctors/lambda_literal_to_method_corrector.rb
+++ lib/rubocop/cop/correctors/lambda_literal_to_method_corrector.rb
@@ -90,7 +90,7 @@ module RuboCop
         # `;`; joining everything with `,` would turn them into extra parameters
         # and change the lambda's arity.
         regular, shadow = arguments.children.partition { |arg| !arg.shadowarg_type? }
-        arg_string = regular.map(&:source).join(', ')
+        arg_string = regular.map { |arg| argument_source(arg) }.join(', ')
         arg_string += "; #{shadow.map(&:source).join(', ')}" unless shadow.empty?
         arg_string
       end
@@ -135,6 +135,23 @@ module RuboCop
         return false if parent.parenthesized_call?
 
         current_node.sibling_index > 1
+      end
+
+      def argument_source(argument)
+        return argument.source unless argument.type?(:optarg, :kwoptarg)
+        return argument.source unless operator_expression?(argument.default_value)
+
+        default_value = argument.default_value
+        name = argument.source_range.begin.join(default_value.source_range.begin).source
+
+        "#{name}(#{default_value.source})"
+      end
+
+      def operator_expression?(node)
+        return true if node.type?(:range, :if, :and, :or)
+        return false unless node.call_type?
+
+        node.operator_method? && !node.method?(:[])
       end
 
       def separating_space?
